39a68e38a858ac95e30078ff70737236f6276305
Timophy Chaptykov <tim.chaptykov@gmail.com>
2017-02-12 19:17:51 +0300

    Active color for android

--- src/components/TappableWrapper/TappableWrapper.js
+++ src/components/TappableWrapper/TappableWrapper.js
@@ -2,15 +2,35 @@ import './TappableWrapper.css';
 import React from 'react';
 import Tappable from 'react-tappable';
 import removeObjectKeys from '../../lib/removeObjectKeys';
+import classnames from '../../lib/classnames';
+import { platform, ANDROID, IOS } from '../../lib/platform.js';
+
+const osname = platform();
 
 export default function TappableWrapper(props) {
-  let Container = !props.onClick ? props.component || 'div' : Tappable;
-  let containerProps = removeObjectKeys(Object.assign({}, props), ['component']);
+  const Container = !props.onClick ? props.component || 'div' : Tappable;
+  const commonProps = {
+    className: classnames('Tappable', props.className, {
+      'Tappable--android': osname === ANDROID,
+      'Tappable--ios': osname === IOS
+    })
+  };
+  const TappableProps = {
+    className: commonProps.className,
+    classes: {
+      active: 'Tappable--active',
+      inactive: 'Tappable--inactive'
+    }
+  };
+
+  let containerProps;
 
   if (props.onClick) {
-    containerProps = removeObjectKeys(Object.assign({}, props, {
+    containerProps = removeObjectKeys(Object.assign({}, props, TappableProps, {
       onTap: props.onClick
-    }), ['onClick']);
+    }), ['onClick', 'component']);
+  } else {
+    containerProps = removeObjectKeys(Object.assign({}, props), ['component']);
   }
 
   return <Container {...containerProps}>{props.children}</Container>;
