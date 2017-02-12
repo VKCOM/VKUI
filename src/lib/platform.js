24374cae0dccbc733e8713436d5fcb689b92dd4c
Timophy Chaptykov <tim.chaptykov@gmail.com>
2017-02-12 19:17:23 +0300

    Platform detection

---
+++ src/lib/platform.js
@@ -0,0 +1,16 @@
+export const ANDROID = 'android';
+export const IOS = 'ios';
+
+let ua;
+let platformName;
+
+export function platform(useragent) {
+  if (!ua) {
+    ua = useragent || (navigator && navigator.userAgent) || '';
+  }
+  if (!platformName) {
+    platformName = /android/i.test(ua) ? ANDROID : IOS;
+  }
+
+  return platformName;
+}
