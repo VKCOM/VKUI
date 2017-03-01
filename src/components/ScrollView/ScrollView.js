3d911b3831792730fb104e7dbd0d41841d484dbc
Timophy Chaptykov <tim.chaptykov@gmail.com>
2017-03-01 17:33:39 +0300

    Fix warning

--- src/components/ScrollView/ScrollView.js
+++ src/components/ScrollView/ScrollView.js
@@ -1,13 +1,16 @@
 import './ScrollView.css';
 import React, { Component } from 'react';
 import getClassName from '../../helpers/getClassName';
+import removeObjectKeys from '../../lib/removeObjectKeys';
 
 const baseClassNames = getClassName('ScrollView');
 
 export default class ScrollView extends Component {
   render() {
     return (
-      <div className={baseClassNames} {...this.props}>{this.props.children}</div>
+      <div className={baseClassNames} {...removeObjectKeys(this.props, ['header'])}>
+        {this.props.children}
+      </div>
     );
   }
 }
