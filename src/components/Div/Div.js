74619442da22fca148d4f44c575233989c6c4ace
Timophy Chaptykov <tim.chaptykov@gmail.com>
2017-05-02 14:38:17 +0300

    External styles for Div component

--- src/components/Div/Div.js
+++ src/components/Div/Div.js
@@ -1,15 +1,22 @@
 import './Div.css';
 import React, { Component, PropTypes } from 'react';
+import { platform, ANDROID, IOS } from '../../lib/platform.js';
 import classnames from '../../lib/classnames';
 import getClassName from '../../helpers/getClassName';
 import removeObjectKeys from '../../lib/removeObjectKeys';
 
+const osname = platform();
 const baseClassNames = getClassName('Div');
 
 export default function Div (props) {
+  const modifiers = {
+    'Div--shadow': osname === ANDROID && props.androidShadow,
+    'Div--border': osname === IOS && props.iosBorder
+  };
+
   return (
     <div
-      className={classnames(baseClassNames, props.className)}
+      className={classnames(baseClassNames, modifiers, props.className)}
       style={props.style}
       {...removeObjectKeys(props, ['className', 'style'])}
     >
