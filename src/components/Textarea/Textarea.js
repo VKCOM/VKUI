906b645d1a1e4276e8e6f85bf3e7eda6be92396e
Timophy Chaptykov <tim.chaptykov@gmail.com>
2017-06-26 20:16:01 +0300

    Textarea autogrow

--- src/components/Textarea/Textarea.js
+++ src/components/Textarea/Textarea.js
@@ -9,37 +9,78 @@ const baseClassNames = getClassName('Textarea');
 export default class Textarea extends Component {
   constructor (props) {
     super(props);
+    this.state = {};
+
     if (typeof props.value !== 'undefined') {
       this.isControlledOutside = true;
     } else {
-      this.state = {
-        value: props.initialValue || ''
-      };
+      this.state.value = props.initialValue || '';
     }
   }
   static propTypes = {
     style: PropTypes.object,
-    initialValue: PropTypes.string
+    initialValue: PropTypes.string,
+    grow: PropTypes.bool
   };
   static defaultProps = {
     style: {},
-    initialValue: ''
+    initialValue: '',
+    grow: true
   };
+  getRef = (element) => {
+    this.element = element;
+  }
   onChange = (e) => {
+    let el = this.element;
+
+    if (el && this.props.grow) {
+      const { offsetHeight, scrollHeight } = el;
+      const style = window.getComputedStyle(el);
+      const paddingTop = parseInt(style.paddingTop);
+      const paddingBottom = parseInt(style.paddingBottom);
+
+      let diff = paddingTop + paddingBottom;
+      let height = 0;
+
+      if (scrollHeight + diff <= offsetHeight) {
+        diff = 0;
+      }
+
+      if (el.value) {
+        this.setState({ height: scrollHeight - diff });
+      }
+
+      const top = document.body.scrollTop;
+
+      this.setState({ height: 0 });
+
+      window.requestAnimationFrame(() => {
+        this.setState({ height: el.scrollHeight - diff });
+        document.body.scrollTop = top;
+      });
+    }
+
     if (!this.isControlledOutside) {
       this.setState({ value: e.target.value });
     }
+
     if (this.props.onChange) {
       this.props.onChange(e);
     }
   }
   render () {
+    const props = this.props;
+    const value = this.isControlledOutside ? props.value : this.state.value;
+    const height = this.state.height || 66;
+
     return (
       <textarea
         className={baseClassNames}
-        {...removeObjectKeys(this.props, ['initialValue'])}
-        value={this.isControlledOutside ? this.props.value : this.state.value}
+        {...removeObjectKeys(props, ['initialValue', 'grow', 'style'])}
+        value={value}
         onChange={this.onChange}
+        ref={this.getRef}
+        style={Object.assign({}, props.style, { height })}
       />
     );
   }
