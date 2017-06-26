1a18ee2a6a280df72e4e3349cb87c094d2820899
Timophy Chaptykov <tim.chaptykov@gmail.com>
2017-06-26 17:03:08 +0300

    Tappable radio buttons

--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -2,6 +2,7 @@ import './Radio.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 import ListItem from '../ListItem/ListItem';
+import Tappable from '../Tappable/Tappable';
 import removeObjectKeys from '../../lib/removeObjectKeys';
 import getClassName from '../../helpers/getClassName';
 
@@ -9,17 +10,15 @@ const baseClassNames = getClassName('Radio');
 
 export default function Radio (props) {
   return (
-    <ListItem>
-      <label className={baseClassNames}>
-        <input
-          {...removeObjectKeys(props, ['children'])}
-          type="radio"
-          className="Radio__self"
-        />
-        <span className="Radio__label">{props.children}</span>
-        <span className="Radio__icon" />
-      </label>
-    </ListItem>
+    <Tappable component="label" onClick={() => {}} className={baseClassNames}>
+      <input
+        {...removeObjectKeys(props, ['children'])}
+        type="radio"
+        className="Radio__self"
+      />
+      <span className="Radio__label">{props.children}</span>
+      <span className="Radio__icon" />
+    </Tappable>
   );
 }
 
