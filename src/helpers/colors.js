9a7373340eb5388c0e6713bc67f48512706d9c47
a.stambultsian <a.stambultsian@corp.vk.com>
2017-12-01 15:49:17 +0300

    squashed colors map

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -1,12 +1,5 @@
 module.exports = {
-  ios: {
-    accentBlue: '#528BCC',
-    red: '#E64646',
-    captionGray: '#909499'
-  },
-  android: {
-    accentBlue: '#528BCC',
-    red: '#E64646',
-    captionGray: '#909499'
-  }
+  accentBlue: '#528BCC',
+  red: '#E64646',
+  captionGray: '#909499'
 };
