b68d800de2a77a01f696585554d43d18da1472ed
ArthurStam <bumbledos@gmail.com>
2017-12-27 19:03:27 +0300

    FormLayout: onSubmit fixes package.json: run global webpack

--- src/components/FormLayout/FormLayout.js
+++ src/components/FormLayout/FormLayout.js
@@ -7,44 +7,56 @@ import classnames from '../../lib/classnames';
 
 const baseClassNames = getClassName('FormLayout');
 
-export default function FormLayout (props) {
-  const children = Array.isArray(props.children) ? props.children : [props.children];
-  const TagName = props.tagName || 'form';
-  const modifiers = {
-    'FormLayout--web': props.mod === 'web'
+export default class FormLayout extends React.Component {
+
+  static propTypes = {
+    children: PropTypes.node,
+    style: PropTypes.object,
+    tagName: PropTypes.string,
+    mod: PropTypes.string,
+    allowSubmit: PropTypes.bool,
+    onSubmit: PropTypes.func
   };
 
-  return (
-    <TagName className={classnames(baseClassNames, modifiers)} {...removeObjectKeys(props, ['tagName', 'mod', 'allowSubmit'])}>
-      <div className={'FormLayout__container'}>
-        {children.map((field, i) => (
-          <label className="FormLayout__row" key={field.key || `row-${i}`}>
-            <div className="FormLayout__separator" />
-            {!!field.props.label && (
-              <div className="FormLayout__label">
-                <div className="FormLayout__label-in">{field.props.label}</div>
+  static defaultProps = {
+    allowSubmit: true
+  };
+
+  onSubmit = (e) => {
+    if (!this.props.allowSubmit) {
+      e.preventDefault();
+    } else {
+      this.props.onSubmit && this.props.onSubmit(e);
+    }
+  };
+
+  render () {
+    const children = Array.isArray(this.props.children) ? this.props.children : [this.props.children];
+    const TagName = this.props.tagName || 'form';
+    const modifiers = {
+      'FormLayout--web': this.props.mod === 'web'
+    };
+
+    return (
+      <TagName className={classnames(baseClassNames, modifiers)} {...removeObjectKeys(this.props, ['tagName', 'mod', 'allowSubmit', 'onSubmit'])} onSubmit={this.onSubmit}>
+        <div className={'FormLayout__container'}>
+          {children.map((field, i) => (
+            <label className="FormLayout__row" key={field.key || `row-${i}`}>
+              <div className="FormLayout__separator" />
+              {!!field.props.label && (
+                <div className="FormLayout__label">
+                  <div className="FormLayout__label-in">{field.props.label}</div>
+                </div>
+              )}
+              <div className="FormLayout__field">
+                {field}
+                <div className="FormLayout__underline" />
               </div>
-            )}
-            <div className="FormLayout__field">
-              {field}
-              <div className="FormLayout__underline" />
-            </div>
-          </label>
-        ))}
-      </div>
-      {TagName === 'form' && props.allowSubmit && <input type="submit" style={{ position: 'absolute', visibility: 'hidden' }} />}
-    </TagName>
-  );
+            </label>
+          ))}
+        </div>
+        {TagName === 'form' && this.props.allowSubmit && <input type="submit" style={{ position: 'absolute', visibility: 'hidden' }} />}
+      </TagName>
+    );
+  }
 }
-
-FormLayout.propTypes = {
-  children: PropTypes.node,
-  style: PropTypes.object,
-  tagName: PropTypes.string,
-  mod: PropTypes.string,
-  allowSubmit: PropTypes.bool
-};
-
-FormLayout.defaultProps = {
-  allowSubmit: true
-};
