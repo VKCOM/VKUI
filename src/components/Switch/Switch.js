f5d26afaf2476c5d0402585e5051cb046b742917
ArthurStam <bumbledos@gmail.com>
2018-03-28 19:00:47 +0300

    minified Switch

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,31 +1,22 @@
 import './Switch.css';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import getClassName from '../../helpers/getClassName';
 import classnames from '../../lib/classnames';
 
 const baseClassNames = getClassName('Switch');
 
-export default class Switch extends Component {
+const Switch = ({ style, className, ...restProps }) => (
+  <label className={classnames(baseClassNames, className)} style={style}>
+    <input type="checkbox" className="Switch__self" {...restProps} />
+    <span className="Switch__pseudo" />
+  </label>
+);
 
-  static propTypes = {
-    style: PropTypes.object,
-    onChange: PropTypes.func,
-    className: PropTypes.string
-  };
+Switch.propTypes = {
+  style: PropTypes.object,
+  onChange: PropTypes.func,
+  className: PropTypes.string
+};
 
-  render () {
-    const { style, className, ...restProps } = this.props;
-
-    return (
-      <label className={classnames(baseClassNames, className)} style={style}>
-        <input
-          type="checkbox"
-          className="Switch__self"
-          {...restProps}
-        />
-        <span className="Switch__pseudo" />
-      </label>
-    );
-  }
-}
+export default Switch;
