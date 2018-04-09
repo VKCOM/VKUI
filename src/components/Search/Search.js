7fbcf99660ae0882073c7261f9046a420a7b8510
ArthurStam <bumbledos@gmail.com>
2018-04-09 19:27:09 +0300

    Search
    fixed proxing props in SearchAndroid and SearchIOS
    initialValue => defaultValue

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,16 +2,33 @@ import React from 'react';
 import { platform, IOS } from '../../lib/platform';
 import SearchIOS from '../SearchIOS/SearchIOS';
 import SearchAndroid from '../SearchAndroid/SearchAndroid';
+import PropTypes from 'prop-types';
 
 const osname = platform();
 
 export default class Search extends React.Component {
 
+  static propTypes = {
+    onClose: PropTypes.func,
+    after: PropTypes.node,
+    before: PropTypes.node,
+    className: PropTypes.string,
+    defaultValue: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    onFocus: PropTypes.func,
+    onBlur: PropTypes.func,
+    placeholder: PropTypes.node,
+    theme: PropTypes.oneOf(['header', 'default'])
+  };
+
   render () {
+    const { onClose, ...iosProps } = this.props;
+    const { after, before, theme, ...androidProps } = this.props;
     if (osname === IOS) {
-      return <SearchIOS {...this.props} />;
+      return <SearchIOS {...iosProps} />;
     } else {
-      return <SearchAndroid {...this.props} />;
+      return <SearchAndroid {...androidProps} />;
     }
   }
 }
