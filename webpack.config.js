8351eaf606f0880f297a77bb08982eebe056541b
ArthurStam <bumbledos@gmail.com>
2018-06-18 10:40:32 +0300

    removed posctss-map switched from map(colors, ...) to var(--...) in CSS

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ const merge = require('webpack-merge');
 const pkg = require('./package.json');
 const cssCustomProperties = require('postcss-custom-properties');
 const cssImport = require('postcss-import');
-const cssMaps = require('postcss-map');
 const autoprefixer = require('autoprefixer');
-const colorsMaps = require('./src/helpers/colors');
 
 const cssTransformOptions = [
   {
@@ -23,9 +21,6 @@ const cssTransformOptions = [
         return [
           cssImport(),
           cssCustomProperties(),
-          cssMaps({
-            maps: [{ colors: colorsMaps.values }]
-          }),
           autoprefixer()
         ];
       }
