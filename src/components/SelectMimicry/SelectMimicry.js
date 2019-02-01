b6f4a3201dd5480882323d93a6a54664c7155a56
Igor Fedorov <iamfedoroff@yandex.ru>
2019-02-01 14:16:51 +0300

    SelectMimicry: multiline, switched to functional component with memo; React 16.6.0

--- src/components/SelectMimicry/SelectMimicry.js
+++ src/components/SelectMimicry/SelectMimicry.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import getClassName from '../../helpers/getClassName';
 import classNames from '../../lib/classNames';
@@ -6,40 +6,39 @@ import Icon24Dropdown from '@vkontakte/icons/dist/24/dropdown';
 
 const baseClassName = getClassName('Select');
 
-export default class SelectMimicry extends Component {
-  static propTypes = {
-    style: PropTypes.object,
-    className: PropTypes.string,
-    children: PropTypes.node,
-    tabIndex: PropTypes.number,
-    placeholder: PropTypes.string,
-    alignment: PropTypes.oneOf(['left', 'center', 'top']),
-    getRootRef: PropTypes.func
-  };
-
-  static defaultProps = {
-    tabIndex: 0
-  };
-
-  render () {
-    const { className, placeholder, children, alignment, getRootRef, ...restProps } = this.props;
-
-    return (
-      <div
-        {...restProps}
-        className={classNames(baseClassName, {
-          'Select--not-selected': !children,
-          'Select--mimicry': true,
-          [`Select--align-${alignment}`]: alignment
-        }, className)}
-        ref={getRootRef}
-      >
-        <div className="Select__container">
-          <div className="Select__title">{children || placeholder}</div>
-          <Icon24Dropdown />
-        </div>
-        <div className="Select__border" />
+const SelectMimicry = ({ className, placeholder, children, alignment, getRootRef, multiline, ...restProps }) => {
+  return (
+    <div
+      {...restProps}
+      className={classNames(baseClassName, 'Select--mimicry', {
+        'Select--not-selected': !children,
+        'Select--multiline': multiline,
+        [`Select--align-${alignment}`]: alignment
+      }, className)}
+      ref={getRootRef}
+    >
+      <div className="Select__container">
+        <div className="Select__title">{children || placeholder}</div>
+        <Icon24Dropdown />
       </div>
-    );
-  }
-}
+      <div className="Select__border" />
+    </div>
+  );
+};
+
+SelectMimicry.propTypes = {
+  style: PropTypes.object,
+  className: PropTypes.string,
+  children: PropTypes.node,
+  tabIndex: PropTypes.number,
+  placeholder: PropTypes.string,
+  alignment: PropTypes.oneOf(['left', 'center', 'top']),
+  getRootRef: PropTypes.func,
+  multiline: PropTypes.bool
+};
+
+SelectMimicry.defaultProps = {
+  tabIndex: 0
+};
+
+export default SelectMimicry;
