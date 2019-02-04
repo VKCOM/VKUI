8b6e52dbf003d4cd20890c36391bb6c3362364f1
mmarkelov <maks-markel@mail.ru>
2019-02-04 15:22:19 +0300

    Rewrite Alert to stateless component

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,5 +1,4 @@
-
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Tappable from '../Tappable/Tappable';
 import PopoutWrapper from '../PopoutWrapper/PopoutWrapper';
@@ -8,54 +7,54 @@ import classNames from '../../lib/classNames';
 
 const baseClassNames = getClassName('Alert');
 
-export default class Alert extends Component {
-  static propTypes = {
-    style: PropTypes.object,
-    className: PropTypes.string,
-    children: PropTypes.node,
-    actionsLayout: PropTypes.oneOf(['vertical', 'horizontal']),
-    actions: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string,
-      action: PropTypes.func,
-      style: PropTypes.oneOf(['primary', 'cancel', 'destructive', 'default'])
-    })),
-    onClose: PropTypes.func
-  };
+const Alert = props => {
+  const { actions, actionsLayout, children, className, style, onClose, ...restProps } = props;
 
-  static defaultProps = {
-    actionsLayout: 'horizontal',
-    actions: []
-  };
-
-  onItemClick = item => () => {
-    item.autoclose && this.props.onClose();
+  const onItemClick = item => () => {
+    item.autoclose && onClose();
     item.action && item.action();
   };
 
-  render () {
-    const { actions, actionsLayout, children, className, style, ...restProps } = this.props;
+  return (
+    <PopoutWrapper className={className} style={style}>
+      <div {...restProps} className={classNames(baseClassNames, {
+        'Alert--v': actionsLayout === 'vertical',
+        'Alert--h': actionsLayout === 'horizontal'
+      })}>
+        <div className="Alert__content">{children}</div>
+        <footer className="Alert__footer">
+          {actions.map((button, i) => (
+            <Tappable
+              component="button"
+              className={classNames('Alert__btn', { [`Alert__btn--${button.style}`]: true })}
+              onClick={onItemClick(button)}
+              key={`alert-action-${i}`}
+            >
+              <span>{button.title}</span>
+            </Tappable>
+          ))}
+        </footer>
+      </div>
+    </PopoutWrapper>
+  );
+};
 
-    return (
-      <PopoutWrapper className={className} style={style}>
-        <div {...restProps} className={classNames(baseClassNames, {
-          'Alert--v': actionsLayout === 'vertical',
-          'Alert--h': actionsLayout === 'horizontal'
-        })}>
-          <div className="Alert__content">{children}</div>
-          <footer className="Alert__footer">
-            {actions.map((button, i) => (
-              <Tappable
-                component="button"
-                className={classNames('Alert__btn', { [`Alert__btn--${button.style}`]: true })}
-                onClick={this.onItemClick(button)}
-                key={`alert-action-${i}`}
-              >
-                <span dangerouslySetInnerHTML={{ __html: button.title }} />
-              </Tappable>
-            ))}
-          </footer>
-        </div>
-      </PopoutWrapper>
-    );
-  }
-}
+Alert.propTypes = {
+  style: PropTypes.object,
+  className: PropTypes.string,
+  children: PropTypes.node,
+  actionsLayout: PropTypes.oneOf(['vertical', 'horizontal']),
+  actions: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    action: PropTypes.func,
+    style: PropTypes.oneOf(['primary', 'cancel', 'destructive', 'default'])
+  })),
+  onClose: PropTypes.func
+};
+
+Alert.defaultProps = {
+  actionsLayout: 'horizontal',
+  actions: []
+};
+
+export default Alert;
