3de843c23344167124a214b306a23a32ca37cdd2
mmarkelov <maks-markel@mail.ru>
2019-02-21 15:40:45 +0300

    Rewrite Spinner to stateless component

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import getClassName from '../../helpers/getClassName';
 import classNames from '../../lib/classNames.js';
@@ -9,40 +9,33 @@ import Spinner16 from '@vkontakte/icons/dist/16/spinner';
 
 const baseClassNames = getClassName('Spinner');
 
-export default class Spinner extends PureComponent {
-  static propTypes = {
-    style: PropTypes.object,
-    size: PropTypes.oneOf(['small', 'regular', 'large', 'medium']),
-    className: PropTypes.string
-  };
-
-  static defaultProps = {
-    size: 'regular'
-  };
-
-  get svgSpinner () {
-    switch (this.props.size) {
-      case 'large':
-        return <Spinner44 className="Spinner__self" />;
-      case 'medium':
-        return <Spinner32 className="Spinner__self" />;
-      case 'small':
-        return <Spinner16 className="Spinner__self" />;
-      default:
-        return <Spinner24 className="Spinner__self" />;
-    }
+const svgSpinner = () => {
+  switch (this.props.size) {
+    case 'large':
+      return <Spinner44 className="Spinner__self" />;
+    case 'medium':
+      return <Spinner32 className="Spinner__self" />;
+    case 'small':
+      return <Spinner16 className="Spinner__self" />;
+    default:
+      return <Spinner24 className="Spinner__self" />;
   }
+};
 
-  render () {
-    const { className, size, ...restProps } = this.props;
+const Spinner = ({ className, size, ...restProps }) => (
+  <div {...restProps} className={classNames(baseClassNames, className)}>
+    {svgSpinner}
+  </div>
+);
 
-    return (
-      <div
-        {...restProps}
-        className={classNames(baseClassNames, className)}
-      >
-        {this.svgSpinner}
-      </div>
-    );
-  }
-}
+Spinner.propTypes = {
+  style: PropTypes.object,
+  size: PropTypes.oneOf(['small', 'regular', 'large', 'medium']),
+  className: PropTypes.string
+};
+
+Spinner.defaultProps = {
+  size: 'regular'
+};
+
+export default Spinner;
