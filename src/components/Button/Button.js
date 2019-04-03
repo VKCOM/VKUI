bb70084a647d9223b7e53385c8b69f398f3bdfbc
Igor Fedorov <iamfedoroff@yandex.ru>
2019-04-03 12:01:53 +0300

    Button: remove incorrect type HTML attr

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -29,7 +29,7 @@ const Button = props => {
   if (props.type === 'cell') {
     return <CellButton {...props} />;
   } else {
-    const { className, size, level, stretched, align, children, before, ...restProps } = props;
+    const { className, size, level, stretched, align, children, before, type, ...restProps } = props;
 
     return <Tappable {...restProps} className={classNames(baseClassName, className, {
       [`Button--sz-${size}`]: true,
