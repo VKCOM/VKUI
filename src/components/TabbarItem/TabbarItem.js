6e883e6ec37ef3237b537491e29d6d6e85745562
mmarkelov <maks-markel@mail.ru>
2019-04-11 14:49:50 +0300

    Rewrite TabbarItem to functional component

--- src/components/TabbarItem/TabbarItem.js
+++ src/components/TabbarItem/TabbarItem.js
@@ -5,36 +5,32 @@ import classNames from '../../lib/classNames';
 
 const baseClassName = getClassName('TabbarItem');
 
-export default class TabbarItem extends React.Component {
-  static propTypes = {
-    className: PropTypes.string,
-    selected: PropTypes.bool,
-    children: PropTypes.node,
-    /**
-     * Тест рядом с иконкой
-     */
-    text: PropTypes.node,
-    /**
-     * Счетчик рядом с иконкой
-     */
-    label: PropTypes.node
-  };
-
-  render () {
-    const { className, children, selected, label, text, ...restProps } = this.props;
-
-    return (
-      <div {...restProps} className={classNames(baseClassName, className, {
-        'TabbarItem--selected': selected
-      })}>
-        <div className="TabbarItem__in">
-          <div className="TabbarItem__icon">
-            {children}
-            {label && <span className="TabbarItem__label">{label}</span>}
-          </div>
-          {text && <div className="TabbarItem__text">{text}</div>}
-        </div>
+const TabbarItem = ({ className, children, selected, label, text, ...restProps }) => (
+  <div {...restProps} className={classNames(baseClassName, className, {
+    'TabbarItem--selected': selected
+  })}>
+    <div className="TabbarItem__in">
+      <div className="TabbarItem__icon">
+        {children}
+        {label && <span className="TabbarItem__label">{label}</span>}
       </div>
-    );
-  }
-}
+      {text && <div className="TabbarItem__text">{text}</div>}
+    </div>
+  </div>
+);
+
+TabbarItem.propTypes = {
+  className: PropTypes.string,
+  selected: PropTypes.bool,
+  children: PropTypes.node,
+  /**
+   * Тест рядом с иконкой
+   */
+  text: PropTypes.node,
+  /**
+   * Счетчик рядом с иконкой
+   */
+  label: PropTypes.node
+};
+
+export default TabbarItem;
