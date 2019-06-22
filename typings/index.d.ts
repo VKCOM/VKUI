dedc006d31b42d518dff1b412d68001240378fc9
mmarkelov <maks-markel@mail.ru>
2019-06-22 12:51:56 +0300

    Add types for Avatar

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -32,6 +32,15 @@ interface AlertProps extends StyleObject {
   children?: React.ReactNode;
 }
 
+interface AvatarProps extends StyleObject {
+  children?: React.ReactNode;
+  getRootRef?: (instance: React.Ref) => void;
+  size?: 80 | 72 | 64 | 56 | 48 | 40 | 36 | 32 | 28;
+  src?: string;
+  type?: 'default' | 'image' | 'app';
+}
+
 export const ActionSheet: React.ComponentType<ActionSheetProps>;
 export const ActionSheetItem: React.ComponentType<ActionSheetItemProps>;
 export const Alert: React.ComponentType<AlertProps>;
+export const Avatar: React.ComponentType<AvatarProps>;
