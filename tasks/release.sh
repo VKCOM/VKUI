df22d88372593b0147929e71cdc7c67f392d57e4
ArthurStam <bumbledos@gmail.com>
2019-06-24 15:08:55 +0300

    release.sh: preventing release if version is not passed

--- tasks/release.sh
+++ tasks/release.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-echo "release: creating version"
-npm version $1
-echo "release: building styleguide"
-npm run styleguide:build
-echo "release: updating styleguide submodule"
-git submodule foreach 'git add -A && git commit -m '"v$1"''
-echo "release: commiting submodule updates"
-git add -A && git commit -m "docs update: v$1"
-echo "release: publishing to npm"
-npm publish --access-public
-echo "release: pushing updates to github"
-git push origin master
-echo "release: pushing new tag to github"
-git push origin --tags
-echo "release: pushing styleguide submodule changes"
-git submodule foreach 'git push origin master'
+if [ ! $1 ]; then
+  echo "Error: you should pass version number"
+else
+  echo "release: creating version"
+  npm version $1
+  echo "release: building styleguide"
+  npm run styleguide:build
+  echo "release: updating styleguide submodule"
+  git submodule foreach 'git add -A && git commit -m '"v$1"''
+  echo "release: commiting submodule updates"
+  git add -A && git commit -m "docs update: v$1"
+  echo "release: publishing to npm"
+  npm publish --access-public
+  echo "release: pushing updates to github"
+  git push origin master
+  echo "release: pushing new tag to github"
+  git push origin --tags
+  echo "release: pushing styleguide submodule changes"
+  git submodule foreach 'git push origin master'
+fi
