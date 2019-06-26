938bc972f19b56f377a5822d5f884e3778a80b78
Danakt <danakt@protonmail.com>
2019-06-27 02:06:19 +0300

    Rewrite FormField

---
+++ src/components/FormField/FormField.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import getClassName from '../../helpers/getClassName';
+import classNames from '../../lib/classNames';
+import { HasRef } from '../../types/props';
+
+const baseClassName = getClassName('FormField');
+
+export interface FormFieldProps extends HasRef {
+  TagName?: string;
+  children?: React.ReactNode;
+  className?: string;
+  style?: object;
+  top?: React.ReactNode;
+  bottom?: React.ReactNode;
+  status?: 'default' | 'error' | 'valid';
+}
+
+const FormField = ({ TagName, className, children, status, getRootRef, ...restProps }: FormFieldProps) => {
+  return React.createElement(
+    TagName,
+    {
+      ...restProps,
+      className: classNames(baseClassName, { [`FormField--s-${status}`]: status !== 'default' }, className),
+      ref: getRootRef
+    },
+
+    children,
+    <div className="FormField__border" />
+  );
+};
+
+FormField.propTypes = {
+  TagName: PropTypes.string,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  style: PropTypes.object,
+  top: PropTypes.node,
+  bottom: PropTypes.node,
+  getRootRef: PropTypes.func,
+  status: PropTypes.oneOf(['default', 'error', 'valid'])
+};
+
+FormField.defaultProps = {
+  status: 'default',
+  TagName: 'div'
+};
+
+export default FormField;
