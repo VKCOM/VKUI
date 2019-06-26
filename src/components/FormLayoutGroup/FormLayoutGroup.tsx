a33b46d12e4233405695397ddfd31bb0304804a6
Danakt <danakt@protonmail.com>
2019-06-27 02:12:12 +0300

    Rewrite FormLayoutGroup

--- src/components/FormLayoutGroup/FormLayoutGroup.tsx
+++ src/components/FormLayoutGroup/FormLayoutGroup.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import getClassName from '../../helpers/getClassName';
 import classNames from '../../lib/classNames';
+import { HasStyleObject, HasChildren, StatusTypes } from '../../types/props';
 
 const baseClassNames = getClassName('FormLayoutGroup');
 
-const FormLayoutGroup = ({ children, top, bottom, className, ...restProps }) => (
-  <div className={classNames(baseClassNames, className)} {...restProps}>{children}</div>
+export interface FormLayoutGroupProps extends HasStyleObject, HasChildren {
+  bottom?: React.ReactNode;
+  status?: StatusTypes;
+  top?: React.ReactNode;
+}
+
+const FormLayoutGroup = ({ children, top, bottom, className, ...restProps }: FormLayoutGroupProps) => (
+  <div className={classNames(baseClassNames, className)} {...restProps}>
+    {children}
+  </div>
 );
 
 FormLayoutGroup.propTypes = {
