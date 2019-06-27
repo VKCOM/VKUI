2bcb25a9599d9e5ab540256bdced629d216bc58d
Danakt <danakt@protonmail.com>
2019-06-28 01:57:31 +0300

    Rewrite getClassName

--- src/helpers/getClassName.ts
+++ src/helpers/getClassName.ts
@@ -1,7 +1,7 @@
 import { IS_PLATFORM_IOS, IS_PLATFORM_ANDROID } from '../lib/platform.js';
 import classNames from '../lib/classNames';
 
-export default function getClassname (base) {
+export default function getClassName (base: string): string {
   return classNames(base, {
     [base + '--ios']: IS_PLATFORM_IOS,
     [base + '--android']: IS_PLATFORM_ANDROID
