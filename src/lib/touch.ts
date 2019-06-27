34292f4ebdb21544c45b7fa2ff8369a9288e5393
Danakt <danakt@protonmail.com>
2019-06-28 02:03:55 +0300

    Rewrite touch

--- src/lib/touch.ts
+++ src/lib/touch.ts
@@ -1,19 +1,18 @@
-
 /**
  * Получает кординату по оси абсцисс из touch- или mouse-события
  *
- * @param {Object} e Бразуерное событие
- * @returns {Number} Координата взаимодействия по оси абсцисс
+ * @param e Бразуерное событие
+ * @returns Координата взаимодействия по оси абсцисс
  */
-const coordX = e => e.clientX || (e.touches && e.touches[0].clientX);
+const coordX = (e: MouseEvent | TouchEvent): number => ('clientX' in e ? e.clientX : e.touches && e.touches[0].clientX);
 
 /**
  * Получает кординату по оси ординат из touch- или mouse-события
  *
- * @param {Object} e Бразуерное событие
- * @returns {Number} Координата взаимодействия по оси ординат
+ * @param e Бразуерное событие
+ * @returns Координата взаимодействия по оси ординат
  */
-const coordY = e => e.clientY || (e.touches && e.touches[0].clientY);
+const coordY = (e: MouseEvent | TouchEvent): number => ('clientY' in e ? e.clientY : e.touches && e.touches[0].clientY);
 
 const isClient = typeof window !== 'undefined';
 const touchEnabled = isClient && 'ontouchstart' in window;
@@ -23,9 +22,9 @@ const touchEnabled = isClient && 'ontouchstart' in window;
  * Если браузер поддерживает pointer events или подключена handjs, вернет события указателя.
  * Если нет, используем события мыши
  *
- * @returns {Array} Массив с названиями событий
+ * @returns Массив с названиями событий
  */
-function getSupportedEvents () {
+function getSupportedEvents (): string[] {
   if (touchEnabled) {
     return ['touchstart', 'touchmove', 'touchend', 'touchcancel'];
   }
