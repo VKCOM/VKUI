9ac11a72bdb83538897a98b39ecebccee2321ec9
Danakt <danakt@protonmail.com>
2019-06-28 01:16:22 +0300

    Rewrite Tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import getClassName from '../../helpers/getClassName';
 import classNames from '../../lib/classNames';
+import { HasStyleObject, HasChildren, HasRef } from '../../types/props';
 
 const baseClassName = getClassName('Tabs');
 
-export default class Tabs extends React.Component {
+export interface TabsProps extends HasStyleObject, HasChildren, HasRef {
+  theme?: 'light' | 'header';
+  type?: 'default' | 'buttons';
+}
+
+export default class Tabs extends React.Component<TabsProps> {
   static propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
@@ -31,7 +37,7 @@ export default class Tabs extends React.Component {
    * @returns {string}
    */
   get theme () {
-    switch (this.props.theme) {
+    switch (this.props.theme as string) {
       case 'white':
         return 'header';
       case 'gray':
@@ -45,10 +51,21 @@ export default class Tabs extends React.Component {
     const { className, children, theme, style, type, getRootRef, ...restProps } = this.props;
 
     return (
-      <div {...restProps} ref={getRootRef} className={classNames(baseClassName, {
-        [`Tabs--${this.theme}`]: true,
-        [`Tabs--${type}`]: true
-      }, className)} style={style}>{children}</div>
+      <div
+        {...restProps}
+        ref={getRootRef}
+        className={classNames(
+          baseClassName,
+          {
+            [`Tabs--${this.theme}`]: true,
+            [`Tabs--${type}`]: true
+          },
+          className
+        )}
+        style={style}
+      >
+        {children}
+      </div>
     );
   }
 }
