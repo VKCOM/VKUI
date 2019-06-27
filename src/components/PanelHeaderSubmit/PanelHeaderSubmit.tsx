0f703638291d86ad99f7720c2960168f66b21baf
Danakt <danakt@protonmail.com>
2019-06-28 00:03:23 +0300

    Rewrite PanelHeaderSubmit

--- src/components/PanelHeaderSubmit/PanelHeaderSubmit.tsx
+++ src/components/PanelHeaderSubmit/PanelHeaderSubmit.tsx
@@ -4,7 +4,16 @@ import Icon24Done from '@vkontakte/icons/dist/24/done';
 import HeaderButton from '../HeaderButton/HeaderButton';
 import { IS_PLATFORM_ANDROID } from '../../lib/platform';
 
-const PanelHeaderSubmit = ({ children, ...restProps }) => (
+export interface PanelHeaderSubmitProps {
+  onClick: () => void;
+
+  /**
+   * iOS only. Текст кнопки
+   */
+  children?: string;
+}
+
+const PanelHeaderSubmit = ({ children, ...restProps }: PanelHeaderSubmitProps) => (
   <HeaderButton primary {...restProps}>
     {IS_PLATFORM_ANDROID ? <Icon24Done /> : children}
   </HeaderButton>
