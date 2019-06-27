4a4128c569c3b9e6a3ca9a2dd09368b665297a89
Danakt <danakt@protonmail.com>
2019-06-27 22:34:46 +0300

    Rewrite InfoRow

--- src/components/InfoRow/InfoRow.tsx
+++ src/components/InfoRow/InfoRow.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import getClassName from '../../helpers/getClassName';
 import classNames from '../../lib/classNames';
+import { HasStyleObject, HasChildren } from '../../types/props';
 
 const baseClassName = getClassName('InfoRow');
 
-const InfoRow = ({ title, className, children, ...restProps }) => (
+export interface InfoRowProps extends HasStyleObject, HasChildren {
+  title: React.ReactNode;
+}
+
+const InfoRow = ({ title, className, children, ...restProps }: InfoRowProps) => (
   <div {...restProps} className={classNames(baseClassName, className)}>
     {title && <div className="InfoRow__title">{title}</div>}
     {children && <div>{children}</div>}
