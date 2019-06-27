4c0577d41eb4d702f9b2d66bbf8fc9aa7150abb4
Danakt <danakt@protonmail.com>
2019-06-28 01:12:56 +0300

    Rewrite Switch

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,12 +1,16 @@
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import getClassName from '../../helpers/getClassName';
 import classNames from '../../lib/classNames';
+import { HasStyleObject, HasRef } from '../../types/props';
 
 const baseClassNames = getClassName('Switch');
 
-const Switch = ({ style, className, getRef, getRootRef, ...restProps }) => (
+export interface SwitchProps extends HasStyleObject, HasRef {
+  getRef?: (instance: HTMLInputElement) => void;
+}
+
+const Switch = ({ style, className, getRef, getRootRef, ...restProps }: SwitchProps) => (
   <label className={classNames(baseClassNames, className)} style={style} ref={getRootRef}>
     <input {...restProps} type="checkbox" className="Switch__self" ref={getRef} />
     <span className="Switch__pseudo" />
