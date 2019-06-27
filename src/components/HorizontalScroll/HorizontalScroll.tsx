151b2daa91d5c1a21bda67dcc9e624e78ba46078
Danakt <danakt@protonmail.com>
2019-06-27 22:33:51 +0300

    Rewrite HorizontalScroll

--- src/components/HorizontalScroll/HorizontalScroll.tsx
+++ src/components/HorizontalScroll/HorizontalScroll.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropType from 'prop-types';
 import classNames from '../../lib/classNames';
+import { HasChildren, HasStyleObject } from '../../types/props';
 
-const HorizontalScroll = ({ children, className, ...restProps }) => (
+interface HorizontalScrollProps extends HasStyleObject, HasChildren {}
+
+const HorizontalScroll = ({ children, className, ...restProps }: HorizontalScroll) => (
   <div {...restProps} className={classNames('HorizontalScroll', className)}>
     <div className="HorizontalScroll__in">{children}</div>
   </div>
