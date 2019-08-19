992a0eafdba852d046c4fb0bc4948a9f18ae51a6
ArthurStam <bumbledos@gmail.com>
2019-08-19 22:39:23 +0300

    Typescript: first blood. Rewrited Button, getClassName, plarform and classNames to typescript. Processing styles by postcss-cli. Package entry is now in common js style.

---
+++ src/lib/platform.ts
@@ -0,0 +1,26 @@
+enum OS {
+  ANDROID = 'android',
+  IOS = 'ios'
+}
+
+export const ANDROID: OS = OS.ANDROID;
+export const IOS: OS = OS.IOS;
+
+let ua: string;
+let platformName: string;
+
+export function platform (useragent?: string): string {
+  if (!ua) {
+    ua = useragent || (navigator && navigator.userAgent) || '';
+  }
+  if (!platformName) {
+    platformName = /android/i.test(ua) ? ANDROID : IOS;
+  }
+
+  return platformName;
+}
+
+const osname = platform();
+
+export const IS_PLATFORM_IOS: boolean = osname === IOS;
+export const IS_PLATFORM_ANDROID: boolean = osname === ANDROID;
