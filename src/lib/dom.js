608f0fe7997836663389b26aa5aa96441d9bd364
Igor Fedorov <iamfedoroff@yandex.ru>
2019-08-22 15:28:01 +0300

    dom.js to dom.ts

--- src/lib/dom.js
+++
@@ -1,7 +0,0 @@
-export const canUseDOM = !!(
-  typeof window !== 'undefined' &&
-  window.document &&
-  window.document.createElement
-);
-
-export const canUseEventListeners = canUseDOM && !!(window.addEventListener || window.attachEvent);
