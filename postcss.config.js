8e9a6dc9683157215d0594b3e04289b3b8768cbb
ArthurStam <bumbledos@gmail.com>
2019-08-26 11:55:50 +0300

    added postcss-csso optimization for production build

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,16 @@
 const cssCustomProperties = require('postcss-custom-properties');
 const cssImport = require('postcss-import');
 const autoprefixer = require('autoprefixer');
+const csso = require('postcss-csso');
 
-module.exports = {
-  plugins: [
-    cssImport(),
-    cssCustomProperties({ preserve: true }),
-    autoprefixer()
-  ]
+let plugins = [
+  cssImport(),
+  cssCustomProperties({ preserve: true }),
+  autoprefixer()
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(csso({ restructure: false }));
 }
+
+module.exports = { plugins }
