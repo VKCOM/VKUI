33c22496e2d8c41a5c734755247966368575c0b3
ArthurStam <bumbledos@gmail.com>
2019-09-04 16:52:25 +0300

    supported separated es6 modules build (for SSR)

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+  plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime'],
+  ignore: ['./src/vkui.js']
+}
