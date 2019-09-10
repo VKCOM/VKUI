5bde3811c23fcf0eb0bb829e8c7d3cd1c10ae620
ArthurStam <bumbledos@gmail.com>
2019-09-10 19:13:50 +0300

    rewrited ActionSheetItem to tsx, added before property

--- src/components/ActionSheetItem/ActionSheetItem.tsx
+++ src/components/ActionSheetItem/ActionSheetItem.tsx
@@ -1,28 +1,48 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from '../../lib/classNames';
 import getClassName from '../../helpers/getClassName';
 import Tappable from '../Tappable/Tappable';
+import usePlatform from '../../hooks/usePlatform';
+import { HasChildren, HasClassName, HasStyleObject } from '../../types/props';
 
-const baseClassNames = getClassName('ActionSheetItem');
+export interface ActionSheetItemProps extends HasClassName, HasChildren, HasStyleObject {
+  theme?: 'default' | 'destructive' | 'cancel',
+  before?: React.ReactNode,
+  // meta?: React.ReactNode,
+  // subtitle?: React.ReactNode,
+  onClick?(): void,
+  autoclose?: boolean
+}
 
-const ActionSheetItem = ({ className, children, autoclose, theme, ...restProps }) => (
-  <Tappable
-    {...restProps}
-    className={classNames(baseClassNames, className, { [`ActionSheetItem--${theme}`]: true })}
-    component={theme === 'cancel' ? 'span' : 'div'}
-  >
-    <span className="ActionSheetItem__in">{children}</span>
-  </Tappable>
-);
-
-ActionSheetItem.propTypes = {
-  theme: PropTypes.oneOf(['default', 'destructive', 'cancel']),
-  children: PropTypes.node,
-  style: PropTypes.object,
-  onClick: PropTypes.func,
-  className: PropTypes.string,
-  autoclose: PropTypes.bool
+const ActionSheetItem: React.FunctionComponent<ActionSheetItemProps> = ({
+  className,
+  children,
+  autoclose,
+  theme,
+  // meta,
+  // subtitle,
+  before,
+  ...restProps
+}: ActionSheetItemProps) => {
+  const platform = usePlatform();
+  return (
+    <Tappable
+      {...restProps}
+      className={
+        classNames(getClassName('ActionSheetItem', platform), className, {[`ActionSheetItem--${theme}`]: true})
+      }
+      component={theme === 'cancel' ? 'span' : 'div'}
+    >
+      {before && <div className="ActionSheetItem__before">{before}</div>}
+      <div className="ActionSheetItem__container">
+        <div className="ActionSheetItem__content">
+          <div className="ActionSheetItem__children">{children}</div>
+          {/* {meta && <div className="ActionSheetItem__meta">{meta}</div>} */}
+          {/* {subtitle && <div className="ActionSheetItem__descr">{subtitle}</div>} */}
+        </div>
+      </div>
+    </Tappable>
+  );
 };
 
 ActionSheetItem.defaultProps = {
