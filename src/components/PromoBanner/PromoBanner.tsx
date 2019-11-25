31339b974a9dba9df364a91604c712f29fbe47fb
Danakt Saushkin <danakt@protonmail.com>
2019-11-25 17:23:20 +0300

    Fix classname

--- src/components/PromoBanner/PromoBanner.tsx
+++ src/components/PromoBanner/PromoBanner.tsx
@@ -3,6 +3,7 @@ import Icon24Dismiss from '@vkontakte/icons/dist/24/dismiss';
 import Button from '../Button/Button';
 import Cell from '../Cell/Cell';
 import Avatar from '../Avatar/Avatar';
+import classNames from '../../lib/classNames';
 
 type BannerData = {
   title?: string;
@@ -39,9 +40,9 @@ export interface PromoBannerProps {
 }
 
 const PromoBanner = (props: PromoBannerProps) => (
-  <div className={PromoBanner + (props.className ? ' ' + props.className : '')}>
+  <div className={classNames('PromoBanner', props.className)}>
     <div className="PromoBanner__head">
-      {props.bannerData.ageRestriction && <span className="PromoBanner__age">{props.bannerData.ageRestriction}</span>}
+      {props.bannerData.ageRestriction && <span className="PromoBanner__age">{props.bannerData.ageRestriction}+</span>}
       <span className="PromoBanner__label">{props.bannerData.advertisingLabel || 'Advertisement'}</span>
 
       {!props.isCloseButtonHidden && (
