25923aa849a97b0bf0ba19a4bf557bc6df65601e
ArthurStam <bumbledos@gmail.com>
2019-12-13 14:50:18 +0300

    FormLayout: TagName => Component

--- src/components/FormLayout/FormLayout.tsx
+++ src/components/FormLayout/FormLayout.tsx
@@ -1,4 +1,11 @@
-import React, { Children, FunctionComponent, HTMLAttributes, ReactElement, Fragment, FormEvent } from 'react';
+import React, {
+  Children,
+  FunctionComponent,
+  HTMLAttributes,
+  ReactElement,
+  FormEvent,
+  ElementType,
+} from 'react';
 import getClassName from '../../helpers/getClassName';
 import classNames from '../../lib/classNames';
 import usePlatform from '../../hooks/usePlatform';
@@ -7,13 +14,13 @@ import { HasChildren, HasRef } from '../../types/props';
 const preventDefault = (e: FormEvent) => e.preventDefault();
 
 export interface FormLayoutProps extends HTMLAttributes<HTMLElement>, HasRef<HTMLElement>, HasChildren {
-  TagName?: string;
+  Component?: ElementType;
 }
 
 const FormLayout: FunctionComponent<FormLayoutProps> = (props: FormLayoutProps) => {
   const {
     children,
-    TagName,
+    Component,
     className,
     getRef,
     onSubmit,
@@ -21,43 +28,42 @@ const FormLayout: FunctionComponent<FormLayoutProps> = (props: FormLayoutProps) 
   } = props;
 
   const platform = usePlatform();
+  return (
+    <Component
+      {...restProps}
+      className={classNames(getClassName('FormLayout', platform), className)}
+      onSubmit={onSubmit}
+      ref={getRef}
+    >
+      <div className="FormLayout__container">
+        {Children.toArray(children).map((field: ReactElement, i: number) => {
+          if (field) {
+            const { status, top, bottom } = field.props;
 
-  // Не используем тут JSX из-за этой проблемы: https://github.com/Microsoft/TypeScript/issues/28892
-  return React.createElement(TagName, {
-    ...restProps,
-    className: classNames(getClassName('FormLayout', platform), className),
-    onSubmit,
-    ref: getRef,
-  },
-  <Fragment>
-    <div className="FormLayout__container">
-      {Children.toArray(children).map((field: ReactElement, i: number) => {
-        if (field) {
-          const { status, top, bottom } = field.props;
-
-          return (
-            <div
-              className={classNames('FormLayout__row', { [`FormLayout__row--s-${status}`]: !!status })}
-              key={field.key || `row-${i}`}
-            >
-              {top && <div className="FormLayout__row-top">{top}</div>}
-              {field}
-              {bottom && <div className="FormLayout__row-bottom">{bottom}</div>}
-            </div>
-          );
-        } else {
-          return null;
-        }
-      })}
-    </div>
-    {TagName === 'form' &&
-      <input type="submit" className="FormLayout__submit" value="" />
-    }
-  </Fragment>);
+            return (
+              <div
+                className={classNames('FormLayout__row', { [`FormLayout__row--s-${status}`]: !!status })}
+                key={field.key || `row-${i}`}
+              >
+                {top && <div className="FormLayout__row-top">{top}</div>}
+                {field}
+                {bottom && <div className="FormLayout__row-bottom">{bottom}</div>}
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+      {Component === 'form' &&
+        <input type="submit" className="FormLayout__submit" value="" />
+      }
+    </Component>
+  );
 };
 
 FormLayout.defaultProps = {
-  TagName: 'form',
+  Component: 'form',
   onSubmit: preventDefault,
 };
 
