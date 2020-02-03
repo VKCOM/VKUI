5a5c74b4e50641dc9ef3f7f0d3d22fb0e463bab6
Artur Stambultsian <bumbledos@gmail.com>
2020-02-04 00:59:43 +0300

    removed client_light and client_dark schemes

--- tasks/generate_scheme.js
+++ tasks/generate_scheme.js
@@ -13,7 +13,7 @@ const pkg = require('../package.json');
 * @param {number} clusterData.alpha_multiplier
 * @return {string} color цвет в браузерном представлении
 */
-function resolveColor (palette, clusterData) {
+function resolveColor(palette, clusterData) {
   const color = palette[clusterData.color_identifier];
   const alphaMultiplier = clusterData.alpha_multiplier ? Number(clusterData.alpha_multiplier) : 1;
 
@@ -30,7 +30,7 @@ function resolveColor (palette, clusterData) {
  * @param {number} multiplier
  * @return {string} цвет в формате rgba
  */
-function ahex2rgba (ahex, multiplier = 1) {
+function ahex2rgba(ahex, multiplier = 1) {
   const opacity = parseInt(ahex.slice(0, 2), 16) / 255 * multiplier;
   const colorHex = ahex.slice(2);
   return opacify(colorHex, opacity);
@@ -41,12 +41,12 @@ function ahex2rgba (ahex, multiplier = 1) {
  * @param {number} opacity прозрачность в диапазоне [0, 1]
  * @return {string} цвет в формате rgba
  */
-function opacify (hex, opacity) {
+function opacify(hex, opacity) {
   return `rgba(${parseInt(hex.slice(0, 2), 16)}, ${parseInt(hex.slice(2, 4), 16)}, ${parseInt(hex.slice(4), 16)}, ${opacity.toFixed(2)})`;
 }
 
-function generateScheme () {
-  Object.keys(scheme).forEach((schemeId) => {
+function generateScheme() {
+  ['bright_light', 'space_gray'].forEach((schemeId) => {
     const clusters = scheme[schemeId].colors;
     let css = '/* stylelint-disable */\n/*\n* Этот файл сгенерирован автоматически. Не надо править его руками.\n*/\n';
     css += schemeId === pkg.defaultSchemeId ? ':root {\n' : `body[scheme="${schemeId}"] {\n`;
