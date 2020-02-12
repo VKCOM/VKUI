f3f3524e33fb88f40b01dca2835f929b4266491e
Artur Stambultsian <bumbledos@gmail.com>
2020-02-12 18:25:15 +0300

    Gradient: new component

---
+++ src/components/Gradient/Gradient.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent, HTMLAttributes } from 'react';
+import classNames from '../../lib/classNames';
+
+export interface GradientProps extends HTMLAttributes<HTMLDivElement> {
+  mode: 'tint' | 'white' | 'black';
+  to: 'top' | 'bottom';
+}
+
+const Gradient: FunctionComponent<GradientProps> = ({ mode, children, style, className, to }: GradientProps) => {
+  return (
+    <div
+      className={classNames('Gradient', `Gradient--md-${mode}`, `Gradient--to-${to}`, className)}
+      style={style}
+    >
+      {children}
+    </div>
+  );
+};
+
+Gradient.defaultProps = {
+  mode: 'tint',
+  to: 'top',
+};
+
+export default Gradient;
