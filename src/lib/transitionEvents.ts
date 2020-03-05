e5fd1bb15160418306b0cf9c60662610b618ab0a
Petrov Anton <case@petrov.website>
2020-03-05 12:58:30 +0400

    Simplify transitionEvents module

--- src/lib/transitionEvents.ts
+++ src/lib/transitionEvents.ts
@@ -1,20 +1,14 @@
 let supported: boolean;
 let prefix: string;
 
-if (typeof document !== 'undefined' && document.createElement) {
-  const d = document.createElement('div');
-
-  for (let i in d.style) {
-    if (d.style.hasOwnProperty(i)) {
-      let m = i.match(/^(moz|webkit|ms|)(transition|animation)$/i);
-      if (m) {
-        supported = true;
-      }
-      if (m && m[1]) {
-        // согласно спецификации, префиксы к событиям должны быть в нижнем регистре
-        // https://www.w3schools.com/jsref/event_transitionend.asp
-        prefix = m[1].toLowerCase();
-      }
+if (typeof document !== 'undefined') {
+  const d = document.body || document.documentElement;
+  if ('style' in d) {
+    if ('transition' in d.style) {
+      supported = true;
+    } else if ('webkitTransition' in d.style) {
+      supported = true;
+      prefix = 'webkit';
     }
   }
 }
