c2387df220072e7eef17d2d79e805454d044b022
Petrov Anton <gg4every@gmail.com>
2020-03-05 20:15:30 +0400

    Simplify getClassName function

--- src/helpers/getClassName.ts
+++ src/helpers/getClassName.ts
@@ -1,9 +1,5 @@
 import { OS, platform } from '../lib/platform';
-import classNames from '../lib/classNames';
 
 export default function getClassname(base: string, osname: OS = platform()): string {
-  return classNames(base, {
-    [base + '--ios']: osname === OS.IOS,
-    [base + '--android']: osname === OS.ANDROID,
-  });
+  return `${base} ${base}--${osname}`;
 }
