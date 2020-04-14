462f4072376aad8b6947e377a94b4d8894365c38
Igor Fedorov <iamfedoroff@yandex.ru>
2020-04-14 17:23:17 +0300

    [SelectMimicry] Fix disabled onClick (fix #635)

--- src/components/SelectMimicry/SelectMimicry.tsx
+++ src/components/SelectMimicry/SelectMimicry.tsx
@@ -24,6 +24,7 @@ const SelectMimicry: FunctionComponent<SelectMimicryProps> = ({
   getRootRef,
   multiline,
   disabled,
+  onClick,
   ...restProps
 }: SelectMimicryProps) => {
   return (
@@ -38,6 +39,7 @@ const SelectMimicry: FunctionComponent<SelectMimicryProps> = ({
       }, className)}
       getRootRef={getRootRef}
       status={status}
+      onClick={disabled ? null : onClick}
     >
       <div className="Select__container">
         <div className="Select__title">{children || placeholder}</div>
