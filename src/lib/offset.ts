56fc94508aa2b53f4feb6bda2f40c424f73ada4b
Anton Petrov <im@petrov.engineer>
2020-04-17 14:09:43 +0400

    Ignore scroll and client offsets

--- src/lib/offset.ts
+++ src/lib/offset.ts
@@ -7,16 +7,10 @@ export interface OffsetRectInterface {
 
 export function getOffsetRect(elem: HTMLElement): OffsetRectInterface {
   const box = elem.getBoundingClientRect();
-  const body = document.body;
-  const doc = document.documentElement;
-  const scrollTop = window.pageYOffset || doc.scrollTop || body.scrollTop;
-  const scrollLeft = window.pageXOffset || doc.scrollLeft || body.scrollLeft;
-  const clientTop = doc.clientTop || body.clientTop || 0;
-  const clientLeft = doc.clientLeft || body.clientLeft || 0;
 
   return {
-    top: box.top + scrollTop - clientTop,
-    left: box.left + scrollLeft - clientLeft,
+    top: box.top,
+    left: box.left,
     width: elem.offsetWidth,
     height: elem.offsetHeight,
   };
