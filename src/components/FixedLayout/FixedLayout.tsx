747d94964af5be07542065068f4f053539e31128
a.korotaev <a.korotaev@corp.vk.com>
2020-05-06 13:06:27 +0300

    fix: prevent setting of state for unmounted FixedLayout instance

--- src/components/FixedLayout/FixedLayout.tsx
+++ src/components/FixedLayout/FixedLayout.tsx
@@ -60,12 +60,14 @@ class FixedLayout extends React.Component<FixedLayoutProps, FixedLayoutState> {
     hasTabbar: PropTypes.bool,
   };
 
+  private onMountResizeTimeout: number;
+
   get document() {
     return this.context.document || document;
   }
 
   componentDidMount() {
-    setTimeout(() => this.doResize());
+    this.onMountResizeTimeout = setTimeout(() => this.doResize());
     window.addEventListener('resize', this.doResize);
 
     this.document.addEventListener(transitionStartEventName, this.onViewTransitionStart);
@@ -73,6 +75,7 @@ class FixedLayout extends React.Component<FixedLayoutProps, FixedLayoutState> {
   }
 
   componentWillUnmount() {
+    clearInterval(this.onMountResizeTimeout);
     window.removeEventListener('resize', this.doResize);
 
     this.document.removeEventListener(transitionStartEventName, this.onViewTransitionStart);
