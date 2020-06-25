c2efeaf2518b3d9e0da3b50044cc351cb01f0eda
NekR <nekr.fabula@gmail.com>
2020-06-25 14:40:12 +0300

    Update release script

--- tasks/release.sh
+++ tasks/release.sh
@@ -5,20 +5,34 @@ if [ ! $1 ]; then
 else
   echo "release: creating build"
   yarn clear && yarn build || exit 1
+
   echo "release: creating version"
   yarn version --new-version $1
+
   echo "release: building styleguide"
   yarn styleguide:build
-  echo "release: updating styleguide submodule"
-  git submodule foreach 'git add -A && git commit -m '"v$1"''
-  echo "release: commiting submodule updates"
-  git add -A && git commit -m "docs update: v$1"
-  echo "release: publishing to npm"
-  yarn publish --access-public --non-interactive
+
+  if [ !$2 ]; then
+    echo "release: updating styleguide submodule"
+    git submodule foreach 'git add -A && git commit -m '"v$1"''
+
+    echo "release: commiting submodule updates"
+    git add -A && git commit -m "docs update: v$1"
+
+    echo "release: pushing styleguide submodule changes"
+    git submodule foreach 'git push origin master'
+  fi
+
   echo "release: pushing updates to github"
-  git push origin master
+  git push origin HEAD
+
   echo "release: pushing new tag to github"
   git push origin --tags
-  echo "release: pushing styleguide submodule changes"
-  git submodule foreach 'git push origin master'
+
+  echo "release: publishing to npm"
+  if [ $2 ]; then
+    yarn publish --access-public --non-interactive --tag $2
+  else
+    yarn publish --access-public --non-interactive
+  fi
 fi
