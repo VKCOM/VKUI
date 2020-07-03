fca0b566625e7df959549ded96e6704b93e4d160
Igor Fedorov <iamfedoroff@yandex.ru>
2020-07-03 14:17:45 +0300

    Allow undefined and null in classNames ObjectClassNames type

--- src/lib/classNames.ts
+++ src/lib/classNames.ts
@@ -1,5 +1,5 @@
 export interface ObjectClassNames {
-  [index: string]: boolean;
+  [index: string]: boolean | undefined | null;
 }
 
 export type ClassName = number | string | ObjectClassNames | false | null | undefined;
