e1a5817c697fa7da188821b30859d685dc3b5184
Anton Petrov <69350443+eolme-fallback@users.noreply.github.com>
2020-08-21 02:07:29 +0400

    Use getDisplayName and fix typings

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,10 +1,20 @@
-import { useContext } from 'react';
-import { OSType } from '../lib/platform';
-import { SSRContext } from '../lib/SSR';
-import { ConfigProviderContext } from '../components/ConfigProvider/ConfigProviderContext';
+import React from 'react';
+import type { FC, ComponentType } from 'react';
+import type { OSType } from '../lib/platform';
+import usePlatform from '../hooks/usePlatform';
+import getDisplayName from '../helpers/getDisplayName';
 
-export default function usePlatform(): OSType {
-  const ssrContext = useContext(SSRContext);
-  const { platform } = useContext(ConfigProviderContext);
-  return ssrContext.platform || platform;
+export interface PlatformProps {
+  platform: OSType
+}
+
+export default function withPlatform<P extends PlatformProps>(Component: ComponentType<P>): ComponentType<P> {
+  const WithPlatform: FC<P> = (props: P) => {
+    const platform = usePlatform();
+    return <Component {...props} platform={platform} />;
+  };
+
+  WithPlatform.displayName = `withPlatform(${getDisplayName(Component)})`;
+
+  return WithPlatform;
 }
