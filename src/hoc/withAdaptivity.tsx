56d142c71d2272ca7ca17f9d98451a3d66bd329b
Anton Petrov <69350443+eolme-fallback@users.noreply.github.com>
2020-08-20 23:19:18 +0400

    Use getDisplayName and fix typings

--- src/hoc/withAdaptivity.tsx
+++ src/hoc/withAdaptivity.tsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
+import type { FC, ComponentType } from 'react';
 import { AdaptivityContext, SizeType, ViewWidth } from '../components/AdaptivityProvider/AdaptivityContext';
+import getDisplayName from '../helpers/getDisplayName';
 
 interface Config {
   sizeX?: boolean;
@@ -8,9 +10,15 @@ interface Config {
 }
 
 export { SizeType, ViewWidth };
+  
+export interface AdaptivityProps {
+  sizeX?: SizeType;
+  sizeY?: SizeType;
+  viewWidth?: ViewWidth;
+}
 
-export default function withAdaptivity<T>(TargetComponent: T, config: Config): T {
-  function AdaptivityConsumer(props: AdaptivityProps) {
+export default function withAdaptivity<P extends AdaptivityProps>(Component: ComponentType<P>, config: Config): ComponentType<P> {
+  const AdaptivityConsumer: FC<P> = (props: P) => {
     const context = useContext(AdaptivityContext);
     let update = false;
 
@@ -22,17 +30,12 @@ export default function withAdaptivity<T>(TargetComponent: T, config: Config): T
     const sizeY = props.sizeY || context.sizeY;
     const viewWidth = context.viewWidth;
 
-    const adaptivityProps: {
-      sizeX?: SizeType;
-      sizeY?: SizeType;
-      viewWidth?: ViewWidth;
-    } = {};
+    const adaptivityProps: AdaptivityProps = {};
     config.sizeX ? adaptivityProps.sizeX = sizeX : undefined;
     config.sizeY ? adaptivityProps.sizeY = sizeY : undefined;
     config.viewWidth ? adaptivityProps.viewWidth = viewWidth : undefined;
 
-    // @ts-ignore
-    const target = <TargetComponent {...props} {...adaptivityProps} />;
+    const target = <Component {...props} {...adaptivityProps} />;
 
     if (update) {
       return <AdaptivityContext.Provider value={{ sizeX, sizeY, viewWidth }}>
@@ -43,14 +46,7 @@ export default function withAdaptivity<T>(TargetComponent: T, config: Config): T
     return target;
   }
   
-  const displayName = Component.displayName || Component.name || 'Component';
-  AdaptivityConsumer.displayName = `withAdaptivity{displayName})`;
+  AdaptivityConsumer.displayName = `withAdaptivity(${getDisplayName(Component)})`;
 
-  return AdaptivityConsumer as unknown as T;
-}
-
-export interface AdaptivityProps {
-  sizeX?: SizeType;
-  sizeY?: SizeType;
-  viewWidth?: ViewWidth;
+  return AdaptivityConsumer;
 }
