3f9ad085b3cdaa0e371ce37e29f30dfd2343f63f
Anton Petrov <69350443+eolme-fallback@users.noreply.github.com>
2020-08-21 01:51:17 +0400

    Use getDisplayName and fix typings

--- src/hoc/withContext.tsx
+++ src/hoc/withContext.tsx
@@ -1,15 +1,15 @@
-import React, { Context } from 'react';
+import React, { useContext } from 'react';
+import type { FC, ComponentType, Context } from 'react';
 
-export default function withContext<T, X>(Component: T, Ctx: Context<X>, prop: string): T {
-  function WithContext(props: {}) {
-    const context = React.useContext<X>(Ctx);
+type PartialContext<P, X, T extends keyof P> = Omit<P, {
+  [K in keyof P]: P[K] extends X ? never : K
+}[T]>;
 
-    // @ts-ignore
+export default function withContext<P, X, U extends keyof P>(Component: ComponentType<P>, Ctx: Context<X>, prop: U) {
+  const WithContext: FC<P> = (props: P) => {
+    const context = useContext<X>(Ctx);
     return <Component {...props} {...{ [prop]: context }} />;
-  }
+  };
 
-  const displayName = Component.displayName || Component.name || 'Component';
-  WithContext.displayName = `withContext{displayName})`;
-
-  return WithContext as unknown as T;
+  return WithContext as ComponentType<PartialContext<P, X, U>>;
 }
