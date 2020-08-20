8fe335d4711be5c22314fdbe61e5f277cea8a64a
Anton Petrov <69350443+eolme-fallback@users.noreply.github.com>
2020-08-20 22:06:57 +0400

    Set better displayName for WithPlatform

--- src/hoc/withPlatform.tsx
+++ src/hoc/withPlatform.tsx
@@ -9,5 +9,9 @@ export default function withPlatform<T>(Component: T): T {
     // @ts-ignore
     return <Component {...props} platform={ssrContext.platform || platform} />;
   }
+
+  const displayName = Component.displayName || Component.name || 'Component';
+  WithPlatform.displayName = `withPlatform{displayName})`;
+
   return WithPlatform as unknown as T;
 }
