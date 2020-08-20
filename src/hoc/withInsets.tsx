5f7199a120a2988791e6b54cae0a143d1680345f
Anton Petrov <69350443+eolme-fallback@users.noreply.github.com>
2020-08-21 01:59:49 +0400

    Use getDisplayName and fix typings

--- src/hoc/withInsets.tsx
+++ src/hoc/withInsets.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import type { FC, ComponentType } from 'react';
+import type { Insets } from '@vkontakte/vk-bridge';
 import useInsets from '../hooks/useInsets';
+import getDisplayName from '../helpers/getDisplayName';
 
-export default function withInsets<T>(Component: T): T {
-  function WithInsets(props: {}) {
+export interface InsetsProps {
+  insets: Insets;
+}
+
+export default function withInsets<P extends InsetsProps>(Component: ComponentType<P>): ComponentType<P> {
+  const WithInsets: FC<P> = (props: P) => {
     const insets = useInsets();
-    // @ts-ignore
     return <Component {...props} insets={insets} />;
-  }
+  };
 
-  const displayName = Component.displayName || Component.name || 'Component';
-  WithInsets.displayName = `withInsets{displayName})`;
+  WithInsets.displayName = `withInsets(${getDisplayName(Component)})`;
 
-  return WithInsets as unknown as T;
+  return WithInsets;
 }
