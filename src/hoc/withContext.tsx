73dbbb3c45fce503a8b8f801002a9df859b6db52
Anton Petrov <69350443+eolme-fallback@users.noreply.github.com>
2020-08-24 02:56:36 +0400

    Fix props types

--- src/hoc/withContext.tsx
+++ src/hoc/withContext.tsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import type { FC, ComponentType, Context } from 'react';
+import type { InheritProps } from '../types'; 
 import getDisplayName from '../helpers/getDisplayName';
 
 type PartialContext<P, X, T extends keyof P> = Omit<P, {
   [K in keyof P]: P[K] extends X ? never : K
 }[T]>;
 
-export default function withContext<P, X, U extends keyof P>(Component: ComponentType<P>, Ctx: Context<X>, prop: U) {
+export default function withContext<P extends InheritProps, X, U extends keyof P>(Component: ComponentType<P>, Ctx: Context<X>, prop: U) {
   const WithContext: FC<P> = (props: P) => {
     const context = useContext<X>(Ctx);
     return <Component {...props} {...{ [prop]: context }} />;
