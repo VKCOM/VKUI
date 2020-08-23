5cc10e71c30f50e983eef1e2fe90f4b46d77e9d0
Anton Petrov <69350443+eolme-fallback@users.noreply.github.com>
2020-08-24 02:57:25 +0400

    Fix props types

--- src/hoc/withInsets.tsx
+++ src/hoc/withInsets.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { FC, ComponentType } from 'react';
 import type { Insets } from '@vkontakte/vk-bridge';
+import type { InheritProps } from '../types'; 
 import useInsets from '../hooks/useInsets';
 import getDisplayName from '../helpers/getDisplayName';
 
@@ -8,7 +9,7 @@ export interface InsetsProps {
   insets: Insets;
 }
 
-export default function withInsets<P extends InsetsProps>(Component: ComponentType<P>): ComponentType<P> {
+export default function withInsets<P extends InheritProps>(Component: ComponentType<P & InsetsProps>): ComponentType<P> {
   const WithInsets: FC<P> = (props: P) => {
     const insets = useInsets();
     return <Component {...props} insets={insets} />;
