ff0837bd6847d4c7e6847e9ba8d0e8eec7876804
Anton Petrov <69350443+eolme-fallback@users.noreply.github.com>
2020-08-24 16:11:33 +0400

    Update and rename getDisplayName.ts to componentUtils.ts

--- src/helpers/componentUtils.ts
+++ src/helpers/componentUtils.ts
@@ -1,5 +1,5 @@
 import type { ComponentType } from 'react';
 
-export default function getDisplayName<T>(WrappedComponent: ComponentType<T>): string {
+export function getDisplayName<T>(WrappedComponent: ComponentType<T>): string {
   return WrappedComponent.displayName || WrappedComponent.name || 'Component';
 }
