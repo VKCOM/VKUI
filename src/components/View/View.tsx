8728fe2395ad2e0e14ea4a1bc0dcf0b035ef81d0
Vadim Kurnakov <v.kurnakov@corp.vk.com>
2020-09-02 16:33:55 +0300

    add creation of custom event to view

--- src/components/View/View.tsx
+++ src/components/View/View.tsx
@@ -176,7 +176,7 @@ class View extends Component<ViewProps, ViewState> {
         visiblePanels: [nextPanel],
         scrolls: removeObjectKeys(prevState.scrolls, [prevState.swipeBackPrevPanel]),
       }, () => {
-        this.document.dispatchEvent(new this.window.CustomEvent(transitionEndEventName));
+        this.document.dispatchEvent(this.createCustomEvent(transitionEndEventName));
         window.scrollTo(0, prevState.scrolls[this.state.activePanel]);
         prevProps.onTransition && prevProps.onTransition({ isBack: true, from: prevPanel, to: nextPanel });
       });
@@ -186,7 +186,7 @@ class View extends Component<ViewProps, ViewState> {
 
     // Начался переход
     if (!prevState.animated && this.state.animated) {
-      this.document.dispatchEvent(new this.window.CustomEvent(transitionStartEventName, { detail: { scrolls } }));
+      this.document.dispatchEvent(this.createCustomEvent(transitionStartEventName, { detail: { scrolls } }));
       const nextPanelElement = this.pickPanel(this.state.nextPanel);
       const prevPanelElement = this.pickPanel(this.state.prevPanel);
 
@@ -199,7 +199,7 @@ class View extends Component<ViewProps, ViewState> {
 
     // Начался свайп назад
     if (!prevState.swipingBack && this.state.swipingBack) {
-      this.document.dispatchEvent(new this.window.CustomEvent(transitionStartEventName, { detail: { scrolls } }));
+      this.document.dispatchEvent(this.createCustomEvent(transitionStartEventName, { detail: { scrolls } }));
       this.props.onSwipeBackStart && this.props.onSwipeBackStart();
       const nextPanelElement = this.pickPanel(this.state.swipeBackNextPanel);
       const prevPanelElement = this.pickPanel(this.state.swipeBackPrevPanel);
@@ -230,6 +230,23 @@ class View extends Component<ViewProps, ViewState> {
       });
     }
   }
+
+  createCustomEvent(type: string, eventInitDict?: any) {
+    if (typeof this.window.CustomEvent !== 'function') {
+      const options = eventInitDict || { bubbles: false, cancelable: false, detail: null };
+
+      const evt = document.createEvent('CustomEvent');
+      evt.initCustomEvent(
+        type,
+        options.bubbles,
+        options.cancelable,
+        options.detail,
+      );
+      return evt;
+    }
+
+    return new this.window.CustomEvent(type, eventInitDict);
+  };
 
   shouldDisableTransitionMotion(): boolean {
     return this.props.configProvider.transitionMotionEnabled === false ||
@@ -287,7 +304,7 @@ class View extends Component<ViewProps, ViewState> {
       const activePanel = this.props.activePanel;
       const isBack = this.state.isBack;
       const prevPanel = this.state.prevPanel;
-      this.document.dispatchEvent(new this.window.CustomEvent(transitionEndEventName));
+      this.document.dispatchEvent(this.createCustomEvent(transitionEndEventName));
       this.setState({
         prevPanel: null,
         nextPanel: null,
@@ -330,7 +347,7 @@ class View extends Component<ViewProps, ViewState> {
       swipebackStartX: 0,
       swipeBackShift: 0,
     }, () => {
-      this.document.dispatchEvent(new this.window.CustomEvent(transitionEndEventName));
+      this.document.dispatchEvent(this.createCustomEvent(transitionEndEventName));
     });
   }
 
