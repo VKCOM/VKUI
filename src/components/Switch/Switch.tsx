7c2949115da07bdd66d11351b6304a0c1658b080
Igor Fedorov <igor.fedorov@corm.vk.com>
2020-09-21 15:38:53 +0300

    add sizeY=compact for switch

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -3,11 +3,13 @@ import getClassName from '../../helpers/getClassName';
 import classNames from '../../lib/classNames';
 import usePlatform from '../../hooks/usePlatform';
 import { HasRef, HasRootRef } from '../../types';
+import { useAdaptivity } from '../../hooks/useAdaptivity';
+import { SizeType } from '../../hoc/withAdaptivity';
 
 export interface SwitchProps extends
   InputHTMLAttributes<HTMLInputElement>,
   HasRootRef<HTMLLabelElement>,
-  HasRef<HTMLInputElement> {}
+  HasRef<HTMLInputElement> { }
 
 const Switch: FunctionComponent<SwitchProps> = ({
   style,
@@ -18,8 +20,13 @@ const Switch: FunctionComponent<SwitchProps> = ({
 }: SwitchProps) => {
   const platform = usePlatform();
 
+  const { sizeY } = useAdaptivity();
+
   return (
-    <label className={classNames(getClassName('Switch', platform), className)} style={style} ref={getRootRef}>
+    <label className={classNames(
+      getClassName('Switch', platform),
+      className,
+      { [`Switch--sizeY-${sizeY}`]: sizeY === SizeType.COMPACT })} style={style} ref={getRootRef}>
       <input {...restProps} type="checkbox" className="Switch__self" ref={getRef} />
       <span className="Switch__pseudo" />
     </label>
