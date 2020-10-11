9ac5a7193a017f3822c0512d374a3ad2b2c126c8
Artem Kamysh <a.kamysh@corp.mail.ru>
2020-10-12 02:35:41 +0300

    No ref in card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,19 +2,21 @@ import React, { FunctionComponent, HTMLAttributes } from 'react';
 import classNames from '../../lib/classNames';
 import getClassname from '../../helpers/getClassName';
 import usePlatform from '../../hooks/usePlatform';
+import { HasRootRef } from '../../types';
 
-export interface CardProps extends HTMLAttributes<HTMLDivElement> {
+export interface CardProps extends HTMLAttributes<HTMLDivElement>, HasRootRef<HTMLInputElement> {
   size?: 's' | 'm' | 'l';
   mode?: 'tint' | 'shadow' | 'outline';
 }
 
-const Card: FunctionComponent<CardProps> = ({ size, mode, children, style, className, ...restProps }: CardProps) => {
+const Card: FunctionComponent<CardProps> = ({ size, mode, children, style, className, getRootRef, ...restProps }: CardProps) => {
   const platform = usePlatform();
 
   return (
     <div
       {...restProps}
       style={style}
+      ref={getRootRef}
       className={classNames(className, getClassname('Card', platform), `Card--sz-${size}`, `Card--md-${mode}`)}
     >
       <div className="Card__in">
