2dc542b3375f8bcf68d4cc94804fa03e88a58e21
ArthurStam <bumbledos@gmail.com>
2020-11-03 17:36:24 +0300

    PanelHeader and Group: vkcom marriage

--- src/components/PanelHeader/PanelHeader.tsx
+++ src/components/PanelHeader/PanelHeader.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes, ReactNode, useContext } from 'react';
+import React, { FC, HTMLAttributes, ReactNode, useContext } from 'react';
 import usePlatform from '../../hooks/usePlatform';
 import getClassname from '../../helpers/getClassName';
 import classNames from '../../lib/classNames';
@@ -11,7 +11,11 @@ import withAdaptivity, { AdaptivityProps, SizeType } from '../../hoc/withAdaptiv
 import { isPrimitiveReactNode } from '../../lib/utils';
 import Text from '../Typography/Text/Text';
 
-export interface PanelHeaderProps extends HTMLAttributes<HTMLDivElement>, HasRef<HTMLDivElement>, HasRootRef<HTMLDivElement>, AdaptivityProps {
+export interface PanelHeaderProps extends
+  HTMLAttributes<HTMLDivElement>,
+  HasRef<HTMLDivElement>,
+  HasRootRef<HTMLDivElement>,
+  AdaptivityProps {
   left?: ReactNode;
   right?: ReactNode;
   separator?: boolean;
@@ -21,28 +25,53 @@ export interface PanelHeaderProps extends HTMLAttributes<HTMLDivElement>, HasRef
    * Если `false`, то шапка будет нулевой высоты и контент панели "залезет" под неё
    */
   visor?: boolean;
+  /**
+   * Если `false`, то шапка будет в потоке
+   */
+  fixed?: boolean;
 }
 
-const PanelHeader = ({
-  className,
-  left,
-  children,
-  right,
-  separator,
-  visor,
-  transparent,
-  shadow,
-  getRef,
-  getRootRef,
-  sizeX,
-  sizeY,
-  ...restProps
-}: PanelHeaderProps) => {
+const PanelHeaderIn: FC<PanelHeaderProps> = ({ children, left, right }) => {
+  const { webviewType } = useContext(ConfigProviderContext);
+  const isPrimitive = isPrimitiveReactNode(children);
+  const platform = usePlatform();
+
+  return (
+    <div className="PanelHeader__in">
+      <div className="PanelHeader__left">
+        {left}
+      </div>
+      <div className="PanelHeader__content">
+        {isPrimitive ? platform === VKCOM ? <Text weight="medium">{children}</Text> : <span>{children}</span> : children}
+      </div>
+      <div className="PanelHeader__right">
+        {webviewType !== WebviewType.VKAPPS && right}
+      </div>
+    </div>
+  );
+};
+
+const PanelHeader: FC<PanelHeaderProps> = (props) => {
+  const {
+    className,
+    left,
+    children,
+    right,
+    separator,
+    visor,
+    transparent,
+    shadow,
+    getRef,
+    getRootRef,
+    sizeX,
+    sizeY,
+    fixed,
+    ...restProps
+  } = props;
   const platform = usePlatform();
   const { webviewType } = useContext(ConfigProviderContext);
   const needShadow = shadow && sizeX === SizeType.REGULAR;
-
-  const isPrimitive = isPrimitiveReactNode(children);
+  const isFixed = platform === VKCOM ? false : fixed;
 
   return (
     <div
@@ -58,38 +87,22 @@ const PanelHeader = ({
             'PanelHeader--vkapps': webviewType === WebviewType.VKAPPS,
             'PanelHeader--no-left': left === undefined,
             'PanelHeader--no-right': right === undefined,
+            'PanelHeader--fixed': isFixed,
           },
+          `PanelHeader--sizeX-${sizeX}`,
           className,
         )
       }
-      ref={getRootRef}
+      ref={isFixed ? getRootRef : getRef}
     >
-      <FixedLayout
-        vertical="top"
-        className={
-          classNames(
-            'PanelHeader__fixed',
-            {
-              'PanelHeader__fixed--shadow': needShadow,
-            },
-          )
-        }
-        getRootRef={getRef}
-      >
-        <div className="PanelHeader__in">
-          <div className="PanelHeader__left">
-            {left}
-          </div>
-          <div className="PanelHeader__content">
-            {isPrimitive ? platform === VKCOM ? <Text weight="medium">{children}</Text> : <span>{children}</span> : children}
-          </div>
-          <div className="PanelHeader__right">
-            {webviewType !== WebviewType.VKAPPS && right}
-          </div>
-        </div>
-      </FixedLayout>
-      {separator && visor && <Separator
-        className={sizeX === SizeType.COMPACT ? 'PanelHeader__separator' : ''}
+      {isFixed ?
+        <FixedLayout className="PanelHeader__fixed" vertical="top">
+          <PanelHeaderIn {...props} />
+        </FixedLayout> :
+        <PanelHeaderIn {...props} />
+      }
+      {separator && visor && platform !== VKCOM && <Separator
+        className="PanelHeader__separator"
         expanded={sizeX === SizeType.REGULAR}
       />}
     </div>
@@ -100,6 +113,7 @@ PanelHeader.defaultProps = {
   separator: true,
   transparent: false,
   visor: true,
+  fixed: true,
 };
 
 export default withAdaptivity(PanelHeader, {
