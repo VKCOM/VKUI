0189db139203d93d6265e2d0aa89cf93dab114dd
Igor Fedorov <iamfedoroff@yandex.ru>
2020-11-06 13:41:23 +0300

    [Group] Fix description style

--- src/components/Group/Group.tsx
+++ src/components/Group/Group.tsx
@@ -1,9 +1,11 @@
-import React, { FunctionComponent, HTMLAttributes, ReactNode } from 'react';
+import React, { FC, HTMLAttributes, ReactNode } from 'react';
 import getClassName from '../../helpers/getClassName';
 import classNames from '../../lib/classNames';
 import { HasRootRef } from '../../types';
 import usePlatform from '../../hooks/usePlatform';
 import Separator from '../Separator/Separator';
+import { hasReactNode } from '../../lib/utils';
+import Caption from '../Typography/Caption/Caption';
 
 export interface GroupProps extends HasRootRef<HTMLDivElement>, HTMLAttributes<HTMLDivElement> {
   header?: ReactNode;
@@ -16,7 +18,7 @@ export interface GroupProps extends HasRootRef<HTMLDivElement>, HTMLAttributes<H
   separator?: 'show' | 'hide' | 'auto';
 }
 
-const Group: FunctionComponent<GroupProps> = (props: GroupProps) => {
+const Group: FC<GroupProps> = (props) => {
   const { header, description, className, children, separator, getRootRef, ...restProps } = props;
   const platform = usePlatform();
   const baseClassNames = getClassName('Group', platform);
@@ -25,7 +27,11 @@ const Group: FunctionComponent<GroupProps> = (props: GroupProps) => {
     <section {...restProps} ref={getRootRef} className={classNames(baseClassNames, className)}>
       {header}
       {children}
-      {description && <div className="Group__description">{description}</div>}
+      {hasReactNode(description) &&
+      <div className="Group__description">
+        <Caption weight="regular" level="1">{description}</Caption>
+      </div>
+      }
       {separator !== 'hide' &&
         <Separator className={classNames('Group__separator', {
           'Group__separator--force': separator === 'show',
