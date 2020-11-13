9acf450d9bfef3acc7122108b57484237a000879
ArthurStam <bumbledos@gmail.com>
2020-11-13 14:38:01 +0300

    index.ts: added missed NativeSelect export

--- src/index.ts
+++ src/index.ts
@@ -100,6 +100,7 @@ export { default as Radio } from './components/Radio/Radio';
 export { default as Checkbox } from './components/Checkbox/Checkbox';
 export { default as Select } from './components/Select/Select';
 export { default as SelectMimicry } from './components/SelectMimicry/SelectMimicry';
+export { default as NativeSelect } from './components/NativeSelect/NativeSelect';
 export { default as CustomSelect } from './components/CustomSelect/CustomSelect';
 export { default as CustomSelectOption } from './components/CustomSelectOption/CustomSelectOption';
 export { default as DatePicker } from './components/DatePicker/DatePicker';
