9cd5929b86f752afce08ac96e62f6073c036da3e
Vladimir Klepov <v.klepov@gmail.com>
2020-11-19 14:19:36 +0300

    TEST: Add danger eslint

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,29 @@
-// import { message, danger } from "danger"
+import { fail, warn } from "danger"
 const dangerJest = require('danger-plugin-jest').default;
+const fs = require('fs')
+const path = require('path')
 
-dangerJest();
+const lintPath = path.join(__dirname, 'lint-results.json')
+
+function lint() {
+  return new Promise((ok, fail) => {
+    return fs.readFile(lintPath, (err, file) => err ? fail(err) : ok(JSON.parse(file)));
+  }).then(lintReport => {
+    for (const { messages, filePath } of lintReport) {
+      const relPath = path.relative(__dirname, filePath)
+      for (const message of messages) {
+        const text = `${message.message} \`${message.ruleId}\``
+        if (message.severity === 2) {
+          fail(text, relPath, message.line);
+        } else if (message.severity === 1) {
+          warn(text, relPath, message.line);
+        }
+      }
+    }
+  });
+}
+
+Promise.all([
+  dangerJest(),
+  lint(),
+]);
