36c0111a7625a13d61d8982754f288447bae48c9
Evgenii Khramkov <ewgeniux@gmail.com>
2020-11-19 12:05:03 +0300

    proper modal dismiss button states

--- src/components/ModalDismissButton/ModalDismissButton.tsx
+++ src/components/ModalDismissButton/ModalDismissButton.tsx
@@ -1,25 +1,15 @@
 import React, { HTMLAttributes, FC } from 'react';
-import PanelHeaderButton from '../PanelHeaderButton/PanelHeaderButton';
-import Icon24Dismiss from '@vkontakte/icons/dist/24/dismiss';
 import { Icon28CancelOutline } from '@vkontakte/icons';
-import withAdaptivity, { AdaptivityProps, ViewWidth } from '../../hoc/withAdaptivity';
-import classNames from '../../lib/classNames';
+import Tappable from '../Tappable/Tappable';
 
-export interface ModalDismissButtonProps extends HTMLAttributes<HTMLButtonElement>, AdaptivityProps {}
+export type ModalDismissButtonProps = HTMLAttributes<HTMLButtonElement>;
 
 const ModalDismissButton: FC<ModalDismissButtonProps> = (props) => {
-  const { viewWidth } = props;
-  const sideButton = viewWidth >= ViewWidth.SMALL_TABLET;
-
   return (
-    <PanelHeaderButton className={classNames('ModalDismissButton', {
-      'ModalDismissButton--side': sideButton,
-    })} {...props}>
-      {sideButton ? <Icon28CancelOutline width={20} height={20} /> : <Icon24Dismiss />}
-    </PanelHeaderButton>
+    <Tappable className="ModalDismissButton" {...props}>
+      <Icon28CancelOutline width={20} height={20} />
+    </Tappable>
   );
 };
 
-export default withAdaptivity(ModalDismissButton, {
-  viewWidth: true,
-});
+export default ModalDismissButton;
