06fd894fa8456250dba5a8a66e7f116170a1a6d1
Evgenii Khramkov <ewgeniux@gmail.com>
2020-12-21 18:04:35 +0300

    remove partial from demos

--- styleguide/Components/IntegrationSelect.js
+++ styleguide/Components/IntegrationSelect.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 
-const schemeOptions = ["full", "embedded", "partial"].map((schemeId) => (
+const schemeOptions = ["full", "embedded"].map((schemeId) => (
   <option value={schemeId} key={schemeId}>{schemeId}</option>
 ))
 
