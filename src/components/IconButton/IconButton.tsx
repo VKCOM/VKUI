7de140962c16cddae58aea7960177c6aa12f4c69
Evgeniya <e.polozova@corp.vk.com>
2021-01-13 19:06:05 +0300

    IconButton: get sizeY directly & update css

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -3,15 +3,18 @@ import Tappable from '../Tappable/Tappable';
 import getClassName from '../../helpers/getClassName';
 import classNames from '../../lib/classNames';
 import usePlatform from '../../hooks/usePlatform';
+import withAdaptivity, { AdaptivityProps } from '../../hoc/withAdaptivity';
 
 export interface IconButtonProps extends ButtonHTMLAttributes<HTMLElement> {
   icon: ReactNode;
   href?: string;
+  sizeY?: AdaptivityProps['sizeY'];
 }
 
 const IconButton: FunctionComponent<IconButtonProps> = ({
   className,
   icon,
+  sizeY,
   ...restProps
 }: IconButtonProps) => {
   const Component = restProps.href ? 'a' : 'button';
@@ -25,6 +28,7 @@ const IconButton: FunctionComponent<IconButtonProps> = ({
       className={classNames(
         getClassName('IconButton', platform),
         className,
+        `IconButton--sizeY-${sizeY}`,
       )}
     >
       {icon}
@@ -32,4 +36,6 @@ const IconButton: FunctionComponent<IconButtonProps> = ({
   );
 };
 
-export default IconButton;
+export default withAdaptivity(IconButton, {
+  sizeY: true,
+});
