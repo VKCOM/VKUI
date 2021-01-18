7a74e31c0cb8e49ec31e9da300a6c556af491295
Evgeniya <e.polozova@corp.vk.com>
2021-01-18 14:47:55 +0300

    TabbarItem: add ability to show badge

--- src/components/TabbarItem/TabbarItem.tsx
+++ src/components/TabbarItem/TabbarItem.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, ReactNode, HTMLAttributes, ElementType } from 'react';
 import getClassName from '../../helpers/getClassName';
 import Counter from '../Counter/Counter';
+import Badge from '../Badge/Badge';
 import classNames from '../../lib/classNames';
 import usePlatform from '../../hooks/usePlatform';
 import { HasLinkProps } from '../../types';
@@ -15,10 +16,14 @@ export interface TabbarItemProps extends HTMLAttributes<HTMLElement>, HasLinkPro
    * Счетчик рядом с иконкой
    */
   label?: ReactNode;
+  /**
+   * Индикатор рядом с иконкой
+   */
+  badge?: boolean;
 }
 
 const TabbarItem: FunctionComponent<TabbarItemProps> = (props: TabbarItemProps) => {
-  const { className, children, selected, label, text, ...restProps } = props;
+  const { className, children, selected, label, text, badge, ...restProps } = props;
   const platform = usePlatform();
   const Component: ElementType = restProps.href ? 'a' : 'div';
 
@@ -33,6 +38,7 @@ const TabbarItem: FunctionComponent<TabbarItemProps> = (props: TabbarItemProps) 
       <div className="TabbarItem__in">
         <div className="TabbarItem__icon">
           {children}
+          {badge && <Badge className="TabbarItem__badge" mode="prominent" />}
           {label && <Counter className="TabbarItem__label" size="s" mode="prominent">{label}</Counter>}
         </div>
         {text && <div className="TabbarItem__text">{text}</div>}
