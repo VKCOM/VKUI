9ef531c8a69dbedf32eaa1502c2bcdf783bce062
Evgeniya <e.polozova@corp.vk.com>
2021-01-22 23:24:16 +0300

    Counter: use typography components

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -2,6 +2,10 @@ import React, { HTMLAttributes } from 'react';
 import classNames from '../../lib/classNames';
 import getClassName from '../../helpers/getClassName';
 import usePlatform from '../../hooks/usePlatform';
+import Caption from '../Typography/Caption/Caption';
+import Text from '../Typography/Text/Text';
+import { VKCOM } from '../../lib/platform';
+import { hasReactNode } from '../../lib/utils';
 
 export interface CounterProps extends HTMLAttributes<HTMLDivElement> {
   /**
@@ -15,16 +19,24 @@ const Counter: React.FunctionComponent<CounterProps> = (props: CounterProps) => 
   const { mode, size, children, className, ...restProps } = props;
 
   const platform = usePlatform();
-  const baseClassName = getClassName('Counter', platform);
+  const captionWeight = platform === VKCOM ? 'medium' : 'regular';
 
   return (
     <div
       {...restProps}
-      className={classNames(className, baseClassName, `Counter--${mode}`, `Counter--s-${size}`)}
+      className={classNames(className,
+        getClassName('Counter', platform),
+        `Counter--${mode}`,
+        `Counter--s-${size}`,
+        className,
+      )}
     >
-      <div className="Counter__in">
-        {children}
-      </div>
+      {hasReactNode(children)
+        ? size === 's'
+          ? <Caption level="2" weight={captionWeight} className="Counter__in">{children}</Caption>
+          : <Text weight="medium" className="Counter__in">{children}</Text>
+        : null
+      }
     </div>
   );
 };
