e102cc95a9dcbb0239a97bea9d06942771a44cb5
Evgeniya <e.polozova@corp.vk.com>
2021-01-26 14:02:55 +0300

    SliderSwitch: pick activeValue from props (to avoid passing it down)

--- src/components/SliderSwitch/SliderSwitch.tsx
+++ src/components/SliderSwitch/SliderSwitch.tsx
@@ -115,7 +115,7 @@ export default class SliderSwitch extends React.Component<SliderSwitchProps, Sli
   }
 
   public render() {
-    const { name, options, className, ...restProps } = this.props;
+    const { name, options, className, activeValue: _activeValue, ...restProps } = this.props;
     const { activeValue, hoveredOptionId } = this.state;
 
     const [firstOption, secondOption] = options;
