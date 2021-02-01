4c7b53022e5c6003f654c15f5bee854cd82fc456
Evgeniya <e.polozova@corp.vk.com>
2021-02-01 19:51:27 +0300

    FormLayoutGroup: add removable

--- src/components/FormLayoutGroup/FormLayoutGroup.tsx
+++ src/components/FormLayoutGroup/FormLayoutGroup.tsx
@@ -1,26 +1,61 @@
-import React, { FunctionComponent, HTMLAttributes } from 'react';
+import React, { FunctionComponent, HTMLAttributes, MouseEvent } from 'react';
 import getClassName from '../../helpers/getClassName';
 import classNames from '../../lib/classNames';
 import usePlatform from '../../hooks/usePlatform';
+import { Removable, RemovePlaceholderProps } from '../Removable/Removable';
+import withAdaptivity, { AdaptivityProps } from '../../hoc/withAdaptivity';
 
-export interface FormLayoutGroupProps extends HTMLAttributes<HTMLDivElement> {
+export interface FormLayoutGroupProps extends HTMLAttributes<HTMLDivElement>, RemovePlaceholderProps {
   mode?: 'vertical' | 'horizontal';
+  /**
+   * Только для режима horizontal.
+   */
+  removable?: boolean;
+  /**
+   * Только для removable. Визуально сдвигает кнопки удаления ниже, компенсируя высоту лейбла (например, свойство `top` у `<Input/>`).
+   */
+  removeButtonOffset?: boolean;
+  /**
+   * Коллбэк срабатывает при клике на контрол удаления.
+   */
+  onRemove?: (e: MouseEvent) => void;
 }
 
-const FormLayoutGroup: FunctionComponent<FormLayoutGroupProps> = ({
+const FormLayoutGroup: FunctionComponent<FormLayoutGroupProps> = withAdaptivity(({
   children,
   className,
   mode,
+  removable,
+  removePlaceholder,
+  removeButtonOffset,
+  onRemove,
+  sizeY,
   ...restProps
-}: FormLayoutGroupProps) => {
+}: FormLayoutGroupProps & Pick<AdaptivityProps, 'sizeY'>) => {
   const platform = usePlatform();
 
+  const wrappedChildren = <div className="FormLayoutGroup__children">{children}</div>;
+
   return (
-    <div className={classNames(getClassName('FormLayoutGroup', platform), `FormLayoutGroup--${mode}`, className)} {...restProps}>
-      {children}
+    <div
+      className={classNames(
+        getClassName('FormLayoutGroup', platform),
+        `FormLayoutGroup--sizeY-${sizeY}`,
+        `FormLayoutGroup--${mode}`,
+        {
+          'FormLayoutGroup--offset': removable && removeButtonOffset,
+        },
+        className,
+      )}
+      {...restProps}>
+      {removable && mode === 'horizontal'
+        ? <Removable removePlaceholder={removePlaceholder} onRemove={onRemove}>{wrappedChildren}</Removable>
+        : wrappedChildren}
     </div>
   );
-};
+}, {
+  sizeY: true,
+});
 
 FormLayoutGroup.defaultProps = {
   mode: 'vertical',
