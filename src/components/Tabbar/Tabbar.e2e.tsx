9ea679138b7dbeb6091ca092706d26e0ac6d2e86
Vladimir Klepov <v.klepov@gmail.com>
2021-02-02 19:16:50 +0300

    test: Add screen tests to Tabbar

---
+++ src/components/Tabbar/Tabbar.e2e.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Icon28UserCircleOutline } from '@vkontakte/icons';
+import Tabbar from './Tabbar';
+import TabbarItem from '../TabbarItem/TabbarItem';
+import { describeScreenshotFuzz } from '../../testing/e2e/utils';
+import { Platform } from '../../lib/platform';
+
+describe('Tabbar', () => {
+  const tabs2 = [
+    <TabbarItem key="0" text="Новости"><Icon28UserCircleOutline /></TabbarItem>,
+    <TabbarItem key="1" text="Сервисы"><Icon28UserCircleOutline /></TabbarItem>,
+  ];
+  const tabs3 = [
+    ...tabs2,
+    <TabbarItem key="2" text="Профиль"><Icon28UserCircleOutline /></TabbarItem>,
+  ];
+  const RelTabbar: typeof Tabbar = (props) => <Tabbar {...props} style={{ position: 'relative' }} />;
+  describeScreenshotFuzz(RelTabbar, [{
+    children: [tabs2, tabs3],
+  }, {
+    children: [tabs2],
+    shadow: [false],
+  }], {
+    platforms: [Platform.ANDROID, Platform.IOS],
+  });
+});
