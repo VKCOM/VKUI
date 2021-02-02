ddca9d8fe6f9596167a1aed0bb3087e0d3ce55db
Vladimir Klepov <v.klepov@gmail.com>
2021-02-02 17:27:13 +0300

    fix: Preserve ref on tooltip target

--- src/components/Tooltip/Tooltip.test.tsx
+++ src/components/Tooltip/Tooltip.test.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { HtmlHTMLAttributes } from 'react';
 import { baselineComponent } from '../../testing/utils';
+import { render, screen } from '@testing-library/react';
 import Tooltip from './Tooltip';
 import View from '../View/View';
 import Panel from '../Panel/Panel';
 import { HasRootRef } from '../../types';
 
-const TooltipTarget = (props: HasRootRef<HTMLDivElement>) => (
+const Wrapper = ({ children }: any) => (
   <View activePanel="pan">
     <Panel id="pan">
-      <div ref={props.getRootRef}>trigger</div>
+      {children}
     </Panel>
   </View>
 );
 
 describe('Tooltip', () => {
-  baselineComponent((props) => <Tooltip isShown {...props}><TooltipTarget /></Tooltip>, {
+  baselineComponent((props) => (
+    <Wrapper>
+      <Tooltip isShown {...props}><div /></Tooltip>
+    </Wrapper>
+  ), {
     forward: false,
+  });
+
+  describe('preserves child ref', () => {
+    it('on DOM child', () => {
+      const ref = jest.fn();
+      render((
+        <Wrapper>
+          <Tooltip><div ref={ref} data-testid="xxx" /></Tooltip>
+        </Wrapper>
+      ));
+      expect(ref).toHaveBeenCalledWith(screen.getByTestId('xxx'));
+    });
+    it('on VKUI child', () => {
+      const RefNode = ({ getRootRef, ...props }: HasRootRef<HTMLDivElement> & HtmlHTMLAttributes<HTMLDivElement>) => (
+        <div ref={getRootRef} {...props} />
+      );
+      const ref = jest.fn();
+      render((
+        <Wrapper>
+          <Tooltip><RefNode getRootRef={ref} data-testid="xxx" /></Tooltip>
+        </Wrapper>
+      ));
+      expect(ref).toHaveBeenCalledWith(screen.getByTestId('xxx'));
+    });
   });
 });
