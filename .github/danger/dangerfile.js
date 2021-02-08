b93359acd627e8180f17873a18c22b38cd4435c0
Vladimir Klepov <v.klepov@gmail.com>
2021-02-08 18:55:41 +0300

    INFRA: Update danger report

--- .github/danger/dangerfile.js
+++ .github/danger/dangerfile.js
@@ -33,14 +33,24 @@ const coveragePath = path.join(rootDir, 'coverage', 'coverage-summary.json');
 function coverage() {
   return readFile(coveragePath).then(file => {
     const { total } = JSON.parse(file)
-    const formatCoverage = (kind, { covered, total, pct }) => `${covered} / ${total} ${kind} (${pct}%)`
-    message(`Code coverage: ${
-      Object.entries(total).map(([kind, cov]) => formatCoverage(kind, cov)).join(', ')
-    }`)
+    const formatCoverage = (kind, { covered, total, pct }) => `
+      <tr>
+        <td>${kind}</td>
+        <td>${covered} / ${total}</td>
+        <td style="text-align: right">${pct.toFixed(2)}%</td>
+      </tr>`;
+    markdown(`## Code coverage\n\n<table>${
+      Object.entries(total)
+        .map(([kind, cov]) => formatCoverage(kind, cov))
+        .join('')
+        .replace(/(^|\n) +/g, '')
+    }</table>`);
   }).catch(err => {
     warn(`Could not read coverage file: "${err.message}"`);
   })
 }
+
+const updatedViaAction = () => danger.github && danger.github.commits.some(commit => commit.commit.message === 'CHORE: Update screenshots');
 
 const updateScreensActionLink = `https://github.com/VKCOM/VKUI/actions?query=workflow%3A"Update+screenshots"`;
 const UPLOAD_BUCKET = 'vkui-screenshot';
@@ -51,10 +61,6 @@ async function uploadFailedScreenshots() {
   const { github } = danger;
   const pathPrefix = github ? github.pr.number : 'local';
   const failedScreens = await access(diffDir).then(() => readdir(diffDir), () => []);
-
-  if (failedScreens.length) {
-    warn(`${failedScreens.length} changed screenshots found — review & update them via ["Update Screenshots" action](${updateScreensActionLink}) before merging.`);
-  }
 
   if (!AWS_ENDPOINT || !AWS_ACCESS_KEY_ID || !AWS_SECRET_KEY) {
     // Silently skip screenshot reporting if credentials missing
@@ -81,8 +87,19 @@ async function uploadFailedScreenshots() {
     warn(`Could not purge old screenshots from S3: "${err.message}"`);
   }
 
+
+  if (!failedScreens.length) {
+    return;
+  }
+
+  if (!updatedViaAction()) {
+    warn(`${failedScreens.length} changed screenshots found — review & update them via ["Update Screenshots" action](${updateScreensActionLink}) before merging.`);
+  }
+
+  let message = '## Changed screenshots\n\n';
+
   for (const failedScreen of failedScreens) {
-    const screenName = path.parse(failedScreen).name;
+    const screenName = path.parse(failedScreen).name.replace(/\-diff$/, '');
     const fileContents = await readFile(path.join(diffDir, failedScreen));
     const key = `${pathPrefix}/${screenName}-${md5(fileContents)}.png`;
     try {
@@ -93,21 +110,24 @@ async function uploadFailedScreenshots() {
         ContentType: 'image/png',
         ACL: 'public-read',
       }).promise();
-      markdown(`
-        <details>
-          <summary>Screenshot <code>${screenName}</code> changed</summary>
+      message += `
+        <details style="margin: 0">
+          <summary style="padding: 8px 0"><code>${screenName}</code></summary>
           <img src="https://${awsHost}/${key}">
         </details>
-      `.replace(/(^|\n) +/g, ''));
+      `.replace(/(^|\n) +/g, '');
     } catch (err) {
       warn(`Could not upload screenshot diff ${screenName}: ${err.message}`);
     }
   }
+
+  markdown(message);
 }
 
 async function checkUpdatedScreenshots() {
-  if (danger.git.modified_files.some(file => /__image_snapshots__/.test(file))) {
-    warn('Some screenshots were modified in this PR');
+  const hasModifiedScreens = danger.git.modified_files.some(file => /__image_snapshots__/.test(file));
+  if (hasModifiedScreens && !updatedViaAction()) {
+    warn(`Some screenshots were manually modified in this PR - please use the [action](${updateScreensActionLink}) next time.`);
   }
 }
 
