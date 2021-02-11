88020219b78db25802b9055d4067da4d8b466c2c
gbowsky <grishaxtail@gmail.com>
2021-02-11 16:36:36 +0300

    ActionSheet: removed necessary animation for desktop platform, prevented triggering onClose twice

--- src/components/ActionSheet/ActionSheet.tsx
+++ src/components/ActionSheet/ActionSheet.tsx
@@ -73,7 +73,7 @@ class ActionSheet extends Component<ActionSheetProps, ActionSheetState> {
 
   waitTransitionFinish(eventHandler: AnimationEndCallback) {
     if (this.isDesktop) {
-      eventHandler();
+      return eventHandler();
     }
 
     if (transitionEvent.supported) {
