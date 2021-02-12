1013bdab587a7b6b6c7c49e8c5b59e0139e35fb9
Vladimir Klepov <v.klepov@gmail.com>
2021-02-12 20:29:17 +0300

    test: Add FormLayoutGroup & FormItem e2e tests

---
+++ src/components/FormItem/FormItem.e2e.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FormItem } from './FormItem';
+import Input from '../Input/Input';
+import { describeScreenshotFuzz } from '../../testing/e2e/utils';
+
+describe('FormItem', () => {
+  describeScreenshotFuzz(FormItem, [{
+    top: [undefined, 'Сверху'],
+    bottom: [undefined, 'Снизу'],
+    removable: [undefined, true],
+    children: [<Input key="0" />],
+  }]);
+});
