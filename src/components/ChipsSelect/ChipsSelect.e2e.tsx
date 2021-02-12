01d70726901d56e550da892997d237c463d94428
Vladimir Klepov <v.klepov@gmail.com>
2021-02-12 19:59:13 +0300

    test: Add e2e test for CustomSelect scroll-on-arrow

---
+++ src/components/ChipsSelect/ChipsSelect.e2e.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ChipsSelect from './ChipsSelect';
+import { screenshot, mount } from '../../testing/e2e';
+import AdaptivityProvider from '../AdaptivityProvider/AdaptivityProvider';
+
+describe('ChipsSelect', () => {
+  const options = new Array(20).fill(0).map((_, i) => ({ value: i, label: `Option #${i}` }));
+  it('Scrolls to item via arrows', async () => {
+    await mount((
+      <AdaptivityProvider>
+        <ChipsSelect id="chips" options={options} />
+      </AdaptivityProvider>
+    ));
+    await page.focus('#chips');
+    for (let i = 0; i < 7; i++) {
+      await page.keyboard.press('ArrowDown');
+    }
+    expect(await screenshot(null, { selector: '.ChipsSelect, .ChipsSelect__options' })).toMatchImageSnapshot();
+  });
+});
