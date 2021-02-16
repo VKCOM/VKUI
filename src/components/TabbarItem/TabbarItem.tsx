3a1f713ca0fafc944943511de8c5b7c39e8f8f0f
ArthurStam <bumbledos@gmail.com>
2021-02-16 12:15:15 +0300

    TabbaItem: Tappable inside

--- src/components/TabbarItem/TabbarItem.tsx
+++ src/components/TabbarItem/TabbarItem.tsx
@@ -1,11 +1,12 @@
-import { FunctionComponent, ReactNode, HTMLAttributes, ElementType, AnchorHTMLAttributes } from 'react';
+import { FunctionComponent, ReactNode } from 'react';
 import { getClassName } from '../../helpers/getClassName';
 import Counter from '../Counter/Counter';
 import { classNames } from '../../lib/classNames';
 import { usePlatform } from '../../hooks/usePlatform';
 import { hasReactNode } from '../../lib/utils';
+import Tappable, { TappableProps } from '../Tappable/Tappable';
 
-export interface TabbarItemProps extends HTMLAttributes<HTMLElement>, AnchorHTMLAttributes<HTMLElement> {
+export interface TabbarItemProps extends Omit<TappableProps, 'label'> { // TODO убрать Omit после удаления свойства label
   selected?: boolean;
   /**
    * Тест рядом с иконкой
@@ -22,14 +23,16 @@ export interface TabbarItemProps extends HTMLAttributes<HTMLElement>, AnchorHTML
 }
 
 const TabbarItem: FunctionComponent<TabbarItemProps> = (props: TabbarItemProps) => {
-  const { children, selected, label, indicator, text, ...restProps } = props;
+  const { children, selected, label, indicator, text, Component, className, ...restProps } = props;
   const platform = usePlatform();
-  const Component: ElementType = restProps.href ? 'a' : 'div';
 
   return (
-    <Component
+    <Tappable
       {...restProps}
-      vkuiClass={classNames(getClassName('TabbarItem', platform), {
+      Component={restProps.href ? 'a' : Component}
+      activeMode="opacity"
+      hasHover={false}
+      vkuiClass={classNames(getClassName('TabbarItem', platform), className, {
         'TabbarItem--selected': selected,
         'TabbarItem--text': !!text,
       })}
@@ -44,8 +47,12 @@ const TabbarItem: FunctionComponent<TabbarItemProps> = (props: TabbarItemProps) 
         </div>
         {text && <div vkuiClass="TabbarItem__text">{text}</div>}
       </div>
-    </Component>
+    </Tappable>
   );
+};
+
+TabbarItem.defaultProps = {
+  Component: 'button',
 };
 
 export default TabbarItem;
