a242bbc10d54aa0f9707ac926cb250cb0ffe29cd
Vladimir Klepov <v.klepov@gmail.com>
2021-03-16 15:39:02 +0300

    test: Add tests for AppRoot classes

--- src/components/AppRoot/AppRoot.test.tsx
+++ src/components/AppRoot/AppRoot.test.tsx
@@ -2,25 +2,55 @@ import { render } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { AppRootContext } from './AppRootContext';
 import AppRoot from './AppRoot';
+import { SizeType } from '../../hoc/withAdaptivity';
 
 describe('AppRoot', () => {
   baselineComponent(AppRoot);
   describe('Manages portal root in embedded mode', () => {
-    it('Creates & injects portal root', () => {
-      let portalRoot: HTMLElement;
-      const { unmount } = render((
-        <AppRoot embedded>
-          <AppRootContext.Consumer>
-            {(ctx) => {
-              portalRoot = ctx.portalRoot;
-              return null;
-            }}
-          </AppRootContext.Consumer>
-        </AppRoot>
-      ));
-      expect(document.body).toContainElement(portalRoot);
-      unmount();
-      expect(document.body).not.toContainElement(portalRoot);
+    describe('Creates & injects portal root', () => {
+      it.each(['embedded'])('in %s mode', (mode) => {
+        let portalRoot: HTMLElement;
+        const { unmount } = render((
+          <AppRoot {...{ [mode]: true }}>
+            <AppRootContext.Consumer>
+              {(ctx) => {
+                portalRoot = ctx.portalRoot;
+                return null;
+              }}
+            </AppRootContext.Consumer>
+          </AppRoot>
+        ));
+        expect(document.body).toContainElement(portalRoot);
+        unmount();
+        expect(document.body).not.toContainElement(portalRoot);
+      });
+    });
+    describe('applies container classes', () => {
+      it('html class="vkui" in full mode', () => {
+        const { unmount } = render(<AppRoot />);
+        expect(document.documentElement).toHaveClass('vkui');
+        unmount();
+        expect(document.documentElement).not.toHaveClass('vkui');
+      });
+      it.each(['embedded', 'full'])('container class in %s mode', (mode) => {
+        const { unmount, container } = render(<AppRoot embedded={mode === 'embedded'} />);
+        expect(container).toHaveClass('vkui__root', mode === 'embedded' ? 'vkui__root--embedded' : '');
+        unmount();
+        expect(container).not.toHaveClass();
+      });
+      it.each(['embedded', 'full'])('adaptivity class in %s mode', (mode) => {
+        const { unmount, container, rerender } = render((
+          <AppRoot embedded={mode === 'embedded'} />
+        ));
+        const adaptiveTarget = mode === 'embedded' ? container : document.body;
+        expect(adaptiveTarget).not.toHaveClass('vkui--sizeX-regular');
+        rerender(<AppRoot embedded={mode === 'embedded'} sizeX={SizeType.REGULAR} />);
+        // adds class
+        expect(adaptiveTarget).toHaveClass('vkui--sizeX-regular');
+        unmount();
+        // removes class on unmount
+        expect(adaptiveTarget).not.toHaveClass();
+      });
     });
     it('Supports multi-instance mode', () => {
       let portalRoot1: HTMLElement;
