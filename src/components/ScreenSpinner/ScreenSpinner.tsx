59954ecf7474fb7b6335252a2405dd3af903367d
Vladimir Klepov <v.klepov@gmail.com>
2021-03-24 14:47:03 +0300

    Use PopoutWrapper hasMask in ScreenSpinner

--- src/components/ScreenSpinner/ScreenSpinner.tsx
+++ src/components/ScreenSpinner/ScreenSpinner.tsx
@@ -12,6 +12,7 @@ const ScreenSpinner: FunctionComponent<ScreenSpinnerProps> = (props: ScreenSpinn
 
   return (
     <PopoutWrapper
+      hasMask={false}
       vkuiClass={getClassName('ScreenSpinner', platform)}
       className={className}
       style={style}
