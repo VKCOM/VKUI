4ef658bad8da7532adc1d40878d90a558812bb56
Evgeniya <e.polozova@corp.vk.com>
2021-03-30 18:53:02 +0300

    Input: add after to screenshots & update them

--- src/components/Input/Input.e2e.tsx
+++ src/components/Input/Input.e2e.tsx
@@ -1,9 +1,11 @@
+import { Icon24User } from '@vkontakte/icons';
 import Input, { InputProps } from './Input';
 import { describeScreenshotFuzz } from '../../testing/e2e/utils';
 
 describe('Input', () => {
   describeScreenshotFuzz((props: InputProps) => <Input value="Иванов" {...props} />, [{
     align: [undefined, 'center', 'right'],
+    after: [undefined, <Icon24User key="icon-24-user" />],
     disabled: [undefined, true],
   }, {
     $adaptivity: 'y',
