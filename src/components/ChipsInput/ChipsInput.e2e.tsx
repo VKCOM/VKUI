5ad411fb281d72122d11f0da06fe3f5733e30ad2
Evgeniya <e.polozova@corp.vk.com>
2021-03-30 18:52:46 +0300

    ChipsInput: add after to screenshots & update them

--- src/components/ChipsInput/ChipsInput.e2e.tsx
+++ src/components/ChipsInput/ChipsInput.e2e.tsx
@@ -1,3 +1,4 @@
+import { Icon16Clear } from '@vkontakte/icons';
 import ChipsInput, { ChipsInputProps } from './ChipsInput';
 import { describeScreenshotFuzz } from '../../testing/e2e/utils';
 
@@ -7,6 +8,7 @@ describe('ChipsInput', () => {
       { value: '1', label: 'Arctic Monkeys' },
       { value: '2', label: 'Звери' },
     ]],
+    after: [undefined, <Icon16Clear key="icon-16-clear" />],
     disabled: [undefined, true],
   }, {
     $adaptivity: 'y',
