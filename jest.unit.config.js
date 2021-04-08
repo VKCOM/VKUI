550ea94fa1f628d7c532457d32588a8e2d57720c
ArthurStam <bumbledos@gmail.com>
2021-04-08 18:01:34 +0400

    returning code coverage

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
   displayName: 'unit',
   roots: [path.join(__dirname, 'src')],
-  // collectCoverage: true,
+  collectCoverage: true,
   collectCoverageFrom: ['src/*/**/*.{ts,tsx}'],
   coveragePathIgnorePatterns: [
     '\\.d\\.ts$',
