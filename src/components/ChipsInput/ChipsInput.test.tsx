64c4c356f2336f25aa01854167832c8d4c5db91d
Evgeniya <e.polozova@corp.vk.com>
2021-04-15 18:43:19 +0300

    ChipsInput: simplify & update unit tests

--- src/components/ChipsInput/ChipsInput.test.tsx
+++ src/components/ChipsInput/ChipsInput.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom/extend-expect';
 import { baselineComponent } from '../../testing/utils';
 import ChipsInput, { ChipsInputOption, ChipsInputProps } from './ChipsInput';
 
@@ -15,7 +14,7 @@ const ChipsInputTest = (props: ChipsInputProps<ChipsInputOption>) => {
 };
 
 const getChipsInput = () => screen.getByTestId('chips-input');
-const getChip = (label: string) => screen.queryByText(label);
+const getChip = () => screen.queryByText('Красный');
 
 describe('ChipsInput', () => {
   baselineComponent(ChipsInput);
@@ -25,13 +24,11 @@ describe('ChipsInput', () => {
       <ChipsInputTest
         value={[
           { value: 'red', label: 'Красный' },
-          { value: 'blue', label: 'Синий' },
         ]}
       />,
     );
 
-    expect(getChip('Красный')).not.toBeNull();
-    expect(getChip('Синий')).not.toBeNull();
+    expect(getChip()).not.toBeNull();
   });
 
   it('adds chips', () => {
@@ -39,7 +36,7 @@ describe('ChipsInput', () => {
 
     userEvent.type(getChipsInput(), 'Красный{enter}');
 
-    expect(getChip('Красный')).not.toBeNull();
+    expect(getChip()).not.toBeNull();
   });
 
   it('does not lose data when adding an already existing chip', () => {
@@ -55,7 +52,7 @@ describe('ChipsInput', () => {
 
     userEvent.type(getChipsInput(), 'Красный{enter}');
 
-    expect(getChip('Красный')).not.toBeNull();
+    expect(getChip()).not.toBeNull();
   });
 
   it('removes chip on hitting backspace', () => {
@@ -63,14 +60,14 @@ describe('ChipsInput', () => {
 
     userEvent.type(getChipsInput(), '{backspace}');
 
-    expect(getChip('Красный')).toBeNull();
+    expect(getChip()).toBeNull();
   });
 
   it('does not delete chips on hitting backspace in readonly mode', () => {
-    render(<ChipsInputTest readOnly value={[{ value: 'blue', label: 'Синий' }]} />);
+    render(<ChipsInputTest readOnly value={[{ value: 'red', label: 'Красный' }]} />);
 
     userEvent.type(getChipsInput(), '{backspace}');
 
-    expect(getChip('Синий')).not.toBeNull();
+    expect(getChip()).not.toBeNull();
   });
 });
