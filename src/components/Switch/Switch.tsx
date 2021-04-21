a026c2b7d19e0e5643102e3d2ec74d92d75d837f
Evgeniya <e.polozova@corp.vk.com>
2021-04-21 13:49:10 +0300

    Switch: use useExternRef()

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -4,6 +4,7 @@ import { classNames } from '../../lib/classNames';
 import { usePlatform } from '../../hooks/usePlatform';
 import { HasRef, HasRootRef } from '../../types';
 import { withAdaptivity, AdaptivityProps } from '../../hoc/withAdaptivity';
+import { useExternRef } from '../../hooks/useExternRef';
 
 export interface SwitchProps extends
   InputHTMLAttributes<HTMLInputElement>,
@@ -20,12 +21,11 @@ const Switch: FunctionComponent<SwitchProps> = ({
   ...restProps
 }: SwitchProps) => {
   const platform = usePlatform();
+  const inputRef = useExternRef(getRef);
 
   return (
-    <label vkuiClass={classNames(
-      getClassName('Switch', platform),
-      `Switch--sizeY-${sizeY}`)} className={className} style={style} ref={getRootRef}>
-      <input {...restProps} type="checkbox" vkuiClass="Switch__self" ref={getRef} />
+    <label vkuiClass={classNames(getClassName('Switch', platform), `Switch--sizeY-${sizeY}`)} className={className} style={style} ref={getRootRef}>
+      <input {...restProps} type="checkbox" vkuiClass="Switch__self" ref={inputRef} />
       <span vkuiClass="Switch__pseudo" />
     </label>
   );
