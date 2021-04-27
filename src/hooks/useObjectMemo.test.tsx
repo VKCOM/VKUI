7186a0f69efeb062a0537c65a142c73b310ff296
Vladimir Klepov <v.klepov@gmail.com>
2021-04-27 10:58:52 +0300

    test: Add useObjectMemo tests

---
+++ src/hooks/useObjectMemo.test.tsx
@@ -0,0 +1,27 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useObjectMemo } from './useObjectMemo';
+
+describe(useObjectMemo, () => {
+  it('has object on first render', () => {
+    const { result } = renderHook(useObjectMemo, {
+      initialProps: { hello: 123 },
+    });
+    expect(result.current).toEqual({ hello: 123 });
+  });
+  it('memoizes object', () => {
+    const stableObject = { hello: 123 };
+    const { result, rerender } = renderHook(useObjectMemo, {
+      initialProps: stableObject,
+    });
+    rerender({ hello: 123 });
+    expect(result.current).toBe(stableObject);
+  });
+  it('updates object on change', () => {
+    const stableObject = { hello: 123 };
+    const { result, rerender } = renderHook(useObjectMemo, {
+      initialProps: stableObject,
+    });
+    rerender({ hello: 234 });
+    expect(result.current).toEqual({ hello: 234 });
+  });
+});
