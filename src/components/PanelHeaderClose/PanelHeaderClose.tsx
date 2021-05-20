4e41ca1d75b9b2dd3063e059def1c8e84b796823
Evgeniya <e.polozova@corp.vk.com>
2021-05-20 20:48:24 +0300

    PanelHeaderClose: add aria-label

--- src/components/PanelHeaderClose/PanelHeaderClose.tsx
+++ src/components/PanelHeaderClose/PanelHeaderClose.tsx
@@ -2,12 +2,13 @@ import { FunctionComponent } from 'react';
 import { Icon28CancelOutline } from '@vkontakte/icons';
 import PanelHeaderButton, { PanelHeaderButtonProps } from '../PanelHeaderButton/PanelHeaderButton';
 import { ANDROID, VKCOM } from '../../lib/platform';
+import { getTitleFromChildren } from '../../lib/utils';
 import { usePlatform } from '../../hooks/usePlatform';
 
 const PanelHeaderClose: FunctionComponent<PanelHeaderButtonProps> = ({ children, ...restProps }: PanelHeaderButtonProps) => {
   const platform = usePlatform();
   return (
-    <PanelHeaderButton {...restProps}>
+    <PanelHeaderButton aria-label={getTitleFromChildren(children)} {...restProps}>
       {platform === ANDROID || platform === VKCOM ? <Icon28CancelOutline /> : children}
     </PanelHeaderButton>
   );
