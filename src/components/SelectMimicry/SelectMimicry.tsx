e362a4ad3dc6a9895ee02f355102a9b264d0addb
Vladimir Klepov <v.klepov@gmail.com>
2021-05-24 17:45:25 +0300

    style: Disallow index imports

--- src/components/SelectMimicry/SelectMimicry.tsx
+++ src/components/SelectMimicry/SelectMimicry.tsx
@@ -5,7 +5,7 @@ import FormField from '../FormField/FormField';
 import { HasAlign, HasRootRef } from '../../types';
 import { withAdaptivity, AdaptivityProps, SizeType } from '../../hoc/withAdaptivity';
 import { usePlatform } from '../../hooks/usePlatform';
-import { getClassName } from '../..';
+import { getClassName } from '../../helpers/getClassName';
 import Headline from '../Typography/Headline/Headline';
 import Text from '../Typography/Text/Text';
 import { VKCOM } from '../../lib/platform';
