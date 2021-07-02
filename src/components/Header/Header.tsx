0c7ae1486888cafdbf39cc22758de8253f673f0f
Evgeniya <e.polozova@corp.vk.com>
2021-07-02 16:29:18 +0300

    Header: renderChildren() => HeaderContent

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { FC, HTMLAttributes, ReactNode, Fragment } from 'react';
+import { ElementType, FC, HTMLAttributes, ReactNode } from 'react';
 import { getClassName } from '../../helpers/getClassName';
 import { classNames } from '../../lib/classNames';
 import { usePlatform } from '../../hooks/usePlatform';
@@ -25,37 +25,38 @@ export interface HeaderProps extends HTMLAttributes<HTMLDivElement>, HasRootRef<
   multiline?: boolean;
 }
 
-function renderChildren({ children, platform, mode }: Pick<HeaderProps, 'children' | 'mode'> & HasPlatform) {
-  switch (platform) {
-    case Platform.ANDROID:
-      switch (mode) {
-        case 'primary':
-          return <Headline vkuiClass="Header__content" weight="medium">{children}</Headline>;
-        case 'secondary':
-          return <Caption Component="h3" vkuiClass="Header__content" level="1" weight="medium" caps>{children}</Caption>;
-        case 'tertiary':
-          return <Headline vkuiClass="Header__content" weight="medium">{children}</Headline>;
-      }
-      break;
-    case Platform.IOS:
-      switch (mode) {
-        case 'primary':
-        case 'tertiary':
-          return <Title vkuiClass="Header__content" weight="semibold" level="3">{children}</Title>;
-        case 'secondary':
-          return <Caption Component="h4" vkuiClass="Header__content" level="1" weight="semibold" caps>{children}</Caption>;
-      }
-      break;
-    case Platform.VKCOM:
-      switch (mode) {
-        case 'primary':
-          return <Headline vkuiClass="Header__content" weight="regular">{children}</Headline>;
-        case 'secondary':
-        case 'tertiary':
-          return <Caption Component="h4" vkuiClass="Header__content" level="1" weight="regular">{children}</Caption>;
-      }
+type HeaderContentProps = Pick<HeaderProps, 'children' | 'mode'> & HasPlatform & { Component: ElementType };
+
+const HeaderContent: FC<HeaderContentProps> = ({ platform, mode, ...restProps }: HeaderContentProps) => {
+  if (platform === Platform.IOS) {
+    switch (mode) {
+      case 'primary':
+      case 'tertiary':
+        return <Title weight="semibold" level="3" {...restProps} />;
+      case 'secondary':
+        return <Caption level="1" weight="semibold" caps {...restProps} />;
+    }
   }
-}
+
+  if (platform === Platform.VKCOM) {
+    switch (mode) {
+      case 'primary':
+        return <Headline weight="regular" {...restProps} />;
+      case 'secondary':
+      case 'tertiary':
+        return <Caption level="1" weight="regular" {...restProps} />;
+    }
+  }
+
+  switch (mode) {
+    case 'primary':
+      return <Headline weight="medium" {...restProps} />;
+    case 'secondary':
+      return <Caption level="1" weight="medium" caps {...restProps} />;
+    case 'tertiary':
+      return <Headline weight="medium" {...restProps} />;
+  }
+};
 
 type HeaderAsideProps = Pick<HeaderProps, 'aside'> & HasPlatform & { Component: ElementType };
 
@@ -88,18 +89,12 @@ const Header: FC<HeaderProps> = ({
     >
       <div vkuiClass="Header__in">
         <div vkuiClass="Header__main">
-          {renderChildren({
-            children: (
-              <Fragment>
-                <div vkuiClass={classNames('Header__content-base', {
-                  'Header__content-base--multiline': multiline,
-                })}>{children}</div>
-                {hasReactNode(indicator) && <Caption vkuiClass="Header__indicator" weight="regular" level="1">{indicator}</Caption>}
-              </Fragment>
-            ),
-            platform,
-            mode,
-          })}
+          <HeaderContent vkuiClass="Header__content" Component="h3" mode={mode} platform={platform}>
+            <span vkuiClass={classNames('Header__content-base', {
+              'Header__content-base--multiline': multiline,
+            })}>{children}</span>
+            {hasReactNode(indicator) && <Caption vkuiClass="Header__indicator" weight="regular" level="1">{indicator}</Caption>}
+          </HeaderContent>
           {hasReactNode(subtitle) && <Caption Component="span" vkuiClass="Header__subtitle" weight="regular" level="1">{subtitle}</Caption>}
         </div>
         {hasReactNode(aside) && <HeaderAside vkuiClass="Header__aside" platform={platform} Component="div">{aside}</HeaderAside>}
