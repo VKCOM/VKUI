ec0ee64a31e3c99fdf308a95c25e68f2bdc39a16
ArthurStam <bumbledos@gmail.com>
2021-07-21 18:39:43 +0300

    fixed tests

--- src/components/ModalCard/ModalCard.e2e.tsx
+++ src/components/ModalCard/ModalCard.e2e.tsx
@@ -4,24 +4,51 @@ import { ModalRoot } from '../ModalRoot/ModalRootAdaptive';
 import { describeScreenshotFuzz } from '../../testing/e2e/utils';
 import AppRoot from '../AppRoot/AppRoot';
 import { Icon56MoneyTransferOutline } from '@vkontakte/icons';
+import { Platform } from '../../lib/platform';
+import { ViewWidth } from '../../components/AdaptivityProvider/AdaptivityContext';
 
-describe('ModalCard', () => {
-  describeScreenshotFuzz<ModalCardProps>((props) => (
-    <div style={{ height: 800 }}>
-      <ModalRoot activeModal="test">
-        <ModalCard nav="test" {...props} />
-      </ModalRoot>
-    </div>
-  ), [{
-    icon: [<Icon56MoneyTransferOutline key="icon" />],
-    header: ['Отправляйте деньги друзьям, используя банковскую карту'],
-    subheader: ['Номер карты получателя не нужен — он сам решит, куда зачислить средства.'],
-    actions: [
-      <Button size="l" mode="primary" key="button">
-        Попробовать
-      </Button>,
-    ],
-  }], {
+const Component = (props: ModalCardProps) => (
+  <div style={{ height: 800 }}>
+    <ModalRoot activeModal="test">
+      <ModalCard nav="test" {...props} />
+    </ModalRoot>
+  </div>
+);
+
+const propSets = [{
+  icon: [<Icon56MoneyTransferOutline key="icon" />],
+  header: ['Отправляйте деньги друзьям, используя банковскую карту'],
+  subheader: ['Номер карты получателя не нужен — он сам решит, куда зачислить средства.'],
+  actions: [
+    <Button size="l" mode="primary" key="button">
+      Попробовать
+    </Button>,
+  ],
+}];
+
+describe('ModalCard for ios and android', () => {
+  describeScreenshotFuzz<ModalCardProps>(Component, propSets, {
     Wrapper: AppRoot,
+    adaptivity: {
+      viewWidth: ViewWidth.MOBILE,
+    },
+    platforms: [Platform.IOS, Platform.ANDROID],
+  });
+});
+
+describe('ModalCard for ios and android tablets', () => {
+  describeScreenshotFuzz<ModalCardProps>(Component, propSets, {
+    Wrapper: AppRoot,
+    adaptivity: {
+      viewWidth: ViewWidth.SMALL_TABLET,
+    },
+    platforms: [Platform.IOS, Platform.ANDROID],
+  });
+});
+
+describe('ModalCard for vkcom', () => {
+  describeScreenshotFuzz<ModalCardProps>(Component, propSets, {
+    Wrapper: AppRoot,
+    platforms: [Platform.VKCOM],
   });
 });
