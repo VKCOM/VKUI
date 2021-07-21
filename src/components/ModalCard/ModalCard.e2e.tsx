2c73694dca99997ec3d6c73eaf515178c9d74df2
ArthurStam <bumbledos@gmail.com>
2021-07-21 19:33:46 +0300

    modal card tests

---
+++ src/components/ModalCard/ModalCard.e2e.tsx
@@ -0,0 +1,54 @@
+import ModalCard, { ModalCardProps } from './ModalCard';
+import Button from '../Button/Button';
+import { ModalRoot } from '../ModalRoot/ModalRootAdaptive';
+import { describeScreenshotFuzz } from '../../testing/e2e/utils';
+import AppRoot from '../AppRoot/AppRoot';
+import { Icon56MoneyTransferOutline } from '@vkontakte/icons';
+import { Platform } from '../../lib/platform';
+import { ViewWidth } from '../../components/AdaptivityProvider/AdaptivityContext';
+
+const Component = (props: ModalCardProps) => (
+  <div style={{ height: 800 }}>
+    <ModalRoot activeModal="test">
+      <ModalCard nav="test" {...props} />
+    </ModalRoot>
+  </div>
+);
+
+const propSets = [{
+  icon: [<Icon56MoneyTransferOutline key="icon" />],
+  header: ['Отправляйте деньги друзьям, используя банковскую карту'],
+  subheader: ['Номер карты получателя не нужен — он сам решит, куда зачислить средства.'],
+  actions: [
+    <Button size="l" mode="primary" key="button">
+      Попробовать
+    </Button>,
+  ],
+}];
+
+describe('ModalCard mobile', () => {
+  describeScreenshotFuzz<ModalCardProps>(Component, propSets, {
+    Wrapper: AppRoot,
+    adaptivity: {
+      viewWidth: ViewWidth.MOBILE,
+    },
+    platforms: [Platform.IOS, Platform.ANDROID],
+  });
+});
+
+describe('ModalCard tablet', () => {
+  describeScreenshotFuzz<ModalCardProps>(Component, propSets, {
+    Wrapper: AppRoot,
+    adaptivity: {
+      viewWidth: ViewWidth.SMALL_TABLET,
+    },
+    platforms: [Platform.IOS, Platform.ANDROID],
+  });
+});
+
+describe('ModalCard', () => {
+  describeScreenshotFuzz<ModalCardProps>(Component, propSets, {
+    Wrapper: AppRoot,
+    platforms: [Platform.VKCOM],
+  });
+});
