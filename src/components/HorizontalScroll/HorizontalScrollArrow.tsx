4e039e6c9453cd7c3be503461f9eb23bd899524d
Evgeniya <e.polozova@corp.vk.com>
2021-07-27 00:45:10 +0300

    HorizontalScrollArrow: fix css for platforms

--- src/components/HorizontalScroll/HorizontalScrollArrow.tsx
+++ src/components/HorizontalScroll/HorizontalScrollArrow.tsx
@@ -1,17 +1,24 @@
 import { FC } from 'react';
 import { Icon24Chevron } from '@vkontakte/icons';
+import { usePlatform } from '../../hooks/usePlatform';
+import { getClassName } from '../../helpers/getClassName';
+import { classNames } from '../../lib/classNames';
 
 export interface HorizontalScrollArrowProps {
   onClick: () => void;
   direction: 'left' | 'right';
 }
 
-const HorizontalScrollArrow: FC<HorizontalScrollArrowProps> = (props: HorizontalScrollArrowProps) => {
-  const { onClick, direction } = props;
+const HorizontalScrollArrow: FC<HorizontalScrollArrowProps> = ({ onClick, direction }: HorizontalScrollArrowProps) => {
+  const platform = usePlatform();
+
   return (
     <button
       type="button"
-      vkuiClass={`HorizontalScroll__arrow HorizontalScroll__arrow-${direction}`}
+      vkuiClass={classNames(
+        getClassName('HorizontalScroll__arrow', platform),
+        `HorizontalScroll__arrow-${direction}`,
+      )}
       onClick={onClick}
     >
       <span vkuiClass="HorizontalScroll__arrow-icon">
