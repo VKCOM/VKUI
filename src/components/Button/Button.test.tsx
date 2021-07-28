8142885a5d1061481b129ec51cba8ba57a751f08
Evgeniya <e.polozova@corp.vk.com>
2021-07-29 02:15:57 +0300

    tests(Button): update tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,29 +2,24 @@ import { render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import Button, { ButtonProps } from './Button';
 
-const ButtonTest = (props: ButtonProps) => <Button Component="div" data-testid="custom-btn" {...props} />;
+const ButtonTest = (props: ButtonProps) => <Button data-testid="custom-btn" {...props} />;
 const button = () => screen.getByTestId('custom-btn');
 
 describe('Button', () => {
   baselineComponent(Button);
 
-  it('button with valid href is handled as a native link', () => {
+  it('Component: Button is handled as a native button', () => {
+    render(<ButtonTest>Native Button</ButtonTest>);
+    expect(button().tagName.toLowerCase()).toMatch('button');
+  });
+
+  it('Component: Button with valid href is handled as a native link', () => {
     render(<ButtonTest href="#">Native Link</ButtonTest>);
-
-    expect(screen.getByRole('link')).toBe(button());
+    expect(button().tagName.toLowerCase()).toMatch('a');
   });
 
-  it('a11y: custom button has role="button" and tabindex', () => {
-    render(<ButtonTest>Custom Button</ButtonTest>);
-
-    expect(button()).toHaveAttribute('role', 'button');
-    expect(button()).toHaveAttribute('tabindex', '0');
-  });
-
-  it('a11y: button with role="link" is handled as a custom link and has tabIndex', () => {
-    render(<ButtonTest role="link">Custom Link</ButtonTest>);
-
-    expect(button()).toHaveAttribute('role', 'link');
-    expect(button()).toHaveAttribute('tabindex', '0');
+  it('Component: Button with valid href overrides passed Component', () => {
+    render(<ButtonTest href="#" Component="div">Native Link</ButtonTest>);
+    expect(button().tagName.toLowerCase()).toMatch('a');
   });
 });
