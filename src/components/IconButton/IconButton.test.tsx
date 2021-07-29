1e2f4b85511397f2a1df5bbc7fb14184b7804789
Evgeniya <e.polozova@corp.vk.com>
2021-07-29 03:29:09 +0300

    tests(IconButton): update tests

--- src/components/IconButton/IconButton.test.tsx
+++ src/components/IconButton/IconButton.test.tsx
@@ -1,16 +1,30 @@
 import { baselineComponent } from '../../testing/utils';
-import IconButton from './IconButton';
+import IconButton, { IconButtonProps } from './IconButton';
 import { render, screen } from '@testing-library/react';
+import { Icon28VoiceOutline } from '@vkontakte/icons';
+
+const IconButtonTest = (props: IconButtonProps) => (
+  <IconButton data-testid="button" aria-label="Тестовая кнопка" {...props}>
+    <Icon28VoiceOutline />
+  </IconButton>
+);
+const button = () => screen.getByTestId('button');
 
 describe('IconButton', () => {
   baselineComponent(IconButton);
 
-  it('correctly handles Component property', () => {
-    const { rerender } = render(<IconButton data-testid="target" />);
-    expect(screen.getByTestId('target').tagName).toEqual('BUTTON');
-    rerender(<IconButton data-testid="target" href="#" />);
-    expect(screen.getByTestId('target').tagName).toEqual('A');
-    rerender(<IconButton data-testid="target" Component="div" />);
-    expect(screen.getByTestId('target').tagName).toEqual('DIV');
+  it('Component: default IconButton is a button', () => {
+    render(<IconButtonTest />);
+    expect(screen.getByRole('button')).toBe(button());
+  });
+
+  it('Component: IconButton w/ href is a link', () => {
+    render(<IconButtonTest href="https://vk.com" />);
+    expect(screen.getByRole('link')).toBe(button());
+  });
+
+  it('Component: IconButton w/ Component and href is [Component]', () => {
+    render(<IconButtonTest href="https://vk.com" Component="div" role="link" />);
+    expect(button().tagName.toLowerCase()).toMatch('div');
   });
 });
