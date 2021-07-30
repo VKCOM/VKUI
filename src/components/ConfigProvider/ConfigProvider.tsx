6f82b5b5fd8e62d0fb2683911f76eccb76592a39
ArthurStam <bumbledos@gmail.com>
2021-07-30 10:23:32 +0300

    ConfigProvider: fixed warn text

--- src/components/ConfigProvider/ConfigProvider.tsx
+++ src/components/ConfigProvider/ConfigProvider.tsx
@@ -85,7 +85,7 @@ const ConfigProvider: FC<ConfigProviderProps> = ({
       return noop;
     }
     if (process.env.NODE_ENV === 'development' && target.hasAttribute('scheme')) {
-      warn('<body scheme> was set before VKUI mount - did you forget scheme="inherit"?');
+      warn('attribute scheme was set before VKUI mount - did you forget scheme="inherit"?');
     }
     target.setAttribute('scheme', scheme);
     return () => target.removeAttribute('scheme');
