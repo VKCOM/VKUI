f9d82d8dc049019944be82bcc545a3857ab32cc9
Evgeniya <e.polozova@corp.vk.com>
2021-07-30 10:43:14 +0300

    tests(ContentCard): add disabled test

--- src/components/ContentCard/ContentCard.test.tsx
+++ src/components/ContentCard/ContentCard.test.tsx
@@ -1,6 +1,25 @@
+import { screen, render } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
-import ContentCard from './ContentCard';
+import ContentCard, { ContentCardProps } from './ContentCard';
+
+const ContentCardTest = (props: ContentCardProps) => (
+  <ContentCard
+    subtitle="VKUI"
+    header="ContentCard example"
+    caption="VKUI Styleguide > Blocks > ContentCard"
+    image="/image.png"
+    {...props}
+    data-testid="card"
+  />
+);
 
 describe('ContentCard', () => {
   baselineComponent((props) => <ContentCard image="/image.png" {...props} />);
+
+  it('onClick: ContentCard without onClick is treated as disabled', () => {
+    render(<ContentCardTest />);
+    const tappable = screen.getByRole('button');
+
+    expect(tappable).toHaveAttribute('aria-disabled', 'true');
+  });
 });
