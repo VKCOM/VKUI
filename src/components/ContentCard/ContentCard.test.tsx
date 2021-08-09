278a786c1424654c799d2f8a0eeaf7520c902f5a
Evgeniya <e.polozova@corp.vk.com>
2021-08-09 21:03:16 +0300

    tests(ContentCard): update unit tests - add tests for img - update wordings

--- src/components/ContentCard/ContentCard.test.tsx
+++ src/components/ContentCard/ContentCard.test.tsx
@@ -1,5 +1,5 @@
 import { screen, render } from '@testing-library/react';
-import { baselineComponent } from '../../testing/utils';
+import { baselineComponent, imgOnlyAttributes } from '../../testing/utils';
 import ContentCard, { ContentCardProps } from './ContentCard';
 
 const ContentCardTest = (props: ContentCardProps) => (
@@ -7,16 +7,44 @@ const ContentCardTest = (props: ContentCardProps) => (
     subtitle="VKUI"
     header="ContentCard example"
     caption="VKUI Styleguide > Blocks > ContentCard"
-    src="/image.png"
     {...props}
     data-testid="card"
   />
 );
+const card = () => screen.getByTestId('card');
+const img = () => card().querySelector('img');
 
 describe('ContentCard', () => {
   baselineComponent((props) => <ContentCard src="/image.png" {...props} />);
 
-  it('onClick: ContentCard without onClick is treated as disabled', () => {
+  it('[img] renders img if src is passed', () => {
+    render(<ContentCardTest src="/image.png" />);
+
+    expect(img()).toBeInTheDocument();
+  });
+
+  it('[img] does not render img if there is no src', () => {
+    render(<ContentCardTest />);
+
+    expect(img()).not.toBeInTheDocument();
+  });
+
+  it('[img] passes ref to img', () => {
+    const refCallback = jest.fn();
+    render(<ContentCardTest src="/image.png" getRef={refCallback} />);
+
+    expect(refCallback).toBeCalled();
+  });
+
+  it('[img] passes all img attributes to img', () => {
+    render(<ContentCardTest src="/image.png" {...imgOnlyAttributes} />);
+
+    Object.keys(imgOnlyAttributes).forEach((attr) => {
+      expect(img()).toHaveAttribute(attr);
+    });
+  });
+
+  it('[onClick] is disabled when there is no onClick', () => {
     render(<ContentCardTest />);
     const tappable = screen.getByRole('button');
 
