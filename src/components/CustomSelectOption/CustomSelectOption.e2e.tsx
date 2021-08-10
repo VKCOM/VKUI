ce1c07d9d04060e685618b92509d76ad5816cddc
ArthurStam <bumbledos@gmail.com>
2021-08-10 16:35:16 +0300

    custom select option tests for different sizeY

--- src/components/CustomSelectOption/CustomSelectOption.e2e.tsx
+++ src/components/CustomSelectOption/CustomSelectOption.e2e.tsx
@@ -1,9 +1,10 @@
 import CustomSelectOption from './CustomSelectOption';
 import { describeScreenshotFuzz } from '../../testing/e2e/utils';
 import Avatar from '../Avatar/Avatar';
+import { SizeType } from '../../hoc/withAdaptivity';
 
 describe('CustomSelectOption', () => {
-  describeScreenshotFuzz(CustomSelectOption, [{
+  const propsSets = [{
     selected: [true],
     before: [<Avatar size={20} key="avatar" />],
     children: ['Мария Саломея Склодовская-Кюри Мария Саломея Склодовская-Кюри', 'Мария Саломея'],
@@ -11,5 +12,8 @@ describe('CustomSelectOption', () => {
   }, {
     children: ['Мария Саломея'],
     hovered: [true],
-  }]);
+  }];
+
+  describeScreenshotFuzz(CustomSelectOption, propsSets, { adaptivity: { sizeY: SizeType.COMPACT } });
+  describeScreenshotFuzz(CustomSelectOption, propsSets, { adaptivity: { sizeY: SizeType.REGULAR } });
 });
