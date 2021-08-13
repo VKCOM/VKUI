97d2cbcd86278ef2b5c053ae67cc7b80e4976f6f
Evgeniya <e.polozova@corp.vk.com>
2021-08-13 17:09:30 +0300

    tests(unit): add WriteBarIcon tests

--- src/components/WriteBarIcon/WriteBarIcon.test.tsx
+++ src/components/WriteBarIcon/WriteBarIcon.test.tsx
@@ -1,6 +1,25 @@
+import { render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
-import { WriteBarIcon } from './WriteBarIcon';
+import { WriteBarIcon, WriteBarIconProps } from './WriteBarIcon';
+
+const WriteBarIconTest = (props: WriteBarIconProps) => <WriteBarIcon data-testid="button" {...props} />;
+const button = () => screen.getByTestId('button');
 
 describe('WriteBarIcon', () => {
   baselineComponent(WriteBarIcon);
+
+  it('shows counter when count={0} is provided', () => {
+    const count = 0;
+
+    render(<WriteBarIconTest mode="attach" count={0} />);
+    const counter = screen.getByText(count.toString());
+
+    expect(counter).toBeInTheDocument();
+  });
+
+  it('a11y: adds default aria-label for assigned mode', () => {
+    render(<WriteBarIconTest mode="send" />);
+
+    expect(button()).toHaveAttribute('aria-label', 'Отправить');
+  });
 });
