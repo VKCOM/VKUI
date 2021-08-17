3b93ef2d724a114ee92fe6e115a40a95961f9393
eolme <im@petrov.engineer>
2021-08-17 17:03:10 +0400

    Deprecate classNamesString

--- src/lib/classNames.ts
+++ src/lib/classNames.ts
@@ -56,6 +56,7 @@ export function classNames() {
   return result;
 }
 
-export {
-  classNames as classNamesString,
-};
+/**
+ * @deprecated use `classNames` instead
+ */
+export const classNamesString = classNames;
