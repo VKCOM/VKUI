13f6c954dca7fedd86aadbb4ef8ffcf4fcbb3faf
Evgeniya <e.polozova@corp.vk.com>
2021-08-20 22:12:38 +0300

    fix(useAdaptivity): update to pick props useAdaptivity() is now able to take in sizeX and sizeY this can be useful for e2e tests

--- src/hooks/useAdaptivity.ts
+++ src/hooks/useAdaptivity.ts
@@ -2,6 +2,23 @@ import { useContext } from 'react';
 import { AdaptivityContext } from '../components/AdaptivityProvider/AdaptivityContext';
 import { AdaptivityProps } from '../hoc/withAdaptivity';
 
-export const useAdaptivity = (): AdaptivityProps => {
-  return useContext(AdaptivityContext);
+export const useAdaptivity = (props?: { [key: string]: any }): AdaptivityProps => {
+  const contextAdaptivityProps = useContext(AdaptivityContext);
+
+  if (props?.sizeX || props?.sizeY) {
+    const {
+      sizeX = contextAdaptivityProps.sizeX,
+      sizeY = contextAdaptivityProps.sizeY,
+    } = props;
+
+    return Object.assign(
+      {},
+      contextAdaptivityProps,
+      {
+        sizeX,
+        sizeY,
+      });
+  }
+
+  return contextAdaptivityProps;
 };
