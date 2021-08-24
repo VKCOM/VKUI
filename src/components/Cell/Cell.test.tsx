52c763c4c9c927a80141b6aeb6e574a4cd1588e4
Evgeniya <e.polozova@corp.vk.com>
2021-08-24 17:27:56 +0300

    test(Cell): fix dragging tests

--- src/components/Cell/Cell.test.tsx
+++ src/components/Cell/Cell.test.tsx
@@ -5,6 +5,8 @@ import { Cell } from './Cell';
 import { ListContext } from '../List/ListContext';
 import List from '../List/List';
 
+const dragger = () => screen.getByLabelText('Перенести ячейку');
+
 describe('Cell', () => {
   baselineComponent(Cell);
 
@@ -12,16 +14,19 @@ describe('Cell', () => {
     it('on mouse up/down', () => {
       const toggleDrag = jest.fn();
       render(<ListContext.Provider value={{ toggleDrag }}><Cell draggable /></ListContext.Provider>);
-      fireEvent.mouseDown(document.querySelector('.Cell__dragger'));
+
+      fireEvent.mouseDown(dragger());
       expect(toggleDrag).toHaveBeenLastCalledWith(true);
-      fireEvent.mouseUp(document.querySelector('.Cell__dragger'));
+
+      fireEvent.mouseUp(dragger());
       expect(toggleDrag).toHaveBeenLastCalledWith(false);
     });
 
     it('stops drag on unmount', () => {
       const toggleDrag = jest.fn();
       const { rerender } = render(<ListContext.Provider value={{ toggleDrag }}><Cell draggable /></ListContext.Provider>);
-      fireEvent.mouseDown(document.querySelector('.Cell__dragger'));
+
+      fireEvent.mouseDown(dragger());
       rerender(<ListContext.Provider value={{ toggleDrag }} />);
       expect(toggleDrag).toHaveBeenLastCalledWith(false);
     });
