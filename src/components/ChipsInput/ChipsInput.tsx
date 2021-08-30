0930902e1d40ee18b93345a6fc66102628405991
Evgeniya <e.polozova@corp.vk.com>
2021-08-30 13:46:13 +0300

    ChipsInput: remove adaptive props

--- src/components/ChipsInput/ChipsInput.tsx
+++ src/components/ChipsInput/ChipsInput.tsx
@@ -14,7 +14,7 @@ import { classNames } from '../../lib/classNames';
 import Chip, { ChipProps } from '../Chip/Chip';
 import { noop } from '../../lib/utils';
 import { useChipsInput } from './useChipsInput';
-import { useAdaptivity, AdaptivityProps } from '../../hooks/useAdaptivity';
+import { useAdaptivity } from '../../hooks/useAdaptivity';
 import { prefixClass } from '../../lib/prefixClass';
 import { useExternRef } from '../../hooks/useExternRef';
 import './ChipsInput.css';
@@ -38,8 +38,7 @@ export interface ChipsInputProps<Option extends ChipsInputOption> extends
   HasRef<HTMLInputElement>,
   HasRootRef<HTMLDivElement>,
   HasAlign,
-  FormFieldProps,
-  AdaptivityProps {
+  FormFieldProps {
   value: Option[];
   inputValue?: string;
   onChange?: (o: Option[]) => void;
@@ -54,9 +53,8 @@ export interface ChipsInputProps<Option extends ChipsInputOption> extends
 const ChipsInput = <Option extends ChipsInputOption>(props: ChipsInputProps<Option>) => {
   const { style, value, onChange, onInputChange, onKeyDown, onBlur, onFocus, children, className, inputValue,
     getRef, getRootRef, placeholder, getOptionValue, getOptionLabel, getNewOptionData, renderChip,
-    after, inputAriaLabel, sizeX: propsSizeX, sizeY: propsSizeY, ...restProps } = props;
-
-  const { sizeY } = useAdaptivity({ sizeX: propsSizeX, sizeY: propsSizeY });
+    after, inputAriaLabel, ...restProps } = props;
+  const { sizeY } = useAdaptivity();
 
   const [focused, setFocused] = useState(false);
   const { fieldValue, addOptionFromInput, removeOption, selectedOptions, handleInputChange } = useChipsInput(props);
