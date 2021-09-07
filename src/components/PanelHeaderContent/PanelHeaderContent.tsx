75bcc8ed0a114543c05c3c31ef0cbe6d9642c0b1
Vladimir Klepov <v.klepov@gmail.com>
2021-09-07 17:17:04 +0300

    refactor: Reference Headline from PanelHeaderContext via class

--- src/components/PanelHeaderContent/PanelHeaderContent.tsx
+++ src/components/PanelHeaderContent/PanelHeaderContent.tsx
@@ -49,8 +49,8 @@ const PanelHeaderContent: FunctionComponent<PanelHeaderContentProps> = ({
           </Caption>
         }
         <div vkuiClass="PanelHeaderContent__children">
-          {hasReactNode(status) ?
-            <Headline Component="span" weight="medium">
+          {hasReactNode(status)
+            ? <Headline vkuiClass="PanelHeaderContent__children-in" Component="span" weight="medium">
               {children}
             </Headline>
             : <span vkuiClass="PanelHeaderContent__children-in">{children}</span>
