29a94e6f7bc980016bc6b4f8b8712a366a072096
Vladimir Klepov <v.klepov@gmail.com>
2021-09-09 22:09:22 +0300

    refactor: FixedLayout to FC

--- src/components/FixedLayout/FixedLayout.tsx
+++ src/components/FixedLayout/FixedLayout.tsx
@@ -6,13 +6,15 @@ import { withContext } from '../../hoc/withContext';
 import { HasPlatform, HasRef, HasRootRef } from '../../types';
 import { withPlatform } from '../../hoc/withPlatform';
 import { withPanelContext } from '../Panel/withPanelContext';
-import { setRef } from '../../lib/utils';
 import { SplitColContext, SplitColContextProps } from '../SplitCol/SplitCol';
 import { TooltipContainer } from '../Tooltip/TooltipContainer';
 import { PanelContextProps } from '../Panel/PanelContext';
 import { DOMProps, withDOM } from '../../lib/dom';
 import { IOS } from '../../lib/platform';
 import { warnOnce } from '../../lib/warnOnce';
+import { useTimeout } from '../../hooks/useTimeout';
+import { useGlobalEventListener } from '../../hooks/useGlobalEventListener';
+import { useExternRef } from '../../hooks/useExternRef';
 import './FixedLayout.css';
 
 export interface FixedLayoutProps extends
@@ -41,62 +43,39 @@ export interface FixedLayoutState {
 
 const warn = warnOnce('FixedLayout');
 
-class FixedLayout extends React.Component<FixedLayoutProps & DOMProps & PanelContextProps, FixedLayoutState> {
-  state: FixedLayoutState = {
-    position: 'absolute',
-    top: null,
-    bottom: null,
-    width: '',
-  };
+const FixedLayout: React.FC<FixedLayoutProps & DOMProps & PanelContextProps> = ({
+  children,
+  style,
+  vertical,
+  getRootRef,
+  getRef,
+  platform,
+  filled,
+  splitCol,
+  panel,
+  getPanelNode,
+  window,
+  document,
+  ...restProps
+}) => {
+  const [transitionOverrideStyle, setTransitionOverrideStyle] = React.useState<React.CSSProperties>({});
+  const [width, setWidth] = React.useState<string>(null);
+  const elRef = useExternRef(getRootRef);
 
-  el: HTMLDivElement;
-
-  private onMountResizeTimeout: number;
-
-  get document() {
-    return this.props.document;
-  }
-
-  get window() {
-    return this.props.window;
-  }
-
-  get currentPanel(): HTMLElement {
-    const elem = this.props.getPanelNode();
-
-    if (process.env.NODE_ENV === 'development' && !elem) {
+  const currentPanel = getPanelNode();
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === 'development' && !currentPanel) {
       warn('Panel element not found');
     }
+  }, [currentPanel]);
+  // Всегда предпологаем, что может быть скролл в случае, если нет document
+  const canTargetPanelScroll = !currentPanel || currentPanel.scrollHeight > currentPanel.clientHeight;
 
-    return elem;
-  }
-
-  get canTargetPanelScroll() {
-    const panelEl = this.currentPanel;
-    if (!panelEl) {
-      return true; // Всегда предпологаем, что может быть скролл в случае, если нет document
-    }
-    return panelEl.scrollHeight > panelEl.clientHeight;
-  }
-
-  componentDidMount() {
-    this.onMountResizeTimeout = setTimeout(() => this.doResize());
-    this.window.addEventListener('resize', this.doResize);
-
-    this.document.addEventListener(transitionStartEventName, this.onViewTransitionStart);
-    this.document.addEventListener(transitionEndEventName, this.onViewTransitionEnd);
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.onMountResizeTimeout);
-    this.window.removeEventListener('resize', this.doResize);
-
-    this.document.removeEventListener(transitionStartEventName, this.onViewTransitionStart);
-    this.document.removeEventListener(transitionEndEventName, this.onViewTransitionEnd);
-  }
-
-  onViewTransitionStart: EventListener = (e: CustomEvent<TransitionStartEventDetail>) => {
-    let panelScroll = e.detail.scrolls[this.props.panel] || 0;
+  const doResize = () => setWidth(splitCol?.colRef?.current ? `${splitCol.colRef.current.offsetWidth}px` : null);
+  useTimeout(doResize, 0).set();
+  useGlobalEventListener(window, 'resize', doResize);
+  useGlobalEventListener(document, transitionStartEventName, (e: CustomEvent<TransitionStartEventDetail>) => {
+    let panelScroll = e.detail.scrolls[panel] || 0;
 
     // support for unstable ViewInfinite
     if (Array.isArray(panelScroll)) {
@@ -104,86 +83,42 @@ class FixedLayout extends React.Component<FixedLayoutProps & DOMProps & PanelCon
       panelScroll = scrolls[scrolls.length - 1];
     }
 
-    const fromPanelHasScroll = this.props.panel === e.detail.from && panelScroll > 0;
-    const toPanelHasScroll = this.props.panel === e.detail.to && panelScroll > 0;
+    const fromPanelHasScroll = panel === e.detail.from && panelScroll > 0;
+    const toPanelHasScroll = panel === e.detail.to && panelScroll > 0;
 
     // если переход назад на Android - анимация только у панели с которой уходим (detail.from), и подстраиваться под скролл надо только на ней
     // на iOS переход между панелями горизонтальный, поэтому там нужно подстраивать хедеры на обеих панелях
-    const panelAnimated = this.props.platform === IOS || !(this.props.panel === e.detail.to && e.detail.isBack);
+    const panelAnimated = platform === IOS || !(panel === e.detail.to && e.detail.isBack);
 
     // Для панелей, с которых уходим всегда выставляется скролл
     // Для панелей на которые приходим надо смотреть, есть ли браузерный скролл и применяется ли к ней анимация перехода:
-    if (fromPanelHasScroll || toPanelHasScroll && this.canTargetPanelScroll && panelAnimated) {
-      this.setState({
+    if (fromPanelHasScroll || toPanelHasScroll && canTargetPanelScroll && panelAnimated) {
+      setTransitionOverrideStyle({
         position: 'absolute',
-        top: this.props.vertical === 'top' || fromPanelHasScroll ? this.el.offsetTop + panelScroll : null,
-        bottom: this.props.vertical === 'bottom' && !fromPanelHasScroll ? -panelScroll : null,
-        width: '',
+        top: vertical === 'top' || fromPanelHasScroll ? elRef.current.offsetTop + panelScroll : null,
+        bottom: vertical === 'bottom' && !fromPanelHasScroll ? -panelScroll : null,
       });
     }
-  };
+  });
+  useGlobalEventListener(document, transitionEndEventName, () => {
+    setTransitionOverrideStyle({});
+    doResize();
+  });
 
-  onViewTransitionEnd: VoidFunction = () => {
-    this.setState({
-      position: null,
-      top: null,
-      bottom: null,
-    });
-
-    this.doResize();
-  };
-
-  doResize = () => {
-    const { colRef } = this.props.splitCol;
-
-    if (colRef && colRef.current) {
-      const node: HTMLElement = colRef.current;
-      const width = node.offsetWidth;
-
-      this.setState({ width: `${width}px`, position: null });
-    } else {
-      this.setState({ width: '', position: null });
-    }
-  };
-
-  getRef: React.RefCallback<HTMLDivElement> = (element) => {
-    this.el = element;
-    setRef(element, this.props.getRootRef);
-  };
-
-  render() {
-    const {
-      children,
-      style,
-      vertical,
-      getRootRef,
-      getRef,
-      platform,
-      filled,
-      splitCol,
-      panel,
-      getPanelNode,
-      window,
-      document,
-      ...restProps
-    } = this.props;
-
-    return (
-      <TooltipContainer
-        {...restProps}
-        fixed
-        ref={this.getRef}
-        vkuiClass={classNames(getClassName('FixedLayout', platform), {
-          'FixedLayout--filled': filled,
-          [`FixedLayout--${vertical}`]: !!vertical,
-        })}
-        style={{ ...style, ...this.state }}
-      >
-        <div vkuiClass="FixedLayout__in" ref={getRef}>{children}</div>
-      </TooltipContainer>
-    );
-  }
-}
+  return (
+    <TooltipContainer
+      {...restProps}
+      fixed
+      ref={elRef}
+      vkuiClass={classNames(getClassName('FixedLayout', platform), {
+        'FixedLayout--filled': filled,
+      }, `FixedLayout--${vertical}`)}
+      style={{ ...style, ...transitionOverrideStyle, width }}
+    >
+      <div vkuiClass="FixedLayout__in" ref={getRef}>{children}</div>
+    </TooltipContainer>
+  );
+};
 
 export default withContext(
   withPlatform(withPanelContext(withDOM<FixedLayoutProps>(FixedLayout))),
