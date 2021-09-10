ea4691c9dbb99d4a4cfe1c11f0efa512df48f92b
Artur Stambultsian <bumbledos@gmail.com>
2021-09-10 12:48:42 +0300

    Revert "e2e: word-wrap break-word for props"
    This reverts commit 4109532c91ee6c72a7c44059db47049c51ec448b.

--- src/testing/e2e/utils.tsx
+++ src/testing/e2e/utils.tsx
@@ -135,7 +135,7 @@ export function describeScreenshotFuzz<Props>(
                   <Wrapper>
                     {multiCartesian(propSets, { adaptive: !isVkCom }).map((props, i) => (
                       <Fragment key={i}>
-                        <div style={{ wordWrap: 'break-word' }}>{prettyProps(props)}</div>
+                        <div>{prettyProps(props)}</div>
                         <div>
                           <AdaptiveComponent {...props} />
                         </div>
