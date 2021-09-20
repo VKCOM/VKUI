185d833b8206a0120523261674c9454bb697ab60
Artur Stambultsian <bumbledos@gmail.com>
2021-09-20 16:36:33 +0300

    defaultFilterFn: enhanced filter algorithm

--- src/lib/select.test.ts
+++ src/lib/select.test.ts
@@ -13,6 +13,20 @@ describe(defaultFilterFn, () => {
     expect(defaultFilterFn('петер', { label: 'Санкт-Петербург' })).toBeTruthy();
   });
 
+  it('ignores quotes', () => {
+    expect(defaultFilterFn('петер', { label: 'Санкт-"Петербург"' })).toBeTruthy();
+  });
+
+  it('ignores braces', () => {
+    expect(defaultFilterFn('пите', { label: 'Санкт-Петербург (Питер)' })).toBeTruthy();
+  });
+
+  it('search from the middle of sentence', () => {
+    expect(defaultFilterFn('имя нельз', { label: 'Группа, чьё имя нельзя называть' })).toBeTruthy();
+    expect(defaultFilterFn('нельзя назы', { label: 'Группа, чьё имя нельзя называть' })).toBeTruthy();
+    expect(defaultFilterFn('имя нельз', { label: 'ельзя ывать' })).toBeFalsy();
+  });
+
   it('skips matches in the middle', () => {
     expect(defaultFilterFn('ерб', { label: 'Санкт-Петербург' })).toBeFalsy();
   });
--- src/lib/select.ts
+++ src/lib/select.ts
@@ -5,6 +5,15 @@ type Option = {
 
 type GetOptionLabel = (option: Option) => string;
 
+const wordSplitters = /\p{Pd}|\p{Z}/u;
+
+/**
+ * Убирает кавычки, скобки, точки и запятые
+ */
+const cleanSentence = (sentence = '') => {
+  return sentence.replace(/\p{Ps}|\p{Pe}|\p{Pi}|\p{Pf}|"|'|,|\./gu, '');
+};
+
 export const defaultFilterFn = (
   query = '',
   option: Option,
@@ -13,11 +22,33 @@ export const defaultFilterFn = (
   if (query.length === 0) {
     return true;
   }
-  const words = getOptionLabel(option).split(/\s|-/);
+
+  query = query.toLowerCase();
+  let label = getOptionLabel(option).toLowerCase();
+
+  if (label.startsWith(query)) {
+    return true;
+  }
+
+  label = cleanSentence(label);
+  if (label.startsWith(query)) {
+    return true;
+  }
+
+  const words: string[] = label.split(wordSplitters);
+  const wordsLength = words.length;
+
+  for (let start = 0; start <= wordsLength - start; start++) {
+    for (let end = start + 2; end <= wordsLength; end++) {
+      words.push(words.slice(start, end).join(' '));
+    }
+  }
+
   for (const word of words) {
-    if (word.toLowerCase().startsWith(query.toLowerCase())) {
+    if (word.startsWith(query)) {
       return true;
     }
   }
+
   return false;
 };
