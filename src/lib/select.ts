cb873380b90894a9d2ac23e72133562728522136
Artur Stambultsian <bumbledos@gmail.com>
2021-09-21 13:35:21 +0300

    defaultFilterFn: fixed regex creating for try catch handling

--- src/lib/select.ts
+++ src/lib/select.ts
@@ -43,7 +43,7 @@ export const defaultFilterFn = (
   // На момент написания флаг u не поддерживался рядом браузеров, поэтому добавили фоллбэк.
   try {
     for (const index of includes) {
-      if (!/\p{L}/u.test(label[index - 1])) {
+      if (!new RegExp('\\p{L}', 'u').test(label[index - 1])) {
         return true;
       }
     }
