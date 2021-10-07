ceb6bf86b0fef6dbba005e1569e448438a8944de
Vladimir Klepov <v.klepov@gmail.com>
2021-10-07 16:15:17 +0300

    fix: Show Tooltip after Root transitions

--- src/components/NavTransitionContext/NavTransitionContext.tsx
+++ src/components/NavTransitionContext/NavTransitionContext.tsx
@@ -3,14 +3,22 @@ import { useObjectMemo } from '../../hooks/useObjectMemo';
 
 interface TransitionContextProps {
   entering: boolean;
-  scrollCompensation: number;
+  scrollCompensation?: number;
 }
 const TransitionContext = React.createContext<TransitionContextProps>({
   entering: false,
   scrollCompensation: 0,
 });
-export const NavTransitionProvider: React.FC<TransitionContextProps> = ({ children, ...ops }) => {
-  const contextValue = useObjectMemo(ops);
+export const useNavTransition = () => React.useContext(TransitionContext);
+export const NavTransitionProvider: React.FC<TransitionContextProps> = ({
+  children,
+  entering,
+  scrollCompensation,
+}) => {
+  const parentContext = useNavTransition();
+  const contextValue = useObjectMemo({
+    entering: parentContext.entering || entering,
+    scrollCompensation: parentContext.scrollCompensation || scrollCompensation || 0,
+  });
   return <TransitionContext.Provider value={contextValue}>{children}</TransitionContext.Provider>;
 };
-export const useNavTransition = () => React.useContext(TransitionContext);
