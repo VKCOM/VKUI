9b4da9cf3c37b323487fbbd80c022f975ef34822
Grigoriy Gorbovskoy <g.gorbovskoy@corp.vk.com>
2021-10-11 13:37:48 +0300

    pkg: added vkui-tokens to peer & dev deps & postcss cfg

--- postcss.config.js
+++ postcss.config.js
@@ -12,6 +12,7 @@ const animationsSource = path.join(__dirname, 'src/styles/animations.css');
 const cssPropSources = [
   path.join(__dirname, 'src/styles/bright_light.css'),
   path.join(__dirname, 'src/styles/constants.css'),
+  path.join(__dirname, 'node_modules/@vkontakte/vkui-tokens/themes/vkBase/cssVars/declarations/onlyColors.css'),
   animationsSource,
 ];
 
