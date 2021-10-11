b6497d469f077fcea5104eaed2f6b60163183112
Artur Stambultsian <bumbledos@gmail.com>
2021-10-11 17:52:09 +0300

    Popper: refactoring

--- src/components/Popper/Popper.tsx
+++ src/components/Popper/Popper.tsx
@@ -42,6 +42,7 @@ export interface PopperProps extends PopperCommonProps {
 
 const ARROW_PADDING = 10;
 const ARROW_WIDTH = 20;
+const ARROW_HEIGHT = 20;
 
 export const Popper: React.FC<PopperProps> = ({
   targetRef,
@@ -78,7 +79,7 @@ export const Popper: React.FC<PopperProps> = ({
     options: {
       offset: [
         arrow ? offsetSkidding - smallTargetOffsetSkidding : offsetSkidding,
-        arrow ? offsetDistance + 8 : offsetDistance,
+        arrow ? offsetDistance + ARROW_HEIGHT : offsetDistance,
       ],
     },
   }, {
