ae71adf337faba6bc0a9e7668a11fb0d7f291bdc
Evgeniya <e.polozova@corp.vk.com>
2021-11-16 16:01:06 +0300

    Tooltip: translate warnings

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -34,9 +34,6 @@ const isDOMTypeElement = (
 };
 
 const baseClassName = getClassName("Tooltip");
-const warn = warnOnce("Tooltip");
-const IS_DEV = process.env.NODE_ENV === "development";
-
 const SimpleTooltip = React.forwardRef<HTMLDivElement, SimpleTooltipProps>(
   function SimpleTooltip(
     { mode = "accent", header, text, arrowRef, style = {}, attributes },
@@ -156,6 +153,7 @@ function isVerticalPlacement(placement: Placement) {
   return placement.startsWith("top") || placement.startsWith("bottom");
 }
 
+const warn = warnOnce("Tooltip");
 const Tooltip: React.FC<TooltipProps> = ({
   children,
   isShown: _isShown,
@@ -174,7 +172,7 @@ const Tooltip: React.FC<TooltipProps> = ({
   const [tooltipArrowRef, setTooltipArrowRef] = React.useState<HTMLElement>();
   const [target, setTarget] = React.useState<HTMLElement>();
 
-  if (IS_DEV) {
+  if (process.env.NODE_ENV === "development") {
     const multiChildren = React.Children.count(children) > 1;
     // Empty children is a noop
     const primitiveChild =
@@ -204,9 +202,9 @@ const Tooltip: React.FC<TooltipProps> = ({
   /* eslint-enable @typescript-eslint/no-unnecessary-type-assertion*/
   /* eslint-enable no-restricted-properties */
 
-  if (IS_DEV && target && !tooltipContainer) {
+  if (process.env.NODE_ENV === "development" && target && !tooltipContainer) {
     throw new Error(
-      "Use TooltipContainer for Tooltip outside Panel (see docs)"
+      "Используйте TooltipContainer, чтобы показать тултип вне Panel (см. документацию)"
     );
   }
 
