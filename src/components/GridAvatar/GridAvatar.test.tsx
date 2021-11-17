599df8e09dd986c909171312d75e2c3a8eee0b53
Egor Smirnov <e.smirnov@corp.vk.com>
2021-11-17 12:32:21 +0300

    add GridAvatar test for forbidden props

--- src/components/GridAvatar/GridAvatar.test.tsx
+++ src/components/GridAvatar/GridAvatar.test.tsx
@@ -1,14 +1,22 @@
 import { render, screen } from '@testing-library/react';
+import { AvatarProps } from '../Avatar/Avatar';
 import { baselineComponent } from '../../testing/utils';
 import { GridAvatar, GridAvatarProps } from './GridAvatar';
 
-const GridAvatarTest = (props: GridAvatarProps) => (<GridAvatar {...props} data-testid="grid_avatar" />);
+const GridAvatarTest = (props: GridAvatarProps & Pick<AvatarProps, 'mode'>) => (<GridAvatar {...props} data-testid="grid_avatar" />);
 
 const avatar = () => screen.getByTestId('grid_avatar');
 const items = () => avatar().querySelectorAll('.vkuiGridAvatar__item');
 
 describe('GridAvatar', () => {
   baselineComponent(GridAvatar);
+
+  it('[forbidden props] don\'t allow to set mode', () => {
+    const { rerender } = render(<GridAvatarTest mode="app" src={['#']} />);
+    expect(avatar()).not.toHaveClass('Avatar--type-app');
+    rerender(<GridAvatarTest mode="image" src={['#']} />);
+    expect(avatar()).not.toHaveClass('Avatar--type-image');
+  });
 
   it('renders 1 grid item', () => {
     render(<GridAvatarTest src={['#']} />);
