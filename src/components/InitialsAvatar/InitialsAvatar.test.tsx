8d1e3dc24e25c549af6e353579a64df72fec419e
Egor Smirnov <e.smirnov@corp.vk.com>
2021-11-17 12:55:54 +0300

    remove img initials test

--- src/components/InitialsAvatar/InitialsAvatar.test.tsx
+++ src/components/InitialsAvatar/InitialsAvatar.test.tsx
@@ -1,18 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import { AvatarProps } from '../../components/Avatar/Avatar';
 import { baselineComponent } from '../../testing/utils';
-import { InitialsAvatar, InitialsAvatarProps } from './InitialsAvatar';
-
-const InitialsAvatarTest = (props: InitialsAvatarProps & AvatarProps) => (<InitialsAvatar {...props} data-testid="initials_avatar" />);
-
-const avatar = () => screen.getByTestId('initials_avatar');
-const img = () => avatar().querySelector('img');
+import { InitialsAvatar } from './InitialsAvatar';
 
 describe('InitialsAvatar', () => {
   baselineComponent(InitialsAvatar);
-
-  it('don\'t show image even if src was passed', () => {
-    render(<InitialsAvatarTest src="#" />);
-    expect(img()).not.toBeInTheDocument();
-  });
 });
