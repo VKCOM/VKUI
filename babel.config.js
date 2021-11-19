f803c76b27ce8ead5b580914c0895c96040fd71f
Artur Stambultsian <bumbledos@gmail.com>
2021-11-19 16:52:01 +0300

    added babel transform runtime polyfill

--- babel.config.js
+++ babel.config.js
@@ -33,7 +33,7 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
-    ['@babel/plugin-transform-runtime', { version: runtimeVersion }],
+    ['@babel/plugin-transform-runtime', { version: runtimeVersion, corejs: 3 }],
     [require.resolve('babel-plugin-auto-import'), {
       'declarations': [
         { members: ['createScopedElement'], path: '#jsxRuntime' }
