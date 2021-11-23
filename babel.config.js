e16ffa14bee65a1029c25a828c1260942ef8d021
Artur Stambultsian <bumbledos@gmail.com>
2021-11-23 20:31:06 +0300

    @babel/plugin-proposal-nullish-coalescing-operator added

--- babel.config.js
+++ babel.config.js
@@ -31,6 +31,7 @@ module.exports = {
     }]
   ],
   plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     ['@babel/plugin-transform-runtime', { version: runtimeVersion, corejs: 3 }],
