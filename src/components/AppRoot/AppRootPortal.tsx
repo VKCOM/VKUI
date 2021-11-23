b1536273f6b5d72e354fe403ce4abc3059f37cda
Artur Stambultsian <bumbledos@gmail.com>
2021-11-23 15:08:52 +0300

    AppRootPortal: simplified forcePortal resolve logic

--- src/components/AppRoot/AppRootPortal.tsx
+++ src/components/AppRoot/AppRootPortal.tsx
@@ -4,7 +4,8 @@ import { AppRootContext } from './AppRootContext';
 
 export const AppRootPortal: React.FC<React.PropsWithChildren<{ className?: string; forcePortal?: boolean }>> = ({ children, className, forcePortal }) => {
   const { portalRoot, mode } = React.useContext(AppRootContext);
-  forcePortal = typeof forcePortal === 'boolean' ? forcePortal : mode !== 'full';
+
+  forcePortal = forcePortal ?? mode !== 'full';
   return portalRoot && forcePortal
     ? createPortal((<div className={className}>{children}</div>), portalRoot)
     : <React.Fragment>{children}</React.Fragment>;
