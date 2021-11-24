e662e13663c5f7e95d53aa79924e006ddd99e0b4
Artur Stambultsian <bumbledos@gmail.com>
2021-11-24 12:02:50 +0300

    switched from @babel/runtime to @babel/runtime-corejs3

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ const isProduction = NODE_ENV === 'production';
 const isDevelopment = NODE_ENV === 'development';
 const useModules = isProduction || isDevelopment;
 const keepCss = Boolean(BABEL_KEEP_CSS);
-const runtimeVersion = require('./package.json').dependencies['@babel/runtime'];
+const runtimeVersion = require('./package.json').dependencies['@babel/runtime-corejs3'];
 
 const testFiles = [
   './src/**/*.test.ts', './src/**/*.test.tsx',
@@ -31,7 +31,6 @@ module.exports = {
     }]
   ],
   plugins: [
-    '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     ['@babel/plugin-transform-runtime', { version: runtimeVersion, corejs: 3 }],
