50c294de1ba1dd4b150264e605afd5e992a6096d
Evgeniya <e.polozova@corp.vk.com>
2021-11-25 17:18:23 +0300

    build(docs): bring back react-docgen-typescript

--- styleguide/config.js
+++ styleguide/config.js
@@ -1,7 +1,89 @@
 const path = require('path');
 const { argv } = require('yargs');
-const { makeFsImporter } = require('react-docgen/dist/importer');
-const { findAllExportedComponentDefinitions } = require('react-docgen/dist/resolver');
+
+/**
+ * react-docgen-typescript performance workaround
+ * gist: https://gist.github.com/wkillerud/36b119b98f9b9009cd50a733391941a4
+ * issue: https://github.com/styleguidist/react-docgen-typescript/issues/112#issuecomment-479772520
+ */
+const fs = require('fs');
+const ts = require('typescript');
+let languageService = null;
+// eslint-disable-next-line no-undef
+const filesCache = new Map();
+const { withCustomConfig } = require('react-docgen-typescript');
+const parser = withCustomConfig('./tsconfig.dist.json');
+const tsConfigFile = getTSConfigFile('./tsconfig.dist.json');
+
+function getTSConfigFile(tsconfigPath) {
+  const basePath = path.dirname(tsconfigPath);
+  const configFile = ts.readConfigFile(tsconfigPath, ts.sys.readFile);
+  return ts.parseJsonConfigFileContent(
+    configFile.config,
+    ts.sys,
+    basePath,
+    {},
+    tsconfigPath,
+  );
+}
+
+function createServiceHost(compilerOptions, files) {
+  return {
+    getScriptFileNames: () => {
+      return [...files.keys()];
+    },
+    getScriptVersion: (fileName) => {
+      const file = files.get(fileName);
+      return (file && file.version.toString()) || '';
+    },
+    getScriptSnapshot: (fileName) => {
+      if (!fs.existsSync(fileName)) {
+        return undefined;
+      }
+
+      let file = files.get(fileName);
+
+      if (file === undefined) {
+        const text = fs.readFileSync(fileName).toString();
+
+        file = { version: 0, text };
+        files.set(fileName, file);
+      }
+
+      return ts.ScriptSnapshot.fromString(file.text);
+    },
+    getCurrentDirectory: () => process.cwd(),
+    getCompilationSettings: () => compilerOptions,
+    getDefaultLibFileName: (options) => ts.getDefaultLibFilePath(options),
+    fileExists: ts.sys.fileExists,
+    readFile: ts.sys.readFile,
+    readDirectory: ts.sys.readDirectory,
+  };
+}
+
+function propsParser(file, source) {
+  filesCache.set(file, {
+    text: source,
+    version: 0,
+  });
+  return parser.parseWithProgramProvider(file, function() {
+    if (languageService) {
+      return languageService.getProgram();
+    }
+
+    const servicesHost = createServiceHost(
+      tsConfigFile.compilerOptions,
+      filesCache,
+    );
+
+    languageService = ts.createLanguageService(
+      servicesHost,
+      ts.createDocumentRegistry(),
+    );
+
+    return languageService.getProgram();
+  });
+}
 
 module.exports = {
   title: 'VKUI styleguide',
@@ -69,12 +151,7 @@ module.exports = {
       },
     },
   },
-  propsParser: (filePath, source) => {
-    return require('react-docgen').parse(source, findAllExportedComponentDefinitions, null, {
-      filename: filePath,
-      importer: makeFsImporter(),
-    });
-  },
+  propsParser: propsParser,
   exampleMode: 'expand',
   assetsDir: path.join(__dirname, `assets`),
   sections: [{
