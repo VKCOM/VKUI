9173c545add506e1436ac895c4c8c20e8269269b
Evgeniya <e.polozova@corp.vk.com>
2021-11-29 19:43:33 +0300

    unstable/ChipsSelect: use named export

--- src/unstable/index.ts
+++ src/unstable/index.ts
@@ -1,8 +1,8 @@
 /**
  * Forms
  */
-export { default as ChipsSelect } from "./../components/ChipsSelect/ChipsSelect";
-export type { ChipsSelectProps } from "./../components/ChipsSelect/ChipsSelect";
+export { ChipsSelect } from "../components/ChipsSelect/ChipsSelect";
+export type { ChipsSelectProps } from "../components/ChipsSelect/ChipsSelect";
 
 export { Dropdown } from "../components/Dropdown/Dropdown";
 export type { DropdownProps } from "../components/Dropdown/Dropdown";
