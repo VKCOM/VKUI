5701865a6e36a916dce4c934199e0d6ecac6c18d
Igor Fedorov <iamfedoroff@yandex.ru>
2021-12-02 09:03:43 +0300

    feat(InitialsAvatar): Add calcInitialsAvatarColor helper

---
+++ src/helpers/avatar.ts
@@ -0,0 +1,8 @@
+import { InitialsAvatarNumberGradients } from '../components/InitialsAvatar/InitialsAvatar';
+
+/**
+ * Вычисляет цвет InitialsAvatar на основании переданного идентификатора объекта
+ */
+export function calcInitialsAvatarColor(objectId: number): InitialsAvatarNumberGradients {
+  return objectId % 6 + 1 as InitialsAvatarNumberGradients;
+}
