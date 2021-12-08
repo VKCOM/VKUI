8336f44023d022e737fb0a80959dccc2abd9b3b5
Artur Stambultsian <bumbledos@gmail.com>
2021-12-08 13:18:06 +0300

    Tappable: updated disabled logic

--- src/components/Tappable/Tappable.tsx
+++ src/components/Tappable/Tappable.tsx
@@ -251,6 +251,7 @@ const Tappable: React.FC<TappableProps> = ({
         hasActive && active && isPresetActiveMode,
       [hoverMode]: hasHover && hovered && !isPresetHoverMode,
       [activeMode]: hasActive && active && !isPresetActiveMode,
+      "Tappable--no-effects": !hasHover && !hasActive,
     }
   );
 
