e9e473007d711e27b62f44d31e45b9dcf3a89d10
Stepan Kurennykh <s.kurennykh@vk.team>
2021-12-13 01:27:43 +0300

    Fix test

--- src/components/ChipsSelect/ChipsSelect.e2e.tsx
+++ src/components/ChipsSelect/ChipsSelect.e2e.tsx
@@ -21,7 +21,7 @@ describe("ChipsSelect", () => {
     }
     expect(
       await screenshot(null, {
-        selector: ".ChipsSelect, .ChipsSelect__options",
+        selector: ".ChipsSelect, .CustomSelectDropdown__CustomScrollView",
       })
     ).toMatchImageSnapshot();
   });
