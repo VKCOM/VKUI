3ebce4bc6ad7fb8ae51f11e1889239b434565486
Stepan Kurennykh <s.kurennykh@vk.team>
2022-01-25 17:25:32 +0300

    refactor tests

--- src/components/SimpleCell/SimpleCell.test.tsx
+++ src/components/SimpleCell/SimpleCell.test.tsx
@@ -2,7 +2,6 @@ import { screen, render } from "@testing-library/react";
 import { baselineComponent } from "../../testing/utils";
 import { SizeType } from "../AdaptivityProvider/AdaptivityContext";
 import SimpleCell from "./SimpleCell";
-import AdaptivityProvider from "../AdaptivityProvider/AdaptivityProvider";
 
 describe("SimpleCell", () => {
   baselineComponent(SimpleCell);
@@ -21,38 +20,5 @@ describe("SimpleCell", () => {
       </SimpleCell>
     );
     expect(screen.getByText("English").tagName.toLowerCase()).toMatch("span");
-  });
-
-  describe("Adaptivity", () => {
-    it("Set SizeX to regular", () => {
-      const innerWidth = window.innerWidth;
-
-      Object.defineProperty(window, "innerWidth", {
-        writable: true,
-        configurable: true,
-        value: 1600,
-      });
-
-      render(
-        <AdaptivityProvider>
-          <SimpleCell
-            data-testid="simple-cell"
-            sizeY={SizeType.REGULAR}
-            indicator="Русский"
-          >
-            Язык
-          </SimpleCell>
-        </AdaptivityProvider>
-      );
-      expect(screen.getByTestId("simple-cell")).toHaveClass(
-        " Tappable--sizeX-regular"
-      );
-
-      Object.defineProperty(window, "innerWidth", {
-        writable: true,
-        configurable: true,
-        value: innerWidth,
-      });
-    });
   });
 });
