dc8a7a84d95c2002e9386c5ce7ce688b65e0108e
Artur Stambultsian <bumbledos@gmail.com>
2022-02-03 17:15:48 +0700

    Title: updated props and classNames

--- src/components/Typography/Title/Title.tsx
+++ src/components/Typography/Title/Title.tsx
@@ -9,6 +9,9 @@ import "./Title.css";
 export interface TitleProps
   extends React.AllHTMLAttributes<HTMLElement>,
     HasComponent {
+  /**
+   * Начертания "heavy", "bold", "semibold", "medium" и "regular" устарели и будут удалены в 5.0.0.
+   */
   weight?:
     | "heavy"
     | "bold"
@@ -48,8 +51,10 @@ const Title: React.FC<TitleProps> = ({
       {...restProps}
       vkuiClass={classNames(
         getClassName("Title", platform),
-        `Title--w-${weight}`,
-        `Title--l-${level}`
+        `Title--l-${level}`,
+        {
+          [`Title--w-${weight}`]: !!weight,
+        }
       )}
     >
       {children}
