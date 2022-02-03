8674ca3e3569aba9806d3e45bf76a4eb16c3bd99
Artur Stambultsian <bumbledos@gmail.com>
2022-02-03 13:44:08 +0700

    supported custom platform

--- src/lib/platform.ts
+++ src/lib/platform.ts
@@ -10,7 +10,11 @@ export const ANDROID = Platform.ANDROID;
 export const IOS = Platform.IOS;
 export const VKCOM = Platform.VKCOM;
 
-export type PlatformType = Platform.ANDROID | Platform.IOS | Platform.VKCOM;
+export type PlatformType =
+  | Platform.ANDROID
+  | Platform.IOS
+  | Platform.VKCOM
+  | string;
 
 export function platform(browserInfo?: BrowserInfo): PlatformType {
   if (!browserInfo) {
