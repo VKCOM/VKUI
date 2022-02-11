be0cd2098581b6fd4e2d710dc836bcdd814ca02b
Evgeniya <e.polozova@corp.vk.com>
2022-02-11 15:42:37 +0300

    CardScroll: export from tokenized

--- src/tokenized/index.ts
+++ src/tokenized/index.ts
@@ -1,2 +1,5 @@
 export { default as Button } from "../components/Button/Button";
 export type { ButtonProps } from "../components/Button/Button";
+
+export { CardScroll } from "../components/CardScroll/CardScroll";
+export type { CardScrollProps } from "../components/CardScroll/CardScroll";
