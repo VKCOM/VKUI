9bc2c0cc73a1b7b629ac6af89b4a3b2978b5e0b6
eolme <im@petrov.engineer>
2022-02-11 21:22:38 +0400

    Fix modal scroll

--- src/components/ModalRoot/ModalRoot.tsx
+++ src/components/ModalRoot/ModalRoot.tsx
@@ -362,7 +362,7 @@ class ModalRootTouchComponent extends React.Component<
 
     if (!this.state.touchDown) {
       modalState.touchStartContentScrollTop =
-        modalState.contentElement?.scrollTop;
+        modalState.contentElement?.scrollTop ?? 0;
       this.setState({ touchDown: true });
     }
 
@@ -828,6 +828,12 @@ function initPageModal(modalState: ModalsStateEntry) {
     modalState.settlingHeight === 100
   ) {
     translateY = 0;
+  }
+
+  // Если модалка уже раскрыта обновляем состояния
+  if (translateY === 0) {
+    expanded = true;
+    collapsed = false;
   }
 
   modalState.expandedRange = expandedRange;
