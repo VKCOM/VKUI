9c105a0a7b9dad04c9f27089e446810c3512be73
Artur Stambultsian <bumbledos@gmail.com>
2022-02-14 18:36:30 +0700

    Subhead: added to styleguide/tokenized, src/tokenized/index.ts, fixed Readme

--- src/tokenized/index.ts
+++ src/tokenized/index.ts
@@ -1,2 +1,8 @@
 export { default as Button } from "../components/Button/Button";
 export type { ButtonProps } from "../components/Button/Button";
+
+export { default as Title } from "../components/Typography/Title/Title";
+export type { TitleProps } from "../components/Typography/Title/Title";
+
+export { default as Subhead } from "../components/Typography/Subhead/Subhead";
+export type { SubheadProps } from "../components/Typography/Subhead/Subhead";
