6d85a43c70a3cab91affde92bc0edc07dcec4aec
Yury Baykov <y.baykov@corp.mail.ru>
2022-02-15 14:43:44 +0300

    add simplecheckbox to tokenized list

--- src/tokenized/index.ts
+++ src/tokenized/index.ts
@@ -1,2 +1,5 @@
 export { default as Button } from "../components/Button/Button";
 export type { ButtonProps } from "../components/Button/Button";
+
+export { default as SimpleCheckbox } from "../components/SimpleCheckbox/SimpleCheckbox";
+export type { SimpleCheckboxProps } from "../components/SimpleCheckbox/SimpleCheckbox";
