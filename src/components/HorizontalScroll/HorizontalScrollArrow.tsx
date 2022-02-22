2c0ea6a58c55c215a9b6ed49a2671907b1a3947b
Artur Stambultsian <bumbledos@gmail.com>
2022-02-22 12:31:21 +0300

    Revert "Перевести на токены HorizontalScrollArrow"

--- src/components/HorizontalScroll/HorizontalScrollArrow.tsx
+++ src/components/HorizontalScroll/HorizontalScrollArrow.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Icon24Chevron } from "@vkontakte/icons";
+import { usePlatform } from "../../hooks/usePlatform";
+import { getClassName } from "../../helpers/getClassName";
 import { classNames } from "../../lib/classNames";
 import Tappable from "../Tappable/Tappable";
 import "./HorizontalScrollArrow.css";
@@ -13,13 +15,15 @@ const HorizontalScrollArrow: React.FC<HorizontalScrollArrowProps> = ({
   onClick,
   direction,
 }: HorizontalScrollArrowProps) => {
+  const platform = usePlatform();
+
   return (
     <Tappable
       Component="button"
       hasHover={false}
       hasActive={false}
       vkuiClass={classNames(
-        "HorizontalScrollArrow",
+        getClassName("HorizontalScrollArrow", platform),
         `HorizontalScrollArrow--${direction}`
       )}
       onClick={onClick}
