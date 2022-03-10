59ce21f97271f29b007ae64b6744d4810dfa32fd
Evgeniya <e.polozova@corp.vk.com>
2022-03-10 18:08:57 +0300

    getNavId: use proper type for warn

--- src/lib/getNavId.ts
+++ src/lib/getNavId.ts
@@ -1,10 +1,12 @@
+import { WarnOnceHandler } from "./warnOnce";
+
 export interface NavIdProps {
   /** Уникальный идентификатор навигационного элемента (вместо id) */
   nav?: string;
   id?: string;
 }
 
-export function getNavId(props: NavIdProps, warn?: (text: string) => any) {
+export function getNavId(props: NavIdProps, warn?: WarnOnceHandler) {
   const id = props.nav || props.id;
   if (process.env.NODE_ENV === "development" && !id && warn) {
     warn('Navigation item should have "nav" or "id" prop');
