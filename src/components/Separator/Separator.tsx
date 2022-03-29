43cf6d2c46fca21d67abbeca427a31a05354ad92
Daniil Suvorov <severecloud@gmail.com>
2022-03-29 19:14:59 +0000

    Separator: deprecate component

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { getClassName } from "../../helpers/getClassName";
 import { classNames } from "../../lib/classNames";
 import { usePlatform } from "../../hooks/usePlatform";
+import { warnOnce } from "../../lib/warnOnce";
 import "./Separator.css";
 
 export interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -12,31 +13,42 @@ export interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
   expanded?: boolean;
 }
 
-const Separator: React.FC<SeparatorProps> = ({
-  wide,
-  expanded,
-  ...restProps
-}) => {
-  const platform = usePlatform();
+const warn = warnOnce("Separator");
 
-  return (
-    <div
-      {...restProps}
-      aria-hidden="true"
-      // eslint-disable-next-line vkui/no-object-expression-in-arguments
-      vkuiClass={classNames(getClassName("Separator", platform), {
-        "Separator--wide": wide,
-      })}
-    >
+/**
+ * @deprecated Этот компонент устарел и будет удален в 5.0.0. Используйте [`Spacing`](#/Spacing).
+ */
+const Separator: React.FC<SeparatorProps> = React.memo(
+  ({ wide, expanded, ...restProps }) => {
+    if (process.env.NODE_ENV === "development") {
+      warn(
+        "Этот компонент устарел и будет удален в 5.0.0. Используйте Spacing."
+      );
+    }
+
+    const platform = usePlatform();
+
+    return (
       <div
+        {...restProps}
+        aria-hidden="true"
         // eslint-disable-next-line vkui/no-object-expression-in-arguments
-        vkuiClass={classNames("Separator__in", {
-          "Separator__in--expanded": expanded,
+        vkuiClass={classNames(getClassName("Separator", platform), {
+          "Separator--wide": wide,
         })}
-      />
-    </div>
-  );
-};
+      >
+        <div
+          // eslint-disable-next-line vkui/no-object-expression-in-arguments
+          vkuiClass={classNames("Separator__in", {
+            "Separator__in--expanded": expanded,
+          })}
+        />
+      </div>
+    );
+  }
+);
+
+Separator.displayName = "Separator";
 
 // eslint-disable-next-line import/no-default-export
-export default React.memo(Separator);
+export default Separator;
