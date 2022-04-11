cf107a9c9f10e133f21c7dfc2ac2f0fdabe87c76
Daniil Suvorov <severecloud@gmail.com>
2022-04-11 20:22:16 +0300

    BREAKING CHANGE: TabbarItem remove label

--- src/components/TabbarItem/TabbarItem.tsx
+++ src/components/TabbarItem/TabbarItem.tsx
@@ -1,17 +1,15 @@
 import * as React from "react";
 import { getClassName } from "../../helpers/getClassName";
-import Counter from "../Counter/Counter";
 import { classNames } from "../../lib/classNames";
 import { usePlatform } from "../../hooks/usePlatform";
 import { hasReactNode } from "../../lib/utils";
 import Tappable from "../Tappable/Tappable";
 import { Platform } from "../../lib/platform";
 import { HasComponent, HasRootRef } from "../../types";
-import { warnOnce } from "../../lib/warnOnce";
 import "./TabbarItem.css";
 
 export interface TabbarItemProps
-  extends Omit<React.AllHTMLAttributes<HTMLElement>, "label">, // TODO убрать Omit после удаления свойства label
+  extends React.AllHTMLAttributes<HTMLElement>,
     HasRootRef<HTMLElement>,
     HasComponent {
   selected?: boolean;
@@ -23,17 +21,11 @@ export interface TabbarItemProps
    * Индикатор над иконкой. Принимает `<Badge mode="prominent" />` или `<Counter size="s" mode="prominent" />`
    */
   indicator?: React.ReactNode;
-  /**
-   * @deprecated будет удалено в 5.0.0. Используйте `indicator`
-   */
-  label?: React.ReactNode;
 }
 
-const warn = warnOnce("TabbarItem");
 const TabbarItem: React.FunctionComponent<TabbarItemProps> = ({
   children,
   selected,
-  label,
   indicator,
   text,
   href,
@@ -42,12 +34,6 @@ const TabbarItem: React.FunctionComponent<TabbarItemProps> = ({
   ...restProps
 }: TabbarItemProps) => {
   const platform = usePlatform();
-
-  if (label && process.env.NODE_ENV === "development") {
-    warn(
-      "Свойство label устарело и будет удалено в 5.0.0. Используйте indicator."
-    );
-  }
 
   return (
     <Component
@@ -78,11 +64,6 @@ const TabbarItem: React.FunctionComponent<TabbarItemProps> = ({
           {children}
           <div vkuiClass="TabbarItem__label">
             {hasReactNode(indicator) && indicator}
-            {!indicator && label && (
-              <Counter size="s" mode="prominent">
-                {label}
-              </Counter>
-            )}
           </div>
         </div>
         {text && <div vkuiClass="TabbarItem__text">{text}</div>}
