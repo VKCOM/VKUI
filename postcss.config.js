087d835dde4883b76a36848bbc47cf9950d401f8
Daniil Suvorov <severecloud@gmail.com>
2022-04-17 13:57:03 +0300

    fix: add onlyVariablesLocal.css for vkBase

--- postcss.config.js
+++ postcss.config.js
@@ -44,6 +44,7 @@ let plugins = [
       "./src/styles/vkcom_light.css",
       "./src/styles/vkcom_dark.css",
       "./node_modules/@vkontakte/vkui-tokens/themes/vkBase/cssVars/declarations/onlyVariables.css",
+      "./node_modules/@vkontakte/vkui-tokens/themes/vkBase/cssVars/declarations/onlyVariablesLocal.css",
       "./node_modules/@vkontakte/vkui-tokens/themes/vkBaseDark/cssVars/declarations/onlyVariablesLocal.css",
       "./node_modules/@vkontakte/vkui-tokens/themes/vkIOS/cssVars/declarations/onlyVariablesLocal.css",
       "./node_modules/@vkontakte/vkui-tokens/themes/vkIOSDark/cssVars/declarations/onlyVariablesLocal.css",
