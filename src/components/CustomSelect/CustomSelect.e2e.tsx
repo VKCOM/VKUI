272371bb26d38d12404564370a0706e1836329ce
Evgeniya <e.polozova@corp.vk.com>
2022-05-17 17:07:37 +0300

    e2e(CustomSelect): update e2e test

--- src/components/CustomSelect/CustomSelect.e2e.tsx
+++ src/components/CustomSelect/CustomSelect.e2e.tsx
@@ -1,11 +1,13 @@
-import { CustomSelect, CustomSelectProps } from "../CustomSelect/CustomSelect";
 import { describeScreenshotFuzz } from "../../testing/e2e/utils";
+import { CustomSelect, CustomSelectProps } from "../CustomSelect/CustomSelect";
+import { SelectType } from "../Select/Select";
 
 describe("CustomSelect", () => {
   describeScreenshotFuzz(
     (props: CustomSelectProps) => (
       <CustomSelect
         placeholder="Не выбрана"
+        {...props}
         options={[
           { value: 1, label: "Россия" },
           { value: 2, label: "Украина" },
@@ -14,7 +16,6 @@ describe("CustomSelect", () => {
             label: "Соединенное королевство Великобритании и Северной Ирландии",
           },
         ]}
-        {...props}
       />
     ),
     [
@@ -27,6 +28,7 @@ describe("CustomSelect", () => {
         value: [3],
       },
       {
+        selectType: [SelectType.plain, SelectType.accent],
         $adaptivity: "y",
       },
     ]
