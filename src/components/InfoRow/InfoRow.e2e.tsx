8d5645e471c1c832732ccb9ae561da5a44b1703e
Daniil Suvorov <severecloud@gmail.com>
2022-06-01 17:51:50 +0300

    feat(InfoRow): tokenized

---
+++ src/components/InfoRow/InfoRow.e2e.tsx
@@ -0,0 +1,14 @@
+import { InfoRow, InfoRowProps } from "./InfoRow";
+import { describeScreenshotFuzz } from "../../testing/e2e/utils";
+
+describe("InfoRow", () => {
+  describeScreenshotFuzz(
+    (props: InfoRowProps) => <InfoRow {...props} />,
+    [
+      {
+        children: ["Команда ВКонтакте", undefined],
+        header: ["Место работы", undefined],
+      },
+    ]
+  );
+});
