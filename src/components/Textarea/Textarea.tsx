229fd6456dd080f31b6cca0a35b7ffd9751e6bd2
i.mirdzhamolov <i.mirdzhamolov@vk.team>
2022-06-07 15:16:20 +0300

    feat(Textarea): remove `React.memo`

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -21,64 +21,62 @@ export interface TextareaProps
 /**
  * @see https://vkcom.github.io/VKUI/#/Textarea
  */
-const TextareaComponent: React.FC<TextareaProps> = React.memo(
-  ({
-    defaultValue = "",
-    grow = true,
-    style,
-    onResize,
-    className,
-    getRootRef,
-    getRef,
-    sizeY,
-    rows = 2,
-    maxHeight,
-    ...restProps
-  }: TextareaProps) => {
-    const [value, onChange] = useEnsuredControl(restProps, { defaultValue });
-    const currentScrollHeight = React.useRef<number>();
-    const elementRef = useExternRef(getRef);
+const TextareaComponent: React.FC<TextareaProps> = ({
+  defaultValue = "",
+  grow = true,
+  style,
+  onResize,
+  className,
+  getRootRef,
+  getRef,
+  sizeY,
+  rows = 2,
+  maxHeight,
+  ...restProps
+}: TextareaProps) => {
+  const [value, onChange] = useEnsuredControl(restProps, { defaultValue });
+  const currentScrollHeight = React.useRef<number>();
+  const elementRef = useExternRef(getRef);
 
-    // autosize input
-    React.useEffect(() => {
-      const el = elementRef.current;
+  // autosize input
+  React.useEffect(() => {
+    const el = elementRef.current;
 
-      if (grow && el?.offsetParent) {
-        el.style.height = "";
-        el.style.height = `${el.scrollHeight}px`;
+    if (grow && el?.offsetParent) {
+      el.style.height = "";
+      el.style.height = `${el.scrollHeight}px`;
 
-        if (el.scrollHeight !== currentScrollHeight.current && onResize) {
-          onResize(el);
-          currentScrollHeight.current = el.scrollHeight;
-        }
+      if (el.scrollHeight !== currentScrollHeight.current && onResize) {
+        onResize(el);
+        currentScrollHeight.current = el.scrollHeight;
       }
-    }, [grow, value, sizeY, elementRef, onResize]);
+    }
+  }, [grow, value, sizeY, elementRef, onResize]);
 
-    return (
-      <FormField
-        vkuiClass={classNames(
-          "Textarea",
-          // TODO. v5.0.0 Новая адаптивность
-          `Textarea--sizeY-${sizeY}`
-        )}
-        className={className}
-        style={style}
-        getRootRef={getRootRef}
-        disabled={restProps.disabled}
-      >
-        <textarea
-          {...restProps}
-          style={{ maxHeight }}
-          rows={rows}
-          vkuiClass="Textarea__el"
-          value={value}
-          onChange={onChange}
-          ref={elementRef}
-        />
-      </FormField>
-    );
-  }
-);
+  return (
+    <FormField
+      vkuiClass={classNames(
+        "Textarea",
+        // TODO. v5.0.0 Новая адаптивность
+        `Textarea--sizeY-${sizeY}`
+      )}
+      className={className}
+      style={style}
+      getRootRef={getRootRef}
+      disabled={restProps.disabled}
+    >
+      <textarea
+        {...restProps}
+        style={{ maxHeight }}
+        rows={rows}
+        vkuiClass="Textarea__el"
+        value={value}
+        onChange={onChange}
+        ref={elementRef}
+      />
+    </FormField>
+  );
+};
 
 // eslint-disable-next-line import/no-default-export
 export const Textarea = withAdaptivity(TextareaComponent, { sizeY: true });
