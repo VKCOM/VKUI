4ee8eb3964222dc1c108e2dd1c7a4c8e45251dcc
Владислав <vladgrom2206.com@gmail.com>
2022-07-11 22:07:24 +0000

    Update ReactComponent
    - Replace pictures with vector icons
    - Change color of unstable icon
    - Slightly fix CSS to align vector icons to the text

--- styleguide/Components/ReactComponent/ReactComponent.js
+++ styleguide/Components/ReactComponent/ReactComponent.js
@@ -10,6 +10,7 @@ import { deprecated } from "../../deprecated";
 import { tokenized } from "../../tokenized";
 import { unstable } from "../../unstable";
 import { TextTooltip } from "@vkui/components/TextTooltip/TextTooltip";
+import { Icon28WarningTriangleOutline } from "@vkontakte/icons";
 import pkg from "../../../package.json";
 import "./ReactComponent.css";
 
@@ -56,12 +57,24 @@ const ReactComponent = ({ component, exampleMode }) => {
               </React.Fragment>
             }
           >
-            <img
+            <svg
               className="ReactComponent__tokenized"
-              width={20}
-              alt="Компонент поддерживает vkui-tokens"
-              src={require("../../assets/static/tokenized.png")}
-            />
+              width={24}
+              viewBox="0 0 28 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              id="tokenized_outline_28"
+            >
+              <path
+                d="M8 4H7C6.20435 4 5.44129 4.31607 4.87868 4.87868C4.31607 5.44129 4 6.20435 4 7V8M24 8V7C24 6.20435 23.6839 5.44129 23.1213 4.87868C22.5587 4.31607 21.7956 4 21 4H20M20 24H21C21.7956 24 22.5587 23.6839 23.1213 23.1213C23.6839 22.5587 24 21.7956 24 21V20M4 20V21C4 21.7956 4.31607 22.5587 4.87868 23.1213C5.44129 23.6839 6.20435 24 7 24H8M18 10H10M14 18V10"
+                stroke="var(--vkui--color_icon_accent)"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Компонент поддерживает vkui-tokens</title>
+              </path>
+            </svg>
           </TextTooltip>
         )}
         {unstable.includes(visibleName) && (
@@ -73,11 +86,11 @@ const ReactComponent = ({ component, exampleMode }) => {
               </React.Fragment>
             }
           >
-            <img
+            <Icon28WarningTriangleOutline
               className="ReactComponent__unstable"
-              width={20}
-              alt="Компонент является нестабильным"
-              src={require("../../assets/static/unstable.png")}
+              width={24}
+              fill="var(--vkui--color_accent_orange)"
+              title="Компонент является нестабильным"
             />
           </TextTooltip>
         )}
