0cfca9be0947e79afbaa7e97193de84816d738d4
Владислав <vladgrom2206.com@gmail.com>
2022-07-14 15:40:45 +0000

    Update Icon28TokenizedOutline
    Attempting to replicate the structure of VKUI icons

--- styleguide/Components/Icon28TokenizedOutline/Icon28TokenizedOutline.js
+++ styleguide/Components/Icon28TokenizedOutline/Icon28TokenizedOutline.js
@@ -1,24 +1,40 @@
 import React from "react";
 
-export const Icon28TokenizedOutline = (props) => {
+export const Icon28TokenizedOutline = ({
+  className,
+  width = 28,
+  height = 28,
+  stroke,
+  title,
+}) => {
   return (
-    <div role="presentational" className={"vkuiIcon " + props.className}>
+    <div
+      role="presentation"
+      className={`vkuiIcon vkuiIcon--28 vkuiIcon--w-${width} vkuiIcon--h-${height} ${
+        className ? className : ""
+      }`}
+      style={{ width: width + "px", height: height + "px" }}
+      title={title}
+    >
       <svg
         viewBox="0 0 28 28"
-        width={props.width}
-        height={props.height}
+        width={width}
+        height={height}
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         id="tokenized_outline_28"
       >
         <path
           d="M8 4H7a3 3 0 0 0-3 3v1m20 0V7a3 3 0 0 0-3-3h-1m0 20h1a3 3 0 0 0 3-3v-1M4 20v1a3 3 0 0 0 3 3h1M18 10h-8m4 8v-8"
-          stroke={props.stroke}
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          stroke={stroke}
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
-          <title>{props.title}</title>
+          <use
+            xlinkHref="#tokenized_outline_28"
+            style={{ fill: "none", color: stroke }}
+          ></use>
         </path>
       </svg>
     </div>
