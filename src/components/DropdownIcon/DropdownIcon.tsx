d4f9a37b142d96a69c51684e0e43067321504081
Evgeniya <e.polozova@corp.vk.com>
2022-07-21 17:37:36 +0300

    feat(DropdownIcon): remove platform

--- src/components/DropdownIcon/DropdownIcon.tsx
+++ src/components/DropdownIcon/DropdownIcon.tsx
@@ -2,9 +2,7 @@ import * as React from "react";
 import { Icon20Dropdown, Icon24ChevronDown } from "@vkontakte/icons";
 import { classNames } from "../../lib/classNames";
 import { SizeType } from "../AdaptivityProvider/AdaptivityContext";
-import { getClassName } from "../../helpers/getClassName";
 import { useAdaptivity } from "../../hooks/useAdaptivity";
-import { usePlatform } from "../../hooks/usePlatform";
 import "./DropdownIcon.css";
 
 export interface DropdownIconProps
@@ -19,17 +17,13 @@ export const DropdownIcon = ({
   flipped = false,
   ...restProps
 }: DropdownIconProps) => {
-  const platform = usePlatform();
   const { sizeY } = useAdaptivity();
 
   const Icon = sizeY === SizeType.COMPACT ? Icon20Dropdown : Icon24ChevronDown;
 
   return (
     <Icon
-      vkuiClass={classNames(
-        getClassName("DropdownIcon", platform),
-        flipped && "DropdownIcon--flipped"
-      )}
+      vkuiClass={classNames("DropdownIcon", flipped && "DropdownIcon--flipped")}
       {...restProps}
     />
   );
