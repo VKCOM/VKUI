c8430e236e5afc7d39959a7ee35631b706713f2a
Stepan Kurennykh <stoope@outlook.com>
2022-09-08 15:59:33 +0300

    fix FixedLayout width calculation

--- src/components/FixedLayout/FixedLayout.tsx
+++ src/components/FixedLayout/FixedLayout.tsx
@@ -44,8 +44,21 @@ export const FixedLayout = ({
   const [width, setWidth] = React.useState<string | undefined>(undefined);
   const { window } = useDOM();
   const { colRef } = React.useContext(SplitColContext);
-  const doResize = () =>
-    setWidth(colRef?.current ? `${colRef.current.offsetWidth}px` : undefined);
+  const doResize = () => {
+    if (colRef?.current) {
+      const computedStyle = getComputedStyle(colRef.current);
+
+      setWidth(
+        `${
+          colRef.current.clientWidth -
+          parseFloat(computedStyle.paddingLeft) -
+          parseFloat(computedStyle.paddingRight)
+        }px`
+      );
+    } else {
+      setWidth(undefined);
+    }
+  };
   React.useEffect(doResize, [colRef]);
   useGlobalEventListener(window, "resize", doResize);
 
