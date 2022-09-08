eb64cd86c9c9a199b0fda8439c51bc0c6cc1f602
Stepan Kurennykh <stoope@outlook.com>
2022-09-08 18:31:32 +0300

    fix PanelHeaderContext onClose firing

--- src/components/PanelHeaderContext/PanelHeaderContext.tsx
+++ src/components/PanelHeaderContext/PanelHeaderContext.tsx
@@ -51,9 +51,11 @@ export const PanelHeaderContext = ({
           elementRef.current &&
           !elementRef.current.contains(event.target as Node)
         ) {
+          event.stopPropagation();
           onClose();
         }
-      })
+      }),
+    { capture: true }
   );
 
   // fallback onAnimationEnd when animationend not supported
