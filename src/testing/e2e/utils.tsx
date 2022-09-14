8bbec2044f2ec363257a5e148123ce8a502b4488
Stepan Kurennykh <stoope@outlook.com>
2022-09-14 19:59:32 +0300

    disable dark theme

--- src/testing/e2e/utils.tsx
+++ src/testing/e2e/utils.tsx
@@ -146,7 +146,7 @@ export function describeScreenshotFuzz<Props>(
   const {
     matchScreenshot,
     platforms = Object.values(Platform),
-    appearances = [Appearance.DARK, Appearance.LIGHT],
+    appearances = [Appearance.LIGHT],
     adaptivity = {},
     Wrapper = AppWrapper,
   } = options;
