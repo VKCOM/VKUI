27cce3dd50f1660ae0ae3c3e3255d51b795f75d6
Stepan Kurennykh <stoope@outlook.com>
2022-09-21 00:26:07 +0500

    fix actions

--- .github/gitflow/push_with_retry.js
+++ .github/gitflow/push_with_retry.js
@@ -3,12 +3,14 @@ const { execSync } = require("child_process");
 // dangerfile depends on this message
 execSync('git config --global user.name "GitHub Action"');
 
+execSync("git pull --rebase --autostash");
+execSync("git add ./**/*.png");
+execSync(`git commit -m "CHORE: Update screenshots"`);
+
 let count = 6;
 
 while (true) {
   try {
-    execSync("git add ./**/*.png");
-    execSync(`git commit -m "CHORE: Update screenshots"`);
     execSync("git push --verbose");
 
     break;
