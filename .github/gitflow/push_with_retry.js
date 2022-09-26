69217322c9804e92dceeda7d2370c69ce4fb0a29
Stepan Kurennykh <stoope@outlook.com>
2022-09-26 15:23:44 +0300

    fix push script

--- .github/gitflow/push_with_retry.js
+++ .github/gitflow/push_with_retry.js
@@ -3,26 +3,45 @@ const { execSync } = require("child_process");
 // dangerfile depends on this message
 execSync('git config --global user.name "GitHub Action"');
 
-execSync("git pull");
-execSync("git add ./**/*.png");
-execSync(`git commit -m "CHORE: Update screenshots"`);
+let count = 10;
 
-let count = 6;
+function retry(cb, onError) {
+  while (true) {
+    try {
+      cb();
 
-while (true) {
-  try {
-    execSync("git push --verbose");
+      break;
+    } catch (e) {
+      console.error(e);
 
-    break;
-  } catch (e) {
-    console.error(e);
+      count -= 1;
 
-    count -= 1;
+      if (count < 0) {
+        throw e;
+      }
 
-    if (count < 0) {
-      throw e;
+      onError();
     }
-
-    execSync("git pull --rebase");
   }
 }
+
+execSync("git pull");
+
+retry(
+  function () {
+    execSync("git add ./**/*.png");
+    execSync(`git commit -m "CHORE: Update screenshots"`);
+  },
+  function () {
+    execSync("git pull --rebase --autostash");
+  }
+);
+
+retry(
+  function () {
+    execSync("git push --verbose");
+  },
+  function () {
+    execSync("git pull --rebase");
+  }
+);
