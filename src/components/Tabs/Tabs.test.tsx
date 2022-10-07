3357104b2c49a346725dc39c691093c873e1f878
i.mirdzhamolov <i.mirdzhamolov@vk.team>
2022-10-07 23:35:36 +0300

    fix(test): add reacts import to `Tabs` test

--- src/components/Tabs/Tabs.test.tsx
+++ src/components/Tabs/Tabs.test.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
 import { TabsItem } from "../TabsItem/TabsItem";
 import { baselineComponent } from "../../testing/utils";
