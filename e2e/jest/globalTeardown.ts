8c54bf757bb562d7ca5c904d28f740894eda633a
Daniil Suvorov <severecloud@gmail.com>
2022-12-13 22:40:18 +0300

    test(e2e): remove listen and close (#3716)
    - `listen` method is deprecated in favor
    async `start` or `startCallback` methods
    - `close` method is deprecated in favor
    async `stop` or `stopCallback` methods

--- e2e/jest/globalTeardown.ts
+++ e2e/jest/globalTeardown.ts
@@ -7,7 +7,7 @@ module.exports = async function teardown(config: JestConfig.GlobalConfig) {
   await baseTeardown(config);
   // I don't know why, but devServer closes in watch mode, too
   if (!config.watch && !config.watchAll) {
-    (global as any)['__DEV_SERVER__'].close();
+    await (global as any)['__DEV_SERVER__'].stop();
     if (useDocker) {
       await stopDocker();
     }
