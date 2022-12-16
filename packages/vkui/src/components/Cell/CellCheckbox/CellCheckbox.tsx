37b2a4acb190a54d5927724285b6fef32a72bd41
Evgeniya <e.polozova@corp.vk.com>
2022-12-16 20:37:58 +0300

    fix(CellCheckbox): use VisuallyHiddenInput

--- packages/vkui/src/components/Cell/CellCheckbox/CellCheckbox.tsx
+++ packages/vkui/src/components/Cell/CellCheckbox/CellCheckbox.tsx
@@ -10,6 +10,7 @@ import { usePlatform } from '../../../hooks/usePlatform';
 import { classNames } from '@vkontakte/vkjs';
 import { Platform } from '../../../lib/platform';
 import { CellProps } from '../Cell';
+import { VisuallyHiddenInput } from '../../VisuallyHiddenInput/VisuallyHiddenInput';
 import styles from './CellCheckbox.module.css';
 
 export type CellCheckboxProps = Pick<CellProps, 'defaultChecked' | 'checked'> &
@@ -29,7 +30,7 @@ export const CellCheckbox = ({ className, style, ...restProps }: CellCheckboxPro
       : Icon24CheckBoxOn;
 
   return (
-    <div
+    <span
       className={classNames(
         styles['CellCheckbox'],
         getPlatformClassName(styles['CellCheckbox'], platform),
@@ -37,13 +38,23 @@ export const CellCheckbox = ({ className, style, ...restProps }: CellCheckboxPro
       )}
       style={style}
     >
-      <input className={styles['CellCheckbox__input']} type="checkbox" {...restProps} />
-      <span className={classNames(styles['CellCheckbox__icon'], styles['CellCheckbox__icon--off'])}>
+      <VisuallyHiddenInput
+        className={styles['CellCheckbox__input']}
+        type="checkbox"
+        {...restProps}
+      />
+      <span
+        className={classNames(styles['CellCheckbox__icon'], styles['CellCheckbox__icon--off'])}
+        aria-hidden
+      >
         <IconOff />
       </span>
-      <span className={classNames(styles['CellCheckbox__icon'], styles['CellCheckbox__icon--on'])}>
+      <span
+        className={classNames(styles['CellCheckbox__icon'], styles['CellCheckbox__icon--on'])}
+        aria-hidden
+      >
         <IconOn />
       </span>
-    </div>
+    </span>
   );
 };
