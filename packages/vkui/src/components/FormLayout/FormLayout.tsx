b82ce4dbd02ac847a5b2601d6c96fa912607bf0e
Evgeniya <e.polozova@corp.vk.com>
2023-01-16 17:14:24 +0300

    fix(FormLayout): use VisuallyHiddenInput

--- packages/vkui/src/components/FormLayout/FormLayout.tsx
+++ packages/vkui/src/components/FormLayout/FormLayout.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { classNames } from '@vkontakte/vkjs';
+import { VisuallyHiddenInput } from '../VisuallyHiddenInput/VisuallyHiddenInput';
 import { HasComponent, HasRef } from '../../types';
 import styles from './FormLayout.module.css';
 
@@ -28,8 +29,8 @@ export const FormLayout = ({
       ref={getRef}
     >
       <div className={styles['FormLayout__container']}>{children}</div>
-      {Component === 'form' && (
-        <input type="submit" className={styles['FormLayout__submit']} value="" />
+      {Component === 'form' && onSubmit !== preventDefault && (
+        <VisuallyHiddenInput type="submit" value="" />
       )}
     </Component>
   );
