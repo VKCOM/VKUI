04cdaea115677d8d387ace9d06e14188295940aa
Evgeniya <e.polozova@corp.vk.com>
2023-02-15 12:41:33 +0300

    build(dev-deps): add @jest-axe w/ types & jest-runner-groups

--- packages/vkui/jest.config.js
+++ packages/vkui/jest.config.js
@@ -7,4 +7,5 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/*/**/{!(*.e2e),}.{ts,tsx}'],
   coveragePathIgnorePatterns: ['\\.d\\.ts$', 'src/types', 'src/testing'],
+  runner: 'groups',
 };
