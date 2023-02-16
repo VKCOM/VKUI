477c7fe47878c1480cda20cfa134effea3618699
Evgeniya <e.polozova@corp.vk.com>
2023-02-16 16:18:33 +0300

    test(a11y): remove unused configuration

--- packages/vkui/src/testing/a11y.tsx
+++ packages/vkui/src/testing/a11y.tsx
@@ -6,24 +6,6 @@ export const axe = configureAxe({
   /**
    * @see https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md
    */
-  //   rules: {
-  //     'aria-required-parent': {
-  //       /*
-  //        * a11y: Certain ARIA roles must be contained by particular parents (aria-required-parent)
-  //        *       мы тестируем компоненты атомарно, поэтому это конкретное правило в нашем случае
-  //        *       не подходит
-  //        */
-  //       enabled: false,
-  //     },
-  //     'label': {
-  //       /*
-  //        * a11y: Form elements must have labels (label)
-  //        *       мы тестируем компоненты атомарно, поэтому это конкретное правило в нашем случае
-  //        *       не подходит: мы не можем задать дефолтные лейблы компонентам форм
-  //        */
-  //       enabled: false,
-  //     },
-  //   },
 });
 expect.extend(toHaveNoViolations);
 
