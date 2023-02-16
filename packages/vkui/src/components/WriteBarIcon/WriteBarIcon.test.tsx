7801b70d3f142f37bc6c31b57fa18791d6327724
Evgeniya <e.polozova@corp.vk.com>
2023-02-16 16:54:12 +0300

    test(WriteBarIcon): separate unit and a11y tests

--- packages/vkui/src/components/WriteBarIcon/WriteBarIcon.test.tsx
+++ packages/vkui/src/components/WriteBarIcon/WriteBarIcon.test.tsx
@@ -6,12 +6,7 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
-import { WriteBarIcon, WriteBarIconProps } from './WriteBarIcon';
-
-const WriteBarIconTest = (props: WriteBarIconProps) => (
-  <WriteBarIcon data-testid="button" {...props} />
-);
-const button = () => screen.getByTestId('button');
+import { WriteBarIcon } from './WriteBarIcon';
 
 describe('WriteBarIcon', () => {
   baselineComponent(WriteBarIcon);
@@ -19,15 +14,9 @@ describe('WriteBarIcon', () => {
   it('shows counter when count={0} is provided', () => {
     const count = 0;
 
-    render(<WriteBarIconTest mode="attach" count={0} />);
+    render(<WriteBarIcon mode="attach" count={0} />);
     const counter = screen.getByText(count.toString());
 
     expect(counter).toBeInTheDocument();
-  });
-
-  it('a11y: adds default aria-label for assigned mode', () => {
-    render(<WriteBarIconTest mode="send" />);
-
-    expect(button()).toHaveAttribute('aria-label', 'Отправить');
   });
 });
