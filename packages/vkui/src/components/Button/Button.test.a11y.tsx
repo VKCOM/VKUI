2224c0d8dd8c548151bcf28e18e7ffcefd3b42e8
Evgeniya <e.polozova@corp.vk.com>
2023-02-16 04:08:22 +0300

    test(a11y/Button): add axe test

---
+++ packages/vkui/src/components/Button/Button.test.a11y.tsx
@@ -0,0 +1,16 @@
+/**
+ * jest-runners-groups
+ * @group a11y
+ */
+
+import * as React from 'react';
+import { a11yBasicTest } from '../../testing/a11y';
+import { Button } from './Button';
+
+describe('Button', () => {
+  a11yBasicTest((props) => (
+    <Button mode="primary" {...props}>
+      Кнопка
+    </Button>
+  ));
+});
