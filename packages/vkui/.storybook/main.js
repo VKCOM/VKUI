ec91e4bdb017441bc5bc0c97d172b5a6bfaaab2d
Daniil Suvorov <severecloud@gmail.com>
2023-02-21 19:54:33 +0300

    feat: add storybook-addon-swc

--- packages/vkui/.storybook/main.js
+++ packages/vkui/.storybook/main.js
@@ -1,22 +1,20 @@
-import WebpackCommonConfig from '../../../webpack.common.config';
-import type { StorybookConfig } from '@storybook/core-common';
-import { Configuration } from 'webpack';
+const WebpackCommonConfig = require('../../../webpack.common.config');
 
 const cssRegExpString = /\.css$/.toString();
 
-function getCssRulesFromConfig(config: Configuration) {
-  return config.module!.rules?.filter((rule) =>
+function getCssRulesFromConfig(config) {
+  return config.module.rules?.filter((rule) =>
     rule === '...' ? false : rule.test?.toString() === cssRegExpString,
   );
 }
 
-function excludeCssRulesFromConfig(config: Configuration) {
-  return config.module!.rules?.filter((rule) =>
+function excludeCssRulesFromConfig(config) {
+  return config.module.rules?.filter((rule) =>
     rule === '...' ? false : rule.test?.toString() !== cssRegExpString,
   );
 }
 
-const config: StorybookConfig = {
+const config = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
@@ -24,8 +22,9 @@ const config: StorybookConfig = {
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
     '@project-tools/storybook-addon-cartesian',
-    './addons/appearance/register',
-    './addons/pointer/register',
+    './addons/appearance',
+    './addons/pointer',
+    'storybook-addon-swc',
   ],
   framework: '@storybook/react',
   core: {
@@ -45,7 +44,7 @@ const config: StorybookConfig = {
     const commonCssRules = getCssRulesFromConfig(WebpackCommonConfig) ?? [];
     const rulesWithoutCss = excludeCssRulesFromConfig(config) ?? [];
 
-    config.module!.rules = [...rulesWithoutCss, ...commonCssRules];
+    config.module.rules = [...rulesWithoutCss, ...commonCssRules];
 
     return config;
   },
