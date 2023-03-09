a53d57015958d2879107aacfb44eeef2b75f0742
Daniil Suvorov <severecloud@gmail.com>
2023-03-09 13:54:25 +0300

    feat(HorizontalScroll): add scrollOnAnyWheel (#4390)
    - closed #4367

--- packages/vkui/src/components/HorizontalScroll/HorizontalScroll.test.tsx
+++ packages/vkui/src/components/HorizontalScroll/HorizontalScroll.test.tsx
@@ -1,6 +1,28 @@
+import * as React from 'react';
+import { fireEvent, render } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { HorizontalScroll } from './HorizontalScroll';
 
 describe('HorizontalScroll', () => {
   baselineComponent(HorizontalScroll);
+
+  it('scrollOnAnyWheel', () => {
+    const ref = React.createRef<HTMLDivElement>();
+    render(
+      <HorizontalScroll getRef={ref} scrollOnAnyWheel>
+        <div />
+        <div />
+      </HorizontalScroll>,
+    );
+
+    const scrollBy: Element['scrollBy'] = jest.fn();
+    ref.current!.scrollBy = scrollBy;
+
+    fireEvent.wheel(ref.current!, {
+      deltaX: 10,
+      deltaY: 10,
+    });
+
+    expect(scrollBy).toBeCalledTimes(1);
+  });
 });
