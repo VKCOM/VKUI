bc488072d19de2755e1bf420cffe5ca466be3b08
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-03-15 19:05:51 +0300

    fix(PopperArrow): add vertical offset for remove white space

--- packages/vkui/src/components/PopperArrow/PopperArrow.tsx
+++ packages/vkui/src/components/PopperArrow/PopperArrow.tsx
@@ -46,6 +46,25 @@ export interface PopperArrowProps extends HasRootRef<HTMLDivElement> {
   arrowClassName?: string;
 }
 
+const DEFAULT_ARROW_WIDTH = 20;
+const DEFAULT_ARROW_HEIGHT = 8;
+
+/**
+ * Примечание 1.
+ *
+ * В компоненте, SVG элемент `<path />` представляет собой стрелку с платформой в виде прямоугольника в 1px. Платформа
+ * служит для исправления проблемы с белой полоской между контентом и стрелкой, которая зачастую проявляется при
+ * изменении масштаба страницы.
+ *
+ * Чтобы исправление сработало нужно:
+ * 1. Прибавить высоту платформы к `height` и `viewBox` SVG контейнера.
+ * 2. Сместить положение SVG контейнера на высоту платформы – сделано в CSS через `translateY(1px)`.
+ *
+ * https://github.com/VKCOM/VKUI/issues/2123
+ */
+const PLATFORM_HEIGHT = 1;
+const ARROW_HEIGHT_WITH_WHITE_SPACE = DEFAULT_ARROW_HEIGHT + PLATFORM_HEIGHT;
+
 export const PopperArrow = ({
   coords,
   arrowClassName,
@@ -61,18 +80,12 @@ export const PopperArrow = ({
     >
       <svg
         className={classNames(styles['PopperArrow__in'], arrowClassName)}
-        width="20"
-        height="8"
-        viewBox="0 0 20 8"
-        fill="none"
+        width={DEFAULT_ARROW_WIDTH}
+        height={ARROW_HEIGHT_WITH_WHITE_SPACE}
+        viewBox={`0 0 ${DEFAULT_ARROW_WIDTH} ${ARROW_HEIGHT_WITH_WHITE_SPACE}`}
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M10 0C13 0 15.9999 8 20 8H0C3.9749 8 7 0 10 0Z"
-          fill="currentColor"
-        />
+        <path d="M10 0c3 0 6 8 10 8v1H0V8c3.975 0 7-8 10-8Z" fill="currentColor" />
       </svg>
     </div>
   );
