e3e5b3d80f70435fa121595afd77d9c17a07374a
Daniil Suvorov <severecloud@gmail.com>
2023-03-27 12:36:16 +0300

    feat(Accordion): add animation Добавляем анимацию для Accordion

--- packages/vkui/src/components/Accordion/Accordion.tsx
+++ packages/vkui/src/components/Accordion/Accordion.tsx
@@ -1,8 +1,10 @@
-import React, { HTMLProps } from 'react';
+import React, { DetailsHTMLAttributes } from 'react';
+import { classNames } from '@vkontakte/vkjs';
 import { HasRef } from '../../types';
 import { AccordionSummary } from './AccordionSummary';
+import styles from './Accordion.module.css';
 
-export type AccordionProps = HTMLProps<HTMLDetailsElement> & HasRef<HTMLDetailsElement>;
+export type AccordionProps = DetailsHTMLAttributes<HTMLDetailsElement> & HasRef<HTMLDetailsElement>;
 
 /**
  * Компонент, позволяет отображать несколько разделов контента в ограниченном
@@ -13,8 +15,29 @@ export type AccordionProps = HTMLProps<HTMLDetailsElement> & HasRef<HTMLDetailsE
  * @version 5.3.0
  * @see  https://vkcom.github.io/VKUI/#/Accordion
  */
-export const Accordion = ({ getRef, ...restProps }: AccordionProps) => (
-  <details ref={getRef} {...restProps} />
-);
+export const Accordion = ({ getRef, className, onToggle, ...restProps }: AccordionProps) => {
+  const [open, setOpen] = React.useState<boolean | undefined>(undefined);
+
+  const toggle: AccordionProps['onToggle'] = (e) => {
+    // @ts-expect-error: TS2339 Property 'open' does not exist on type 'EventTarget'
+    setOpen(e.target.open);
+
+    onToggle && onToggle(e);
+  };
+
+  return (
+    <details
+      ref={getRef}
+      className={classNames(
+        styles['Accordion'],
+        open && styles['Accordion--open'],
+        open === false && styles['Accordion--close'],
+        className,
+      )}
+      onToggle={toggle}
+      {...restProps}
+    />
+  );
+};
 
 Accordion.Summary = AccordionSummary;
