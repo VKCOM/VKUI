38ebe0d89e7aa118e0ab1b9cb82bc9a05c4c7437
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-04-19 16:52:19 +0300

    tech(playwright): reset mouse pos for every tests

--- packages/vkui/src/components/SegmentedControl/SegmentedControl.e2e.tsx
+++ packages/vkui/src/components/SegmentedControl/SegmentedControl.e2e.tsx
@@ -4,12 +4,9 @@ import { SegmentedControlPlayground } from './SegmentedControl.e2e-playground';
 
 test('SegmentedControl', async ({
   mount,
-  page,
   expectScreenshotClippedToContent,
   componentPlaygroundProps,
 }) => {
   await mount(<SegmentedControlPlayground {...componentPlaygroundProps} />);
-  // см. https://github.com/VKCOM/VKUI/pull/4652 ("Подводные камни" -> "Firefox")
-  await page.hover('body', { position: { x: 0, y: 0 } });
   await expectScreenshotClippedToContent();
 });
