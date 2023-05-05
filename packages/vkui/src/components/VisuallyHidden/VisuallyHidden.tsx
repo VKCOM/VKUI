5bf0ad48f19ad1ef7fe690ffd6cb7615bd94565c
Evgeniya <e.polozova@corp.vk.com>
2023-05-05 18:42:20 +0300

    docs(VisuallyHidden): fix to show docs

--- packages/vkui/src/components/VisuallyHidden/VisuallyHidden.tsx
+++ packages/vkui/src/components/VisuallyHidden/VisuallyHidden.tsx
@@ -9,12 +9,11 @@ interface VisuallyHiddenProps
     HasComponent {}
 
 /**
- * @since v5.4.0
- * @see https://vkcom.github.io/VKUI/#/VisuallyHidden
- * @description
- *
  * Компонент-обертка. Позволяет скрыть контент визуально, но оставить его
  * доступным для ассистивных технологий. По умолчанию — `span`.
+ *
+ * @since v5.4.0
+ * @see https://vkcom.github.io/VKUI/#/VisuallyHidden
  */
 export const VisuallyHidden = ({
   Component = 'span',
