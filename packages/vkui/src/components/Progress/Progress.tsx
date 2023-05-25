2aba239e75388029287d2005c3ecf81bdb48d65e
Daniil Suvorov <severecloud@gmail.com>
2023-05-25 19:09:46 +0300

    feat(Progress): add `appearance` - closed #5067

--- packages/vkui/src/components/Progress/Progress.tsx
+++ packages/vkui/src/components/Progress/Progress.tsx
@@ -7,6 +7,10 @@ import styles from './Progress.module.css';
 export interface ProgressProps
   extends React.HTMLAttributes<HTMLDivElement>,
     HasRootRef<HTMLDivElement> {
+  /**
+   * Стиль отображения прогрессбара
+   */
+  appearance?: 'accent' | 'positive' | 'negative';
   value?: number;
 }
 
@@ -16,7 +20,13 @@ const PROGRESS_MAX_VALUE = 100;
 /**
  * @see https://vkcom.github.io/VKUI/#/Progress
  */
-export const Progress = ({ value = 0, getRootRef, className, ...restProps }: ProgressProps) => {
+export const Progress = ({
+  value = 0,
+  getRootRef,
+  className,
+  appearance = 'accent',
+  ...restProps
+}: ProgressProps) => {
   const progress = clamp(value, PROGRESS_MIN_VALUE, PROGRESS_MAX_VALUE);
   const title = `${progress} / ${PROGRESS_MAX_VALUE}`;
 
@@ -29,7 +39,15 @@ export const Progress = ({ value = 0, getRootRef, className, ...restProps }: Pro
       aria-valuemin={PROGRESS_MIN_VALUE}
       aria-valuemax={PROGRESS_MAX_VALUE}
       ref={getRootRef}
-      className={classNames(styles['Progress'], className)}
+      className={classNames(
+        styles['Progress'],
+        {
+          accent: styles['Progress--appearance-accent'],
+          positive: styles['Progress--appearance-positive'],
+          negative: styles['Progress--appearance-negative'],
+        }[appearance],
+        className,
+      )}
     >
       <div className={styles['Progress__in']} style={{ width: `${progress}%` }} />
     </div>
--- packages/vkui/src/components/Progress/Readme.md
+++ packages/vkui/src/components/Progress/Readme.md
@@ -6,6 +6,14 @@
       <FormItem id="progresslabel" top="Прогресс">
         <Progress aria-labelledby="progresslabel" value={40} />
       </FormItem>
+
+      <FormItem id="progresslabelNegative" top="Ошибка">
+        <Progress appearance="negative" aria-labelledby="progresslabelNegative" value={75} />
+      </FormItem>
+
+      <FormItem id="progresslabelPositive" top="Завершено">
+        <Progress appearance="negative" aria-labelledby="progresslabelPositive" value={100} />
+      </FormItem>
     </Group>
   </Panel>
 </View>
