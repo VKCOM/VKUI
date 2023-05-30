9c436a5e528e3a173bf9076da71fa69a647b9aff
Vadim Khizhnyakov <v.khizhnyakov@vk.team>
2023-05-30 14:27:21 +0300

    fix(useTodayDate): add visibilitychange eventlistener to prevent skipped state update after tab freeze

--- packages/vkui/src/hooks/useTodayDate.ts
+++ packages/vkui/src/hooks/useTodayDate.ts
@@ -1,5 +1,6 @@
 import React from 'react';
-import { getMillisecondsToTomorrow } from '../lib/date';
+import { getMillisecondsToTomorrow, isSameDay } from '../lib/date';
+import { useDOM } from '../lib/dom';
 
 /**
  * Опционально обновляемая дата сегодняшнего дня
@@ -11,23 +12,55 @@ import { getMillisecondsToTomorrow } from '../lib/date';
  * @param listenDayChangesForUpdate - флаг по которому определяется, будет ли создаваться подписка на смену календарного дня
  */
 export function useTodayDate(listenDayChangesForUpdate = false) {
+  const { document } = useDOM();
   const [todayDate, setTodayDate] = React.useState(() => new Date());
 
   React.useEffect(() => {
-    if (!listenDayChangesForUpdate) {
+    if (!listenDayChangesForUpdate || !document) {
       return;
     }
 
-    const timeToDayChange = getMillisecondsToTomorrow(todayDate);
+    let timeout: NodeJS.Timeout | null = null;
 
-    const timeout = setTimeout(() => {
-      setTodayDate(new Date());
-    }, timeToDayChange);
+    const recalcTimeout = () => {
+      if (document.visibilityState === 'visible') {
+        const now = new Date();
+
+        const timeToDayChange = getMillisecondsToTomorrow(now);
+
+        // Удаляем старый таймаут
+        if (timeout) {
+          clearTimeout(timeout);
+        }
+
+        // Создаем новый таймаут
+        timeout = setTimeout(() => {
+          setTodayDate(now);
+        }, timeToDayChange);
+
+        // Если todayDate не обновился в таймаут - обновить при заходе на вкладку
+        if (!isSameDay(todayDate, now)) {
+          setTodayDate(now);
+        }
+      }
+    };
+
+    if (!timeout) {
+      recalcTimeout();
+    }
+
+    // Создаем слушатель visibilitychange, чтобы предотвратить пропуск обновления стейта после заморозки вкладки
+    // Если человек ее долго не трогал или закрывал крышку ноута и тп
+    // https://developer.chrome.com/blog/page-lifecycle-api/
+    document.addEventListener('visibilitychange', recalcTimeout);
 
     return () => {
-      clearTimeout(timeout);
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+      document.removeEventListener('visibilitychange', recalcTimeout);
     };
-  }, [listenDayChangesForUpdate, todayDate]);
+  }, [document, listenDayChangesForUpdate, todayDate]);
 
   return todayDate;
 }
