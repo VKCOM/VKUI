117670344d87a176ff8bf4c306e3d2c24bab4a4f
Evgeniya <e.polozova@corp.vk.com>
2023-06-13 20:21:26 +0300

    test(WriteBarIcon): add additional a11y test

--- packages/vkui/src/components/WriteBarIcon/WriteBarIcon.test.tsx
+++ packages/vkui/src/components/WriteBarIcon/WriteBarIcon.test.tsx
@@ -6,6 +6,12 @@ import { WriteBarIcon } from './WriteBarIcon';
 describe('WriteBarIcon', () => {
   baselineComponent((props) => <WriteBarIcon aria-label="WriteBarIcon" {...props} />);
 
+  it('a11y: adds default aria-label for assigned mode', () => {
+    render(<WriteBarIcon data-testid="button" mode="send" />);
+
+    expect(screen.getByTestId('button')).toHaveAttribute('aria-label', 'Отправить');
+  });
+
   it('shows counter when count={0} is provided', () => {
     const count = 0;
 
