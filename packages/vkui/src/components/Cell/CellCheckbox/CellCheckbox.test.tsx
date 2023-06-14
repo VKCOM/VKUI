7cce597bda02dda671fd67da35eb139390c9c9ba
Evgeniya <e.polozova@corp.vk.com>
2023-06-14 16:12:04 +0300

    test(CellCheckbox): test baseline stuff properly

--- packages/vkui/src/components/Cell/CellCheckbox/CellCheckbox.test.tsx
+++ packages/vkui/src/components/Cell/CellCheckbox/CellCheckbox.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { baselineComponent } from '../../../testing/utils';
-import { Cell } from '../Cell';
+import { CellCheckbox } from './CellCheckbox';
 
 describe('CellCheckbox', () => {
   baselineComponent((props) => (
-    <Cell mode="selectable" {...props}>
-      CellCheckbox
-    </Cell>
+    <label>
+      CellCheckbox <CellCheckbox {...props} />
+    </label>
   ));
 });
