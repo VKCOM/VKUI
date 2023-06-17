dab7f3819bbbaa13d2b10527a4baf79b2f63e20b
scffs <khidey@inbox.ru>
2023-06-17 22:22:32 +0700

    fix(Math.ts): updated `decimatedClamp` implementation
    Signed-off-by: scffs <khidey@inbox.ru>

--- packages/vkui/src/helpers/math.ts
+++ packages/vkui/src/helpers/math.ts
@@ -14,12 +14,11 @@ function decimatedClamp(val: number, min: number, max: number, step?: number) {
   if (step == null || step <= 0) {
     return clamp(val, min, max);
   }
-  const prec = precision(step);
-  // Round value to nearest min + k1 * step
-  const decimatedOffset = precisionRound(Math.round((val - min) / step) * step, prec);
-  // Round range length _down_ to nearest min + k2 * step
-  const decimatedRange = precisionRound(Math.floor((max - min) / step) * step, prec);
-  return min + clamp(decimatedOffset, 0, decimatedRange);
+  // Round decimated offset using rounded and * step.
+  const decimatedOffset = Math.round((val - min) / step) * step;
+  // Calculate decimated value with clamped offset.
+  const decimatedValue = min + clamp(decimatedOffset, 0, max - min);
+  return precisionRound(decimatedValue, precision(step));
 }
 
 export function rescale(
