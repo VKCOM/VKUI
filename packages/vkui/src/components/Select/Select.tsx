eceb38e90d3a09c2f61c05f746a288134e75ea72
Daniil Suvorov <severecloud@gmail.com>
2023-06-19 17:34:25 +0300

    fix(Select): nativeProps Вынес свойства которые не используются в NativeSelect
    Не уверен что можно что-то сейчас сделать на уровне типизации,
    https://github.com/Microsoft/TypeScript/issues/12936
    
    - Fixes #4378

--- packages/vkui/src/components/Select/Select.tsx
+++ packages/vkui/src/components/Select/Select.tsx
@@ -8,31 +8,38 @@ export type SelectType = 'default' | 'plain' | 'accent';
 /**
  * @see https://vkcom.github.io/VKUI/#/Select
  */
-export const Select = ({
-  children,
-  options = [],
-  popupDirection,
-  renderOption,
-  allowClearButton,
-  ClearButton,
-  ...props
-}: SelectProps) => {
+export const Select = ({ children, ...props }: SelectProps) => {
+  const {
+    options = [],
+    searchable,
+    emptyText,
+    onInputChange,
+    filterFn,
+    popupDirection,
+    renderOption,
+    renderDropdown,
+    fetching,
+    onClose,
+    onOpen,
+    icon,
+    ClearButton,
+    allowClearButton,
+    dropdownOffsetDistance,
+    fixDropdownWidth,
+    forceDropdownPortal,
+    selectType,
+    autoHideScrollbar,
+    autoHideScrollbarDelay,
+    ...nativeProps // TODO: https://github.com/Microsoft/TypeScript/issues/12936
+  } = props;
+
   const hasPointer = useAdaptivityHasPointer();
 
   return (
     <React.Fragment>
-      {(hasPointer === undefined || hasPointer) && (
-        <CustomSelect
-          options={options}
-          popupDirection={popupDirection}
-          renderOption={renderOption}
-          allowClearButton={allowClearButton}
-          ClearButton={ClearButton}
-          {...props}
-        />
-      )}
+      {(hasPointer === undefined || hasPointer) && <CustomSelect {...props} />}
       {(hasPointer === undefined || !hasPointer) && (
-        <NativeSelect {...props}>
+        <NativeSelect {...nativeProps}>
           {options.map(({ label, value }) => (
             <option value={value} key={`${value}`}>
               {label}
