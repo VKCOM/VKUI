ea4b3923727b92535f09ab49a5fff8578e4edf4e
Andrey Medvedev <andr.medv.spb@gmail.com>
2023-06-20 16:14:09 +0300

    Generate screenshots only for VKCOM

--- packages/vkui/src/components/PullToRefresh/PullToRefresh.e2e.tsx
+++ packages/vkui/src/components/PullToRefresh/PullToRefresh.e2e.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import { test } from '@vkui-e2e/test';
+import { Platform } from '../../lib/platform';
 import { PullToRefreshPlayground } from './PullToRefresh.e2e-playground';
 
-test('PullToRefresh: renders slider properly in default environment', async ({
-  page,
-  mount,
-  expectScreenshotClippedToContent,
-  componentPlaygroundProps,
-}) => {
-  await mount(<PullToRefreshPlayground {...componentPlaygroundProps} />);
-  await page.mouse.move(100, 100);
-  await page.mouse.down();
-  await page.mouse.move(100, 800, { steps: 8 });
+test.describe('PullToRefresh', () => {
+  // we are interested in VKCOM only as we need to test here mostly
+  // whether the spinner is positioned properly
+  test.use({ onlyForPlatforms: [Platform.VKCOM] });
 
-  await expectScreenshotClippedToContent();
-});
+  test('PullToRefresh: renders spinner properly in default environment', async ({
+    page,
+    mount,
+    expectScreenshotClippedToContent,
+    componentPlaygroundProps,
+  }) => {
+    await mount(<PullToRefreshPlayground {...componentPlaygroundProps} />);
+    await page.mouse.move(100, 100);
+    await page.mouse.down();
+    await page.mouse.move(100, 800, { steps: 8 });
 
-test('PullToRefresh: renders slider properly when parent has padding', async ({
-  page,
-  mount,
-  expectScreenshotClippedToContent,
-  componentPlaygroundProps,
-}) => {
-  await mount(<PullToRefreshPlayground {...componentPlaygroundProps} paddingLeft="150px" />);
-  await page.mouse.move(200, 100);
-  await page.mouse.down();
-  await page.mouse.move(200, 800, { steps: 8 });
+    await expectScreenshotClippedToContent();
+  });
 
-  await expectScreenshotClippedToContent();
+  test('PullToRefresh: renders spinner properly when parent has padding', async ({
+    page,
+    mount,
+    expectScreenshotClippedToContent,
+    componentPlaygroundProps,
+  }) => {
+    await mount(<PullToRefreshPlayground {...componentPlaygroundProps} paddingLeft="150px" />);
+    await page.mouse.move(200, 100);
+    await page.mouse.down();
+    await page.mouse.move(200, 800, { steps: 8 });
+
+    await expectScreenshotClippedToContent();
+  });
 });
