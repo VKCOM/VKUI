edec8f4c536b1eb4a89428a41361e6d6c5f097d2
Andrey Medvedev <andr.medv.spb@gmail.com>
2023-06-20 14:18:51 +0300

    Add e2e test for PullToRefresh
    To check that slider is positioned by center
    even though parent has padding

--- packages/vkui/src/components/PullToRefresh/PullToRefresh.e2e-playground.tsx
+++ packages/vkui/src/components/PullToRefresh/PullToRefresh.e2e-playground.tsx
@@ -1,70 +1,30 @@
 import * as React from 'react';
 import { ComponentPlayground, type ComponentPlaygroundProps } from '@vkui-e2e/playground-helpers';
-import { AdaptivityProvider } from '../AdaptivityProvider/AdaptivityProvider';
-import { AppRoot } from '../AppRoot/AppRoot';
-import { ConfigProvider } from '../ConfigProvider/ConfigProvider';
+import { Platform } from '../../lib/platform';
 import { Div } from '../Div/Div';
 import { PullToRefresh, type PullToRefreshProps } from './PullToRefresh';
 
-export const PullToRefreshPlayground = (props: ComponentPlaygroundProps) => {
+export const PullToRefreshPlayground = (
+  props: ComponentPlaygroundProps & { paddingLeft?: string },
+) => {
+  const { paddingLeft, ...playgroundProps } = props;
   return (
-    <ComponentPlayground
-      {...props}
-      propSets={[
-        {
-          isFetching: [false],
-        },
-      ]}
-    >
-      {({ appearance, ...props }: ComponentPlaygroundProps & PullToRefreshProps) => (
-        <ConfigProvider appearance={appearance}>
-          <AdaptivityProvider hasPointer>
-            <AppRoot>
-              <Div>
-                <PullToRefresh {...props}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a sollicitudin
-                  lectus, a commodo sapien. Vivamus a urna leo. Integer iaculis dignissim urna, sit
-                  amet vestibulum diam bibendum a. Donec eu arcu ut augue porttitor faucibus.
-                  Vestibulum nec pretium tortor, sit amet congue nunc. Aenean ullamcorper ex sem,
-                  sed interdum quam consequat et. Vestibulum a ex non diam fringilla feugiat. Nunc
-                  eu tellus sed leo elementum cursus. Mauris blandit porta egestas. Curabitur eget
-                  justo elementum, malesuada lacus ut, congue mauris. Integer orci nisi, convallis
-                  vitae dapibus sit amet, molestie a risus. Aenean ultricies lacus eros, sit amet
-                  condimentum urna malesuada et. Sed quis dolor tempus orci fringilla volutpat in
-                  sed velit. Aenean aliquet bibendum pretium.
-                  <br />
-                  <br />
-                  Cras pulvinar lobortis purus. Donec placerat suscipit leo vitae sodales. Phasellus
-                  convallis lorem vitae arcu finibus pellentesque. In imperdiet vel leo a euismod.
-                  Nam sed odio a neque venenatis suscipit a placerat magna. Mauris magna nisl,
-                  consequat nec augue vitae, ultricies scelerisque ante. Phasellus pharetra risus
-                  eget imperdiet sodales. Integer dignissim auctor semper. Nulla odio odio, euismod
-                  ut interdum in, bibendum sed massa. Proin rutrum molestie massa in ultrices. Donec
-                  eu euismod turpis, eget lobortis lorem. Nulla facilisi. Nam lacinia posuere
-                  turpis, sed laoreet turpis auctor nec.
-                  <br />
-                  <br />
-                </PullToRefresh>
-              </Div>
-            </AppRoot>
-          </AdaptivityProvider>
-        </ConfigProvider>
+    <ComponentPlayground {...playgroundProps} platform={Platform.VKCOM} propSets={[]}>
+      {({ ...props }: PullToRefreshProps) => (
+        <Div style={{ width: '300px', paddingLeft }}>
+          <PullToRefresh {...props}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a sollicitudin lectus, a
+            commodo sapien. Vivamus a urna leo. Integer iaculis dignissim urna, sit amet vestibulum
+            diam bibendum a. Donec eu arcu ut augue porttitor faucibus. Vestibulum nec pretium
+            tortor, sit amet congue nunc. Aenean ullamcorper ex sem, sed interdum quam consequat et.
+            Vestibulum a ex non diam fringilla feugiat. Nunc eu tellus sed leo elementum cursus.
+            Mauris blandit porta egestas. Curabitur eget justo elementum, malesuada lacus ut, congue
+            mauris. Integer orci nisi, convallis vitae dapibus sit amet, molestie a risus. Aenean
+            ultricies lacus eros, sit amet condimentum urna malesuada et. Sed quis dolor tempus orci
+            fringilla volutpat in sed velit. Aenean aliquet bibendum pretium.
+          </PullToRefresh>
+        </Div>
       )}
     </ComponentPlayground>
   );
 };
-
-// export const SliderPlaygroundForKeyboardTest = ({
-//   appearance,
-//   ...restProps
-// }: ComponentPlaygroundProps & (SliderProps | SliderMultipleProps)) => {
-//   return (
-//     <ConfigProvider appearance={appearance}>
-//       <AdaptivityProvider hasPointer>
-//         <AppRoot>
-//           <Slider style={{ minWidth: '320px' }} {...restProps} />
-//         </AppRoot>
-//       </AdaptivityProvider>
-//     </ConfigProvider>
-//   );
-// };
