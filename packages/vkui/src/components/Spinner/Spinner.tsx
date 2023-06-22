c237e4467fef276bb8ec412607b0ab7fba4ebaf1
Evgenia Polozova <e.polozova@corp.vk.com>
2023-06-22 17:25:27 +0300

    fix: Доступность спиннеров (#5262)
    * fix(ScreenSpinner): clean up classes & css
    
    * fix(Spinner): use visually hidden text
    
    * docs(styleguide): update a11y
    use aria for Platforms & Versions modals
    
    * docs(Spinner): update docs w/ a11y notes
    
    * docs(PanelSpinner): update docs w/ a11y notes
    
    * docs(ScreenSpinner): update docs w/ a11y notes
    
    * docs: update a11y headings

--- packages/vkui/src/components/Spinner/Spinner.tsx
+++ packages/vkui/src/components/Spinner/Spinner.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Icon16Spinner, Icon24Spinner, Icon32Spinner, Icon44Spinner } from '@vkontakte/icons';
 import { classNames } from '@vkontakte/vkjs';
+import { warnOnce } from '../../lib/warnOnce';
+import { VisuallyHidden } from '../VisuallyHidden/VisuallyHidden';
 import styles from './Spinner.module.css';
 
 export interface SpinnerProps extends React.HTMLAttributes<HTMLSpanElement> {
@@ -8,12 +10,15 @@ export interface SpinnerProps extends React.HTMLAttributes<HTMLSpanElement> {
   disableAnimation?: boolean;
 }
 
+const warn = warnOnce('Spinner');
 /**
  * @see https://vkcom.github.io/VKUI/#/Spinner
  */
 export const Spinner = React.memo(
   ({
     size = 'regular',
+    children = 'Загружается...',
+    // TODO [>=6]: Удалить автоматическое приведение aria-label
     'aria-label': ariaLabel = 'Загружается...',
     disableAnimation,
     className,
@@ -33,15 +38,21 @@ export const Spinner = React.memo(
       large: 22,
     }[size];
 
+    // TODO [>=6]: Удалить варнинг
+    if (process.env.NODE_ENV === 'development') {
+      if (ariaLabel && !children) {
+        warn(
+          'a11y: Пожалуйста, передавайте ваш текст для ассистивных технологий в children вместо aria-label.',
+        );
+      }
+    }
+
     return (
-      <span
-        role="status"
-        aria-label={ariaLabel}
-        {...restProps}
-        className={classNames(styles['Spinner'], className)}
-      >
+      <span role="status" {...restProps} className={classNames(styles['Spinner'], className)}>
         <SpinnerIcon>
           {!disableAnimation && (
+            // TODO [a11y]: use reduced motion hook?
+            //              https://github.com/VKCOM/VKUI/pull/4673
             <animateTransform
               attributeName="transform"
               attributeType="XML"
@@ -53,6 +64,7 @@ export const Spinner = React.memo(
             />
           )}
         </SpinnerIcon>
+        <VisuallyHidden>{children ?? ariaLabel}</VisuallyHidden>
       </span>
     );
   },
