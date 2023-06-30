9f3143c5d204bb49c4a0551e0a1f5ca39086bfd3
Daniil Suvorov <severecloud@gmail.com>
2023-06-30 13:25:26 +0300

    fix(Routing): update children type (#5395)
    - fixes #1413

--- packages/vkui/src/components/View/View.test.tsx
+++ packages/vkui/src/components/View/View.test.tsx
@@ -17,7 +17,7 @@ describe.each([
 ])('%s', (name, View) => {
   beforeAll(() => jest.useFakeTimers());
   afterAll(() => jest.useRealTimers());
-  baselineComponent(View, {
+  baselineComponent((props: ViewProps) => <View {...props} />, {
     // TODO [a11y]: "Exceeded timeout of 5000 ms for a test.
     //              Add a timeout value to this test to increase the timeout, if this is a long-running test. See https://jestjs.io/docs/api#testname-fn-timeout."
     a11y: false,
