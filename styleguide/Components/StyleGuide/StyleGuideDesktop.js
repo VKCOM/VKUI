aa805a0db3211aff824e4303dcfa0bc69ba711f9
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-07-03 11:54:57 +0300

    fix(docs): refactor styleguide layout (#5398)
    **Версия:** >= 5
    
    Шапка и сайдбар прыгают при открытии модалки с версиями.
    
    1. Перейти, например, на https://vkcom.github.io/VKUI/5.5.5/#/Button
    2. Немного проскролить страницу.
    3. Вызвать из шапки модалку со всеми версиями.
    
    1. Основная причина была в том, что в компонент `SplitCol` для сайдбар
      не передавалось св-во `fixed` – теперь передаётся.
    1. После указания `fixed`,  переписал стили под лэйаут документации
      styleguide, чтобы починить скролл. Получилось убрать перебивание VKUI
      стилей, которые нами не приветствуются.
    1. У `styleguide/Components/Heading` заменил `margin` на `padding`,
      чтобы исправить проблему со "схлопыванием `margin`".

--- styleguide/Components/StyleGuide/StyleGuideDesktop.js
+++ styleguide/Components/StyleGuide/StyleGuideDesktop.js
@@ -19,11 +19,26 @@ export const StyleGuideDesktop = ({
         popout={popout}
         modal={<StyleGuideModal activeModal={activeModal} />}
       >
-        <SplitCol minWidth={340} width="30%" maxWidth={480} className="StyleGuide__sidebar">
-          <div className="StyleGuide__sidebarIn">{toc}</div>
+        <SplitCol minWidth={340} width="30%" maxWidth={480} fixed>
+          <div className="StyleGuide__sidebar">
+            <div className="StyleGuide__sidebarIn">{toc}</div>
+          </div>
         </SplitCol>
-        <SplitCol width="100%" className="StyleGuide__content">
-          <div className="StyleGuide__contentIn">{children}</div>
+        <SplitCol
+          width="70%"
+          style={{
+            /**
+             * `min-width` в контексте Flexbox по умолчанию имеет значение `auto`,
+             * из-за чего элементы при переполнении будут выходить за границы контейнера.
+             *
+             * Подробности по ссылке https://stackoverflow.com/a/66689926/2903061
+             */
+            minWidth: 0,
+          }}
+        >
+          <div className="StyleGuide__content">
+            <div className="StyleGuide__contentIn">{children}</div>
+          </div>
         </SplitCol>
       </SplitLayout>
     </div>
