7ae14010b40a7e09ca8eb70ba6e7b23c65287323
Daniil Suvorov <severecloud@gmail.com>
2023-07-11 14:12:04 +0300

    feat(Progress): add height Добавляем свойство `height` для возможности задать свою высоту

--- packages/vkui/src/components/Progress/Progress.test.tsx
+++ packages/vkui/src/components/Progress/Progress.test.tsx
@@ -1,6 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { Progress } from './Progress';
 
 describe('Progress', () => {
   baselineComponent(Progress);
+
+  it('Custom height', () => {
+    render(<Progress data-testid="progress" height={10} />);
+
+    expect(screen.getByTestId('progress').style.height).toBe('10px');
+    expect(screen.getByTestId('progress').style.borderRadius).toBe('5px');
+  });
 });
