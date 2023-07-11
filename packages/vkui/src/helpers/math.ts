76829ce53dfc74d6bbea3159de34642aef2cb1cc
scffs <khidey@inbox.ru>
2023-07-11 18:22:48 +0700

    fix(math.ts): update functions
    Signed-off-by: scffs <khidey@inbox.ru>

--- packages/vkui/src/helpers/math.ts
+++ packages/vkui/src/helpers/math.ts
@@ -6,19 +6,34 @@ export function precisionRound(number: number, precision = 1) {
   return Math.round(number * factor) / factor;
 }
 
-function precision(number: number) {
-  return (`${number}`.split('.')[1] || '').length;
+/**
+ * Решение скопировано без изменений у MUI:
+ * https://github.com/mui/material-ui/blob/v5.13.7/packages/mui-base/src/useSlider/useSlider.ts#L89-L105
+ */
+function getDecimalPrecision(num: number) {
+  // This handles the case when num is very small (0.00000001), js will turn this into 1e-8.
+  // When num is bigger than 1 or less than -1 it won't get converted to this notation so it's fine.
+  if (Math.abs(num) < 1) {
+    const parts = num.toExponential().split('e-');
+    const matissaDecimalPart = parts[0].split('.')[1];
+    return (matissaDecimalPart ? matissaDecimalPart.length : 0) + parseInt(parts[1], 10);
+  }
+
+  const decimalPart = num.toString().split('.')[1];
+  return decimalPart ? decimalPart.length : 0;
+}
+
+function roundValueToStep(value: number, step: number, min: number) {
+  const nearest = Math.round((value - min) / step) * step + min;
+  return Number(nearest.toFixed(getDecimalPrecision(step)));
 }
 
 function decimatedClamp(val: number, min: number, max: number, step?: number) {
   if (step == null || step <= 0) {
     return clamp(val, min, max);
   }
-  // Round decimated offset using rounded and * step.
-  const decimatedOffset = Math.round((val - min) / step) * step;
-  // Calculate decimated value with clamped offset.
-  const decimatedValue = min + clamp(decimatedOffset, 0, max - min);
-  return precisionRound(decimatedValue, precision(step));
+  const roundedValue = roundValueToStep(val, step, min);
+  return clamp(roundedValue, min, max);
 }
 
 export function rescale(
