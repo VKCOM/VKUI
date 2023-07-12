73d7099ff7155fd5b128c5861c39fafb79f09fa9
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2023-07-12 22:11:40 +0700

    fix(Math.ts): updated `decimatedClamp` implementation (#5284)

--- packages/vkui/src/helpers/math.ts
+++ packages/vkui/src/helpers/math.ts
@@ -6,20 +6,34 @@ export function precisionRound(number: number, precision = 1) {
   return Math.round(number * factor) / factor;
 }
 
-function precision(number: number) {
-  return (`${number}`.split('.')[1] || '').length;
+/**
+ * Решение скопировано без изменений у MUI:
+ * https://github.com/mui/material-ui/blob/v5.13.7/packages/mui-base/src/useSlider/useSlider.ts#L89-L105
+ */
+function getDecimalPrecision(num: number) {
+  // This handles the case when num is very small (0.00000001), js will turn this into 1e-8.
+  // When num is bigger than 1 or less than -1 it won't get converted to this notation so it's fine.
+  if (Math.abs(num) < 1) {
+    const parts = num.toExponential().split('e-');
+    const matissaDecimalPart = parts[0].split('.')[1];
+    return (matissaDecimalPart ? matissaDecimalPart.length : 0) + parseInt(parts[1], 10);
+  }
+
+  const decimalPart = num.toString().split('.')[1];
+  return decimalPart ? decimalPart.length : 0;
+}
+
+function roundValueToStep(value: number, step: number, min: number) {
+  const nearest = Math.round((value - min) / step) * step + min;
+  return Number(nearest.toFixed(getDecimalPrecision(step)));
 }
 
 function decimatedClamp(val: number, min: number, max: number, step?: number) {
   if (step == null || step <= 0) {
     return clamp(val, min, max);
   }
-  const prec = precision(step);
-  // Round value to nearest min + k1 * step
-  const decimatedOffset = precisionRound(Math.round((val - min) / step) * step, prec);
-  // Round range length _down_ to nearest min + k2 * step
-  const decimatedRange = precisionRound(Math.floor((max - min) / step) * step, prec);
-  return min + clamp(decimatedOffset, 0, decimatedRange);
+  const roundedValue = roundValueToStep(val, step, min);
+  return clamp(roundedValue, min, max);
 }
 
 export function rescale(
