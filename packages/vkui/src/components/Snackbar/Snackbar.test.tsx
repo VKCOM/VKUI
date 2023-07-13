62ee82026c64b54ee6d4ec388f563ec291d121c6
Andrey Medvedev <andr.medv.spb@gmail.com>
2023-07-13 12:12:58 +0300

    Test that we really set offsetY

--- packages/vkui/src/components/Snackbar/Snackbar.test.tsx
+++ packages/vkui/src/components/Snackbar/Snackbar.test.tsx
@@ -1,6 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { noop } from '@vkontakte/vkjs';
 import { baselineComponent } from '../../testing/utils';
 import { Snackbar } from './Snackbar';
 
 describe('Snackbar', () => {
   baselineComponent(Snackbar);
+
+  it('custom offsetY', () => {
+    render(<Snackbar data-testid="snackbar" onClose={noop} offsetY={200} />);
+
+    expect(screen.getByTestId('snackbar').style.bottom).toBe('200px');
+  });
 });
