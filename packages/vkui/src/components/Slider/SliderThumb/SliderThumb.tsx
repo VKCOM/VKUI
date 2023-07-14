7d733a3a41619b4ce3e544faefa19a9559077acc
Andrey Medvedev <andr.medv.spb@gmail.com>
2023-07-14 17:21:42 +0300

    Make sure we udpate tooltip position every time user changes the value

--- packages/vkui/src/components/Slider/SliderThumb/SliderThumb.tsx
+++ packages/vkui/src/components/Slider/SliderThumb/SliderThumb.tsx
@@ -4,13 +4,11 @@ import { useExternRef } from '../../../hooks/useExternRef';
 import { useFocusVisible } from '../../../hooks/useFocusVisible';
 import {
   arrowMiddleware,
-  autoUpdate,
   convertFloatingDataToReactCSSProperties,
   flipMiddleware,
   offsetMiddleware,
   shiftMiddleware,
   useFloating,
-  type UseFloatingMiddleware,
 } from '../../../lib/floating';
 import type { HasDataAttribute, HasRootRef } from '../../../types';
 import { FocusVisible } from '../../FocusVisible/FocusVisible';
@@ -35,26 +33,21 @@ export const SliderThumb = ({
 }: SliderThumbProps) => {
   const { focusVisible, onBlur, onFocus } = useFocusVisible(false);
   const [arrowRef, setArrowRef] = React.useState<HTMLDivElement | null>(null);
+
   const memoizedMiddlewares = React.useMemo(() => {
-    const middlewares: UseFloatingMiddleware[] = [
+    return [
       offsetMiddleware({
         crossAxis: 0,
         mainAxis: 15,
       }),
-    ];
+      flipMiddleware(),
+      shiftMiddleware({ padding: 8 }),
 
-    middlewares.push(flipMiddleware());
-    middlewares.push(shiftMiddleware({ padding: 8 }));
-
-    // см. https://floating-ui.com/docs/arrow#order
-    middlewares.push(
       arrowMiddleware({
         element: arrowRef,
         padding: 14,
       }),
-    );
-
-    return middlewares;
+    ];
   }, [arrowRef]);
 
   const {
@@ -64,12 +57,21 @@ export const SliderThumb = ({
     refs,
     strategy: floatingPositionStrategy,
     middlewareData: { arrow: arrowCoords },
+    update: updateTooltipPosition,
   } = useFloating({
     placement: 'top',
     open: focusVisible,
     middleware: memoizedMiddlewares,
-    whileElementsMounted: autoUpdate,
   });
+
+  React.useEffect(
+    function udpateTooltipPositionOnValueChange() {
+      if (focusVisible && withTooltip && inputProps && inputProps.value !== 'undefined') {
+        updateTooltipPosition();
+      }
+    },
+    [inputProps, updateTooltipPosition, focusVisible, withTooltip],
+  );
 
   const handleRootRef = useExternRef<HTMLSpanElement>(getRootRef, refs.setReference);
   return (
@@ -91,7 +93,7 @@ export const SliderThumb = ({
         onFocus={onFocus}
       />
       <FocusVisible visible={focusVisible} mode="outside" />
-      {withTooltip && focusVisible && Boolean(inputProps?.value !== 'undefined') && (
+      {Boolean(withTooltip && focusVisible && inputProps && inputProps.value !== 'undefined') && (
         <TooltipBase
           appearance="neutral"
           getRootRef={refs.setFloating}
@@ -103,7 +105,7 @@ export const SliderThumb = ({
           arrowCoords={arrowCoords}
           arrowPlacement={resolvedPlacement}
           getArrowRef={setArrowRef}
-          text={`Value: ${inputProps?.value}`}
+          text={inputProps && inputProps.value}
         />
       )}
     </span>
