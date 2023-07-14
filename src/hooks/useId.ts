0106673dfd6d2f4fec0d5afa6ccdf0e4406bcf45
Daniil Suvorov <severecloud@gmail.com>
2023-07-14 23:25:39 +0300

    fix: remove useId hack

--- src/hooks/useId.ts
+++ src/hooks/useId.ts
@@ -1,11 +1,5 @@
 import * as React from "react";
 
-// Workaround for https://github.com/webpack/webpack/issues/14814
-// https://github.com/eps1lon/material-ui/blob/8d5f135b4d7a58253a99ab56dce4ac8de61f5dc1/packages/mui-utils/src/useId.ts#L21
-const maybeReactUseId: undefined | (() => string) = (React as any)[
-  "useId".toString()
-];
-
 let id = 0;
 
 // TODO: Remove after React 18
@@ -15,4 +9,4 @@ function useIncrementingCounterID(): string {
   return `:r${state}:`;
 }
 
-export const useId: () => string = maybeReactUseId ?? useIncrementingCounterID;
+export const useId: () => string = useIncrementingCounterID;
