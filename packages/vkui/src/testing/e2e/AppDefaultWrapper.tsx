a5c42ea1e8b27cf7cc244202215b48600e63ac23
Daniil Suvorov <severecloud@gmail.com>
2023-07-19 11:42:48 +0300

    test(e2e): remove Group (#5394)

--- packages/vkui/src/testing/e2e/AppDefaultWrapper.tsx
+++ packages/vkui/src/testing/e2e/AppDefaultWrapper.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { AppRoot, type AppRootProps } from '../../components/AppRoot/AppRoot';
-import { Group } from '../../components/Group/Group';
-import { Panel } from '../../components/Panel/Panel';
-import { View } from '../../components/View/View';
 
 export type AppWrapperProps = AppRootProps;
 
 export const AppDefaultWrapper = ({ children, ...restProps }: AppWrapperProps) => (
   <AppRoot mode="embedded" {...restProps}>
-    <View activePanel="panel">
-      <Panel id="panel">
-        <Group>{children}</Group>
-      </Panel>
-    </View>
+    <div
+      style={{
+        border: '8px solid var(--playwright-border)',
+        background: 'var(--playwright-background)',
+      }}
+    >
+      {children}
+    </div>
   </AppRoot>
 );
