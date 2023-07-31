574b36486314746cbfc19166f4fe9ecd20081006
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-07-31 16:35:33 +0300

    docs(PlaygroundRenderer): fix typo

--- styleguide/Components/Playground/PlaygroundRenderer.js
+++ styleguide/Components/Playground/PlaygroundRenderer.js
@@ -8,7 +8,7 @@ const PlaygroundRenderer = ({ name, preview, previewProps, tabBody, exampleIndex
     layout = true, // Обвесы в виде SplitLayout, SplitCol, etc
     iframe = true, // Рендерить пример в айфреме
     adaptivity: _adaptivity, // Продвинутые контролеры настроек адаптивности
-    showCustomPanelHeaderAfterProps = false, // Контролер настроек связанных с пользовательским слотом `after` у `PanelHeader`
+    showCustomPanelHeaderAfterProps = false, // Контроллер настроек связанных с пользовательским слотом `after` у `PanelHeader`
     integration,
     containerStyle,
     config,
