94d441425b84d435e3e0273bf2c83d1cedd9a589
Daniil Suvorov <severecloud@gmail.com>
2023-08-03 10:21:07 +0300

    feat: add makePostcssPlugins

--- packages/vkui/webpack.styles.config.js
+++ packages/vkui/webpack.styles.config.js
@@ -1,5 +1,32 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { makePostcssPlugins } = require('./scripts/postcss');
+
+/**
+ * Конфигурация для css
+ * @param {Object} config - Конфигурация.
+ * @param {boolean | undefined} config.isCssModulesFile - Сборка module.css файлов.
+ * @returns {import('postcss').Plugin[]}
+ */
+function makeCssRuleUse({ isCssModulesFile = false } = {}) {
+  return [
+    MiniCssExtractPlugin.loader,
+    {
+      loader: 'css-loader',
+      options: {
+        modules: false,
+      },
+    },
+    {
+      loader: 'postcss-loader',
+      options: {
+        postcssOptions: {
+          plugins: makePostcssPlugins({ isVKUIPackageBuild: true, isCssModulesFile }),
+        },
+      },
+    },
+  ];
+}
 
 module.exports = {
   // CSS is optimized via postcss, we dont care about JS
@@ -35,16 +62,13 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: false,
-            },
-          },
-          'postcss-loader',
-        ],
+        exclude: /\.module\.css$/,
+        use: makeCssRuleUse(),
+      },
+      {
+        test: /\.css$/,
+        include: /\.module\.css$/,
+        use: makeCssRuleUse({ isCssModulesFile: true }),
       },
     ],
   },
--- postcss.config.js
+++ postcss.config.js
@@ -1,64 +1,7 @@
-const path = require('path');
-const postcssGlobalData = require('@csstools/postcss-global-data');
-const restructureVariable = require('@project-tools/postcss-restructure-variable');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const postcssCustomMedia = require('postcss-custom-media');
-const cssCustomProperties = require('postcss-custom-properties');
-const cssImport = require('postcss-import');
-const cssModules = require('postcss-modules');
-const { VKUI_PACKAGE, VKUI_TOKENS_CSS, generateScopedName } = require('./shared');
-
-const IS_VKUI_PACKAGE_BUILD = Boolean(process.env.IS_VKUI_PACKAGE_BUILD);
+const { makePostcssPlugins } = require('./packages/vkui/scripts/postcss');
 
 module.exports = () => {
-  const plugins = [
-    cssImport(),
-    restructureVariable(VKUI_TOKENS_CSS.map((pathSegment) => path.join(__dirname, pathSegment))),
-    postcssGlobalData({
-      files: [
-        './node_modules/@vkontakte/vkui-tokens/themes/vkBase/cssVars/declarations/onlyVariables.css',
-        VKUI_PACKAGE.PATHS.CSS_CONSTANTS,
-        VKUI_PACKAGE.PATHS.CSS_CUSTOM_MEDIAS,
-      ].map((pathSegment) => path.join(__dirname, pathSegment)),
-    }),
-    cssCustomProperties({
-      preserve: true,
-      disableDeprecationNotice: true,
-    }),
-    autoprefixer(),
-    postcssCustomMedia(),
-  ];
-
-  // Обрабатываем только при сборке библиотеки.
-  if (IS_VKUI_PACKAGE_BUILD) {
-    plugins.push(
-      cssModules({
-        generateScopedName,
-        getJSON: () => void 0,
-      }),
-    );
-  }
-
-  if (process.env.NODE_ENV === 'production') {
-    plugins.push(
-      cssnano({
-        preset: [
-          'default',
-          {
-            // Отключаем из-за того, что `postcss-calc` меняет порядок операндов при умножении -1 на переменную
-            // Подробности здесь https://github.com/VKCOM/VKUI/issues/2963
-            calc: false,
-            // Включаем если собираем пакет @vkontakte/vkui.
-            // В остальных кейсах пустые CSS блоки удаляются раньше, чем их обработает
-            // `css-loader` с настройками для CSS Modules.
-            // Подробности здесь https://github.com/webpack-contrib/css-loader/issues/266
-            discardEmpty: IS_VKUI_PACKAGE_BUILD,
-          },
-        ],
-      }),
-    );
-  }
+  const plugins = makePostcssPlugins();
 
   return { plugins };
 };
