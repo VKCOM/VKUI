058e70597e4f8ac1a1e86ede19194b88a91c33ee
Daniil Suvorov <severecloud@gmail.com>
2023-08-21 13:30:27 +0300

    fix(useAdaptivityHasPointer): two pass rendering (#5666)
    Не используем двойной рендер если
    значение было проброшено через контекст

---
+++ packages/vkui/src/hooks/useAdaptivityHasPointer.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { renderHook } from '@testing-library/react-hooks';
+import { AdaptivityProvider } from '../components/AdaptivityProvider/AdaptivityProvider';
+import { useAdaptivityHasPointer } from './useAdaptivityHasPointer';
+
+describe(useAdaptivityHasPointer, () => {
+  it('returns on client', () => {
+    const { result } = renderHook(useAdaptivityHasPointer, {});
+    expect(result.current).toEqual(true);
+  });
+
+  it('context hasPointer={true}', () => {
+    const { result } = renderHook(useAdaptivityHasPointer, {
+      wrapper: ({ children }) => (
+        <AdaptivityProvider hasPointer={true}>{children}</AdaptivityProvider>
+      ),
+    });
+    expect(result.current).toEqual(true);
+  });
+
+  it('context hasPointer={false}', () => {
+    const { result } = renderHook(useAdaptivityHasPointer, {
+      wrapper: ({ children }) => (
+        <AdaptivityProvider hasPointer={false}>{children}</AdaptivityProvider>
+      ),
+    });
+    expect(result.current).toEqual(false);
+  });
+});
