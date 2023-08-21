36710d705f23425acb26201ecf6d074c33a22b66
Daniil Suvorov <severecloud@gmail.com>
2023-08-21 12:20:13 +0300

    fix(SelectMimicry): useAutoFocus React в свойстве `autoFocus` обрабатывает только button, input, textarea и select Добавляем хук для авто фокусировки элементов

---
+++ packages/vkui/src/hooks/useAutoFocus.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useIsomorphicLayoutEffect } from '../lib/useIsomorphicLayoutEffect';
+
+export function useAutoFocus(
+  ref: React.RefObject<HTMLElement | null>,
+  autoFocus: boolean | undefined,
+) {
+  useIsomorphicLayoutEffect(() => {
+    if (!autoFocus || !ref.current) {
+      return;
+    }
+
+    ref.current.focus();
+  }, []);
+}
