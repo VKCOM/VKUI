fc868063b16e5d688b401d9cee233d5dc6897a5d
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-08-24 18:18:43 +0300

    fix(Checkbox): add min-width: 0 for flex items (#5691)
    * tech(playwright): add TEXT_ELLIPSIS class name
    * fix(Checkbox): add min-width: 0 for flex items
    * CHORE: Update screenshots

--- packages/vkui/src/testing/e2e/constants.ts
+++ packages/vkui/src/testing/e2e/constants.ts
@@ -1,6 +1,7 @@
 // см. packages/vkui/playwright/index.css
 export const TEST_CLASS_NAMES = {
   CONTENT: 'vkuiTestContent',
+  TEXT_ELLIPSIS: 'vkuiTestTextEllipsis',
 };
 
 export const DEFAULT_CROP_TO_CONTENT_SELECTOR = '#root > *';
