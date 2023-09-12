3e7dc2461a695d0509bd016f1c1ce3b47f37268e
Andrey Medvedev <andr.medv.spb@gmail.com>
2023-09-12 12:29:30 +0300

    Allow to pass test-id to modal content

--- packages/vkui/src/components/ModalPage/ModalPage.test.tsx
+++ packages/vkui/src/components/ModalPage/ModalPage.test.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { ModalPage } from './ModalPage';
 
@@ -7,5 +8,19 @@ describe('ModalPage', () => {
     // TODO [a11y]: "ARIA dialog and alertdialog nodes should have an accessible name (aria-dialog-name)"
     //              https://dequeuniversity.com/rules/axe/4.5/aria-dialog-name?application=axeAPI
     a11y: false,
+  });
+
+  test('testid is for content', () => {
+    const { rerender } = render(<ModalPage nav="id" />);
+
+    expect(screen.queryByTestId('modal-page-id')).not.toBeTruthy();
+    expect(screen.queryByTestId('modal-content-id')).not.toBeTruthy();
+
+    rerender(
+      <ModalPage nav="id" data-testid="modal-page-id" modalContentTestId="modal-content-id" />,
+    );
+
+    expect(screen.queryByTestId('modal-page-id')).toBeTruthy();
+    expect(screen.queryByTestId('modal-content-id')).toBeTruthy();
   });
 });
