eb477cce8fd3b8019b84e0a8d436dc288088394e
Daniil Suvorov <severecloud@gmail.com>
2023-09-17 17:04:14 +0300

    feat(ImageBase): Clickable (#5803)

---
+++ packages/vkui/src/components/Clickable/Clickable.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { classNames } from '@vkontakte/vkjs';
+import { useFocusVisible } from '../../hooks/useFocusVisible';
+import { callMultiple } from '../../lib/callMultiple';
+import { FocusVisible, FocusVisibleMode } from '../FocusVisible/FocusVisible';
+import { RootComponent, RootComponentProps } from '../RootComponent/RootComponent';
+import styles from './Clickable.module.css';
+
+export interface ClickableProps<T> extends RootComponentProps<T> {
+  baseClassName?: string;
+  focusVisibleMode?: FocusVisibleMode;
+}
+
+/**
+ * Некликабельный компонент. Отключаем возможность нажимать на компонент.
+ */
+const NonClickable = <T,>({
+  href,
+  onClick,
+  onClickCapture,
+  ...restProps
+}: RootComponentProps<T>) => <RootComponent {...restProps} />;
+
+/**
+ * Кликабельный компонент. Добавляем кучу обвесов
+ */
+const RealClickable = <T,>({
+  baseClassName,
+  children,
+  focusVisibleMode = 'inside',
+  ...restProps
+}: ClickableProps<T>) => {
+  const { focusVisible, onBlur, onFocus } = useFocusVisible();
+
+  return (
+    <RootComponent
+      baseClassName={classNames(baseClassName, styles['Clickable__host'])}
+      onBlur={callMultiple(onBlur, restProps.onBlur)}
+      onFocus={callMultiple(onFocus, restProps.onFocus)}
+      {...restProps}
+    >
+      {children}
+      <FocusVisible visible={focusVisible} mode={focusVisibleMode} />
+    </RootComponent>
+  );
+};
+
+/**
+ * Проверяем, является ли компонент кликабельным
+ */
+function checkClickable<T>(props: ClickableProps<T>): boolean {
+  return (
+    (props.href !== undefined ||
+      props.onClick !== undefined ||
+      props.onClickCapture !== undefined) &&
+    !props.disabled
+  );
+}
+
+/**
+ * Определяет правильный компонент и его свойства
+ *
+ * - если передан Component, используем его
+ * - при передаче `href` превратится в `a`,
+ * - при передаче `onClick` превратится в `div` c `role="button"` и `tabIndex="0"`.
+ * - иначе используется `div`.
+ */
+function component<T>({
+  Component,
+  onClick,
+  onClickCapture,
+  href,
+  disabled,
+}: RootComponentProps<T>): RootComponentProps<T> {
+  if (Component !== undefined) {
+    return { Component };
+  } else if (href !== undefined) {
+    return { 'Component': 'a', 'role': 'link', 'aria-disabled': disabled };
+  } else if (onClick !== undefined || onClickCapture !== undefined) {
+    return {
+      'Component': 'div',
+      'role': 'button',
+      'tabIndex': disabled ? undefined : 0,
+      'aria-disabled': disabled,
+    };
+  }
+
+  return {};
+}
+
+/**
+ * Базовый кликабельный корневой компонент.
+ *
+ * - при передаче `href` превратится в `a`,
+ * - при передаче `onClick` превратится в `div` c `role="button"` и `tabIndex="0"`.
+ * - иначе используется `div`.
+ */
+export const Clickable = <T,>({ focusVisibleMode = 'inside', ...restProps }: ClickableProps<T>) => {
+  const commonProps = component(restProps);
+  const isClickable = checkClickable(restProps);
+
+  if (isClickable) {
+    return <RealClickable focusVisibleMode={focusVisibleMode} {...commonProps} {...restProps} />;
+  }
+
+  return <NonClickable {...commonProps} {...restProps} />;
+};
