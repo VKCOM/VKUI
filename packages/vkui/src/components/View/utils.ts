9525dedc812c4d123c60ebd59547e1c3d3f94597
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-09-19 19:15:16 +0300

    feat(View): increase swipe back zone (#5725)
    Увеличил зону срабатывания свайпа назад, тем самым приблизив поведение к нативным приложениям. В частности, как в приложении ВКонтакте на iOS.
    
    > **Note**
    >
    > Для `ViewInfitnite` повторил всё, что для `View`.
    
    h.2 Побочные изменения
    
    > Спасибо @shevchux и @mendrew за замечания ❤️ Иначе пропустил бы эти кейсы)
    
    Во-первых, немного пошатал тесты.
    
    Во-вторых, оптимизировал код: убрал из зависимостей `useEffect` лишнее; состояние `swipeBackPrevented` теперь храню в `useRef` вместо `useState`.
    
    Во-третьих, поправил следующие кейсы:
    
    h.3 Кейс 1. Элементы с горизонтальным скроллом.
    
    Добавил обнаружение смещения горизонтального скролла (в частности необходимо для `HorizontallScroll`), чтобы предотвращать свайп бек если пользователей взаимодействует с элементами, у которых есть горизонтальный скролл.
    
    Свайп бек включается:
    - либо если положение скролла находится в начале, иначе свайп бэк игнорируется. Если мы проскролили к началу, то нам необходимо отжать палец и заново синицировать свайп бэк, чтобы он сработал;
    - либо если потянули за край экрана слева (спасибо, @shevchux за предложение).
    
    > Вдохновился поведением у приложения ВКонтакте на iOS.
    
    > Использую состояние `swipeBackPrevented`, чтобы вызывать функцию обнаружения только один раз во время начала жеста. Чтобы получить родителей со скроллом, применяю утилиту [getOverflowAncestors()](https://github.com/floating-ui/floating-ui/blob/%40floating-ui/react-dom%402.0.2/packages/utils/test/getOverflowAncestors.test.ts) из бибилотеки Floating UI.
    
    h.3 Кейс 2. Компонент [Gallery](https://vkcom.github.io/VKUI/5.7.2/#/Gallery).
    
    Добавил `stopPropagation()` на `onDragStart` для `Gallery`, чтобы предотвращать свайп бэк.
    
    ---
    
    Добавил в доки `View` (стайлгайд и сторибук) примеры с кейсами 1 и 2.
    
    По замечанию @shevchux, в доке у `CellButton`'ов выставил `stopPropagation={false}`, чтобы не путало пользователей. Иначе свайп бэк блокируется.

--- packages/vkui/src/components/View/utils.ts
+++ packages/vkui/src/components/View/utils.ts
@@ -1,3 +1,4 @@
+import { getOverflowAncestors } from '../../lib/floating';
 import { TouchEvent } from '../Touch/Touch';
 
 const swipeBackExcludedSelector = 'input, textarea, [data-vkui-swipe-back=false]';
@@ -7,3 +8,21 @@ export function swipeBackExcluded(e: TouchEvent) {
   // eslint-disable-next-line no-restricted-properties
   return Boolean(target?.closest?.(swipeBackExcludedSelector));
 }
+
+export function hasHorizontalScrollableElementWithScrolledToLeft(node: HTMLElement) {
+  return getOverflowAncestors(node).some((node) =>
+    'scrollLeft' in node ? node.scrollLeft > 0 : false,
+  );
+}
+
+export const SWIPE_BACK_EDGE_SIZE_THRESHOLD = 20;
+
+export const SWIPE_BACK_SHIFT_THRESHOLD = 10;
+
+export const getSwipeBackPredicates = (startX: number, shiftX: number, innerWidth: number) => {
+  const swipedToOpposite = shiftX < 0;
+  const swipeBackTriggered = shiftX >= SWIPE_BACK_SHIFT_THRESHOLD;
+  const viewportStartEdgeTouched = startX <= SWIPE_BACK_EDGE_SIZE_THRESHOLD;
+  const viewportEndEdgeTouched = startX >= innerWidth - SWIPE_BACK_EDGE_SIZE_THRESHOLD;
+  return { swipedToOpposite, swipeBackTriggered, viewportStartEdgeTouched, viewportEndEdgeTouched };
+};
