460cade230ddb8d0ae10bf4df09c921d2ede82b7
Victoria Zhizhonkova <indarklight@gmail.com>
2023-09-25 17:47:08 +0700

    fix(WriteBar): icons shift (#5836)

--- packages/vkui/src/components/WriteBar/WriteBar.e2e.tsx
+++ packages/vkui/src/components/WriteBar/WriteBar.e2e.tsx
@@ -1,8 +1,26 @@
 import * as React from 'react';
 import { test } from '@vkui-e2e/test';
-import { WriteBarPlayground } from './WriteBar.e2e-playground';
+import { Platform } from '../../lib/platform';
+import { WriteBarIosIconsPlayground, WriteBarPlayground } from './WriteBar.e2e-playground';
+
+test.use({
+  toMatchSnapshot: {
+    threshold: 0.04,
+  },
+});
 
 test('WriteBar', async ({ mount, expectScreenshotClippedToContent, componentPlaygroundProps }) => {
   await mount(<WriteBarPlayground {...componentPlaygroundProps} />);
   await expectScreenshotClippedToContent();
+});
+
+test.describe('WriteBar', () => {
+  test.use({
+    onlyForPlatforms: [Platform.IOS],
+  });
+  // Проверяем, что иконки в iOS не сдвигаются при изменении value
+  test('icons', async ({ mount, expectScreenshotClippedToContent, componentPlaygroundProps }) => {
+    await mount(<WriteBarIosIconsPlayground {...componentPlaygroundProps} />);
+    await expectScreenshotClippedToContent();
+  });
 });
