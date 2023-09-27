a7dfd7aa44b702005d51de52441ae3e4ab6867c3
Andrey Medvedev <andr.medv.spb@gmail.com>
2023-09-27 18:59:10 +0300

    Use less concrete name for the variable
    Simplify ImageBase icon color setup

--- packages/vkui/src/components/SimpleCell/SimpleCell.tsx
+++ packages/vkui/src/components/SimpleCell/SimpleCell.tsx
@@ -128,9 +128,7 @@ export const SimpleCell = ({
         className,
       )}
     >
-      <div className={classNames(styles['SimpleCell__before'], 'vkuiInternalSimpleCell__before')}>
-        {before}
-      </div>
+      <div className={styles['SimpleCell__before']}>{before}</div>
       <div className={styles['SimpleCell__middle']}>
         {subhead && (
           <Subhead
