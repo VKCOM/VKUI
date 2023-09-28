876c2a8103667311a5a2173ad1460642c37377d2
Andrey Medvedev <andr.medv.spb@gmail.com>
2023-09-28 19:32:17 +0300

    fix(HorizontalCell): Don't render HorizontalCell__content if it's empty (#5884)

--- packages/vkui/src/components/HorizontalCell/HorizontalCell.test.tsx
+++ packages/vkui/src/components/HorizontalCell/HorizontalCell.test.tsx
@@ -1,7 +1,28 @@
 import * as React from 'react';
+import { render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { HorizontalCell } from './HorizontalCell';
+import styles from './HorizontaCell.module.css';
 
 describe('HorizontalCell', () => {
   baselineComponent((props) => <HorizontalCell {...props}>HorizontalCell</HorizontalCell>);
+
+  test('content part is not rendered if there is nothing to render', () => {
+    const { rerender } = render(
+      <HorizontalCell size="s">
+        <div>Children data</div>
+      </HorizontalCell>,
+    );
+
+    expect(screen.queryByText('Children data')).toBeTruthy();
+    expect(document.querySelector(`.${styles.HorizontalCell__content}`)).toBeNull();
+
+    rerender(
+      <HorizontalCell size="s" header="Author name">
+        <div>Children data</div>
+      </HorizontalCell>,
+    );
+
+    expect(document.querySelector(`.${styles.HorizontalCell__content}`)).not.toBeNull();
+  });
 });
