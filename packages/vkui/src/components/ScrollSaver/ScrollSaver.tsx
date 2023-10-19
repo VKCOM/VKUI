72eddf8847b093b6de6d2887214a98cf12679e23
Andrey Medvedev <andr.medv.spb@gmail.com>
2023-10-19 17:36:26 +0300

    Split ScrollSaver to components
    ScrollSaver is a component-wrapper which expects children
    and search for the ref in the children props
    
    ScrollSaverWithoutChildren is a component that receives ref from the
    outside as elementRef prop to support dynamic scroll control.
    
    useScrollSaver is a hook that allows to use scroll saver using hook.
    Expects ref as a prop to control scroll position

--- packages/vkui/src/components/ScrollSaver/ScrollSaver.tsx
+++ packages/vkui/src/components/ScrollSaver/ScrollSaver.tsx
@@ -6,49 +6,37 @@ import { useIsomorphicLayoutEffect } from '../../lib/useIsomorphicLayoutEffect';
 import { HasRootRef } from '../../types';
 import { useScrollSaverCache } from './ScrollSaverContext';
 
-export interface ScrollSaverProps {
+interface ScrollSaverHookProps<T = HTMLElement> {
   /*
    * Уникальный идентификатор элемента скролл которого надо запомнить.
    * Важно задавать id, так как на одной панели может понадобится запомнить позиции нескольких скролл-боксов.
    **/
   id: string;
   /*
-   * Если передан реакт-компонент, то он должен поддерживать getRootRef.
-   **/
-  children: React.ReactElement<HasRootRef<any>> | React.ReactElement;
-  /*
-   * Режим для получения рефа на элемент для скролла через getRef проп, вместо getRootRef (актуально для компонента HorizontalScroll)
-   **/
-  useGetRef?: boolean;
-  /*
    * Режим сохранения скролла: по умолчанию `forward`.
    * `forward` - позиция скролла сохраняется только при переходе вперёд и восстанавливается при переходе назад.
    * `always` - позиция скролла сохраняется и при переходе вперёд и при переходе назад.
    **/
   saveMode?: 'forward' | 'always';
+  /* Ref элемента, скроллом которого надо управлять */
+  ref: React.RefObject<T>;
 }
 
-/**
- * Компонент-обертка для сохранения позиции скролла элемента при переходах между View и Panel.
- * По умолчанию позволяет восстановить значение скролла при возвращении назад.
- */
-export function ScrollSaver({
-  id,
-  children: childrenProp,
-  saveMode = 'forward',
-  useGetRef,
-}: ScrollSaverProps) {
+function useScrollSaver({ ref, id, saveMode = 'forward' }: ScrollSaverHookProps) {
   const uniqueId = useUniqueId(id);
-  const [childrenRef, children] = usePatchChildrenRef(childrenProp, useGetRef);
   const direction = useNavDirection();
   const scrollSaverCache = useScrollSaverCache();
 
   useIsomorphicLayoutEffect(
     function handleScrollPosition() {
       const scrollId = uniqueId;
-      const childrenNode = childrenRef.current;
+      const refNode = ref.current;
 
       function restoreScrollPosition() {
+        if (!refNode) {
+          return;
+        }
+
         const scrollPosition = scrollSaverCache[scrollId];
 
         const shouldRestoreMovingBackwards = direction === 'backwards' && saveMode === 'forward';
@@ -64,26 +52,63 @@ export function ScrollSaver({
 
         const { inlineStart, blockStart } = scrollSaverCache[scrollId];
         if (inlineStart) {
-          childrenNode.scrollLeft = inlineStart;
+          refNode.scrollLeft = inlineStart;
         }
         if (blockStart) {
-          childrenNode.scrollTop = blockStart;
+          refNode.scrollTop = blockStart;
         }
       }
 
       restoreScrollPosition();
 
       return function saveScrollPositionOnUnmount() {
+        if (!refNode) {
+          return;
+        }
         scrollSaverCache[scrollId] = {
-          inlineStart: childrenNode.scrollLeft,
-          blockStart: childrenNode.scrollTop,
+          inlineStart: refNode.scrollLeft,
+          blockStart: refNode.scrollTop,
         };
       };
     },
-    [direction, uniqueId, saveMode, childrenRef],
+    [direction, uniqueId, saveMode, ref],
   );
 
+  return ref;
+}
+
+interface ScrollSaverProps extends Omit<ScrollSaverHookProps, 'ref'> {
+  /*
+   * Если передан реакт-компонент, то он должен поддерживать getRootRef.
+   **/
+  children: React.ReactElement<HasRootRef<any>> | React.ReactElement;
+  /*
+   * Режим для получения рефа на элемент для скролла через getRef проп, вместо getRootRef (актуально для компонента HorizontalScroll)
+   **/
+  useGetRef?: boolean;
+}
+
+/**
+ * Компонент-обертка для сохранения позиции скролла элемента при переходах между View и Panel.
+ * По умолчанию позволяет восстановить значение скролла при возвращении назад.
+ */
+export function ScrollSaver(props: ScrollSaverProps) {
+  const [childrenRef, children] = usePatchChildrenRef(props.children, props.useGetRef);
+  useScrollSaver({ ref: childrenRef, id: props.id, saveMode: props.saveMode });
+
   return children;
+}
+
+interface ScrollSaverWithoutChildren<T = HTMLElement> extends Omit<ScrollSaverHookProps, 'ref'> {
+  elementRef: React.RefObject<T>;
+  children?: React.ReactElement | null;
+}
+
+/* Компонентный Вариант useScrollSaver хука для динамического рендеринга, чтобы можно было пробросить и использовать любой ref */
+export function ScrollSaverWithoutChildren(props: ScrollSaverWithoutChildren) {
+  useScrollSaver({ ref: props.elementRef, id: props.id, saveMode: props.saveMode });
+
+  return props.children;
 }
 
 function useUniqueId(id: string) {
