2bae3fc6d136b714b0c5e524840fc40455c082fd
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-10-24 17:15:06 +0300

    fix(ConfigProvider): exclude prop with undefined value

--- packages/vkui/src/lib/utils.ts
+++ packages/vkui/src/lib/utils.ts
@@ -73,3 +73,19 @@ export function removeClassNameFromElement(element: HTMLElement, classNameToRemo
 
   element.setAttribute('class', classNamesArray.join(' '));
 }
+
+type ExcludeKeysWithUndefined<T> = {
+  [P in keyof T]?: Exclude<T[P], undefined>;
+};
+
+export const excludeKeysWithUndefined = <T extends Record<string | number | symbol, any>>(
+  obj: T,
+): ExcludeKeysWithUndefined<T> => {
+  const filteredObj: ExcludeKeysWithUndefined<T> = {};
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key) && obj[key] !== undefined) {
+      filteredObj[key] = obj[key];
+    }
+  }
+  return filteredObj;
+};
