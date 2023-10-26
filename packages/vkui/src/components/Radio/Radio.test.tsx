8b33cae444caf839d7e873b26e1eb539f6f74dab
Andrey Medvedev <andr.medv.spb@gmail.com>
2023-10-26 13:49:30 +0300

    Allow to pass data-* attributes to label element of Radio (#6037)
    Довольно сложно тестировать `Radio` элемент в e2e тестах с помощью `data-testid`. Из-за того, что мы передаём data-testid в input, реализованный с использованием [VisuallyHidden](https://vkcom.github.io/VKUI/#/VisuallyHidden). Известно, по крайне мере, о проблемах WebdriverIO, который не может нормально взаимодействовать с таким элементом.
    
    * Изменения
    Добавили возможность передавать `data-*` аттрибуты в `label` компонента `Radio` c помощью пропа `labelProps`.
    
    На текущий момент ограничили типы `labelProps` до data-* аттрибутов, чтобы не было конфликтов с `Tappable` и не возникало желания переопределять пропы `Tappable`.

--- packages/vkui/src/components/Radio/Radio.test.tsx
+++ packages/vkui/src/components/Radio/Radio.test.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { VisuallyHidden } from '../VisuallyHidden/VisuallyHidden';
 import { Radio } from './Radio';
@@ -12,4 +13,43 @@ describe('Radio', () => {
       <Radio aria-labelledby="radio" {...props} />
     </>
   ));
+
+  test('label and input can receive data-testid', () => {
+    render(
+      <React.Fragment>
+        <Radio
+          name="pay"
+          value="cash"
+          data-testid="input-cash-id"
+          labelProps={{ 'data-testid': 'label-cash-id' }}
+          defaultChecked
+        >
+          Оплата наличными
+        </Radio>
+        <Radio
+          name="pay"
+          value="card"
+          data-testid="input-card-id"
+          labelProps={{ 'data-testid': 'label-card-id' }}
+        >
+          Оплата картой
+        </Radio>
+      </React.Fragment>,
+    );
+
+    const inputCash = screen.getByTestId<HTMLInputElement>('input-cash-id');
+    const labelCash = screen.getByTestId<HTMLLabelElement>('label-cash-id');
+
+    expect(inputCash.checked).toBeTruthy();
+    expect(inputCash.matches('input')).toBeTruthy();
+    expect(labelCash.matches('label')).toBeTruthy();
+
+    const inputCard = screen.getByTestId<HTMLInputElement>('input-card-id');
+    expect(inputCard.checked).toBeFalsy();
+    const labelCard = screen.getByTestId<HTMLLabelElement>('label-card-id');
+
+    fireEvent.click(labelCard);
+    expect(inputCard.checked).toBeTruthy();
+    expect(inputCash.checked).toBeFalsy();
+  });
 });
