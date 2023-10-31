f62ff97e26d90b7fbc451f7ef7c2804bee9a5b4c
Daniil Suvorov <severecloud@gmail.com>
2023-10-31 14:57:53 +0300

    test: add waitFor

--- packages/vkui/src/components/PullToRefresh/PullToRefresh.e2e.tsx
+++ packages/vkui/src/components/PullToRefresh/PullToRefresh.e2e.tsx
@@ -13,7 +13,8 @@ test.describe('PullToRefresh', () => {
     expectScreenshotClippedToContent,
     componentPlaygroundProps,
   }) => {
-    await mount(<PullToRefreshPlayground {...componentPlaygroundProps} />);
+    const result = await mount(<PullToRefreshPlayground {...componentPlaygroundProps} />);
+    await result.waitFor();
     await page.mouse.move(100, 100);
     await page.mouse.down();
     await page.mouse.move(100, 380, { steps: 10 });
@@ -27,7 +28,10 @@ test.describe('PullToRefresh', () => {
     expectScreenshotClippedToContent,
     componentPlaygroundProps,
   }) => {
-    await mount(<PullToRefreshPlayground {...componentPlaygroundProps} paddingLeft="150px" />);
+    const result = await mount(
+      <PullToRefreshPlayground {...componentPlaygroundProps} paddingLeft="150px" />,
+    );
+    await result.waitFor();
     await page.mouse.move(200, 100);
     await page.mouse.down();
     await page.mouse.move(200, 380, { steps: 10 });
