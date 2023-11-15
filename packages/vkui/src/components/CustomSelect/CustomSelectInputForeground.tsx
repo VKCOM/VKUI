04717fa3a1b7924e73cf57faf08d860e13711d65
Andrey Medvedev <andr.medv.spb@gmail.com>
2023-11-15 15:31:23 +0300

    Remove unused prop from the CustomSelectInputForeground

--- packages/vkui/src/components/CustomSelect/CustomSelectInputForeground.tsx
+++ packages/vkui/src/components/CustomSelect/CustomSelectInputForeground.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { classNames } from '@vkontakte/vkjs';
-import { getFormFieldModeFromSelectType } from '../../lib/select';
 import { HasAlign, HTMLAttributesWithRootRef } from '../../types';
 import { FormFieldProps } from '../FormField/FormField';
 import { RootComponent } from '../RootComponent/RootComponent';
