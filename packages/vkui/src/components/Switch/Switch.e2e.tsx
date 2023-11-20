258824ccf8d9ee284fed30703ab588d82930ba24
Evgenia Polozova <eugpoloz@gmail.com>
2023-11-20 19:56:44 +0300

    e2e(Switch): add focus visible state visual test

--- packages/vkui/src/components/Switch/Switch.e2e.tsx
+++ packages/vkui/src/components/Switch/Switch.e2e.tsx
@@ -1,8 +1,28 @@
 import * as React from 'react';
 import { test } from '@vkui-e2e/test';
-import { SwitchPlayground } from './Switch.e2e-playground';
+import { Platform } from '../../lib/platform';
+import { SwitchFocusVisiblePlayground, SwitchPlayground } from './Switch.e2e-playground';
 
 test('Switch', async ({ mount, expectScreenshotClippedToContent, componentPlaygroundProps }) => {
   await mount(<SwitchPlayground {...componentPlaygroundProps} />);
   await expectScreenshotClippedToContent();
+});
+
+test.describe('Switch', () => {
+  test.use({
+    onlyForPlatforms: [Platform.ANDROID],
+    onlyForAppearances: ['light'],
+  });
+
+  test('State: Focus Visible', async ({
+    mount,
+    page,
+    expectScreenshotClippedToContent,
+    componentPlaygroundProps,
+  }) => {
+    await mount(<SwitchFocusVisiblePlayground {...componentPlaygroundProps} />);
+    await page.emulateMedia({ reducedMotion: 'reduce' });
+    await page.keyboard.press('Tab');
+    await expectScreenshotClippedToContent();
+  });
 });
