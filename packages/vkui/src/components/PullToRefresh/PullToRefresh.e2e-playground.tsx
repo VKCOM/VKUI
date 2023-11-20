4a649c2e8b1d69180c2e2395148bcea50d455a2a
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-11-20 11:42:57 +0300

    fix(playwright): fix PullToRefresh for WebKit

--- packages/vkui/src/components/PullToRefresh/PullToRefresh.e2e-playground.tsx
+++ packages/vkui/src/components/PullToRefresh/PullToRefresh.e2e-playground.tsx
@@ -13,15 +13,28 @@ export const PullToRefreshPlayground = (
       {({ ...props }: PullToRefreshProps) => (
         <Div style={{ width: '300px', paddingLeft }}>
           <PullToRefresh {...props}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a sollicitudin lectus, a
-            commodo sapien. Vivamus a urna leo. Integer iaculis dignissim urna, sit amet vestibulum
-            diam bibendum a. Donec eu arcu ut augue porttitor faucibus. Vestibulum nec pretium
-            tortor, sit amet congue nunc. Aenean ullamcorper ex sem, sed interdum quam consequat et.
-            Vestibulum a ex non diam fringilla feugiat. Nunc eu tellus sed leo elementum cursus.
-            Mauris blandit porta egestas. Curabitur eget justo elementum, malesuada lacus ut, congue
-            mauris. Integer orci nisi, convallis vitae dapibus sit amet, molestie a risus. Aenean
-            ultricies lacus eros, sit amet condimentum urna malesuada et. Sed quis dolor tempus orci
-            fringilla volutpat in sed velit. Aenean aliquet bibendum pretium.
+            <div
+              style={{
+                /**
+                 * Playwright >= 1.40 (https://github.com/microsoft/playwright/releases/tag/v1.40.0)
+                 * В WebKit начал выделяется текст, поэтому отключаем выделение.
+                 *
+                 * > Note: необходимо именно использую префикс `-webkit-*`, т.к. React не поллифилит.
+                 */
+                WebkitUserSelect: 'none',
+              }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a sollicitudin lectus, a
+              commodo sapien. Vivamus a urna leo. Integer iaculis dignissim urna, sit amet
+              vestibulum diam bibendum a. Donec eu arcu ut augue porttitor faucibus. Vestibulum nec
+              pretium tortor, sit amet congue nunc. Aenean ullamcorper ex sem, sed interdum quam
+              consequat et. Vestibulum a ex non diam fringilla feugiat. Nunc eu tellus sed leo
+              elementum cursus. Mauris blandit porta egestas. Curabitur eget justo elementum,
+              malesuada lacus ut, congue mauris. Integer orci nisi, convallis vitae dapibus sit
+              amet, molestie a risus. Aenean ultricies lacus eros, sit amet condimentum urna
+              malesuada et. Sed quis dolor tempus orci fringilla volutpat in sed velit. Aenean
+              aliquet bibendum pretium.
+            </div>
           </PullToRefresh>
         </Div>
       )}
