c9d7b91cb857eb4a303612df0b93b82e640b1edd
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-11-21 23:45:38 +0300

    feat: extend usePatchChildrenRef()
    - 2-ой аргумент: добавляем возможность прокидывать параметры помимо
      `ref`/`getRootRef`.
    - 3-ий аргумент: возможность прокинуть дополнительный обработчик `ref`.
    
    В `lib/utils` добавились новые валидаторы `isDOMTypeElement()` и
     `isValidNotReactFragmentElement()`.

--- packages/vkui/src/hooks/usePatchChildrenRef.ts
+++ packages/vkui/src/hooks/usePatchChildrenRef.ts
@@ -1,39 +1,66 @@
 import * as React from 'react';
+import { getMergedSameEventsByProps } from '../helpers/getMergedSameEventsByProps';
+import { isDOMTypeElement, isValidNotReactFragmentElement } from '../lib/utils';
 import { warnOnce } from '../lib/warnOnce';
+import type { HasRootRef } from '../types';
 import { useEffectDev } from './useEffectDev';
 import { useExternRef } from './useExternRef';
 
-type ChildrenElement<T> = React.ReactElement<{ getRootRef?: React.Ref<T> }>;
-
-const isDOMTypeElement = (element: React.ReactElement): element is React.DOMElement<any, any> => {
-  return typeof element.type === 'string';
-};
-
 const warn = warnOnce('usePatchChildrenRef');
 
-export const usePatchChildrenRef = <T = HTMLElement>(
-  children?: ChildrenElement<T>,
-): [React.MutableRefObject<T | null>, ChildrenElement<T> | undefined] => {
-  const childRef =
-    React.isValidElement(children) &&
-    (isDOMTypeElement(children) ? (children.ref as React.Ref<T>) : children.props.getRootRef);
-  const patchedRef = useExternRef<T>(childRef);
+type InjectProps<T> = Omit<React.HTMLAttributes<T>, keyof React.DOMAttributes<T>> & {
+  ref?: React.Ref<T>;
+};
+
+type ExpectedReactElement<T> = React.ReactElement<HasRootRef<T> & React.DOMAttributes<T>>;
+
+type ChildrenElement<T> =
+  | ExpectedReactElement<T>
+  | React.ReactText
+  | React.ReactFragment
+  | React.ReactPortal
+  | boolean
+  | null
+  | undefined;
+
+export const usePatchChildrenRef = <ElementType extends HTMLElement = HTMLElement>(
+  children?: ChildrenElement<ElementType>,
+  injectProps?: InjectProps<ElementType>,
+  refHook?: React.Ref<ElementType>,
+): [React.MutableRefObject<ElementType | null>, ChildrenElement<ElementType> | undefined] => {
+  const isValidElementResult = isValidNotReactFragmentElement(children);
+  const isDOMTypeElementResult =
+    isValidElementResult &&
+    isDOMTypeElement<React.HTMLAttributes<ElementType>, ElementType>(children);
+
+  const childRef = useExternRef<ElementType>(
+    isDOMTypeElementResult
+      ? children.ref
+      : isValidElementResult
+      ? children.props.getRootRef
+      : undefined,
+    refHook,
+  );
+
+  const mergedEventsByInjectProps = getMergedSameEventsByProps(
+    injectProps ? injectProps : {},
+    isValidElementResult ? children.props : {},
+  );
+
+  const props = isDOMTypeElementResult
+    ? { ref: childRef, ...injectProps, ...mergedEventsByInjectProps }
+    : isValidElementResult
+    ? { getRootRef: childRef, ...injectProps, ...mergedEventsByInjectProps }
+    : undefined;
 
   useEffectDev(() => {
-    if (!patchedRef.current) {
+    if (!childRef.current) {
       warn(
         'Кажется, в children передан компонент, который не поддерживает свойство getRootRef. Мы не можем получить ссылку на корневой dom-элемент этого компонента',
         'error',
       );
     }
-  }, [children?.type, patchedRef]);
+  }, [isValidElementResult ? children.type : null, childRef]);
 
-  return [
-    patchedRef,
-    React.isValidElement(children)
-      ? React.cloneElement(children, {
-          [isDOMTypeElement(children) ? 'ref' : 'getRootRef']: patchedRef,
-        })
-      : children,
-  ];
+  return [childRef, isValidElementResult ? React.cloneElement(children, props) : children];
 };
