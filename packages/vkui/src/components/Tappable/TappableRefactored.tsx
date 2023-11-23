9959f5f8e772f7f3df7240bdf3d5b0b067fa6b9d
Evgenia Polozova <eugpoloz@gmail.com>
2023-11-23 15:23:00 +0300

    fix(TappableRefactored): add hover and active states

--- packages/vkui/src/components/Tappable/TappableRefactored.tsx
+++ packages/vkui/src/components/Tappable/TappableRefactored.tsx
@@ -11,12 +11,46 @@ import { Ripple } from '../Ripple/Ripple';
 import { Touch, type TouchProps } from '../Touch/Touch';
 import styles from './TappableRefactored.module.css';
 
+const hoverStateModeClassNames = {
+  background: styles['TappableRefactored--hover-background'],
+  opacity: styles['TappableRefactored--hover-opacity'],
+};
+
+type HoverStatePresetMode = keyof typeof hoverStateModeClassNames;
+type HoverStateMode = HoverStatePresetMode | false;
+
+const isHoverStatePresetMode = (mode: HoverStateMode): mode is HoverStatePresetMode =>
+  mode === 'background' || mode === 'opacity';
+
+const activeStateModeClassNames = {
+  background: styles['TappableRefactored--active-background'],
+  opacity: styles['TappableRefactored--active-opacity'],
+};
+
+type ActiveStatePresetMode = keyof typeof activeStateModeClassNames;
+type ActiveStateMode = ActiveStatePresetMode | false;
+
+const isActiveStatePresetMode = (mode: ActiveStateMode): mode is ActiveStatePresetMode =>
+  mode === 'background' || mode === 'opacity';
+
 export interface TappableRefactoredProps extends TouchProps {
   baseClassName?: string;
   /**
    * Стиль аутлайна focus visible. Если передать произвольную строку, она добавится как css-класс во время focus-visible
    */
   focusVisibleMode?: FocusVisibleMode;
+  /**
+   * Один из двух дефолтных стилей подсветки состояния `:active`.
+   *
+   * Если вам нужен собственный эффект, передайте `false` и воспользуйтесь магией `:active` в css.
+   */
+  activeMode?: ActiveStateMode;
+  /**
+   * Один из двух дефолтных стилей подсветки состояния `:hover`.
+   *
+   * Если вам нужен собственный эффект, передайте `false` и воспользуйтесь магией `:hover` в css.
+   */
+  hoverMode?: HoverStateMode;
 }
 
 export const TappableRefactored = ({
@@ -33,6 +67,16 @@ export const TappableRefactored = ({
     mode: focusVisibleMode,
   });
 
+  // State: Hover
+  const hoverModeClassName = isHoverStatePresetMode(hoverMode)
+    ? hoverStateModeClassNames[hoverMode]
+    : hoverMode;
+
+  // State: Active
+  const activeModeClassName = isActiveStatePresetMode(activeMode)
+    ? activeStateModeClassNames[activeMode]
+    : activeMode;
+
   // Ripple Effect
   const { x, y, onTappableStart } = useRippleEffect();
 
@@ -45,7 +89,13 @@ export const TappableRefactored = ({
       onFocus={callMultiple(onFocus, restProps.onFocus)}
       slideThreshold={20}
       usePointerHover
-      className={classNames(styles['TappableRefactored'], className, focusVisibleClassNames)}
+      className={classNames(
+        styles['TappableRefactored'],
+        hoverMode && hoverModeClassName,
+        activeMode && activeModeClassName,
+        focusVisibleClassNames,
+        className,
+      )}
     >
       {children}
       {!disabled && <Ripple clientX={x} clientY={y} />}
