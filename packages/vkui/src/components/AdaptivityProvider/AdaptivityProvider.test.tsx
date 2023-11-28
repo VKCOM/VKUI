f7fc74d7596ed259392e3e066b5513b837fa0dad
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-11-28 12:11:02 +0300

    tech(AdaptivityProvider): resolve TODO
    migration_v6.md уже обновлён

--- packages/vkui/src/components/AdaptivityProvider/AdaptivityProvider.test.tsx
+++ packages/vkui/src/components/AdaptivityProvider/AdaptivityProvider.test.tsx
@@ -1,260 +1,119 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
-import { type BridgeAdaptivity, useBridgeAdaptivity } from '../../hooks/useBridgeAdaptivity';
+import { render } from '@testing-library/react';
 import { ViewHeight, ViewWidth } from '../../lib/adaptivity';
 import { baselineComponent } from '../../testing/utils';
 import { AdaptivityContext, type AdaptivityProps } from './AdaptivityContext';
 import { AdaptivityProvider, type AdaptivityProviderProps } from './AdaptivityProvider';
 
-jest.mock('../../hooks/useBridgeAdaptivity', () => {
-  const bridgeMock = jest.fn(() => {
-    return {
-      type: '',
-      viewportWidth: 0,
-      viewportHeight: 0,
-    };
-  });
-
-  return {
-    useBridgeAdaptivity: bridgeMock,
-  };
+const CatchAdaptivityProviderContext = React.forwardRef<AdaptivityProps>((_, ref) => {
+  const adaptivityProps = React.useContext(AdaptivityContext);
+  React.useImperativeHandle(ref, () => adaptivityProps, [adaptivityProps]);
+  return null;
 });
 
-function CatchAdaptivityProviderContext() {
-  const { sizeX, sizeY, viewWidth, viewHeight } = React.useContext(AdaptivityContext);
-
-  return (
-    <div
-      data-testid="adaptivity-provider-context"
-      data-size-x={sizeX}
-      data-size-y={sizeY}
-      data-view-width={viewWidth}
-      data-view-height={viewHeight}
-    />
-  );
-}
-
-function renderAdaptiveProvider(props?: AdaptivityProviderProps): AdaptivityProps {
-  render(
+const TestAdaptiveProvider = React.forwardRef<AdaptivityProps, AdaptivityProviderProps>(
+  (props, ref) => (
     <AdaptivityProvider {...props}>
-      <CatchAdaptivityProviderContext />
-    </AdaptivityProvider>,
-  );
-  const elWithContextData = screen.getByTestId('adaptivity-provider-context');
-
-  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  return {
-    sizeX: elWithContextData.dataset.sizeX,
-    sizeY: elWithContextData.dataset.sizeY,
-    viewWidth: elWithContextData.dataset.viewWidth
-      ? Number(elWithContextData.dataset.viewWidth)
-      : undefined,
-    viewHeight: elWithContextData.dataset.viewHeight
-      ? Number(elWithContextData.dataset.viewHeight)
-      : undefined,
-  } as AdaptivityProps;
-}
-
-function renderAdaptiveProviderWithBridge(
-  type: BridgeAdaptivity['type'],
-  viewportWidth = 0,
-  viewportHeight = 0,
-  hasPointer?: boolean,
-) {
-  (useBridgeAdaptivity as jest.Mock).mockReturnValue({
-    type,
-    viewportWidth,
-    viewportHeight,
-  });
-
-  return renderAdaptiveProvider({ hasPointer });
-}
+      <CatchAdaptivityProviderContext ref={ref} />
+    </AdaptivityProvider>
+  ),
+);
 
 describe('AdaptivityProvider', () => {
-  beforeEach(() => {
-    (useBridgeAdaptivity as jest.Mock).mockReturnValue({
-      type: '',
-      viewportWidth: 0,
-      viewportHeight: 0,
-    });
-  });
-
   baselineComponent(AdaptivityProvider, { forward: false, a11y: false, getRootRef: false });
 
-  describe('without bridge', () => {
-    it('should return undefined adaptivity props', () => {
-      const result = renderAdaptiveProvider();
-      expect(result).toEqual({
-        sizeX: undefined,
-        sizeY: undefined,
-        viewWidth: undefined,
-        viewHeight: undefined,
-      });
-    });
-
-    it('should define sizeX and sizeY adaptivity props', () => {
-      const result = renderAdaptiveProvider({
-        viewWidth: ViewWidth.DESKTOP,
-        viewHeight: ViewHeight.MEDIUM,
-      });
-      expect(result).toEqual({
-        sizeX: 'regular',
-        sizeY: 'compact',
-        viewWidth: ViewWidth.DESKTOP,
-        viewHeight: ViewHeight.MEDIUM,
-      });
+  it('should return undefined adaptivity props', () => {
+    const adaptivityPropsRef = React.createRef<AdaptivityProps>();
+    render(<TestAdaptiveProvider ref={adaptivityPropsRef} />);
+    expect(adaptivityPropsRef.current).toEqual({
+      sizeX: undefined,
+      sizeY: undefined,
+      viewWidth: undefined,
+      viewHeight: undefined,
     });
   });
 
-  describe('with bridge', () => {
-    it('should apply bridge adaptivity [viewWidth] SMALL_MOBILE', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 300, 420);
-      expect(result).toEqual({
-        sizeX: 'compact',
-        sizeY: 'regular',
-        viewWidth: ViewWidth.SMALL_MOBILE,
-        viewHeight: ViewHeight.SMALL,
-      });
-    });
-
-    it('should apply bridge adaptivity [viewWidth] MOBILE', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 320, 420);
-      expect(result).toEqual({
-        sizeX: 'compact',
-        sizeY: 'regular',
-        viewWidth: ViewWidth.MOBILE,
-        viewHeight: ViewHeight.SMALL,
-      });
-    });
-
-    it('should apply bridge adaptivity [viewWidth] SMALL_TABLET', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 768, 420);
-      expect(result).toEqual({
-        sizeX: 'regular',
-        sizeY: 'compact',
-        viewWidth: ViewWidth.SMALL_TABLET,
-        viewHeight: ViewHeight.SMALL,
-      });
-    });
-
-    it('should apply bridge adaptivity [viewWidth] TABLET', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 1024, 420);
-      expect(result).toEqual({
-        sizeX: 'regular',
-        sizeY: 'compact',
-        viewWidth: ViewWidth.TABLET,
-        viewHeight: ViewHeight.SMALL,
-      });
-    });
-
-    it('should apply bridge adaptivity [viewWidth] DESKTOP', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 1280, 420);
-      expect(result).toEqual({
-        sizeX: 'regular',
-        sizeY: 'compact',
-        viewWidth: ViewWidth.DESKTOP,
-        viewHeight: ViewHeight.SMALL,
-      });
-    });
-
-    it('should apply bridge adaptivity [viewHeight] EXTRA_SMALL', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 320, 340);
-      expect(result).toEqual({
-        sizeX: 'compact',
-        sizeY: 'compact',
-        viewWidth: ViewWidth.MOBILE,
-        viewHeight: ViewHeight.EXTRA_SMALL,
-      });
-    });
-
-    it('should apply bridge adaptivity [viewHeight] SMALL', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 320, 415);
-      expect(result).toEqual({
-        sizeX: 'compact',
-        sizeY: 'regular',
-        viewWidth: ViewWidth.MOBILE,
-        viewHeight: ViewHeight.SMALL,
-      });
-    });
-
-    it('should apply bridge adaptivity [viewHeight] MEDIUM', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 320, 720);
-      expect(result).toEqual({
-        sizeX: 'compact',
-        sizeY: 'regular',
-        viewWidth: ViewWidth.MOBILE,
-        viewHeight: ViewHeight.MEDIUM,
-      });
-    });
-
-    it('should apply bridge adaptivity [both] SMALL_MOBILE / EXTRA_SMALL', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 300, 340);
-      expect(result).toEqual({
-        sizeX: 'compact',
-        sizeY: 'compact',
-        viewWidth: ViewWidth.SMALL_MOBILE,
-        viewHeight: ViewHeight.EXTRA_SMALL,
-      });
-    });
-
-    it('should apply bridge adaptivity [both] SMALL_TABLET / SMALL', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 768, 415);
-      expect(result).toEqual({
-        sizeX: 'regular',
-        sizeY: 'compact',
-        viewWidth: ViewWidth.SMALL_TABLET,
-        viewHeight: ViewHeight.SMALL,
-      });
-    });
-
-    it('should apply bridge adaptivity [both] TABLET / MEDIUM', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 1024, 720);
-      expect(result).toEqual({
-        sizeX: 'regular',
-        sizeY: 'compact',
-        viewWidth: ViewWidth.TABLET,
-        viewHeight: ViewHeight.MEDIUM,
-      });
-    });
-
-    it('should apply bridge adaptivity [both] DESKTOP / MEDIUM', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 1280, 720);
-      expect(result).toEqual({
+  it.each([
+    {
+      inProps: { viewWidth: ViewWidth.DESKTOP, viewHeight: ViewHeight.MEDIUM },
+      outProps: {
         sizeX: 'regular',
         sizeY: 'compact',
         viewWidth: ViewWidth.DESKTOP,
         viewHeight: ViewHeight.MEDIUM,
-      });
-    });
-
-    it('should ignore custom mouse with bridge [viewWidth] SMALL_TABLET', () => {
-      const result = renderAdaptiveProviderWithBridge('adaptive', 768, 420, false);
-      expect(result).toEqual({
+      },
+    },
+    {
+      inProps: { viewWidth: ViewWidth.MOBILE, viewHeight: ViewHeight.EXTRA_SMALL },
+      outProps: {
+        sizeX: 'compact',
+        sizeY: 'compact',
+        viewWidth: ViewWidth.MOBILE,
+        viewHeight: ViewHeight.EXTRA_SMALL,
+      },
+    },
+    {
+      inProps: { viewWidth: ViewWidth.SMALL_TABLET, viewHeight: ViewHeight.MEDIUM },
+      outProps: {
         sizeX: 'regular',
         sizeY: 'compact',
         viewWidth: ViewWidth.SMALL_TABLET,
-        viewHeight: ViewHeight.SMALL,
-      });
-    });
-
-    it('should use bridge force_mobile preset', () => {
-      const result = renderAdaptiveProviderWithBridge('force_mobile');
-      expect(result).toEqual({
-        sizeX: 'compact',
-        sizeY: 'regular',
-        viewWidth: ViewWidth.MOBILE,
-        viewHeight: undefined,
-      });
-    });
-
-    it('should use bridge force_mobile_compact preset', () => {
-      const result = renderAdaptiveProviderWithBridge('force_mobile_compact');
-      expect(result).toEqual({
+        viewHeight: ViewHeight.MEDIUM,
+      },
+    },
+    {
+      inProps: {
+        sizeX: 'compact' as const,
+        viewWidth: ViewWidth.SMALL_TABLET,
+        viewHeight: ViewHeight.MEDIUM,
+      },
+      outProps: {
         sizeX: 'compact',
         sizeY: 'compact',
-        viewWidth: ViewWidth.MOBILE,
-        viewHeight: undefined,
-      });
-    });
+        viewWidth: ViewWidth.SMALL_TABLET,
+        viewHeight: ViewHeight.MEDIUM,
+      },
+    },
+    {
+      inProps: {
+        sizeY: 'regular' as const,
+        viewWidth: ViewWidth.SMALL_TABLET,
+        viewHeight: ViewHeight.MEDIUM,
+      },
+      outProps: {
+        sizeX: 'regular',
+        sizeY: 'regular',
+        viewWidth: ViewWidth.SMALL_TABLET,
+        viewHeight: ViewHeight.MEDIUM,
+      },
+    },
+    {
+      inProps: {
+        sizeX: 'regular' as const,
+        sizeY: 'regular' as const,
+        viewWidth: ViewWidth.SMALL_TABLET,
+        viewHeight: ViewHeight.MEDIUM,
+      },
+      outProps: {
+        sizeX: 'regular',
+        sizeY: 'regular',
+        viewWidth: ViewWidth.SMALL_TABLET,
+        viewHeight: ViewHeight.MEDIUM,
+      },
+    },
+    {
+      inProps: {
+        sizeX: 'regular' as const,
+        sizeY: 'regular' as const,
+      },
+      outProps: {
+        sizeX: 'regular',
+        sizeY: 'regular',
+      },
+    },
+  ])('should define sizeX and sizeY adaptivity props', ({ inProps, outProps }) => {
+    const adaptivityPropsRef = React.createRef<AdaptivityProps>();
+    render(<TestAdaptiveProvider ref={adaptivityPropsRef} {...inProps} />);
+    expect(adaptivityPropsRef.current).toEqual(outProps);
   });
 });
--- packages/vkui/src/components/AdaptivityProvider/AdaptivityProvider.tsx
+++ packages/vkui/src/components/AdaptivityProvider/AdaptivityProvider.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import { hasMouse as _hasPointer } from '@vkontakte/vkjs';
-import { BridgeAdaptivity, useBridgeAdaptivity } from '../../hooks/useBridgeAdaptivity';
-import { BREAKPOINTS, ViewHeight, ViewWidth } from '../../lib/adaptivity';
-import { warnOnce } from '../../lib/warnOnce';
+import { ViewHeight, ViewWidth } from '../../lib/adaptivity';
 import { HasChildren } from '../../types';
 import { AdaptivityContext, type AdaptivityProps } from './AdaptivityContext';
-
-const warn = warnOnce('AdaptivityProvider');
 
 export interface AdaptivityProviderProps extends AdaptivityProps, HasChildren {}
 
@@ -16,130 +12,40 @@ export interface AdaptivityProviderProps extends AdaptivityProps, HasChildren {}
 export const AdaptivityProvider = ({
   viewWidth,
   viewHeight,
-  sizeX,
-  sizeY,
+  sizeX: sizeXProp,
+  sizeY: sizeYProp,
   hasPointer,
   hasHover,
   children,
 }: AdaptivityProviderProps) => {
-  // TODO [>=6]: удалить использование хука (#5049)
-  /* eslint-disable @typescript-eslint/naming-convention */
-  const LEGACY_isPerhapsPropsByBridgeTypeAdaptive =
-    viewWidth !== undefined && viewHeight !== undefined;
-  const LEGACY_isPerhapsPropsByBridgeTypeForceMobile =
-    viewWidth !== undefined && sizeX !== undefined && sizeY !== undefined;
-  const LEGACY_disableInternalUseBridgeAdaptivity =
-    LEGACY_isPerhapsPropsByBridgeTypeAdaptive || LEGACY_isPerhapsPropsByBridgeTypeForceMobile;
-  const LEGACY_bridge = useBridgeAdaptivity(LEGACY_disableInternalUseBridgeAdaptivity);
-  /* eslint-enable @typescript-eslint/naming-convention */
-
-  if (process.env.NODE_ENV === 'development') {
-    // TODO [>=6]: удалить warn
-    if (!LEGACY_disableInternalUseBridgeAdaptivity) {
-      warn("[@vkontakte/vk-bridge] Интеграция VKUI с @vkontakte/vk-bridge устарела и будет удалена в v6. Используйте хук `useAdaptivity()` из @vkontakte/vk-bridge-react и результат передайте в компонент (см. https://github.com/VKCOM/VKUI/issues/5049)"); // prettier-ignore
-    }
-  }
-
-  const adaptivity = React.useMemo(
-    () =>
-      calculateAdaptivity(
-        {
-          viewWidth,
-          viewHeight,
-          sizeX,
-          sizeY,
-          hasPointer,
-          hasHover,
-        },
-        LEGACY_bridge,
-      ),
-    [viewWidth, viewHeight, sizeX, sizeY, hasPointer, hasHover, LEGACY_bridge],
-  );
-
-  return <AdaptivityContext.Provider value={adaptivity}>{children}</AdaptivityContext.Provider>;
-};
-
-function calculateAdaptivity(
-  { viewWidth, viewHeight, sizeX, sizeY, hasPointer, hasHover }: AdaptivityProps,
-  LEGACY_bridge: BridgeAdaptivity, // eslint-disable-line @typescript-eslint/naming-convention
-) {
-  // TODO [>=6]: удалить блок кода c использованием LEGACY_bridge (#5049)
-  //  https://github.com/VKCOM/VKUI/blob/v5.5.5/packages/vkui/src/components/AdaptivityProvider/AdaptivityProvider.tsx#L46-L92
-  if (LEGACY_bridge.type === 'adaptive') {
-    const { viewportWidth, viewportHeight } = LEGACY_bridge;
-
-    if (viewportWidth >= BREAKPOINTS.DESKTOP) {
-      viewWidth = ViewWidth.DESKTOP;
-    } else if (viewportWidth >= BREAKPOINTS.TABLET) {
-      viewWidth = ViewWidth.TABLET;
-    } else if (viewportWidth >= BREAKPOINTS.SMALL_TABLET) {
-      viewWidth = ViewWidth.SMALL_TABLET;
-    } else if (viewportWidth >= BREAKPOINTS.MOBILE) {
-      viewWidth = ViewWidth.MOBILE;
-    } else {
-      viewWidth = ViewWidth.SMALL_MOBILE;
-    }
-
-    if (viewportHeight >= BREAKPOINTS.MEDIUM_HEIGHT) {
-      viewHeight = ViewHeight.MEDIUM;
-    } else if (viewportHeight >= BREAKPOINTS.MOBILE_LANDSCAPE_HEIGHT) {
-      viewHeight = ViewHeight.SMALL;
-    } else {
-      viewHeight = ViewHeight.EXTRA_SMALL;
-    }
-
-    if (viewWidth <= ViewWidth.MOBILE) {
-      sizeX = 'compact';
-    } else {
-      sizeX = 'regular';
-    }
-
-    if (
-      (viewWidth >= ViewWidth.SMALL_TABLET && _hasPointer) ||
-      viewHeight <= ViewHeight.EXTRA_SMALL
-    ) {
-      sizeY = 'compact';
-    } else {
-      sizeY = 'regular';
-    }
-  } else if (
-    LEGACY_bridge.type === 'force_mobile' ||
-    LEGACY_bridge.type === 'force_mobile_compact'
-  ) {
-    viewWidth = ViewWidth.MOBILE;
-    sizeX = 'compact';
-
-    if (LEGACY_bridge.type === 'force_mobile_compact') {
-      sizeY = 'compact';
-    } else {
-      sizeY = 'regular';
-    }
-  } else {
-    if (sizeX === undefined && viewWidth !== undefined) {
+  const adaptivity = React.useMemo(() => {
+    const nextProps: AdaptivityProps = {
+      viewWidth,
+      viewHeight,
+      sizeX: sizeXProp,
+      sizeY: sizeYProp,
+      hasPointer,
+      hasHover,
+    };
+    if (sizeXProp === undefined && viewWidth !== undefined) {
       if (viewWidth <= ViewWidth.MOBILE) {
-        sizeX = 'compact';
+        nextProps.sizeX = 'compact';
       } else {
-        sizeX = 'regular';
+        nextProps.sizeX = 'regular';
       }
     }
-    if (sizeY === undefined && viewWidth !== undefined && viewHeight !== undefined) {
+    if (sizeYProp === undefined && viewWidth !== undefined && viewHeight !== undefined) {
       if (
         (viewWidth >= ViewWidth.SMALL_TABLET && _hasPointer) ||
         viewHeight <= ViewHeight.EXTRA_SMALL
       ) {
-        sizeY = 'compact';
+        nextProps.sizeY = 'compact';
       } else {
-        sizeY = 'regular';
+        nextProps.sizeY = 'regular';
       }
     }
-  }
+    return nextProps;
+  }, [viewWidth, viewHeight, sizeXProp, sizeYProp, hasPointer, hasHover]);
 
-  return {
-    viewWidth,
-    viewHeight,
-    sizeX,
-    sizeY,
-    hasPointer,
-    hasHover,
-  };
-}
+  return <AdaptivityContext.Provider value={adaptivity}>{children}</AdaptivityContext.Provider>;
+};
