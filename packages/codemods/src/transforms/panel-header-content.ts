296a1147aa2126f093a372c9d39bc0eb4dd2b98e
Victoria Zhizhonkova <indarklight@gmail.com>
2023-12-04 21:41:06 +0700

    fix: remove console.log

--- packages/codemods/src/transforms/panel-header-content.ts
+++ packages/codemods/src/transforms/panel-header-content.ts
@@ -13,7 +13,6 @@ export default function transformer(file: FileInfo, api: API, options: JSCodeShi
   const elements = source.find(j.JSXOpeningElement).filter((path) => {
     if (path.value.name.type === 'JSXMemberExpression') {
       const property = path.value.name;
-      console.log(property);
       if (
         property.object.type === 'JSXIdentifier' &&
         property.object.name === localName &&
