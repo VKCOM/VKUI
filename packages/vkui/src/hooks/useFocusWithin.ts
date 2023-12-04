8eedc1db549f157ec14ddab232d85f19c72165ea
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-12-04 15:22:34 +0300

    fix(useFocusWithin): refactor logic
    Из-за проверки `focus !== focusWithin` была проблема, если в рамках `FormField` навигироваться через фокус между внутренними элементами.
    
    Также, избавился от `useGlobalEventListener` – тем самым теперь создаётся лишь 1 эффект вместо 4-х.

--- packages/vkui/src/hooks/useFocusWithin.ts
+++ packages/vkui/src/hooks/useFocusWithin.ts
@@ -1,30 +1,38 @@
 import * as React from 'react';
 import { useDOM } from '../lib/dom';
 import { useIsomorphicLayoutEffect } from '../lib/useIsomorphicLayoutEffect';
-import { useGlobalEventListener } from './useGlobalEventListener';
+
+const isFocusWithin = <T extends Element>(ref: T, document: Document) =>
+  ref.contains(document.activeElement);
 
 export function useFocusWithin(ref: React.RefObject<HTMLElement | null>): boolean {
   const { document } = useDOM();
-  const isFocusWithin = () => {
-    if (!ref.current || !document) {
-      return false;
+  const [focusWithin, setFocusWithin] = React.useState<boolean>(() =>
+    ref.current && document ? isFocusWithin(ref.current, document) : false,
+  );
+
+  useIsomorphicLayoutEffect(function handleAutoFocus() {
+    /* istanbul ignore if: невозможный кейс, т.к. в SSR эффекты не вызываются. Проверка на будущее, если вдруг эффект будет вызываться. */
+    if (!document) {
+      return;
     }
 
-    return ref.current.contains(document.activeElement);
-  };
+    const handleFocusOrBlurEvents = () => {
+      if (ref.current) {
+        setFocusWithin(isFocusWithin(ref.current, document));
+      }
+    };
 
-  const [focusWithin, setFocusWithin] = React.useState<boolean>(isFocusWithin);
+    // Вызываем в начале, чтобы проверить autoFocus
+    void handleFocusOrBlurEvents();
 
-  const listener = () => {
-    const focus = isFocusWithin();
-    focus !== focusWithin && setFocusWithin(focus);
-  };
-
-  // Проверяем autoFocus
-  useIsomorphicLayoutEffect(listener, []);
-
-  useGlobalEventListener(document, 'focus', listener, { capture: true });
-  useGlobalEventListener(document, 'blur', listener, { capture: true });
+    document.addEventListener('focus', handleFocusOrBlurEvents, { capture: true });
+    document.addEventListener('blur', handleFocusOrBlurEvents, { capture: true });
+    return () => {
+      document.removeEventListener('focus', handleFocusOrBlurEvents, { capture: true });
+      document.removeEventListener('blur', handleFocusOrBlurEvents, { capture: true });
+    };
+  }, []);
 
   return focusWithin;
 }
