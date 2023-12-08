408f22b78ff2d83d6a49df531278b9cc28fc1117
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-12-08 16:04:32 +0300

    refactor: migration_v6.md (#6238)
    * refactor: migration_v6.md
    
    - Добавил блок "Содержание". Для это в Styleguide добавил возможность навигироваться по заголовкам.
    - Отформатировал текста.
    - Изменил структуру.
    
    * fix(migration_v6.md): add link to "Интеграция с VK Mini Apps"
    
    * chore: fix typo
    
    * chore: fix typo x2
    
    * fix(migration_v6.md): fix some headers
    
    ---------
    
    Co-authored-by: Victoria Zhizhonkova <indarklight@gmail.com>

--- styleguide/Components/Link/LinkRenderer.js
+++ styleguide/Components/Link/LinkRenderer.js
@@ -1,10 +1,46 @@
 import * as React from 'react';
 import { Link } from '@vkui';
+import { generateIdByReactNode } from '../../utils';
 
-export const LinkRenderer = ({ href: _href, ...restProps }) => {
-  const href = _href?.replace('https://vkcom.github.io/VKUI/', '');
+const resolveHref = (hrefProp, children) => {
+  if (hrefProp) {
+    if (hrefProp === '{{anchor}}') {
+      const id = generateIdByReactNode(children);
+      return {
+        scrollTo() {
+          const el = document.getElementById(id);
+          if (el) {
+            window.scrollTo({ behavior: 'smooth', top: el.offsetTop });
+          }
+        },
+        native: undefined,
+      };
+    }
 
-  return <Link href={href} {...restProps} />;
+    return {
+      scrollTo: null,
+      native: hrefProp.replace('https://vkcom.github.io/VKUI/', ''),
+    };
+  }
+};
+
+export const LinkRenderer = ({ href: hrefProp, onClick, children, ...restProps }) => {
+  const href = resolveHref(hrefProp, children);
+
+  const handleClick = (event) => {
+    if (onClick) {
+      onClick(event);
+    }
+    if (href.scrollTo !== null) {
+      href.scrollTo();
+    }
+  };
+
+  return (
+    <Link href={href.native} onClick={handleClick} {...restProps}>
+      {children}
+    </Link>
+  );
 };
 
 export default LinkRenderer;
