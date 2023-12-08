d53791d5e8dd7ba78207bc9ca8373eb5f572b9fb
Victoria Zhizhonkova <indarklight@gmail.com>
2023-12-08 12:58:10 +0700

    tech: add SimpleGrid component

---
+++ packages/vkui/src/components/SimpleGrid/SimpleGrid.stories.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { CanvasFullLayout, DisableCartesianParam } from '../../storybook/constants';
+import { ContentCard } from '../ContentCard/ContentCard';
+import { SimpleGrid, SimpleGridProps } from './SimpleGrid';
+
+const story: Meta<SimpleGridProps> = {
+  title: 'Layout/SimpleGrid',
+  component: SimpleGrid,
+  parameters: { ...CanvasFullLayout, ...DisableCartesianParam },
+};
+
+export default story;
+
+type Story = StoryObj<SimpleGridProps>;
+
+export const Playground: Story = {
+  args: {
+    gap: 8,
+  },
+  render: (args) => (
+    <SimpleGrid {...args}>
+      {Array.from({ length: 5 }, (_, index) => {
+        return (
+          <ContentCard
+            key={index}
+            subtitle="ALBUM"
+            header="Halsey – Badlands"
+            caption="Blue Vinyl · EU · 2015"
+            text="Badlands is the story about dreams and cruel reality..."
+          />
+        );
+      })}
+    </SimpleGrid>
+  ),
+  decorators: [
+    (Component) => (
+      <div style={{ width: '80%', border: '1px dotted red' }}>
+        <Component />
+      </div>
+    ),
+  ],
+};
---
+++ packages/vkui/src/components/SimpleGrid/SimpleGrid.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import { classNames } from '@vkontakte/vkjs';
+import { calculateGap } from '../../lib/layouts/helpers';
+import type { GridGaps } from '../../lib/layouts/types';
+import { CSSCustomProperties, HTMLAttributesWithRootRef } from '../../types';
+import { RootComponent } from '../RootComponent/RootComponent';
+import styles from './SimpleGrid.module.css';
+
+const marginClassNames = {
+  'auto': styles['SimpleGrid--margin-auto'],
+  'auto-inline': styles['SimpleGrid--margin-auto-inline'],
+  'auto-block': styles['SimpleGrid--margin-auto-block'],
+};
+
+export interface SimpleGridProps extends HTMLAttributesWithRootRef<HTMLDivElement> {
+  /**
+   * Количество колонок
+   */
+  columns?: number;
+  /**
+   * Отступы между элементами
+   * Через массив можно задать отступ между строками и столбцами [row, column]
+   */
+  gap?: GridGaps;
+  /**
+   * Управляет отступами вокруг контейнера
+   * Значение `none` позволяет отключить отступы
+   * Значение `auto` позволяет задать платформенные отступы
+   * Значение `auto-inline` позволяет задать платформенные inline-отступы
+   * Значение `auto-block` позволяет задать платформенные block-отступы
+   */
+  margin?: 'none' | 'auto' | 'auto-inline' | 'auto-block';
+  /**
+   * Вместо задания количества колонок, можно указать минимальную ширину элементов
+   */
+  minColWidth?: number;
+}
+
+export const SimpleGrid = ({
+  columns = 1,
+  gap: gapProp,
+  style: styleProp,
+  margin = 'none',
+  minColWidth,
+  ...props
+}: SimpleGridProps) => {
+  const style: CSSCustomProperties = {};
+  const [rowGap, columnGap] = calculateGap(gapProp);
+  if (typeof rowGap === 'number') {
+    style['--vkui_internal--grid_row_gap'] = `${rowGap}px`;
+  }
+  if (typeof columnGap === 'number') {
+    style['--vkui_internal--grid_column_gap'] = `${columnGap}px`;
+  }
+  style['--vkui_internal--grid_columns'] = `${columns}`;
+  if (minColWidth) {
+    style['--vkui_internal--min_col_width'] = `${minColWidth}px`;
+  }
+
+  return (
+    <RootComponent
+      {...props}
+      baseClassName={classNames(
+        styles.SimpleGrid,
+        margin !== 'none' && marginClassNames[margin],
+        minColWidth && styles['SimpleGrid--with-min-width'],
+      )}
+      style={{ ...styleProp, ...style }}
+    />
+  );
+};
