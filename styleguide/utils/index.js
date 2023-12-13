VKCOM/VKUI

commit 408f22b78ff2d83d6a49df531278b9cc28fc1117

refactor: migration_v6.md (#6238)
* refactor: migration_v6.md

- Добавил блок "Содержание". Для это в Styleguide добавил возможность навигироваться по заголовкам.
- Отформатировал текста.
- Изменил структуру.

* fix(migration_v6.md): add link to "Интеграция с VK Mini Apps"

* chore: fix typo

* chore: fix typo x2

* fix(migration_v6.md): fix some headers

---------

Co-authored-by: Victoria Zhizhonkova <indarklight@gmail.com>

styleguide/utils/index.js
@@ -1,3 +1,6 @@
+import { kebabCase } from './third-party/lodash';
+import { getTextFromChildren } from './third-party/react-children-utils';
+
 export const perfLogger = {
   _isEnabled: localStorage.getItem('vkui:perf-logging') === 'true',
   set isEnabled(value) {
@@ -29,3 +32,11 @@ export const getDeprecatedFromComponentTags = (component) => {
 };
 
 export * from './useViewPortSize';
+
+export const generateIdByReactNode = (children) => {
+  const text = getTextFromChildren(children);
+  if (typeof text === 'string') {
+    return kebabCase(text);
+  }
+  return;
+};

styleguide/utils/third-party/lodash/index.js
@@ -1,0 +1,33 @@
+/* eslint-disable */
+
+/**
+ * Original https://github.com/lodash/lodash/blob/main/src/kebabCase.ts
+ */
+
+import { words } from './words';
+
+/**
+ * Converts `string` to
+ * [kebab case](https://en.wikipedia.org/wiki/Letter_case#Special_case_styles).
+ *
+ * @since 3.0.0
+ * @category String
+ * @param {string} [string=''] The string to convert.
+ * @returns {string} Returns the kebab cased string.
+ * @see camelCase, lowerCase, snakeCase, startCase, upperCase, upperFirst
+ * @example
+ *
+ * kebabCase('Foo Bar')
+ * // => 'foo-bar'
+ *
+ * kebabCase('fooBar')
+ * // => 'foo-bar'
+ *
+ * kebabCase('__FOO_BAR__')
+ * // => 'foo-bar'
+ */
+export const kebabCase = (string) =>
+  words(string.toString().replace(/['\u2019]/g, '')).reduce(
+    (result, word, index) => result + (index ? '-' : '') + word.toLowerCase(),
+    '',
+  );

styleguide/utils/third-party/lodash/unicodeWords.js
@@ -1,0 +1,87 @@
+/* eslint-disable */
+
+/**
+ * Original https://github.com/lodash/lodash/blob/main/src/.internal/unicodeWords.ts
+ */
+
+/** Used to compose unicode character classes. */
+const rsAstralRange = '\\ud800-\\udfff';
+const rsComboMarksRange = '\\u0300-\\u036f';
+const reComboHalfMarksRange = '\\ufe20-\\ufe2f';
+const rsComboSymbolsRange = '\\u20d0-\\u20ff';
+const rsComboMarksExtendedRange = '\\u1ab0-\\u1aff';
+const rsComboMarksSupplementRange = '\\u1dc0-\\u1dff';
+const rsComboRange =
+  rsComboMarksRange +
+  reComboHalfMarksRange +
+  rsComboSymbolsRange +
+  rsComboMarksExtendedRange +
+  rsComboMarksSupplementRange;
+const rsDingbatRange = '\\u2700-\\u27bf';
+const rsLowerRange = 'a-z\\xdf-\\xf6\\xf8-\\xff';
+const rsMathOpRange = '\\xac\\xb1\\xd7\\xf7';
+const rsNonCharRange = '\\x00-\\x2f\\x3a-\\x40\\x5b-\\x60\\x7b-\\xbf';
+const rsPunctuationRange = '\\u2000-\\u206f';
+const rsSpaceRange =
+  ' \\t\\x0b\\f\\xa0\\ufeff\\n\\r\\u2028\\u2029\\u1680\\u180e\\u2000\\u2001\\u2002\\u2003\\u2004\\u2005\\u2006\\u2007\\u2008\\u2009\\u200a\\u202f\\u205f\\u3000';
+const rsUpperRange = 'A-Z\\xc0-\\xd6\\xd8-\\xde';
+const rsVarRange = '\\ufe0e\\ufe0f';
+const rsBreakRange = rsMathOpRange + rsNonCharRange + rsPunctuationRange + rsSpaceRange;
+
+/** Used to compose unicode capture groups. */
+const rsApos = "['\u2019]";
+const rsBreak = `[${rsBreakRange}]`;
+const rsCombo = `[${rsComboRange}]`;
+const rsDigit = '\\d';
+const rsDingbat = `[${rsDingbatRange}]`;
+const rsLower = `[${rsLowerRange}]`;
+const rsMisc = `[^${rsAstralRange}${
+  rsBreakRange + rsDigit + rsDingbatRange + rsLowerRange + rsUpperRange
+}]`;
+const rsFitz = '\\ud83c[\\udffb-\\udfff]';
+const rsModifier = `(?:${rsCombo}|${rsFitz})`;
+const rsNonAstral = `[^${rsAstralRange}]`;
+const rsRegional = '(?:\\ud83c[\\udde6-\\uddff]){2}';
+const rsSurrPair = '[\\ud800-\\udbff][\\udc00-\\udfff]';
+const rsUpper = `[${rsUpperRange}]`;
+const rsZWJ = '\\u200d';
+
+/** Used to compose unicode regexes. */
+const rsMiscLower = `(?:${rsLower}|${rsMisc})`;
+const rsMiscUpper = `(?:${rsUpper}|${rsMisc})`;
+const rsOptContrLower = `(?:${rsApos}(?:d|ll|m|re|s|t|ve))?`;
+const rsOptContrUpper = `(?:${rsApos}(?:D|LL|M|RE|S|T|VE))?`;
+const reOptMod = `${rsModifier}?`;
+const rsOptVar = `[${rsVarRange}]?`;
+const rsOptJoin = `(?:${rsZWJ}(?:${[rsNonAstral, rsRegional, rsSurrPair].join('|')})${
+  rsOptVar + reOptMod
+})*`;
+const rsOrdLower = '\\d*(?:1st|2nd|3rd|(?![123])\\dth)(?=\\b|[A-Z_])';
+const rsOrdUpper = '\\d*(?:1ST|2ND|3RD|(?![123])\\dTH)(?=\\b|[a-z_])';
+const rsSeq = rsOptVar + reOptMod + rsOptJoin;
+const rsEmoji = `(?:${[rsDingbat, rsRegional, rsSurrPair].join('|')})${rsSeq}`;
+
+const reUnicodeWords = RegExp(
+  [
+    `${rsUpper}?${rsLower}+${rsOptContrLower}(?=${[rsBreak, rsUpper, '$'].join('|')})`,
+    `${rsMiscUpper}+${rsOptContrUpper}(?=${[rsBreak, rsUpper + rsMiscLower, '$'].join('|')})`,
+    `${rsUpper}?${rsMiscLower}+${rsOptContrLower}`,
+    `${rsUpper}+${rsOptContrUpper}`,
+    rsOrdUpper,
+    rsOrdLower,
+    `${rsDigit}+`,
+    rsEmoji,
+  ].join('|'),
+  'g',
+);
+
+/**
+ * Splits a Unicode `string` into an array of its words.
+ *
+ * @private
+ * @param {string} The string to inspect.
+ * @returns {Array} Returns the words of `string`.
+ */
+export function unicodeWords(string) {
+  return string.match(reUnicodeWords);
+}

styleguide/utils/third-party/lodash/words.js
@@ -1,0 +1,42 @@
+/* eslint-disable */
+
+/**
+ * Original https://github.com/lodash/lodash/blob/main/src/words.ts
+ */
+import { unicodeWords } from './unicodeWords';
+
+const hasUnicodeWord = RegExp.prototype.test.bind(
+  /[a-z][A-Z]|[A-Z]{2}[a-z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]|[^a-zA-Z0-9 ]/,
+);
+
+/** Used to match words composed of alphanumeric characters. */
+// eslint-disable-next-line no-control-regex
+const reAsciiWord = /[^\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+/g;
+
+function asciiWords(string) {
+  return string.match(reAsciiWord);
+}
+
+/**
+ * Splits `string` into an array of its words.
+ *
+ * @since 3.0.0
+ * @category String
+ * @param {string} [string=''] The string to inspect.
+ * @param {RegExp|string} [pattern] The pattern to match words.
+ * @returns {Array} Returns the words of `string`.
+ * @example
+ *
+ * words('fred, barney, & pebbles')
+ * // => ['fred', 'barney', 'pebbles']
+ *
+ * words('fred, barney, & pebbles', /[^, ]+/g)
+ * // => ['fred', 'barney', '&', 'pebbles']
+ */
+export function words(string, pattern) {
+  if (pattern === undefined) {
+    const result = hasUnicodeWord(string) ? unicodeWords(string) : asciiWords(string);
+    return result || [];
+  }
+  return string.match(pattern) || [];
+}

styleguide/utils/third-party/react-children-utils/index.js
@@ -1,0 +1,40 @@
+/* eslint-disable */
+
+// copied from react-children-utilities
+// @see https://github.com/fernandopasik/react-children-utilities/tree/main
+import { Children, isValidElement } from 'react';
+
+export const childToString = (child) => {
+  if (typeof child === 'undefined' || child === null || typeof child === 'boolean') {
+    return '';
+  }
+
+  if (JSON.stringify(child) === '{}') {
+    return '';
+  }
+
+  return child.toString();
+};
+
+export const getTextFromChildren = (children) => {
+  if (!(children instanceof Array) && !isValidElement(children)) {
+    return childToString(children);
+  }
+
+  return Children.toArray(children).reduce((text, child) => {
+    let newText = '';
+
+    const isValidElementResult = isValidElement(child);
+    const hasChildren = isValidElementResult && 'children' in child.props;
+
+    if (isValidElementResult && hasChildren) {
+      newText = getTextFromChildren(child.props.children);
+    } else if (isValidElementResult && !hasChildren) {
+      newText = '';
+    } else {
+      newText = childToString(child);
+    }
+
+    return text.concat(newText);
+  }, '');
+};

styleguide/Components/Heading/HeadingRenderer.js
@@ -1,24 +1,43 @@
 import * as React from 'react';
 import { classNames, Headline, Title } from '@vkui';
+import { generateIdByReactNode } from '../../utils';
 import './Heading.css';
 
 const HeadingRenderer = ({ level, children, className }) => {
+  const id = generateIdByReactNode(children);
   switch (level) {
     case 1:
       return (
-        <Title className={classNames('Heading', 'Heading--1', className)} level="1">
+        <Title
+          id={id}
+          className={classNames('Heading', 'Heading--1', className)}
+          level="1"
+          Component="h1"
+        >
           {children}
         </Title>
       );
     case 2:
       return (
-        <Title className={classNames('Heading', 'Heading--2', className)} weight="2" level="2">
+        <Title
+          id={id}
+          className={classNames('Heading', 'Heading--2', className)}
+          weight="2"
+          level="2"
+          Component="h2"
+        >
           {children}
         </Title>
       );
     default:
       return (
-        <Headline className={classNames('Heading', 'Heading--3', className)} weight="1" level="1">
+        <Headline
+          id={id}
+          className={classNames('Heading', 'Heading--3', className)}
+          weight="1"
+          level="1"
+          Component="h3"
+        >
           {children}
         </Headline>
       );

styleguide/Components/Link/LinkRenderer.js
@@ -1,10 +1,46 @@
 import * as React from 'react';
 import { Link } from '@vkui';
+import { generateIdByReactNode } from '../../utils';
 
-export const LinkRenderer = ({ href: _href, ...restProps }) => {
-  const href = _href?.replace('https://vkcom.github.io/VKUI/', '');
+const resolveHref = (hrefProp, children) => {
+  if (hrefProp) {
+    if (hrefProp === '{{anchor}}') {
+      const id = generateIdByReactNode(children);
+      return {
+        scrollTo() {
+          const el = document.getElementById(id);
+          if (el) {
+            window.scrollTo({ behavior: 'smooth', top: el.offsetTop });
+          }
+        },
+        native: undefined,
+      };
+    }
 
-  return <Link href={href} {...restProps} />;
+    return {
+      scrollTo: null,
+      native: hrefProp.replace('https://vkcom.github.io/VKUI/', ''),
+    };
+  }
+};
+
+export const LinkRenderer = ({ href: hrefProp, onClick, children, ...restProps }) => {
+  const href = resolveHref(hrefProp, children);
+
+  const handleClick = (event) => {
+    if (onClick) {
+      onClick(event);
+    }
+    if (href.scrollTo !== null) {
+      href.scrollTo();
+    }
+  };
+
+  return (
+    <Link href={href.native} onClick={handleClick} {...restProps}>
+      {children}
+    </Link>
+  );
 };
 
 export default LinkRenderer;

commit 2b93be2a4b05753bd26b500559c3c2493061c110

docs(styleguide): rm third-party folder
Используем `lodash` и `react-children-utilities`.

- caused by #6238

styleguide/utils/index.js
@@ -1,5 +1,5 @@
-import { kebabCase } from './third-party/lodash';
-import { getTextFromChildren } from './third-party/react-children-utils';
+import kebabCase from 'lodash/kebabCase';
+import { onlyText } from 'react-children-utilities';
 
 export const perfLogger = {
   _isEnabled: localStorage.getItem('vkui:perf-logging') === 'true',
@@ -34,7 +34,7 @@ export const getDeprecatedFromComponentTags = (component) => {
 export * from './useViewPortSize';
 
 export const generateIdByReactNode = (children) => {
-  const text = getTextFromChildren(children);
+  const text = onlyText(children);
   if (typeof text === 'string') {
     return kebabCase(text);
   }

styleguide/utils/third-party/lodash/index.js
@@ -1,33 +1,0 @@
-/* eslint-disable */
-
-/**
- * Original https://github.com/lodash/lodash/blob/main/src/kebabCase.ts
- */
-
-import { words } from './words';
-
-/**
- * Converts `string` to
- * [kebab case](https://en.wikipedia.org/wiki/Letter_case#Special_case_styles).
- *
- * @since 3.0.0
- * @category String
- * @param {string} [string=''] The string to convert.
- * @returns {string} Returns the kebab cased string.
- * @see camelCase, lowerCase, snakeCase, startCase, upperCase, upperFirst
- * @example
- *
- * kebabCase('Foo Bar')
- * // => 'foo-bar'
- *
- * kebabCase('fooBar')
- * // => 'foo-bar'
- *
- * kebabCase('__FOO_BAR__')
- * // => 'foo-bar'
- */
-export const kebabCase = (string) =>
-  words(string.toString().replace(/['\u2019]/g, '')).reduce(
-    (result, word, index) => result + (index ? '-' : '') + word.toLowerCase(),
-    '',
-  );

styleguide/utils/third-party/lodash/unicodeWords.js
@@ -1,87 +1,0 @@
-/* eslint-disable */
-
-/**
- * Original https://github.com/lodash/lodash/blob/main/src/.internal/unicodeWords.ts
- */
-
-/** Used to compose unicode character classes. */
-const rsAstralRange = '\\ud800-\\udfff';
-const rsComboMarksRange = '\\u0300-\\u036f';
-const reComboHalfMarksRange = '\\ufe20-\\ufe2f';
-const rsComboSymbolsRange = '\\u20d0-\\u20ff';
-const rsComboMarksExtendedRange = '\\u1ab0-\\u1aff';
-const rsComboMarksSupplementRange = '\\u1dc0-\\u1dff';
-const rsComboRange =
-  rsComboMarksRange +
-  reComboHalfMarksRange +
-  rsComboSymbolsRange +
-  rsComboMarksExtendedRange +
-  rsComboMarksSupplementRange;
-const rsDingbatRange = '\\u2700-\\u27bf';
-const rsLowerRange = 'a-z\\xdf-\\xf6\\xf8-\\xff';
-const rsMathOpRange = '\\xac\\xb1\\xd7\\xf7';
-const rsNonCharRange = '\\x00-\\x2f\\x3a-\\x40\\x5b-\\x60\\x7b-\\xbf';
-const rsPunctuationRange = '\\u2000-\\u206f';
-const rsSpaceRange =
-  ' \\t\\x0b\\f\\xa0\\ufeff\\n\\r\\u2028\\u2029\\u1680\\u180e\\u2000\\u2001\\u2002\\u2003\\u2004\\u2005\\u2006\\u2007\\u2008\\u2009\\u200a\\u202f\\u205f\\u3000';
-const rsUpperRange = 'A-Z\\xc0-\\xd6\\xd8-\\xde';
-const rsVarRange = '\\ufe0e\\ufe0f';
-const rsBreakRange = rsMathOpRange + rsNonCharRange + rsPunctuationRange + rsSpaceRange;
-
-/** Used to compose unicode capture groups. */
-const rsApos = "['\u2019]";
-const rsBreak = `[${rsBreakRange}]`;
-const rsCombo = `[${rsComboRange}]`;
-const rsDigit = '\\d';
-const rsDingbat = `[${rsDingbatRange}]`;
-const rsLower = `[${rsLowerRange}]`;
-const rsMisc = `[^${rsAstralRange}${
-  rsBreakRange + rsDigit + rsDingbatRange + rsLowerRange + rsUpperRange
-}]`;
-const rsFitz = '\\ud83c[\\udffb-\\udfff]';
-const rsModifier = `(?:${rsCombo}|${rsFitz})`;
-const rsNonAstral = `[^${rsAstralRange}]`;
-const rsRegional = '(?:\\ud83c[\\udde6-\\uddff]){2}';
-const rsSurrPair = '[\\ud800-\\udbff][\\udc00-\\udfff]';
-const rsUpper = `[${rsUpperRange}]`;
-const rsZWJ = '\\u200d';
-
-/** Used to compose unicode regexes. */
-const rsMiscLower = `(?:${rsLower}|${rsMisc})`;
-const rsMiscUpper = `(?:${rsUpper}|${rsMisc})`;
-const rsOptContrLower = `(?:${rsApos}(?:d|ll|m|re|s|t|ve))?`;
-const rsOptContrUpper = `(?:${rsApos}(?:D|LL|M|RE|S|T|VE))?`;
-const reOptMod = `${rsModifier}?`;
-const rsOptVar = `[${rsVarRange}]?`;
-const rsOptJoin = `(?:${rsZWJ}(?:${[rsNonAstral, rsRegional, rsSurrPair].join('|')})${
-  rsOptVar + reOptMod
-})*`;
-const rsOrdLower = '\\d*(?:1st|2nd|3rd|(?![123])\\dth)(?=\\b|[A-Z_])';
-const rsOrdUpper = '\\d*(?:1ST|2ND|3RD|(?![123])\\dTH)(?=\\b|[a-z_])';
-const rsSeq = rsOptVar + reOptMod + rsOptJoin;
-const rsEmoji = `(?:${[rsDingbat, rsRegional, rsSurrPair].join('|')})${rsSeq}`;
-
-const reUnicodeWords = RegExp(
-  [
-    `${rsUpper}?${rsLower}+${rsOptContrLower}(?=${[rsBreak, rsUpper, '$'].join('|')})`,
-    `${rsMiscUpper}+${rsOptContrUpper}(?=${[rsBreak, rsUpper + rsMiscLower, '$'].join('|')})`,
-    `${rsUpper}?${rsMiscLower}+${rsOptContrLower}`,
-    `${rsUpper}+${rsOptContrUpper}`,
-    rsOrdUpper,
-    rsOrdLower,
-    `${rsDigit}+`,
-    rsEmoji,
-  ].join('|'),
-  'g',
-);
-
-/**
- * Splits a Unicode `string` into an array of its words.
- *
- * @private
- * @param {string} The string to inspect.
- * @returns {Array} Returns the words of `string`.
- */
-export function unicodeWords(string) {
-  return string.match(reUnicodeWords);
-}

styleguide/utils/third-party/lodash/words.js
@@ -1,42 +1,0 @@
-/* eslint-disable */
-
-/**
- * Original https://github.com/lodash/lodash/blob/main/src/words.ts
- */
-import { unicodeWords } from './unicodeWords';
-
-const hasUnicodeWord = RegExp.prototype.test.bind(
-  /[a-z][A-Z]|[A-Z]{2}[a-z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]|[^a-zA-Z0-9 ]/,
-);
-
-/** Used to match words composed of alphanumeric characters. */
-// eslint-disable-next-line no-control-regex
-const reAsciiWord = /[^\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+/g;
-
-function asciiWords(string) {
-  return string.match(reAsciiWord);
-}
-
-/**
- * Splits `string` into an array of its words.
- *
- * @since 3.0.0
- * @category String
- * @param {string} [string=''] The string to inspect.
- * @param {RegExp|string} [pattern] The pattern to match words.
- * @returns {Array} Returns the words of `string`.
- * @example
- *
- * words('fred, barney, & pebbles')
- * // => ['fred', 'barney', 'pebbles']
- *
- * words('fred, barney, & pebbles', /[^, ]+/g)
- * // => ['fred', 'barney', '&', 'pebbles']
- */
-export function words(string, pattern) {
-  if (pattern === undefined) {
-    const result = hasUnicodeWord(string) ? unicodeWords(string) : asciiWords(string);
-    return result || [];
-  }
-  return string.match(pattern) || [];
-}

styleguide/utils/third-party/react-children-utils/index.js
@@ -1,40 +1,0 @@
-/* eslint-disable */
-
-// copied from react-children-utilities
-// @see https://github.com/fernandopasik/react-children-utilities/tree/main
-import { Children, isValidElement } from 'react';
-
-export const childToString = (child) => {
-  if (typeof child === 'undefined' || child === null || typeof child === 'boolean') {
-    return '';
-  }
-
-  if (JSON.stringify(child) === '{}') {
-    return '';
-  }
-
-  return child.toString();
-};
-
-export const getTextFromChildren = (children) => {
-  if (!(children instanceof Array) && !isValidElement(children)) {
-    return childToString(children);
-  }
-
-  return Children.toArray(children).reduce((text, child) => {
-    let newText = '';
-
-    const isValidElementResult = isValidElement(child);
-    const hasChildren = isValidElementResult && 'children' in child.props;
-
-    if (isValidElementResult && hasChildren) {
-      newText = getTextFromChildren(child.props.children);
-    } else if (isValidElementResult && !hasChildren) {
-      newText = '';
-    } else {
-      newText = childToString(child);
-    }
-
-    return text.concat(newText);
-  }, '');
-};