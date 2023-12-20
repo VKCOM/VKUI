12c0662db26c96e3889059638448fcfcb4d71f8f
Victoria Zhizhonkova <indarklight@gmail.com>
2023-12-20 15:52:01 +0700

    Update docusaurus.config.ts

--- docs-beta/docusaurus.config.ts
+++ docs-beta/docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   url: 'https://vkcom.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/pull/6317/test1/docs-beta/index.html/',
+  baseUrl: '/pull/6317/test1/docs-beta/',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
