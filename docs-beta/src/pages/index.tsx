43550e40ee368f714b99283fa4524e939a7d5590
Victoria Zhizhonkova <indarklight@gmail.com>
2023-12-20 13:12:20 +0700

    Update index.tsx

--- docs-beta/src/pages/index.tsx
+++ docs-beta/src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link className="button button--secondary button--lg" to="/docs/intro">
-            VKUI Tutorial - 3min ⏱️
+            VKUI Tutorial - 2min ⏱️
           </Link>
         </div>
       </div>
