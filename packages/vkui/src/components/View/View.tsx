VKCOM/VKUI

commit 9525dedc812c4d123c60ebd59547e1c3d3f94597

feat(View): increase swipe back zone (#5725)
Увеличил зону срабатывания свайпа назад, тем самым приблизив поведение к нативным приложениям. В частности, как в приложении ВКонтакте на iOS.

> **Note**
>
> Для `ViewInfitnite` повторил всё, что для `View`.

h.2 Побочные изменения

> Спасибо @shevchux и @mendrew за замечания ❤️ Иначе пропустил бы эти кейсы)

Во-первых, немного пошатал тесты.

Во-вторых, оптимизировал код: убрал из зависимостей `useEffect` лишнее; состояние `swipeBackPrevented` теперь храню в `useRef` вместо `useState`.

Во-третьих, поправил следующие кейсы:

h.3 Кейс 1. Элементы с горизонтальным скроллом.

Добавил обнаружение смещения горизонтального скролла (в частности необходимо для `HorizontallScroll`), чтобы предотвращать свайп бек если пользователей взаимодействует с элементами, у которых есть горизонтальный скролл.

Свайп бек включается:
- либо если положение скролла находится в начале, иначе свайп бэк игнорируется. Если мы проскролили к началу, то нам необходимо отжать палец и заново синицировать свайп бэк, чтобы он сработал;
- либо если потянули за край экрана слева (спасибо, @shevchux за предложение).

> Вдохновился поведением у приложения ВКонтакте на iOS.

> Использую состояние `swipeBackPrevented`, чтобы вызывать функцию обнаружения только один раз во время начала жеста. Чтобы получить родителей со скроллом, применяю утилиту [getOverflowAncestors()](https://github.com/floating-ui/floating-ui/blob/%40floating-ui/react-dom%402.0.2/packages/utils/test/getOverflowAncestors.test.ts) из бибилотеки Floating UI.

h.3 Кейс 2. Компонент [Gallery](https://vkcom.github.io/VKUI/5.7.2/#/Gallery).

Добавил `stopPropagation()` на `onDragStart` для `Gallery`, чтобы предотвращать свайп бэк.

---

Добавил в доки `View` (стайлгайд и сторибук) примеры с кейсами 1 и 2.

По замечанию @shevchux, в доке у `CellButton`'ов выставил `stopPropagation={false}`, чтобы не путало пользователей. Иначе свайп бэк блокируется.

packages/vkui/src/components/View/View.tsx
@@ -18,10 +18,12 @@ import { NavTransitionProvider } from '../NavTransitionContext/NavTransitionCont
 import { NavTransitionDirectionProvider } from '../NavTransitionDirectionContext/NavTransitionDirectionContext';
 import { useSplitCol } from '../SplitCol/SplitColContext';
 import { Touch, TouchEvent } from '../Touch/Touch';
-import { swipeBackExcluded } from './utils';
+import {
+  getSwipeBackPredicates,
+  hasHorizontalScrollableElementWithScrolledToLeft,
+  swipeBackExcluded,
+} from './utils';
 import styles from './View.module.css';
-
-const SWIPE_BACK_AREA = 70;
 
 enum SwipeBackResults {
   fail = 1,
@@ -58,25 +60,6 @@ export interface ViewProps extends HTMLAttributesWithRootRef<HTMLElement>, NavId
   history?: string[];
 
   children: React.ReactElement | Iterable<React.ReactElement>;
-}
-
-export interface ViewState {
-  animated: boolean;
-
-  visiblePanels: string[];
-  activePanel: string | null;
-  isBack: boolean | undefined;
-  prevPanel: string | null;
-  nextPanel: string | null;
-
-  swipingBack: boolean;
-  swipeBackStartX: number;
-  swipeBackShift: number;
-  swipeBackNextPanel: string | null;
-  swipeBackPrevPanel: string | null;
-  swipeBackResult: SwipeBackResults | null;
-
-  browserSwipe: boolean;
 }
 
 const warn = warnOnce('View');
@@ -122,8 +105,8 @@ export const View = ({
   const [prevPanel, setPrevPanel] = React.useState<string | null>(null);
   const [nextPanel, setNextPanel] = React.useState<string | null>(null);
 
+  const swipeBackPrevented = React.useRef<boolean>(false);
   const [swipingBack, setSwipingBack] = React.useState<boolean | undefined>(undefined);
-  const [swipeBackPrevented, setSwipeBackPrevented] = React.useState<boolean>(false);
   const [swipeBackStartX, setSwipeBackStartX] = React.useState<number>(0);
   const [swipeBackShift, setSwipeBackShift] = React.useState<number>(0);
   const [swipeBackNextPanel, setSwipeBackNextPanel] = React.useState<string | null>(null);
@@ -152,9 +135,12 @@ export const View = ({
   );
 
   const disableAnimation =
-    configProvider?.transitionMotionEnabled === false ||
-    !splitCol?.animate ||
-    platform === Platform.VKCOM;
+    !configProvider.transitionMotionEnabled || !splitCol.animate || platform === Platform.VKCOM;
+  const iOSSwipeBackSimulationEnabled =
+    !disableAnimation &&
+    platform === Platform.IOS &&
+    configProvider.isWebView &&
+    Boolean(onSwipeBack);
 
   const pickPanel = (id: string | null) => {
     if (id === null) {
@@ -250,37 +236,52 @@ export const View = ({
     [onSwipeBackCancel, onSwipeBackSuccess, swipeBackNextPanel, swipeBackResult],
   );
 
-  const onMoveX = (event: TouchEvent): void => {
-    if (
-      platform !== Platform.IOS ||
-      swipeBackPrevented ||
-      swipeBackExcluded(event) ||
-      disableAnimation
-    ) {
+  const handleTouchMoveXForNativeIOSSwipeBackOrSwipeNext = (event: TouchEvent) => {
+    if (browserSwipe) {
+      return;
+    }
+    const { swipeBackTriggered, viewportStartEdgeTouched, viewportEndEdgeTouched } =
+      getSwipeBackPredicates(event.startX, event.shiftX, window!.innerWidth);
+
+    if ((viewportStartEdgeTouched || viewportEndEdgeTouched) && swipeBackTriggered) {
+      setBrowserSwipe(true);
+    }
+  };
+
+  const handleTouchMoveXForIOSSwipeBackSimulation = (event: TouchEvent) => {
+    if (swipeBackPrevented.current || swipeBackExcluded(event)) {
       return;
     }
 
-    if (!configProvider?.isWebView) {
-      if (
-        (event.startX <= SWIPE_BACK_AREA || event.startX >= window!.innerWidth - SWIPE_BACK_AREA) &&
-        !browserSwipe
-      ) {
-        setBrowserSwipe(true);
+    const { swipedToOpposite, swipeBackTriggered, viewportStartEdgeTouched } =
+      getSwipeBackPredicates(event.startX, event.shiftX, window!.innerWidth);
+
+    if (animated && swipeBackTriggered) {
+      return;
+    }
+
+    if (!swipingBack && history && history.length > 1) {
+      if (swipedToOpposite) {
+        swipeBackPrevented.current = true;
+        return;
       }
 
-      return;
-    }
+      if (!swipeBackTriggered) {
+        return;
+      }
 
-    if (!onSwipeBack || (animated && event.startX <= SWIPE_BACK_AREA)) {
-      return;
-    }
-
-    if (!swipingBack && event.startX <= SWIPE_BACK_AREA && history && history.length > 1) {
+      if (
+        !viewportStartEdgeTouched &&
+        hasHorizontalScrollableElementWithScrolledToLeft(event.originalEvent.target as HTMLElement)
+      ) {
+        swipeBackPrevented.current = true;
+        return;
+      }
       // Начался свайп назад
       if (onSwipeBackStart) {
         const payload = onSwipeBackStart(activePanel);
         if (payload === 'prevent') {
-          setSwipeBackPrevented(true);
+          swipeBackPrevented.current = true;
           return;
         }
       }
@@ -298,46 +299,32 @@ export const View = ({
     }
 
     if (swipingBack) {
-      let swipeBackShift = 0;
       if (event.shiftX < 0) {
-        swipeBackShift = 0;
+        setSwipeBackShift(0);
       } else if (event.shiftX > window!.innerWidth - swipeBackStartX) {
-        swipeBackShift = window!.innerWidth;
+        setSwipeBackShift(window!.innerWidth);
       } else {
-        swipeBackShift = event.shiftX;
+        setSwipeBackShift(event.shiftX);
       }
-      setSwipeBackShift(swipeBackShift);
     }
   };
 
-  const onEnd = React.useCallback(
-    (event: TouchEvent): void => {
-      if (swipingBack) {
-        const speed = (swipeBackShift / event.duration) * 1000;
-        if (swipeBackShift === 0) {
-          onSwipeBackCancel();
-        } else if (swipeBackShift >= (window!.innerWidth ?? 0)) {
-          onSwipeBackSuccess();
-        } else if (speed > 250 || swipeBackStartX + swipeBackShift > window!.innerWidth / 2) {
-          setSwipeBackResult(SwipeBackResults.success);
-        } else {
-          setSwipeBackResult(SwipeBackResults.fail);
-        }
+  const handleTouchEndForIOSSwipeBackSimulation = (event: TouchEvent) => {
+    swipeBackPrevented.current = false;
+
+    if (swipingBack) {
+      const speed = (swipeBackShift / event.duration) * 1000;
+      if (swipeBackShift === 0) {
+        onSwipeBackCancel();
+      } else if (swipeBackShift >= (window!.innerWidth ?? 0)) {
+        onSwipeBackSuccess();
+      } else if (speed > 250 || swipeBackShift >= window!.innerWidth / 2) {
+        setSwipeBackResult(SwipeBackResults.success);
+      } else {
+        setSwipeBackResult(SwipeBackResults.fail);
       }
-      if (swipeBackPrevented) {
-        setSwipeBackPrevented(false);
-      }
-    },
-    [
-      onSwipeBackCancel,
-      onSwipeBackSuccess,
-      swipeBackShift,
-      swipeBackStartX,
-      swipingBack,
-      swipeBackPrevented,
-      window,
-    ],
-  );
+    }
+  };
 
   const calcPanelSwipeStyles = (panelId: string | undefined): React.CSSProperties => {
     if (!canUseDOM || !window) {
@@ -473,8 +460,6 @@ export const View = ({
     disableAnimation,
     document,
     flushTransition,
-    onSwipeBackStart,
-    panels,
     platform,
     prevActivePanel,
     prevBrowserSwipe,
@@ -485,7 +470,6 @@ export const View = ({
     scroll,
     swipeBackNextPanel,
     swipeBackResult,
-    swipingBack,
     swipingBackTransitionEndHandler,
     waitTransitionFinish,
   ]);
@@ -503,8 +487,14 @@ export const View = ({
           disableAnimation && styles['View--no-motion'],
           className,
         )}
-        onMoveX={onMoveX}
-        onEnd={onEnd}
+        onMoveX={
+          iOSSwipeBackSimulationEnabled
+            ? handleTouchMoveXForIOSSwipeBackSimulation
+            : platform === Platform.IOS
+            ? handleTouchMoveXForNativeIOSSwipeBackOrSwipeNext
+            : undefined
+        }
+        onEnd={iOSSwipeBackSimulationEnabled ? handleTouchEndForIOSSwipeBackSimulation : undefined}
       >
         <div className={styles['View__panels']}>
           {panels.map((panel: React.ReactElement) => {

commit 03d7a58f34b8325f1d15da89cffd309a8f5e7615

fix(View): Restore scroll position on swipe back cancel (#6325)
Мы очень хитро восстанавливаем позицию скролла, надеясь на то, что это всегда будет работать в useEffect, который сработает перед обработкой события transition (перед завершением анимации), где окончательно сбросятся все состояния.
Но в случае, если пользователь вернул панель назад, мы просто сбрасываем состояние компонента, отвечающее, за свайп. Логика, отвечающая за восстановление скролла не срабатывает, потому что это не считается как `failure`.

Вот тут мы решаем `swipeBack` был успешным (`success`), был отменён (`fail`), то есть свайп не закончен, либо это вообще не считается свайпом, потому что панель по завершении жеста осталась на той же позиции.
https://github.com/VKCOM/VKUI/blob/a4719b49f887c2584eec6655d72e373e62409c59/packages/vkui/src/components/View/View.tsx#L301-L316
Если это не свайп вовсе (пользователь вернул панель на место), то мы просто сбрасываем состояние свайпа с помощью функции `onSwipeBackCancel`.

Но в такой ситуации не сработает условие для восстановления скролла при отмене свайпа.
https://github.com/VKCOM/VKUI/blob/8dbb1de9855af8c772abcb719848175654e39a8a/src/components/View/View.tsx#L494-L500

- caused by #5725

-- Изменения
Вынес логику по восстановлению скролла при отмене свайпа в отдельный useEffect, потому что изначальный слишком большой.
Смотрю на переменные `prevSwipingBack`, `swipingBack`, чтобы понять был ли всё же свайп, потому что `swipeBackResult` нам ни о чем не скажет, даже если бы мы его устанавливали, то он не был бы тут же очищен в том же рендере из-за вызова `onSwipeBackCancel`. Также проверяю `prevSwipeBackShift`, который равен нулю, то есть смещения после жеста у панели нет, что значит, что пользователь жестом свайпа вернул панель туда откуда взял.

packages/vkui/src/components/View/View.tsx
@@ -113,6 +113,7 @@ export const View = ({
   const prevSwipingBack = usePrevious(swipingBack);
   const prevBrowserSwipe = usePrevious(browserSwipe);
   const prevSwipeBackResult = usePrevious(swipeBackResult);
+  const prevSwipeBackShift = usePrevious(swipeBackShift);
   const prevSwipeBackPrevPanel = usePrevious(swipeBackPrevPanel);
   const prevOnTransition = usePrevious(onTransition);
 
@@ -439,11 +440,6 @@ export const View = ({
       );
     }
 
-    // Если свайп назад отменился (когда пользователь недостаточно сильно свайпнул)
-    if (prevSwipeBackResult === 'fail' && !swipeBackResult && activePanel !== null) {
-      scroll?.scrollTo(0, scrolls.current[activePanel]);
-    }
-
     // Закончился Safari свайп
     if (prevActivePanel !== activePanelProp && browserSwipe) {
       setBrowserSwipe(false);
@@ -475,6 +471,33 @@ export const View = ({
     swipingBackTransitionEndHandler,
     waitTransitionFinish,
   ]);
+
+  React.useEffect(
+    function restoreScrollPositionWhenSwipeBackIsCancelled() {
+      // Если свайп назад отменился (когда пользователь недостаточно сильно свайпнул)
+      const swipeBackCancelledInTheMiddleOfAction =
+        prevSwipeBackResult === 'fail' && !swipeBackResult;
+      const swipeBackCancelledByMovingPanelBackToInitialPoint =
+        prevSwipingBack && !swipingBack && prevSwipeBackShift === 0;
+
+      if (
+        (swipeBackCancelledInTheMiddleOfAction ||
+          swipeBackCancelledByMovingPanelBackToInitialPoint) &&
+        activePanel !== null
+      ) {
+        scroll?.scrollTo(0, scrolls.current[activePanel]);
+      }
+    },
+    [
+      prevSwipeBackResult,
+      swipeBackResult,
+      prevSwipingBack,
+      swipingBack,
+      prevSwipeBackShift,
+      activePanel,
+      scroll,
+    ],
+  );
 
   return (
     <NavViewIdContext.Provider value={id}>

packages/vkui/src/components/View/View.test.tsx
@@ -249,7 +249,8 @@ describe('View', () => {
       expect(document.getElementById('p1')).toBeTruthy();
       expect(document.getElementById('p2')).toBeNull();
     });
-    it('restores scroll after swipeBack', () => {
+
+    it('restores scroll after cancelled swipeBack (mouse up during the move)', () => {
       let y = 101;
       scrollsCache['scroll']['p1'] = 22;
       const [MockScroll, scrollTo] = mockScrollContext(() => y);
@@ -262,7 +263,21 @@ describe('View', () => {
       });
       fireEvent.mouseUp(view);
       rerender(<SwipeBack activePanel="p1" history={['p1']} />);
-      expect(scrollTo).toBeCalledWith(0, 22);
+      expect(scrollTo).toHaveBeenCalledWith(0, 22);
+    });
+
+    it('restores scroll when swipeBack cancelled because user moves panel back to starting point', () => {
+      const currentScrollPosition = 22;
+      const startPosition = { clientX: 0, clientY: 100 };
+      const [MockScroll, scrollTo] = mockScrollContext(() => currentScrollPosition);
+      const { view, rerender, SwipeBack } = setupSwipeBack({ Wrapper: MockScroll });
+      fireEvent.mouseDown(view, startPosition);
+      fireEvent.mouseMove(view, { clientX: SWIPE_BACK_SHIFT_THRESHOLD, clientY: 100 });
+      fireEvent.mouseMove(view, startPosition);
+      fireEvent.mouseUp(view);
+
+      rerender(<SwipeBack activePanel="p2" history={['p2']} />);
+      expect(scrollTo).toHaveBeenCalledWith(0, currentScrollPosition);
     });
 
     describe('horizontal scrollable elements', () => {