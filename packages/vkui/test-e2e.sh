eb443c53c74e6de10003ddcab2c5867d923c02de
Evgenia Polozova <eugpoloz@gmail.com>
2024-01-11 16:12:23 +0300

    ci(e2e): use sharding locally with special thx to @inomdzhon for the bash script! ❤️

---
+++ packages/vkui/test-e2e.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for n in $(echo 1 2 3 4 5 6 7 8 9 10)
+do
+  mkdir -p tmp
+  touch tmp/.env
+  echo "SHARD='$n/10'" >> tmp/.env
+  yarn run -T cross-env SHARD=$n/10 docker compose --env-file tmp/.env up --abort-on-container-exit
+  rm tmp/.env
+done
