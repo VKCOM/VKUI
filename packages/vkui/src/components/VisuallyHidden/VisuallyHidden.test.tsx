7af29b31752199bf14acc9745461b69c090618cd
Andrey Medvedev <andr.medv.spb@gmail.com>
2024-01-15 12:06:36 +0300

    fix(Switch): Show focus in android talkback (#6365)
    Действительно, на Android в Talkback не видно фокуса при фокусировании на инпуте, потому что мы прячем этот инпут с помощью `VisuallyHidden` компонента.
    Как вариант можно убрать использование VisuallyHidden и спрятать компонент позади визульной части.
    
    Изменения: 
    - Добавил модификатор для VisuallyHidden, когда речь идет о `Component=input`. Явно задал размер равный размеру родительского элемента, чтобы выделение визуально было подобно размеру switch. Но изменение затронуло не только Switch, но и другие компоненты, в которых `<VisuallyHidden Component="input" \>`.
    - добавил `role='switch'`
    - добавил `aria-checked`. Так как значение этого аттрибута должно соответствовать значению инпута, то добавил переменную состояния.
    - обновил пример в Storybook, добавив историю с испольованием SimpleCell чтобы как-то учесть пожелания из https://github.com/VKCOM/VKUI/issues/4931

--- packages/vkui/src/components/VisuallyHidden/VisuallyHidden.test.tsx
+++ packages/vkui/src/components/VisuallyHidden/VisuallyHidden.test.tsx
@@ -1,6 +1,22 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { VisuallyHidden } from './VisuallyHidden';
+import styles from './VisuallyHidden.module.css';
 
 describe('VisuallyHidden', () => {
   baselineComponent(VisuallyHidden);
+
+  it('uses modifier to keep screen reader focus for input components', () => {
+    const { rerender } = render(<VisuallyHidden data-testid="visually-hidden" />);
+
+    const element = screen.getByTestId('visually-hidden');
+    expect(element).toHaveClass(styles['VisuallyHidden']);
+    expect(element).not.toHaveClass(styles['VisuallyHidden--focusable-input']);
+
+    rerender(<VisuallyHidden data-testid="visually-hidden" Component="input" />);
+    const inputElement = screen.getByTestId('visually-hidden');
+    expect(inputElement).toHaveClass(styles['VisuallyHidden']);
+    expect(inputElement).toHaveClass(styles['VisuallyHidden--focusable-input']);
+  });
 });
