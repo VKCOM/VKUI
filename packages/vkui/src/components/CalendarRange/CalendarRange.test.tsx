b90d2726db5428c8e8774e3bfd43c7a86538c2f4
Andrey Medvedev <andr.medv.spb@gmail.com>
2024-01-19 11:46:23 +0300

    fix(DateRangeInput): Treat array of nulls as empty value to set start date on click (#6215) (#6404)
    - Правим пример в сторибуке, чтобы DateRangeInput реагировал на изменения дат.
    - исправляем поведение при выборе даты, если value={[null, null]}. В этом случае onChange возвращал всегда [null, null], что бы мы не выбрали в DateRangeInput.
    Временное решение без этого фикса это передавать `undefined`. Будет актуально для v5, если не выпустим там патч.
    - Меняем тип value c Array на Tuple, так как value может содержать только два элемента.

--- packages/vkui/src/components/CalendarRange/CalendarRange.test.tsx
+++ packages/vkui/src/components/CalendarRange/CalendarRange.test.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { CalendarRange } from './CalendarRange';
 
@@ -6,5 +8,18 @@ describe('CalendarRange', () => {
     // TODO [a11y]: "Exceeded timeout of 5000 ms for a test.
     //              Add a timeout value to this test to increase the timeout, if this is a long-running test. See https://jestjs.io/docs/api#testname-fn-timeout."
     a11y: false,
+  });
+
+  it('calls onChange when initial value is [null, null]', () => {
+    const onChangeStub = jest.fn();
+    render(
+      <CalendarRange data-testid="calendar-range" onChange={onChangeStub} value={[null, null]} />,
+    );
+
+    fireEvent.click(screen.getAllByText('6')[0]);
+    expect(onChangeStub).not.toHaveBeenLastCalledWith([null, null]);
+
+    fireEvent.click(screen.getAllByText('6')[1]);
+    expect(onChangeStub).not.toHaveBeenLastCalledWith([null, null]);
   });
 });
