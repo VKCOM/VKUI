3251af9951645e6842d40c87249e5db4e4a53cae
Daniil Suvorov <severecloud@gmail.com>
2024-01-25 20:15:04 +0300

    test(e2e): use name

--- packages/vkui/src/components/Mark/Mark.e2e.tsx
+++ packages/vkui/src/components/Mark/Mark.e2e.tsx
@@ -3,10 +3,7 @@ import { test } from '@vkui-e2e/test';
 import { Mark } from './Mark';
 import { MarkPlayground } from './Mark.e2e-playground';
 
-test(
-  Mark.toString(),
-  async ({ mount, expectScreenshotClippedToContent, componentPlaygroundProps }) => {
-    await mount(<MarkPlayground {...componentPlaygroundProps} />);
-    await expectScreenshotClippedToContent();
-  },
-);
+test(Mark.name, async ({ mount, expectScreenshotClippedToContent, componentPlaygroundProps }) => {
+  await mount(<MarkPlayground {...componentPlaygroundProps} />);
+  await expectScreenshotClippedToContent();
+});
