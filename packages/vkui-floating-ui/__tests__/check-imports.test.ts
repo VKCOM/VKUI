VKCOM/VKUI

commit 0be73a38dce04a6bb864d1dd6cabf31e53339077

build(vkui-floating-ui): fix exports path (#6486)

packages/vkui-floating-ui/index.mjs
@@ -60,6 +60,11 @@ function getPackageJSONTemplate(subPackagePath) {
 }`;
 }
 
+function getOutputPathByBasename(filePath, outputDir) {
+  const filename = path.basename(filePath);
+  return path.join(outputDir, filename);
+}
+
 /**
  * Собираем отдельный файл
  *
@@ -68,8 +73,7 @@ function getPackageJSONTemplate(subPackagePath) {
  * @param {'es6' | 'commonjs'} moduleType тип модуля
  */
 async function build(filePath, outputDir, moduleType) {
-  const filename = path.basename(filePath);
-  const outputFilePath = path.join(outputDir, filename);
+  const outputFilePath = getOutputPathByBasename(filePath, outputDir);
 
   const output = await swc.transformFile(filePath, {
     module: {
@@ -105,11 +109,9 @@ async function build(filePath, outputDir, moduleType) {
 
 async function main() {
   for (const [subPackagePath, subPackageFiles] of FLOATING_UI_SUB_PACKAGES_ENTRIES_MAP) {
-    const {
-      types: subPackageTypesPath,
-      module: subPackageESMBundlePath,
-      default: subPackageUMDBundlePath,
-    } = subPackageFiles;
+    const subPackageTypesPath = path.join(FLOATING_UI_PATH, subPackageFiles.types);
+    const subPackageESMBundlePath = path.join(FLOATING_UI_PATH, subPackageFiles.module);
+    const subPackageUMDBundlePath = path.join(FLOATING_UI_PATH, subPackageFiles.default);
 
     await createFolder(subPackagePath);
 
@@ -118,12 +120,15 @@ async function main() {
     await createFolder(outputPath);
 
     await Promise.all([
-      build(path.join(FLOATING_UI_PATH, subPackageUMDBundlePath), outputPath, 'commonjs'),
-      build(path.join(FLOATING_UI_PATH, subPackageESMBundlePath), outputPath, 'es6'),
+      build(subPackageUMDBundlePath, outputPath, 'commonjs'),
+      build(subPackageESMBundlePath, outputPath, 'es6'),
     ]);
 
     await Promise.all([
-      await fsPromises.writeFile(subPackageTypesPath, getTypeTemplate(subPackagePath)),
+      await fsPromises.writeFile(
+        getOutputPathByBasename(subPackageTypesPath, outputPath),
+        getTypeTemplate(subPackagePath),
+      ),
       await fsPromises.writeFile(
         path.join(subPackagePath, 'package.json'),
         getPackageJSONTemplate(subPackagePath),

commit 680cd5a7d6b79621aa86b57399d18ac50bdfae0c

fix(vkui-floating-ui): refactor build
- [x] Unit-тесты – проверяем корректность импортов, чтобы избежать инцидент #6487 в будущем. В `globalSetup` запускаем сборку, чтобы не падать при "чистой" репе или в CI.

h2. Проблема

Проблема была из-за того, что в #6486 были поправлены пути в `"exports" -> "./utils/dom"`, чтобы сборка их находила в `node_modules/@floating-ui/`, но я не обратил внимание, что в импортах внутри самих модулей пути тоже надо было поправить.

Получилось так, что модуль `utils/dom` лежит тут `packages/floating-ui/utils/dom/dist/floating-ui.utils.dom.*.js`, а другой модуль `packages/floating-ui/dom/dist/floating-ui.utils.dom.*.js` импортит его как `import { } from '../../utils/dom/floating-ui.utils.dom.*.js';`, т.е. без `dist/` в пути. Теперь `utils/dom` без папки `dist/`.

<img width="320" alt="image" src="https://github.com/VKCOM/VKUI/assets/5850354/8b792d47-82f2-47a7-8e5f-33ba6b24c9a8"> <img width="320" alt="image" src="https://github.com/VKCOM/VKUI/assets/5850354/b0ecc526-126d-43ef-98a5-787cec5b25fc">

_До и После_

- caused by #6486

h2. Решение

Так как структура пакета может таким образом шататься, создаём файлы только ориентируясь на те пути, что указаны в поле `"exports"`, а то до этого, что в скрипте мы сами создавали папку `dist/`.

h2. Изменения

- добавил `.eslintrc.json` и `tsconfig.json`, чтобы заработали линты;
- `index.mjs` переименовал в `main.mjs`, теперь он просто возвращает функцию, а выполнением занимается `build.mjs` и **Jest** через поле `globalSetup`.
- в `.gitingore` исключаем конкретные папки;
- в глобальном `.prettieringore` исключаем `*.d.ts`, `*.umd.js`, `*.esm.js` из `packages/vkui-floating-ui/`;

---

h2. Релизы

Для теста зарелизил версию `patch` версию с тегом `alpha`, но не ожидал, что в CI не учитывается создание пререлиза, поэтому вместо `v0.1.4-alpha`, на выходе получил [`v0.1.4`](https://www.npmjs.com/package/@vkontakte/vkui-floating-ui/v/0.1.4) 🌚

.prettierignore
@@ -21,6 +21,9 @@ playwright-report/
 blob-report/
 all-blob-reports/
 
+packages/vkui-floating-ui/**/*.d.ts
+packages/vkui-floating-ui/**/*.esm.js
+packages/vkui-floating-ui/**/*.umd.js
 packages/vkui-floating-ui/**/package.json
 !packages/vkui-floating-ui/package.json
 

packages/vkui-floating-ui/.gitignore
@@ -1,2 +1,5 @@
-**/dist
-**/package.json
+core/
+dom/
+react-dom/
+utils/
+

packages/vkui-floating-ui/build.mjs
@@ -1,0 +1,3 @@
+import main from './main.mjs';
+
+await main();

packages/vkui-floating-ui/index.mjs
@@ -1,140 +1,0 @@
-import * as fs from 'node:fs';
-import * as fsPromises from 'node:fs/promises';
-import * as path from 'node:path';
-import * as url from 'node:url';
-import swc from '@swc/core';
-import pkg from './package.json' assert { type: 'json' };
-
-function removePathFromStart(subPackagePath) {
-  return subPackagePath.replace(/^\.\//, '');
-}
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const __filename = url.fileURLToPath(import.meta.url);
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const __dirname = path.dirname(__filename);
-
-const FLOATING_UI_SUB_PACKAGES_ENTRIES_MAP = Object.entries(pkg.exports).filter(
-  ([subPackagePath]) => !subPackagePath.includes('package.json'),
-);
-
-const FLOATING_UI_SUB_PACKAGES_NAMES_REGEX_GROUP = FLOATING_UI_SUB_PACKAGES_ENTRIES_MAP.map(
-  ([subPackagePath]) => removePathFromStart(subPackagePath),
-)
-  .join('|')
-  .replace('/', '\\/');
-const FLOATING_UI_SUB_PACKAGES_NAME_REGEX = RegExp(
-  `"@floating-ui\\/(${FLOATING_UI_SUB_PACKAGES_NAMES_REGEX_GROUP})"`,
-  'g',
-);
-
-const NODE_MODULES_PATH = path.resolve(__dirname, '../../node_modules');
-const FLOATING_UI_PATH = path.join(NODE_MODULES_PATH, '@floating-ui');
-const OUTPUT_DIR = './dist';
-
-/**
- * Создание папки, если не существует
- *
- * @param {string} folderName путь к папке
- */
-async function createFolder(folderName) {
-  if (!fs.existsSync(folderName)) {
-    await fsPromises.mkdir(folderName);
-  }
-}
-
-function getTypeTemplate(subPackagePath) {
-  const normalizedPath = removePathFromStart(subPackagePath);
-  return `declare module '${pkg.name}/${normalizedPath}' {
-  export * from '@floating-ui/${normalizedPath}';
-}`;
-}
-
-function getPackageJSONTemplate(subPackagePath) {
-  const subPackageName = removePathFromStart(subPackagePath).replaceAll('/', '.');
-  return `{
-  "sideEffects": false,
-  "main": "./dist/floating-ui.${subPackageName}.umd.js",
-  "module": "./dist/floating-ui.${subPackageName}.esm.js",
-  "types": "./dist/floating-ui.${subPackageName}.d.ts"
-}`;
-}
-
-function getOutputPathByBasename(filePath, outputDir) {
-  const filename = path.basename(filePath);
-  return path.join(outputDir, filename);
-}
-
-/**
- * Собираем отдельный файл
- *
- * @param {string} filePath путь к файлу
- * @param {string} outputDir выходная папка
- * @param {'es6' | 'commonjs'} moduleType тип модуля
- */
-async function build(filePath, outputDir, moduleType) {
-  const outputFilePath = getOutputPathByBasename(filePath, outputDir);
-
-  const output = await swc.transformFile(filePath, {
-    module: {
-      type: moduleType,
-    },
-    jsc: {
-      externalHelpers: true,
-      target: 'es5',
-    },
-    cwd: __dirname,
-  });
-
-  const outputCode = output.code.replace(FLOATING_UI_SUB_PACKAGES_NAME_REGEX, (_, name) => {
-    const [, foundFilesName] =
-      FLOATING_UI_SUB_PACKAGES_ENTRIES_MAP.find(([subPackage]) => subPackage.includes(name)) || [];
-
-    if (!foundFilesName) {
-      return;
-    }
-
-    const { module: subPackageESMBundlePath, default: subPackageUMDBundlePath } = foundFilesName;
-
-    const bundleRelativePathsByModuleType = {
-      es6: path.relative(outputDir, subPackageESMBundlePath),
-      commonjs: path.relative(outputDir, subPackageUMDBundlePath),
-    };
-
-    return `'${bundleRelativePathsByModuleType[moduleType]}'`;
-  });
-
-  await fsPromises.writeFile(outputFilePath, outputCode);
-}
-
-async function main() {
-  for (const [subPackagePath, subPackageFiles] of FLOATING_UI_SUB_PACKAGES_ENTRIES_MAP) {
-    const subPackageTypesPath = path.join(FLOATING_UI_PATH, subPackageFiles.types);
-    const subPackageESMBundlePath = path.join(FLOATING_UI_PATH, subPackageFiles.module);
-    const subPackageUMDBundlePath = path.join(FLOATING_UI_PATH, subPackageFiles.default);
-
-    await createFolder(subPackagePath);
-
-    const outputPath = path.join(subPackagePath, OUTPUT_DIR);
-
-    await createFolder(outputPath);
-
-    await Promise.all([
-      build(subPackageUMDBundlePath, outputPath, 'commonjs'),
-      build(subPackageESMBundlePath, outputPath, 'es6'),
-    ]);
-
-    await Promise.all([
-      await fsPromises.writeFile(
-        getOutputPathByBasename(subPackageTypesPath, outputPath),
-        getTypeTemplate(subPackagePath),
-      ),
-      await fsPromises.writeFile(
-        path.join(subPackagePath, 'package.json'),
-        getPackageJSONTemplate(subPackagePath),
-      ),
-    ]);
-  }
-}
-
-await main();

packages/vkui-floating-ui/jest.config.ts
@@ -1,0 +1,11 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  globalSetup: '<rootDir>/main.mjs',
+  transform: {
+    '^.+\\.ts': '@swc/jest',
+  },
+  testEnvironment: 'node',
+};
+
+export default config;

packages/vkui-floating-ui/main.mjs
@@ -1,0 +1,158 @@
+import * as fs from 'node:fs';
+import * as fsPromises from 'node:fs/promises';
+import * as path from 'node:path';
+import * as url from 'node:url';
+import swc from '@swc/core';
+import pkg from './package.json' assert { type: 'json' };
+
+function removePathFromStart(subPackagePath) {
+  return subPackagePath.replace(/^\.\//, '');
+}
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function convertToFloatingNodeModulesPath(relativePathToFile) {
+  const nodeModulesPath = path.resolve(__dirname, '../../node_modules');
+  const floatingUINodeModulesPath = path.join(nodeModulesPath, '@floating-ui');
+  return path.join(floatingUINodeModulesPath, relativePathToFile);
+}
+
+function getTypeTemplate(subPackagePath) {
+  const normalizedPath = removePathFromStart(subPackagePath);
+  return `declare module '${pkg.name}/${normalizedPath}' {
+  export * from '@floating-ui/${normalizedPath}';
+}`;
+}
+
+function getPackageJSONTemplate(subPackageDirPath, subPackageFiles) {
+  return `{
+  "sideEffects": false,
+  "main": "${path.relative(subPackageDirPath, subPackageFiles.default)}",
+  "module": "${path.relative(subPackageDirPath, subPackageFiles.module)}",
+  "types": "${path.relative(subPackageDirPath, subPackageFiles.types)}"
+}`;
+}
+
+/**
+ * Трансформируем в необходимый модуль.
+ *
+ * @param {string} filePath путь к файлу
+ * @param {'es6' | 'commonjs'} moduleType тип модуля
+ */
+async function transformFileTo(filePath, moduleType) {
+  const { code } = await swc.transformFile(filePath, {
+    module: {
+      type: moduleType,
+    },
+    jsc: {
+      externalHelpers: true,
+      target: 'es5',
+    },
+    cwd: __dirname,
+  });
+  return code;
+}
+
+async function getPatchedFloatingUIFile(
+  output,
+  moduleType,
+  subPackageDirPath,
+  floatingUISubPackagesEntriesMap,
+) {
+  const floatingUISubPackagesNames = floatingUISubPackagesEntriesMap
+    .map(([subPackagePath]) => removePathFromStart(subPackagePath))
+    .join('|')
+    .replace('/', '\\/');
+  const floatingUISubPackagesRegExpNames = RegExp(
+    `"@floating-ui\\/(${floatingUISubPackagesNames})"`,
+    'g',
+  );
+  return output.replace(floatingUISubPackagesRegExpNames, (_, name) => {
+    const [, foundFilesName] =
+      floatingUISubPackagesEntriesMap.find(([subPackage]) => subPackage.includes(name)) || [];
+
+    if (!foundFilesName) {
+      return;
+    }
+
+    const { module: subPackageESMBundlePath, default: subPackageUMDBundlePath } = foundFilesName;
+
+    const bundleRelativePathsByModuleType = {
+      es6: path.relative(
+        path.join(__dirname, path.dirname(subPackageDirPath)),
+        path.join(__dirname, subPackageESMBundlePath),
+      ),
+      commonjs: path.relative(
+        path.join(__dirname, path.dirname(subPackageDirPath)),
+        path.join(__dirname, subPackageUMDBundlePath),
+      ),
+    };
+
+    return `'${bundleRelativePathsByModuleType[moduleType]}'`;
+  });
+}
+
+async function createFile(filePath, code) {
+  const dir = path.dirname(filePath);
+  if (!fs.existsSync(dir)) {
+    await fsPromises.mkdir(dir, { recursive: true });
+  }
+  await fsPromises.writeFile(filePath, code);
+}
+
+async function cleanup(floatingUISubPackagesEntriesMap) {
+  await Promise.all(
+    floatingUISubPackagesEntriesMap.map(([subPackageDirPath]) => {
+      if (fs.existsSync(subPackageDirPath)) {
+        return fsPromises.rm(subPackageDirPath, { recursive: true });
+      }
+      return Promise.resolve();
+    }),
+  );
+}
+
+export default async function main() {
+  const floatingUISubPackagesEntriesMap = Object.entries(pkg.exports).filter(
+    ([subPackagePath]) => !subPackagePath.includes('package.json'),
+  );
+
+  await cleanup(floatingUISubPackagesEntriesMap);
+
+  for (const [subPackageDirPath, subPackageFilesPaths] of floatingUISubPackagesEntriesMap) {
+    const subPackagePackageJSON = path.join(subPackageDirPath, 'package.json');
+    const {
+      types: subPackageTypesPath,
+      module: subPackageESMBundlePath,
+      default: subPackageUMDBundlePath,
+    } = subPackageFilesPaths;
+
+    const [commonjsCode, es6Code] = await Promise.all([
+      transformFileTo(convertToFloatingNodeModulesPath(subPackageESMBundlePath), 'es6'),
+      transformFileTo(convertToFloatingNodeModulesPath(subPackageUMDBundlePath), 'commonjs'),
+    ]);
+    const [commonjsPatchedCode, es6PatchedCode] = await Promise.all([
+      getPatchedFloatingUIFile(
+        es6Code,
+        'es6',
+        subPackageESMBundlePath,
+        floatingUISubPackagesEntriesMap,
+      ),
+      getPatchedFloatingUIFile(
+        commonjsCode,
+        'commonjs',
+        subPackageUMDBundlePath,
+        floatingUISubPackagesEntriesMap,
+      ),
+    ]);
+    await Promise.all([
+      createFile(subPackageESMBundlePath, es6PatchedCode),
+      createFile(subPackageUMDBundlePath, commonjsPatchedCode),
+      createFile(subPackageTypesPath, getTypeTemplate(subPackageDirPath)),
+      createFile(
+        subPackagePackageJSON,
+        getPackageJSONTemplate(subPackageDirPath, subPackageFilesPaths),
+      ),
+    ]);
+  }
+}

packages/vkui-floating-ui/__tests__/check-imports.test.ts
@@ -1,0 +1,22 @@
+describe('Check imports', () => {
+  it('should import like UMD module', () => {
+    expect(() => require('../core')).toBeTruthy();
+    expect(() => require('../dom')).toBeTruthy();
+    expect(() => require('../react-dom')).toBeTruthy();
+    expect(() => require('../utils')).toBeTruthy();
+    expect(() => require('../utils/dom')).toBeTruthy();
+  });
+
+  it('should import like ES6 module', async () => {
+    // @ts-expect-error TS2306: не нравится *.d.ts файл, пишет 'File 'core/<path>' is not a module'
+    expect(async () => await import('../core')).toBeTruthy();
+    // @ts-expect-error TS2306: не нравится *.d.ts файл, пишет 'File 'dom/<path>' is not a module'
+    expect(async () => await import('../dom')).toBeTruthy();
+    // @ts-expect-error TS2306: не нравится *.d.ts файл, пишет 'File 'react/<path>' is not a module'
+    expect(async () => await import('../react-dom')).toBeTruthy();
+    // @ts-expect-error TS2306: не нравится *.d.ts файл, пишет 'File 'utils/<path>' is not a module'
+    expect(async () => await import('../utils')).toBeTruthy();
+    // @ts-expect-error TS2306: не нравится *.d.ts файл, пишет 'File 'utils/dom/<path>' is not a module'
+    expect(async () => await import('../utils/dom')).toBeTruthy();
+  });
+});