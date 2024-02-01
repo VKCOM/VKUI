8c6d26367a41cd743b8026bfb0b8ceba513681fb
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2024-02-01 12:35:29 +0300

    fix(codemode): wait tooltip transform first before text-tooltip (#6499)
    Исправляет порядок срабатывания трансформации `transforms/tooltip` и `transforms/text-tooltip` при использовании флага `--all`. До этого сначала срабатывал `transforms/text-tooltip` и заменял `unstable_TextTooltip` на `Tooltip` из-за чего `transforms/tooltip` перетирал `Tooltip` на `OnboardingTooltip`.
    
    Под unit-тесты были сделано следующее:
    
    - Вынес получение пути до **fixtures** в фукнцию `getTestFixturesInputPath`.
    - Экспортирую `applyTransform` из `jscodeshift/dist/testUtils`.
    - В `getAvailableCodemods` добавил фильтрацию и по `'.ts'` расширению.

--- packages/codemods/src/getAvailableCodemods.ts
+++ packages/codemods/src/getAvailableCodemods.ts
@@ -4,12 +4,30 @@ export const TRANSFORM_DIR = `${__dirname}/transforms`;
 
 let CODEMODS: string[] = [];
 
-export default function getAvailableCodemods() {
+const swapElementsOfArray = (a: number, b: number, array: unknown[]) => {
+  if (a === -1 || b === -1) {
+    return;
+  }
+
+  const valueA = array[a];
+  const valueB = array[b];
+
+  array[b] = valueA;
+  array[a] = valueB;
+};
+
+export default function getAvailableCodemods(dirname = TRANSFORM_DIR) {
   if (CODEMODS.length === 0) {
     CODEMODS = fs
-      .readdirSync(TRANSFORM_DIR)
-      .filter((fname) => fname.endsWith('.js'))
+      .readdirSync(dirname)
+      .filter((fname) => fname.endsWith('.js') || fname.endsWith('.ts'))
       .map((fname) => fname.slice(0, -3));
+
+    swapElementsOfArray(
+      CODEMODS.findIndex((fname) => fname === 'text-tooltip'),
+      CODEMODS.findIndex((fname) => fname === 'tooltip'),
+      CODEMODS,
+    );
   }
 
   return CODEMODS;
