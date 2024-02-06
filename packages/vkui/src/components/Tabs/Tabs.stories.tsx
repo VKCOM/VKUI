6b12dafe54cda45982e95eb7f9d8a54d4d9eed67
Daniil Suvorov <severecloud@gmail.com>
2024-02-06 14:28:31 +0300

    docs: tabs swipe example

--- packages/vkui/src/components/Tabs/Tabs.stories.tsx
+++ packages/vkui/src/components/Tabs/Tabs.stories.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { useArgs } from '@storybook/preview-api';
 import { Meta, StoryObj } from '@storybook/react';
+import { HTMLAttributesWithRootRef } from 'src/types';
 import { withSinglePanel } from '../../storybook/VKUIDecorators';
 import { CanvasFullLayout, DisableCartesianParam } from '../../storybook/constants';
+import { Div } from '../Div/Div';
 import { Group } from '../Group/Group';
 import { HorizontalScroll } from '../HorizontalScroll/HorizontalScroll';
 import { TabsItem } from '../TabsItem/TabsItem';
@@ -121,6 +123,133 @@ export const WithHorizontalScroll: Story = {
   decorators: [
     (Component) => (
       <Group style={{ maxWidth: 500 }}>
+        <Component />
+      </Group>
+    ),
+    withSinglePanel,
+  ],
+};
+
+interface TabsContentProps extends HTMLAttributesWithRootRef<HTMLDivElement> {
+  onChangePosition?(e: number): void;
+}
+
+const TabsContent = ({ getRootRef, onChangePosition, ...props }: TabsContentProps) => {
+  const scroll: React.UIEventHandler<HTMLDivElement> = (e) => {
+    const newValue = +(Math.ceil(e.currentTarget.scrollLeft) / e.currentTarget.clientWidth).toFixed(
+      2,
+    );
+    onChangePosition?.(newValue);
+  };
+
+  return (
+    <div
+      ref={getRootRef}
+      onScroll={scroll}
+      style={{
+        display: 'flex',
+        scrollSnapType: 'x mandatory',
+        overflowX: 'scroll',
+        scrollbarWidth: 'none',
+      }}
+      {...props}
+    />
+  );
+};
+
+const TabsContentItem = (props: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    style={{
+      width: '100%',
+      flex: '0 0 auto',
+      scrollSnapAlign: 'start',
+    }}
+    {...props}
+  />
+);
+
+interface IndicatorProps {
+  position?: number;
+}
+
+const Indicator = ({ position = 0 }: IndicatorProps) => {
+  return (
+    <div
+      style={{
+        backgroundColor: 'var(--vkui--color_background_accent)',
+        borderRadius: 'inherit',
+        bottom: '0',
+        left: '0',
+        height: 2,
+        position: 'absolute',
+        transformOrigin: '0 0',
+        transition: 'transform .05s',
+        width: `${100 / 5}%`,
+        transform: `translateX(${100 * position}%)`,
+      }}
+    />
+  );
+};
+
+TabsContent.Item = TabsContentItem;
+
+function SwipeApp() {
+  const [value, setValue] = React.useState(0);
+  const rowRef = React.useRef<HTMLDivElement | null>(null);
+
+  const setTab = (tabNumber: number) => {
+    if (!rowRef.current) {
+      return;
+    }
+
+    rowRef.current.scrollTo({
+      left: tabNumber * rowRef.current.clientWidth,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <>
+      <Tabs>
+        <HorizontalScroll arrowSize="m">
+          <div style={{ display: 'flex', position: 'relative' }}>
+            <Indicator position={value} />
+            <TabsItem onClick={() => setTab(0)}>Сообщества</TabsItem>
+            <TabsItem onClick={() => setTab(1)}>Новости</TabsItem>
+            <TabsItem onClick={() => setTab(2)}>Рекомендации</TabsItem>
+            <TabsItem onClick={() => setTab(3)}>Друзья</TabsItem>
+            <TabsItem onClick={() => setTab(4)}>Фотографии</TabsItem>
+          </div>
+        </HorizontalScroll>
+      </Tabs>
+      <TabsContent getRootRef={rowRef} onChangePosition={setValue}>
+        <TabsContent.Item>
+          <Div>Сообщества</Div>
+        </TabsContent.Item>
+        <TabsContent.Item>
+          <Div>Новости</Div>
+        </TabsContent.Item>
+        <TabsContent.Item>
+          <Div>Рекомендации</Div>
+        </TabsContent.Item>
+        <TabsContent.Item>
+          <Div>Друзья</Div>
+        </TabsContent.Item>
+        <TabsContent.Item>
+          <Div>Фотографии</Div>
+        </TabsContent.Item>
+      </TabsContent>
+    </>
+  );
+}
+
+export const PlaygroundSwipe: Story = {
+  render: function Render() {
+    return <SwipeApp />;
+  },
+  decorators: [
+    (Component) => (
+      <Group>
         <Component />
       </Group>
     ),
