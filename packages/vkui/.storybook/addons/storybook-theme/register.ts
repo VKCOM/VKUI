0bcd1bc8f71af09fb454632ee078c82f163c2602
Victoria Zhizhonkova <indarklight@gmail.com>
2024-02-09 18:04:38 +0700

    chore: add storybook dark theme

---
+++ packages/vkui/.storybook/addons/storybook-theme/register.ts
@@ -0,0 +1,18 @@
+import { addons, types } from '@storybook/manager-api';
+import { StorybookTheme, getLocalStorageValue } from './StorybookTheme';
+import { vkuiDarkTheme, vkuiLightTheme } from './vkuiThemes';
+import { ADDON_ID } from './constants';
+
+const prefersDark = window.matchMedia?.('(prefers-color-scheme: dark)');
+const currentTheme = getLocalStorageValue() || (prefersDark.matches && 'dark') || 'light';
+
+addons.setConfig({
+  theme: currentTheme === 'dark' ? vkuiDarkTheme : vkuiLightTheme,
+});
+
+addons.register(ADDON_ID, () => {
+  addons.add(ADDON_ID, {
+    type: types.experimental_SIDEBAR_TOP,
+    render: StorybookTheme,
+  });
+});
---
+++ packages/vkui/.storybook/addons/storybook-theme/vkuiThemes.ts
@@ -0,0 +1,22 @@
+import { create } from '@storybook/theming';
+import darkLogo from './vkui-logo-dark.svg';
+import lightLogo from './vkui-logo-light.svg';
+
+const commonProps = {
+  brandTitle: 'VKUI Storybook',
+  brandUrl: 'https://vkcom.github.io/VKUI',
+  brandTarget: '_blank',
+};
+
+export const vkuiDarkTheme = create({
+  base: 'dark',
+  brandImage: lightLogo,
+  appPreviewBg: '#000',
+  ...commonProps,
+});
+
+export const vkuiLightTheme = create({
+  base: 'light',
+  brandImage: darkLogo,
+  ...commonProps,
+});
