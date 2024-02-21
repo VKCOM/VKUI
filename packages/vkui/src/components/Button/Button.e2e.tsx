591d5c1fa974758f2990d3e239e1e8fe07d5676a
Daniil Suvorov <severecloud@gmail.com>
2024-02-21 15:10:05 +0300

    test(e2e): Button add state Добавил скриншотных тестов, которые покрывают весь css для кнопок
    - hover и active состояния
    - адаптивность для singleIcon

--- packages/vkui/src/components/Button/Button.e2e.tsx
+++ packages/vkui/src/components/Button/Button.e2e.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { test } from '@vkui-e2e/test';
+import { ViewWidth } from '../../lib/adaptivity';
 import {
   ButtonPlayground,
   ButtonWithCounterPlayground,
   ButtonWithPaddingsPlayground,
+  ButtonWithStatePlayground,
 } from './Button.e2e-playground';
 
 test('Button', async ({ mount, expectScreenshotClippedToContent, componentPlaygroundProps }) => {
@@ -26,6 +28,18 @@ test.describe('Button', () => {
     componentPlaygroundProps,
   }) => {
     await mount(<ButtonWithPaddingsPlayground {...componentPlaygroundProps} />);
+    await expectScreenshotClippedToContent();
+  });
+});
+
+test.describe('Button', () => {
+  test.use({
+    adaptivityProviderProps: {
+      viewWidth: ViewWidth.SMALL_TABLET,
+    },
+  });
+  test('state', async ({ mount, expectScreenshotClippedToContent, componentPlaygroundProps }) => {
+    await mount(<ButtonWithStatePlayground {...componentPlaygroundProps} />);
     await expectScreenshotClippedToContent();
   });
 });
