c15a26b815e619a93f5c40146ff71dc9cb39ddf3
Victoria Zhizhonkova <indarklight@gmail.com>
2024-03-29 16:10:54 +0700

    chore: fix typo

--- packages/vkui/src/components/HorizontalCell/HorizontalCell.e2e.tsx
+++ packages/vkui/src/components/HorizontalCell/HorizontalCell.e2e.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { test } from '@vkui-e2e/test';
-import { HorizontalCellPlayground } from './HorizontalCell.e2e-plyaground';
+import { HorizontalCellPlayground } from './HorizontalCell.e2e-playground';
 
 test('HorizontalCell', async ({
   mount,
