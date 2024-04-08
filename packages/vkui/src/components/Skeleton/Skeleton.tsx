461495b817197f5c8d4c0902a3357e968ba3f7aa
Daniil Suvorov <severecloud@gmail.com>
2024-04-08 13:42:31 +0300

    fix: rm js

--- packages/vkui/src/components/Skeleton/Skeleton.tsx
+++ packages/vkui/src/components/Skeleton/Skeleton.tsx
@@ -1,103 +1,9 @@
 import * as React from 'react';
 import { classNames } from '@vkontakte/vkjs';
 import { useExternRef } from '../../hooks/useExternRef';
-import { useGlobalEventListener } from '../../hooks/useGlobalEventListener';
-import { usePrevious } from '../../hooks/usePrevious';
-import { useDOM } from '../../lib/dom';
 import { CSSCustomProperties, HTMLAttributesWithRootRef } from '../../types';
 import { RootComponent } from '../RootComponent/RootComponent';
 import styles from './Skeleton.module.css';
-
-const ANIMATION_DURATION = 1500;
-const CUSTOM_PROPERTY_GRADIENT_LEFT = '--vkui_internal--skeleton_gradient_left';
-
-/**
- * Синхронизирует анимацию скелетонов с помощью временных отрезков
- *
- * ## visibilitychange
- *
- * В синхронизацию не заложен механизм перехода на оптимизации браузеров при
- * переходе на другую вкладку, поскольку нет уверенности в реальности таких
- * кейсов со скелетонами. Если такой кейс принесут, необходимо обработать
- * событие `visibilitychange` используя функцию `syncAnimation`
- *
- * https://developer.chrome.com/blog/page-lifecycle-api/
- */
-function useSkeletonSyncAnimation(
-  disableAnimation: boolean,
-  rootRef: React.MutableRefObject<HTMLElement | null>,
-) {
-  const isAnimationStarted = React.useRef<boolean>(false);
-  const timer = React.useRef<NodeJS.Timeout | undefined>(undefined);
-
-  /* eslint-disable no-restricted-properties -- используем класс из css modules*/
-  const syncAnimation = React.useCallback(() => {
-    const el = rootRef.current;
-    if (el === null) {
-      return;
-    }
-
-    clearTimeout(timer.current);
-    el.classList.add(styles['Skeleton--disableAnimation']);
-    isAnimationStarted.current = false;
-
-    const delay = ANIMATION_DURATION - (performance.now() % ANIMATION_DURATION);
-
-    timer.current = setTimeout(() => {
-      if (rootRef.current === null) {
-        return;
-      }
-
-      rootRef.current.classList.remove(styles['Skeleton--disableAnimation']);
-      isAnimationStarted.current = true;
-    }, delay);
-
-    return () => clearTimeout(timer.current);
-  }, [rootRef]);
-  /* eslint-enable no-restricted-properties*/
-
-  React.useEffect(() => {
-    if (disableAnimation) {
-      isAnimationStarted.current = false;
-      return;
-    }
-
-    if (isAnimationStarted.current) {
-      return;
-    }
-
-    return syncAnimation();
-  }, [disableAnimation, syncAnimation]);
-
-  return isAnimationStarted.current;
-}
-
-/**
- * Вычисляет позицию скелетона
- */
-function useSkeletonPosition(rootRef: React.MutableRefObject<HTMLElement | null>) {
-  const { document, window } = useDOM();
-  const [skeletonGradientLeft, setSkeletonGradientLeft] = React.useState('0');
-  const prevSkeletonGradientLeft = usePrevious(skeletonGradientLeft);
-
-  const updatePosition = React.useCallback(() => {
-    const el = rootRef.current;
-    if (!el || !document) {
-      return;
-    }
-
-    const value = -(el.getBoundingClientRect().left - document.body.getBoundingClientRect().left);
-    const gradientValue = value === 0 ? '0' : `${value}px`;
-    if (prevSkeletonGradientLeft !== gradientValue) {
-      setSkeletonGradientLeft(gradientValue);
-    }
-  }, [document, prevSkeletonGradientLeft, rootRef]);
-
-  React.useEffect(updatePosition, [updatePosition]);
-  useGlobalEventListener(window, 'resize', updatePosition);
-
-  return skeletonGradientLeft;
-}
 
 export interface SkeletonProps extends HTMLAttributesWithRootRef<HTMLDivElement | HTMLSpanElement> {
   width?: React.CSSProperties['width'];
@@ -159,9 +65,6 @@ export const Skeleton = ({
 }: SkeletonProps) => {
   const rootRef = useExternRef(getRootRef);
 
-  const disableAnimation = !useSkeletonSyncAnimation(still, rootRef);
-  const skeletonGradientLeft = useSkeletonPosition(rootRef);
-
   const skeletonStyle: React.CSSProperties & CSSCustomProperties = {
     width,
     height,
@@ -170,7 +73,6 @@ export const Skeleton = ({
     maxWidth,
     maxInlineSize,
     borderRadius: circle ? '50%' : borderRadius,
-    [CUSTOM_PROPERTY_GRADIENT_LEFT]: skeletonGradientLeft,
   };
 
   if (colorFrom) {
@@ -183,11 +85,8 @@ export const Skeleton = ({
 
   return (
     <RootComponent
-      Component={children ? 'div' : 'span'}
-      baseClassName={classNames(
-        styles['Skeleton'],
-        disableAnimation && styles['Skeleton--disableAnimation'],
-      )}
+      Component="span"
+      baseClassName={classNames(styles['Skeleton'], still && styles['Skeleton--disableAnimation'])}
       getRootRef={rootRef}
       style={{ ...skeletonStyle, ...style }}
       {...restProps}
