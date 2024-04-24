fb95c40d2db4cdf229a6c12165697bffbdf4e972
Andrey Medvedev <andr.medv.spb@gmail.com>
2024-04-24 14:50:02 +0300

    Make a snapshot of an issue in the playground

---
+++ packages/vkui/src/components/ImageBase/ImageBase.e2e.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { test } from '@vkui-e2e/test';
+import { Platform } from '../../lib/platform';
+import { ImageWithParentWithBorderRadius } from './ImageBase.e2e-playground';
+
+test.describe('Image', () => {
+  test.use({
+    onlyForPlatforms: [Platform.ANDROID],
+    onlyForAppearances: ['light'],
+  });
+
+  test('Parent with border-radius: Image does not have visible corners from Image background', async ({
+    mount,
+    expectScreenshotClippedToContent,
+    componentPlaygroundProps,
+  }) => {
+    // мы проверяем, что изображение из img полностью перекрывае фон BaseImage
+    // потому что есть неприятный эффект, где видно край фона в одном из углов обрезанных с помощью border-radius и overflow: hidden у родителя
+    await mount(<ImageWithParentWithBorderRadius {...componentPlaygroundProps} />);
+    await expectScreenshotClippedToContent();
+  });
+});
