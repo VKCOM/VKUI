a8dd31a752be17b8887cb66959bb38001f925628
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2024-05-13 15:50:44 +0300

    e2e(Link): add icon

--- packages/vkui/src/components/Link/Link.e2e-playground.tsx
+++ packages/vkui/src/components/Link/Link.e2e-playground.tsx
@@ -1,3 +1,4 @@
+import { Icon24ExternalLinkOutline } from '@vkontakte/icons';
 import { ComponentPlayground, type ComponentPlaygroundProps } from '@vkui-e2e/playground-helpers';
 import { Link, type LinkProps } from './Link';
 
@@ -5,9 +6,10 @@ export const LinkFocusVisiblePlayground = (props: ComponentPlaygroundProps) => (
   <ComponentPlayground {...props}>
     {(props: LinkProps) => (
       <div style={{ width: 300, padding: 10 }}>
-        Нажимая «Продолжить», вы принимаете&nbsp;
+        Нажимая «Продолжить», вы принимаете{' '}
         <Link href="#" {...props}>
-          пользовательское соглашение
+          пользовательское соглашение&nbsp;
+          <Icon24ExternalLinkOutline width={16} height={16} />
         </Link>
         ...
       </div>
