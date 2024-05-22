404c88f3abc233061d94ec688d6d37ca9d963702
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2024-05-22 15:38:18 +0300

    feat(Switch): use m3 redesign

--- packages/vkui/src/components/Switch/Switch.stories.tsx
+++ packages/vkui/src/components/Switch/Switch.stories.tsx
@@ -7,6 +7,9 @@ const story: Meta<SwitchProps> = {
   title: 'Blocks/Switch',
   component: Switch,
   parameters: { ...CanvasFullLayout, ...DisableCartesianParam },
+  args: {
+    name: 'switch',
+  },
   argTypes: {
     disabled: {
       control: 'boolean',
@@ -23,7 +26,7 @@ export const Playground: Story = {};
 export const WithSimpleCellLabel: Story = {
   render: function Render(args) {
     return (
-      <SimpleCell Component="label" after={<Switch {...args} />}>
+      <SimpleCell Component="label" after={<Switch name="switch" {...args} />}>
         Комментарии к записям
       </SimpleCell>
     );
