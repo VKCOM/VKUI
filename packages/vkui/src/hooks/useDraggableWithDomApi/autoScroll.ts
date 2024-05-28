VKCOM/VKUI

commit 43e5c5c9779d2f52a785b98d80e82c7677cd8006

feat(Cell): add auto scroll for draggable mode (#5833)
* tech: extend e2e test infra

- Импортиурем файл с утилитами
- Добавляем запуск для конткретных движков

* feat(Cell): add auto scroll for draggable mode

* fix(review): reword checkIfElementIsInsideYEdgesOfViewport args

* fix(review): refactor useDraggable

* chore: add tests

* chore: add comments with references

* refactor: revert now() moving to lib/date

* review: mv condition for icon to var

* review: extend dom.test.ts

packages/vkui/src/hooks/useDraggableWithDomApi/autoScroll.ts
@@ -1,0 +1,75 @@
+import { getNodeScroll, getScrollHeight, getScrollRect } from '../../lib/dom';
+import { rafSchd } from '../../lib/rafSchd';
+
+const SCROLL_SPEED = 10;
+export const EDGE_SIZE = 50;
+export const OUTBOX_OFFSET = -30;
+
+export const getAutoScrollingData = (clientY: number, scrollEl: Element | Window) => {
+  const scrollTop = Math.floor(getNodeScroll(scrollEl).scrollTop);
+
+  const { relative, edges } = getScrollRect(scrollEl);
+  const viewportHeight = relative.height;
+  const documentHeight = getScrollHeight(scrollEl);
+  const maxScrollY = documentHeight - viewportHeight;
+  const canScrollUp = scrollTop > 0;
+  const canScrollDown = scrollTop < maxScrollY;
+
+  const [edgeTop, edgeBottom] = edges.y;
+  const topDistance = clientY - edgeTop;
+  const bottomDistance = edgeBottom - clientY;
+  const isInTopEdge = topDistance <= EDGE_SIZE;
+  const isInBottomEdge = bottomDistance <= EDGE_SIZE;
+
+  const result = {
+    shouldScrolling:
+      (canScrollUp && isInTopEdge && topDistance >= OUTBOX_OFFSET) ||
+      (canScrollDown && isInBottomEdge && bottomDistance >= OUTBOX_OFFSET),
+    y: 0,
+  };
+
+  // Inspired by https://github.com/SortableJS/Sortable/issues/1907#issuecomment-1495403785
+  if (isInTopEdge) {
+    result.y = -1 * ((EDGE_SIZE - topDistance) / EDGE_SIZE) * SCROLL_SPEED;
+  } else if (isInBottomEdge) {
+    result.y = ((EDGE_SIZE - bottomDistance) / EDGE_SIZE) * SCROLL_SPEED;
+  }
+
+  return result;
+};
+
+export type AutoScrollingDataFn = () => { shouldScrolling: boolean; y: number };
+
+export const createAutoScrollController = (scrollEl: Element | Window) => {
+  let isRunning = false;
+  const scheduledScroll = rafSchd(scroll);
+
+  function scroll(fn: AutoScrollingDataFn) {
+    const { shouldScrolling, y } = fn();
+    if (shouldScrolling) {
+      isRunning = true;
+      scrollEl.scrollBy(0, y);
+      scheduledScroll(fn);
+    } else {
+      isRunning = false;
+      scheduledScroll.cancel();
+    }
+  }
+
+  const tryAutoScroll = (fn: AutoScrollingDataFn) => {
+    scheduledScroll(fn);
+  };
+
+  const stop = () => {
+    isRunning = false;
+    scheduledScroll.cancel();
+  };
+
+  return {
+    tryAutoScroll,
+    stop,
+    get isRunning() {
+      return isRunning;
+    },
+  };
+};

packages/vkui/src/hooks/useDraggableWithDomApi/autoScroll.test.ts
@@ -1,0 +1,148 @@
+import { requestAnimationFrameMock } from '../../testing/utils';
+import {
+  createAutoScrollController,
+  EDGE_SIZE,
+  getAutoScrollingData,
+  OUTBOX_OFFSET,
+} from './autoScroll';
+
+const VIEWPORT_WIDTH = 1280;
+const VIEWPORT_HEIGHT = 768;
+const SCROLL_HEIGHT = 2000;
+const MAX_SCROLL_Y = SCROLL_HEIGHT - VIEWPORT_HEIGHT;
+
+const setScrollTop = (scrollEl: HTMLElement, scrollTop: number) => {
+  window.scrollY = scrollEl.scrollTop = scrollTop;
+  scrollEl.getBoundingClientRect = jest.fn(
+    () => new DOMRect(0, scrollTop > 0 ? -1 * scrollTop : 0, VIEWPORT_WIDTH, VIEWPORT_HEIGHT),
+  );
+};
+
+const initScrollElData = (scrollEl: HTMLElement, scrollTop: number) => {
+  setScrollTop(scrollEl, scrollTop);
+
+  jest.spyOn(scrollEl, 'scrollHeight', 'get').mockImplementation(() => SCROLL_HEIGHT);
+
+  Object.defineProperty(scrollEl, 'scrollBy', {
+    value: (_: number, y: number) => {
+      setScrollTop(scrollEl, scrollTop + y);
+    },
+    writable: false,
+  });
+};
+
+describe('getAutoScrollingData', () => {
+  let scrollEl = document.createElement('div');
+
+  describe('top edge', () => {
+    beforeEach(() => {
+      scrollEl = document.createElement('div');
+    });
+
+    it('should be falsy for scrollTop is top', () => {
+      initScrollElData(scrollEl, 0);
+      const { shouldScrolling } = getAutoScrollingData(0, scrollEl);
+      expect(shouldScrolling).toBeFalsy();
+    });
+
+    test.each([
+      [0, 1],
+      [EDGE_SIZE - 1, 1],
+      [OUTBOX_OFFSET, 1],
+      [OUTBOX_OFFSET + 10, 1],
+    ])('should be truthy for scrollTop is not on top', (clientY, scrollTop) => {
+      initScrollElData(scrollEl, scrollTop);
+      const { shouldScrolling } = getAutoScrollingData(clientY, scrollEl);
+      expect(shouldScrolling).toBeTruthy();
+    });
+
+    test.each([
+      [EDGE_SIZE + 1, 1],
+      [OUTBOX_OFFSET - 1, 1],
+    ])(
+      `should be falsy for clientY (%i) is not equal edge size (${EDGE_SIZE}) or outbox size (${OUTBOX_OFFSET})`,
+      (clientY, scrollTop) => {
+        initScrollElData(scrollEl, scrollTop);
+        const { shouldScrolling } = getAutoScrollingData(clientY, scrollEl);
+        expect(shouldScrolling).toBeFalsy();
+      },
+    );
+  });
+
+  describe('bottom edge', () => {
+    beforeEach(() => {
+      scrollEl = document.createElement('div');
+    });
+
+    it('should be falsy for scrollTop is bigger than max scroll y', () => {
+      initScrollElData(scrollEl, SCROLL_HEIGHT - VIEWPORT_HEIGHT);
+      const { shouldScrolling } = getAutoScrollingData(900, scrollEl);
+      expect(shouldScrolling).toBeFalsy();
+    });
+
+    test.each([
+      [VIEWPORT_HEIGHT - EDGE_SIZE - 1, 0],
+      [VIEWPORT_HEIGHT + -1 * OUTBOX_OFFSET + 1, 0],
+    ])(
+      `should be falsy for clientY (%i) is not equal edge size (${EDGE_SIZE}) or outbox size (${OUTBOX_OFFSET})`,
+      (clientY, scrollTop) => {
+        initScrollElData(scrollEl, scrollTop);
+        const { shouldScrolling } = getAutoScrollingData(clientY, scrollEl);
+        expect(shouldScrolling).toBeFalsy();
+      },
+    );
+  });
+});
+
+const getInitialAutoScrollData = (initialScrollTop: number) => {
+  const scrollEl = document.createElement('div');
+  initScrollElData(scrollEl, initialScrollTop);
+  return { scrollEl, controller: createAutoScrollController(scrollEl) };
+};
+
+describe('createAutoScrollController', () => {
+  beforeEach(() => {
+    requestAnimationFrameMock.init();
+  });
+  const SCROLL_TOP_OFFSET = 2;
+  it('should scroll to top', async () => {
+    const { scrollEl, controller } = getInitialAutoScrollData(SCROLL_TOP_OFFSET);
+
+    controller.tryAutoScroll(() => getAutoScrollingData(0, scrollEl));
+    requestAnimationFrameMock.triggerNextAnimationFrame();
+    expect(controller.isRunning).toBeTruthy();
+    requestAnimationFrameMock.triggerNextAnimationFrame();
+    expect(controller.isRunning).toBeFalsy();
+    expect(scrollEl.scrollTop).toBeLessThanOrEqual(0);
+  });
+
+  const SCROLL_BOTTOM_OFFSET = MAX_SCROLL_Y - 2;
+  it('should scroll to bottom', async () => {
+    const { scrollEl, controller } = getInitialAutoScrollData(SCROLL_BOTTOM_OFFSET);
+
+    controller.tryAutoScroll(() => getAutoScrollingData(VIEWPORT_HEIGHT, scrollEl));
+
+    requestAnimationFrameMock.triggerNextAnimationFrame();
+    expect(controller.isRunning).toBeTruthy();
+
+    requestAnimationFrameMock.triggerNextAnimationFrame();
+    expect(controller.isRunning).toBeFalsy();
+
+    expect(scrollEl.scrollTop).toBeGreaterThanOrEqual(MAX_SCROLL_Y);
+  });
+
+  it('should stop scroll', async () => {
+    const { scrollEl, controller } = getInitialAutoScrollData(100);
+
+    controller.tryAutoScroll(() => getAutoScrollingData(0, scrollEl));
+
+    requestAnimationFrameMock.triggerNextAnimationFrame();
+    expect(controller.isRunning).toBeTruthy();
+
+    controller.stop();
+    requestAnimationFrameMock.triggerAllAnimationFrames();
+    expect(controller.isRunning).toBeFalsy();
+
+    expect(scrollEl.scrollTop).toBeLessThanOrEqual(90);
+  });
+});

packages/vkui/jest.setup.js
@@ -1,1 +1,45 @@
 require('@testing-library/jest-dom');
+
+// Не реализован в JSDOM.
+// https://jestjs.io/docs/manual-mocks
+global.DOMRect = class DOMRect {
+  top = 0;
+  right = 0;
+  bottom = 0;
+  left = 0;
+  width = 0;
+  height = 0;
+  constructor(x = 0, y = 0, width = 0, height = 0) {
+    this.x = x;
+    this.y = y;
+    this.top = y;
+    this.right = x + width;
+    this.bottom = y + height;
+    this.left = x;
+    this.width = width;
+    this.height = height;
+  }
+  static fromRect(other) {
+    return new DOMRect(other?.x, other?.y, other?.width, other?.height);
+  }
+  toJSON() {
+    const { x, y, top, right, bottom, left, width, height } = this;
+    return JSON.stringify({ x, y, top, right, bottom, left, width, height });
+  }
+};
+
+// Не реализован в JSDOM.
+// Объявление скопировано из документации https://jestjs.io/docs/manual-mocks
+Object.defineProperty(global, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // устарело
+    removeListener: jest.fn(), // устарело
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});

packages/vkui/src/components/Cell/Cell.module.css
@@ -2,19 +2,9 @@
   position: relative;
 }
 
-/**
- * CMP:
- * List
- */
-:global(.vkuiInternalList--dragging) .Cell:not(.Cell--dragging) {
-  transition: transform 0.3s ease;
-  pointer-events: none;
-}
-
 .Cell--dragging {
   background-color: var(--vkui--color_background_secondary);
   box-shadow: var(--vkui--elevation3);
-  z-index: var(--vkui_internal--z_index_cell_dragging);
 }
 
 .Cell--selectable.Cell--disabled {

packages/vkui/src/components/Cell/Cell.test.tsx
@@ -5,44 +5,14 @@ import { Platform } from '../../lib/platform';
 import { baselineComponent } from '../../testing/utils';
 import { ConfigProvider } from '../ConfigProvider/ConfigProvider';
 import { List } from '../List/List';
-import { ListContext } from '../List/ListContext';
 import { Cell } from './Cell';
 
 const label = 'Перенести ячейку';
-const dragger = () => screen.getByLabelText(label);
 
 describe('Cell', () => {
   baselineComponent((props) => <Cell {...props}>Cell</Cell>);
 
   describe('Controls dragging', () => {
-    it('on mouse up/down', () => {
-      const toggleDrag = jest.fn();
-      render(
-        <ListContext.Provider value={{ toggleDrag }}>
-          <Cell draggable draggerLabel={label} />
-        </ListContext.Provider>,
-      );
-
-      fireEvent.mouseDown(dragger());
-      expect(toggleDrag).toHaveBeenLastCalledWith(true);
-
-      fireEvent.mouseUp(dragger());
-      expect(toggleDrag).toHaveBeenLastCalledWith(false);
-    });
-
-    it('stops drag on unmount', () => {
-      const toggleDrag = jest.fn();
-      const { rerender } = render(
-        <ListContext.Provider value={{ toggleDrag }}>
-          <Cell draggable draggerLabel={label} />
-        </ListContext.Provider>,
-      );
-
-      fireEvent.mouseDown(dragger());
-      rerender(<ListContext.Provider value={{ toggleDrag }} />);
-      expect(toggleDrag).toHaveBeenLastCalledWith(false);
-    });
-
     it('does not reorder dragged item on click', () => {
       const initialList = ['eugpoloz', 'arthurstam', 'xyz'];
       let updatedList = [...initialList];

packages/vkui/src/components/Cell/Cell.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import { classNames, noop } from '@vkontakte/vkjs';
+import type { SwappedItemRange } from '../../hooks/useDraggableWithDomApi';
 import { useExternRef } from '../../hooks/useExternRef';
 import { usePlatform } from '../../hooks/usePlatform';
 import { Platform } from '../../lib/platform';
-import { HasRootRef } from '../../types';
-import { ListContext } from '../List/ListContext';
-import { Removable, RemovableProps } from '../Removable/Removable';
-import { SimpleCell, SimpleCellProps } from '../SimpleCell/SimpleCell';
-import { CellCheckbox, CellCheckboxProps } from './CellCheckbox/CellCheckbox';
+import type { HasRootRef } from '../../types';
+import { Removable, type RemovableProps } from '../Removable/Removable';
+import { SimpleCell, type SimpleCellProps } from '../SimpleCell/SimpleCell';
+import { CellCheckbox, type CellCheckboxProps } from './CellCheckbox/CellCheckbox';
 import { CellDragger } from './CellDragger/CellDragger';
-import { useDraggable } from './useDraggable';
+import { DEFAULT_DRAGGABLE_LABEL } from './constants';
 import styles from './Cell.module.css';
 
 export interface CellProps
@@ -39,7 +39,7 @@ export interface CellProps
    * Эти числа нужны для того, чтобы разработчик понимал, с какого индекса на какой произошел переход. В песочнице
    * есть рабочий пример с обработкой этих чисел и перерисовкой списка.
    */
-  onDragFinish?: ({ from, to }: { from: number; to: number }) => void;
+  onDragFinish?(swappedItemRange: SwappedItemRange): void;
   /**
    * aria-label для кнопки перетаскивания ячейки
    */
@@ -65,11 +65,12 @@ export const Cell = ({
   checked,
   defaultChecked,
   getRootRef,
-  draggerLabel = 'Перенести ячейку',
+  draggerLabel = DEFAULT_DRAGGABLE_LABEL,
   className,
   style,
   ...restProps
 }: CellProps) => {
+  const [dragging, setDragging] = React.useState(false);
   const selectable = mode === 'selectable';
   const removable = mode === 'removable';
   const Component = selectable ? 'label' : ComponentProps;
@@ -78,40 +79,26 @@ export const Cell = ({
 
   const rootElRef = useExternRef(getRootRef);
 
-  const { dragging, ...draggableProps } = useDraggable({
-    rootElRef,
-    onDragFinish,
-  });
-
-  const { toggleDrag } = React.useContext(ListContext);
-  React.useEffect(() => {
-    if (dragging) {
-      toggleDrag(true);
-      return () => toggleDrag(false);
-    }
-    return undefined;
-  }, [dragging, toggleDrag]);
-
-  let dragger;
-  if (draggable) {
-    dragger = (
-      <CellDragger
-        className={styles['Cell__dragger']}
-        aria-label={draggerLabel}
-        {...draggableProps}
-      />
-    );
-  }
+  const dragger = draggable ? (
+    <CellDragger
+      elRef={rootElRef}
+      className={styles['Cell__dragger']}
+      aria-label={draggerLabel}
+      disabled={disabled}
+      onDragStateChange={setDragging}
+      onDragFinish={onDragFinish}
+    />
+  ) : null;
 
   let checkbox;
   if (selectable) {
     const checkboxProps: CellCheckboxProps = {
       name,
       value,
-      onChange,
       defaultChecked,
       checked,
       disabled,
+      onChange,
     };
     checkbox = <CellCheckbox className={styles['Cell__checkbox']} {...checkboxProps} />;
   }
@@ -122,8 +109,8 @@ export const Cell = ({
 
   const cellClasses = classNames(
     styles['Cell'],
-    platform === Platform.IOS && styles['Cell--ios'],
     dragging && styles['Cell--dragging'],
+    platform === Platform.IOS && styles['Cell--ios'],
     removable && styles['Cell--removable'],
     Component === 'label' && styles['Cell--selectable'],
     disabled && styles['Cell--disabled'],

packages/vkui/src/components/Cell/CellDragger/CellDragger.module.css
@@ -1,4 +1,11 @@
+/* stylelint-disable @project-tools/stylelint-atomic, selector-max-universal */
 .CellDragger {
   cursor: ns-resize;
   color: var(--vkui--color_icon_secondary);
+  user-select: none;
+  touch-action: manipulation;
+}
+
+.CellDragger__icon {
+  pointer-events: none;
 }

packages/vkui/src/components/Cell/CellDragger/CellDragger.tsx
@@ -1,43 +1,56 @@
 import * as React from 'react';
 import { Icon24Reorder, Icon24ReorderIos } from '@vkontakte/icons';
 import { classNames } from '@vkontakte/vkjs';
+import {
+  type DraggableProps,
+  UseDraggableProps,
+  useDraggableWithDomApi,
+} from '../../../hooks/useDraggableWithDomApi';
 import { usePlatform } from '../../../hooks/usePlatform';
 import { Platform } from '../../../lib/platform';
+import { useIsomorphicLayoutEffect } from '../../../lib/useIsomorphicLayoutEffect';
 import { HTMLAttributesWithRootRef } from '../../../types';
 import { Touch } from '../../Touch/Touch';
-import { DraggableProps } from '../useDraggable';
 import styles from './CellDragger.module.css';
 
-type CellDraggerProps = DraggableProps &
-  Omit<HTMLAttributesWithRootRef<HTMLElement>, keyof DraggableProps>;
+interface CellDraggerProps
+  extends UseDraggableProps,
+    Omit<HTMLAttributesWithRootRef<HTMLElement>, keyof DraggableProps> {
+  disabled?: boolean;
+  onDragStateChange?(dragging: boolean): void;
+}
 
 export const CellDragger = ({
-  onDragStart,
-  onDragMove,
-  onDragEnd,
-  onClick,
+  elRef,
+  disabled,
   className,
+  onDragStateChange,
+  onDragFinish,
   ...restProps
 }: CellDraggerProps) => {
   const platform = usePlatform();
+  const Icon = platform === Platform.IOS ? Icon24ReorderIos : Icon24Reorder;
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    if (onClick) {
-      onClick(event);
+  const { dragging, onDragStart, onDragMove, onDragEnd } = useDraggableWithDomApi({
+    elRef,
+    onDragFinish,
+  });
+
+  useIsomorphicLayoutEffect(() => {
+    if (onDragStateChange) {
+      onDragStateChange(dragging);
     }
-  };
+  }, [dragging, onDragStateChange]);
 
   return (
     <Touch
       className={classNames(styles['CellDragger'], className)}
-      onStart={onDragStart}
-      onMoveY={onDragMove}
-      onEnd={onDragEnd}
-      onClick={handleClick}
+      onStart={disabled ? undefined : onDragStart}
+      onMoveY={disabled ? undefined : onDragMove}
+      onEnd={disabled ? undefined : onDragEnd}
       {...restProps}
     >
-      {platform === Platform.IOS ? <Icon24ReorderIos /> : <Icon24Reorder />}
+      <Icon className={styles['CellDragger__icon']} />
     </Touch>
   );
 };

commit 393811833e60caf937c005363130303365060e69

fix(Cell): rm OUTBOX_OFFSET
- caused by #5833

Удаляем проверку на OUTBOX_OFFSET, т.к. из-за неё нельзя включить автоскролл при уводе элемента за область видимости.

packages/vkui/src/hooks/useDraggableWithDomApi/autoScroll.ts
@@ -3,7 +3,6 @@ import { rafSchd } from '../../lib/rafSchd';
 
 const SCROLL_SPEED = 10;
 export const EDGE_SIZE = 50;
-export const OUTBOX_OFFSET = -30;
 
 export const getAutoScrollingData = (clientY: number, scrollEl: Element | Window) => {
   const scrollTop = Math.floor(getNodeScroll(scrollEl).scrollTop);
@@ -22,9 +21,7 @@ export const getAutoScrollingData = (clientY: number, scrollEl: Element | Window
   const isInBottomEdge = bottomDistance <= EDGE_SIZE;
 
   const result = {
-    shouldScrolling:
-      (canScrollUp && isInTopEdge && topDistance >= OUTBOX_OFFSET) ||
-      (canScrollDown && isInBottomEdge && bottomDistance >= OUTBOX_OFFSET),
+    shouldScrolling: (canScrollUp && isInTopEdge) || (canScrollDown && isInBottomEdge),
     y: 0,
   };
 

packages/vkui/src/hooks/useDraggableWithDomApi/autoScroll.test.ts
@@ -1,10 +1,5 @@
 import { requestAnimationFrameMock } from '../../testing/utils';
-import {
-  createAutoScrollController,
-  EDGE_SIZE,
-  getAutoScrollingData,
-  OUTBOX_OFFSET,
-} from './autoScroll';
+import { createAutoScrollController, EDGE_SIZE, getAutoScrollingData } from './autoScroll';
 
 const VIEWPORT_WIDTH = 1280;
 const VIEWPORT_HEIGHT = 768;
@@ -48,19 +43,16 @@ describe('getAutoScrollingData', () => {
     test.each([
       [0, 1],
       [EDGE_SIZE - 1, 1],
-      [OUTBOX_OFFSET, 1],
-      [OUTBOX_OFFSET + 10, 1],
+      [-30, 1],
+      [-20, 1],
     ])('should be truthy for scrollTop is not on top', (clientY, scrollTop) => {
       initScrollElData(scrollEl, scrollTop);
       const { shouldScrolling } = getAutoScrollingData(clientY, scrollEl);
       expect(shouldScrolling).toBeTruthy();
     });
 
-    test.each([
-      [EDGE_SIZE + 1, 1],
-      [OUTBOX_OFFSET - 1, 1],
-    ])(
-      `should be falsy for clientY (%i) is not equal edge size (${EDGE_SIZE}) or outbox size (${OUTBOX_OFFSET})`,
+    test.each([[EDGE_SIZE + 1, 1]])(
+      `should be falsy for clientY (%i) is not equal edge size (${EDGE_SIZE})`,
       (clientY, scrollTop) => {
         initScrollElData(scrollEl, scrollTop);
         const { shouldScrolling } = getAutoScrollingData(clientY, scrollEl);
@@ -80,11 +72,8 @@ describe('getAutoScrollingData', () => {
       expect(shouldScrolling).toBeFalsy();
     });
 
-    test.each([
-      [VIEWPORT_HEIGHT - EDGE_SIZE - 1, 0],
-      [VIEWPORT_HEIGHT + -1 * OUTBOX_OFFSET + 1, 0],
-    ])(
-      `should be falsy for clientY (%i) is not equal edge size (${EDGE_SIZE}) or outbox size (${OUTBOX_OFFSET})`,
+    test.each([[VIEWPORT_HEIGHT - EDGE_SIZE - 1, 0]])(
+      `should be falsy for clientY (%i) is not equal edge size (${EDGE_SIZE})`,
       (clientY, scrollTop) => {
         initScrollElData(scrollEl, scrollTop);
         const { shouldScrolling } = getAutoScrollingData(clientY, scrollEl);