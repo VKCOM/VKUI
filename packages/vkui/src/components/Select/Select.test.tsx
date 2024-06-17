13b1885f6e9bb5c50bd017e4d19392659af0996f
Daniil Suvorov <severecloud@gmail.com>
2024-06-17 13:51:24 +0300

    fix(Select): use conditional render (#7011)
    * fix(Select): use conditional render
    * fix: deviceType forced is need display

--- packages/vkui/src/components/Select/Select.test.tsx
+++ packages/vkui/src/components/Select/Select.test.tsx
@@ -1,12 +1,12 @@
 import { baselineComponent } from '../../testing/utils';
-import { AdaptivityProvider } from '../AdaptivityProvider/AdaptivityProvider';
+import { PlatformProvider } from '../PlatformProvider/PlatformProvider';
 import { Select } from './Select';
 
 describe('Select', () => {
   baselineComponent((props) => (
-    <AdaptivityProvider hasPointer>
+    <PlatformProvider value="vkcom">
       <label htmlFor="select-id">Select an option</label>
       <Select id="select-id" options={[]} {...props} />
-    </AdaptivityProvider>
+    </PlatformProvider>
   ));
 });
