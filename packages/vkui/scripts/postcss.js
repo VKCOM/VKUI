b4e54ddd6c131763c545dce4058c3e25739dc5c2
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2024-06-19 19:27:46 +0300

    build(css): fix source-map generated by webpack
    **PostCSS** >= 8.4.38 при импорте файлов со стилями из `@vkontakte/vkui`
    
    ```css
    @import 'node_modules/@vkontakte/vkui/dist/components.css';
    /* или */
    @import 'node_modules/@vkontakte/vkui/dist/vkui.css';
    ```
    
    пишет в консоль предупреждения
    
    ```
    original.line and original.column are not numbers -- you probably meant to omit the original mapping entirely and only map the generated position. If so, pass null for the original mapping instead of an object with empty or null values.
    ```
    
    Ругается на некорректный **source-map**. Вот пути до них:
    
    ```
    node_modules/@vkontakte/vkui/dist/components.css.map
    node_modules/@vkontakte/vkui/dist/vkui.css.map
    ```
    
    h2. Решение
    
    Мы собираем выше упомянутые стили через **Webpack**. Но есть несколько нюансов:
    
    - в **mode** используется значение `none`;
    - CSS минифицируется через **cssnano** в конфигурационном файле **PostCSS**.
    
    Это конфликтует с оптимизациями **Webpack**, а также аффектит **souce-map** файлы.
    
    Поправил конфигурационный файл следующим образом:
    
    - в **mode** теперь выставляем `production`;
    - настраиваем **CssMinimizerPlugin** с теми же параметрами, что используем для **cssnano**;
    - отключаем **cssnano** в конфигурационном файле PostCSS.
    
    ✅ теперь **source-map** файл генерируется корректно.

--- packages/vkui/scripts/postcss.js
+++ packages/vkui/scripts/postcss.js
@@ -12,6 +12,24 @@ const { VKUI_PACKAGE, VKUI_TOKENS_CSS, generateScopedName } = require('../../../
 
 const rootDirectory = path.join(__dirname, '../../..');
 
+function getMinimizerOptions(isVKUIPackageBuild = false) {
+  return {
+    preset: [
+      'default',
+      {
+        // Отключаем из-за того, что `postcss-calc` меняет порядок операндов при умножении -1 на переменную
+        // Подробности здесь https://github.com/VKCOM/VKUI/issues/2963
+        calc: false,
+        // Включаем если собираем пакет @vkontakte/vkui.
+        // В остальных кейсах пустые CSS блоки удаляются раньше, чем их обработает
+        // `css-loader` с настройками для CSS Modules.
+        // Подробности здесь https://github.com/webpack-contrib/css-loader/issues/266
+        discardEmpty: isVKUIPackageBuild,
+      },
+    ],
+  };
+}
+
 /**
  * Конфигурация postcss плагинов
  * @param {Object} config - Конфигурация.
@@ -19,6 +37,7 @@ const rootDirectory = path.join(__dirname, '../../..');
  * @param {boolean | undefined} config.isProduction - Продакшн сборка.
  * @param {boolean | undefined} config.isCssModulesFile - Сборка module.css файлов.
  * @param {boolean | undefined} config.isESNext - Отдельная сборка cssm.
+ * @param {boolean | undefined} config.disableMinimizer - Отключает cssnano.
  * @returns {import('postcss').Plugin[]}
  */
 function makePostcssPlugins({
@@ -27,6 +46,7 @@ function makePostcssPlugins({
   isCssModulesFile = false,
   isESNext = false,
   isStorybook = process.env.SANDBOX === '.storybook',
+  disableMinimizer = false,
 } = {}) {
   const plugins = [
     // Обработка css импортов
@@ -78,29 +98,14 @@ function makePostcssPlugins({
   }
 
   // Уменьшение размера для продакшен сборки
-  if (isProduction && !isESNext) {
-    plugins.push(
-      cssnano({
-        preset: [
-          'default',
-          {
-            // Отключаем из-за того, что `postcss-calc` меняет порядок операндов при умножении -1 на переменную
-            // Подробности здесь https://github.com/VKCOM/VKUI/issues/2963
-            calc: false,
-            // Включаем если собираем пакет @vkontakte/vkui.
-            // В остальных кейсах пустые CSS блоки удаляются раньше, чем их обработает
-            // `css-loader` с настройками для CSS Modules.
-            // Подробности здесь https://github.com/webpack-contrib/css-loader/issues/266
-            discardEmpty: isVKUIPackageBuild,
-          },
-        ],
-      }),
-    );
+  if (!disableMinimizer && isProduction && !isESNext) {
+    plugins.push(cssnano(getMinimizerOptions(isVKUIPackageBuild)));
   }
 
   return plugins;
 }
 
 module.exports = {
+  getMinimizerOptions,
   makePostcssPlugins,
 };
