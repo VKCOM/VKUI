40e3e104fce5485133cdc280ae5841246f404060
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2024-06-20 14:02:49 +0300

    fix(Spacing): padding style wasn't applied (#7035)
    Не применялся padding из-за невалидного `calc()`. Но так как в `calc()` было деление на `2px`, это не срабатывало. Также к числу `sizeValue` не добавлялась единица измерения (`px`), что тоже считается не валидным.
    
    ❌ `calc(16 / 2px)`
    ❌ `calc(16px / 2px)`
    ✅ `calc(16px / 2)`
    
    > [!NOTE]
    > **React**, перед тем как применить стили, высчитывает `calc()` перед ставкой если можно.
    > `calc(16px / 2)` → `calc(8px)`
    
    Решил сразу перенести всё в CSS, потому что:
    - можно использовать CSS logical с последующим фоллбеком;
    - более явно видим какие **vkui-tokens** применяются;
    - убираем логику пересчёта из JS.
    
    В JS модифицируем входящие `className` и `style`, дабы не создавать дополнительные переменные. В случае такого простого компонента `Spacing` лишние операции ни к чему.

--- packages/vkui/src/components/Spacing/Spacing.test.tsx
+++ packages/vkui/src/components/Spacing/Spacing.test.tsx
@@ -1,6 +1,42 @@
+import { render } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
-import { Spacing } from './Spacing';
+import {
+  CUSTOM_CSS_TOKEN_FOR_USER_GAP,
+  sizesClassNames,
+  Spacing,
+  type SpacingSize,
+} from './Spacing';
+
+const sizes = Object.keys(sizesClassNames) as SpacingSize[];
 
 describe('Spacing', () => {
   baselineComponent(Spacing);
+
+  it('should use default size', () => {
+    const h = render(<Spacing />);
+    expect(h.container.firstElementChild).toHaveClass(sizesClassNames.m);
+  });
+
+  it('should preserve use className', () => {
+    const userClassName = 'test';
+    const h = render(<Spacing className={userClassName} />);
+    expect(h.container.firstElementChild).toHaveClass(`${sizesClassNames.m} ${userClassName}`);
+  });
+
+  it.each(sizes)('should use union sizes', (size) => {
+    const h = render(<Spacing size={size} />);
+    expect(h.container.firstElementChild).toHaveClass(sizesClassNames[size]);
+  });
+
+  it('should use custom size', () => {
+    const h = render(<Spacing size={16} />);
+    expect(h.container.firstElementChild).toHaveStyle(`${CUSTOM_CSS_TOKEN_FOR_USER_GAP}: 16`);
+  });
+
+  it('should preserve user style', () => {
+    const h = render(<Spacing size={16} style={{ fontSize: 12 }} />);
+    expect(h.container.firstElementChild).toHaveStyle(
+      `font-size: 12px; ${CUSTOM_CSS_TOKEN_FOR_USER_GAP}: 16`,
+    );
+  });
 });
