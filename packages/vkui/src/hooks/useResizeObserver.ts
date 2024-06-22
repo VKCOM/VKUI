af0c7df97024bb9ff27e507d113de4f797479a10
e.muhamethanov <eldarmu.1m@gmail.com>
2024-06-22 22:21:25 +0300

    fix: improve code of useResizeObserver

--- packages/vkui/src/hooks/useResizeObserver.ts
+++ packages/vkui/src/hooks/useResizeObserver.ts
@@ -1,41 +1,46 @@
 import * as React from 'react';
 import { CustomResizeObserver } from '../lib/floating/customResizeObserver';
-import { useIsomorphicLayoutEffect } from "../lib/useIsomorphicLayoutEffect";
+import { useIsomorphicLayoutEffect } from '../lib/useIsomorphicLayoutEffect';
 import { useStableCallback } from './useStableCallback';
 
 interface UseResizeObserverConfig {
-    // Флаг определяет, нужно ли использовать MutationObserver
-    // Это позволяет покрыть большую часть кейсов и не переусложнять код
-    useMutationObserver?: boolean,
+  // Флаг определяет, нужно ли использовать MutationObserver
+  // Это позволяет покрыть большую часть кейсов и не переусложнять код
+  useMutationObserver?: boolean;
 }
 
 const DEFAULT_CONFIG: UseResizeObserverConfig = {
-    useMutationObserver: false,
-}
+  useMutationObserver: false,
+};
 
 /**
  * Хук вызывает переданный коллбэк при изменении размеров элемента.
  */
-export function useResizeObserver(ref: React.MutableRefObject<HTMLElement | null>, callback: () => void, config: UseResizeObserverConfig = DEFAULT_CONFIG) {
-    const stableCallback = useStableCallback(callback);
+export function useResizeObserver(
+  ref: React.MutableRefObject<HTMLElement | null>,
+  callback: (rect: DOMRect) => void,
+  config: UseResizeObserverConfig = DEFAULT_CONFIG,
+) {
+  const stableCallback = useStableCallback(callback);
 
-    useIsomorphicLayoutEffect(
-        function addResizeObserverHandler() {
-            if (!ref.current) {
-                return;
-            }
+  useIsomorphicLayoutEffect(
+    function addResizeObserverHandler() {
+      if (!ref.current) {
+        return;
+      }
+      const element = ref.current;
+      const observer = new CustomResizeObserver(() => {
+        stableCallback(element.getBoundingClientRect());
+      });
+      if (config.useMutationObserver) {
+        observer.observeUsingMutationObserver(element);
+      } else {
+        observer.observe(element);
+        observer.appendToTheDOM();
+      }
 
-            const observer = new CustomResizeObserver(stableCallback);
-            if (config.useMutationObserver) {
-                observer.observeUsingMutationObserver(ref.current);
-            }
-            else {
-                observer.observe(ref.current);
-            }
-            observer.appendToTheDOM()
-
-            return () => observer.disconnect();
-        },
-        [ref],
-    );
+      return () => observer.disconnect();
+    },
+    [ref],
+  );
 }
