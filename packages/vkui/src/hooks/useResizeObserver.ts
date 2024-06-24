f16f3b71ebdf414970edb794b3dccc633861560d
EldarMuhamethanov <61377022+EldarMuhamethanov@users.noreply.github.com>
2024-06-24 17:41:35 +0300

    fix(CustomScrollView): add resize container handler (#7060)
    1. Добавил хук useResizeObserver, внутри которого используется CustomResizeObserver
    2. Переиспользовал хук useResizeObserver в CustomScrollView для отслеживания изменений размеров

---
+++ packages/vkui/src/hooks/useResizeObserver.ts
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { CustomResizeObserver } from '../lib/floating/customResizeObserver';
+import { useIsomorphicLayoutEffect } from '../lib/useIsomorphicLayoutEffect';
+import { useStableCallback } from './useStableCallback';
+
+/**
+ * Хук вызывает переданный коллбэк при изменении размеров элемента.
+ */
+export function useResizeObserver(
+  ref: React.MutableRefObject<HTMLElement | null>,
+  callback: (element: HTMLElement) => void,
+) {
+  const stableCallback = useStableCallback(callback);
+
+  useIsomorphicLayoutEffect(
+    function addResizeObserverHandler() {
+      /* istanbul ignore if: невозможный кейс (в SSR вызова этой функции не будет) */
+      if (!ref.current) {
+        return;
+      }
+      const element = ref.current;
+      const observer = new CustomResizeObserver(() => stableCallback(element));
+      observer.observe(element);
+      observer.appendToTheDOM();
+
+      return () => observer.disconnect();
+    },
+    [ref],
+  );
+}
