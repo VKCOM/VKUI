a3bc2a38328d54dd0b3966b91c0efe05107b67b5
Daniil Suvorov <severecloud@gmail.com>
2024-06-25 14:07:11 +0300

    fix(useMediaQueries): use context
    - see #6920
    
    ---
    
    ## Описание
    
    React компилятор не разрешает писать в переменные за пределами хука
    
    > Writing to a variable defined outside a component or hook is not allowed.
    
    ## Изменения
    
    Используем контекст, вместо переменной

--- packages/vkui/src/hooks/useMediaQueries.ts
+++ packages/vkui/src/hooks/useMediaQueries.ts
@@ -3,48 +3,25 @@ import { MEDIA_QUERIES, type MediaQueries } from '../lib/adaptivity';
 import { mediaQueryNull } from '../lib/browser';
 import { useDOM } from '../lib/dom';
 
-interface StoredMediaQueries {
-  /**
-   * Постоянная ссылка на окно. Это необходима, чтобы пересоздать медиа выражение если окно вдруг измениться.
-   *
-   * > `styleguide`
-   * >
-   * > В данный момент изменения окна существует в рамках песочницы. По сути мы храним ссылку на окно ради этого кейса
-   * > в песочнице.
-   */
-  window: Window | undefined;
-  /**
-   * Хранит созданные медиа выражения.
-   *
-   * Достаточно единожды создать экземпляры `MediaQueryList`, а дальше подписываться на события
-   * через метод `addEventListener(type, listener)`.
-   *
-   * {@link https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList}
-   */
-  mediaQueries: MediaQueries | null;
-}
-
-const storedMediaQueries: StoredMediaQueries = {
-  window: undefined,
-  mediaQueries: null,
-};
+const MediaQueriesCacheContext = React.createContext(new Map<Window | undefined, MediaQueries>());
 
 /**
  * Возвращает медиа выражения определенные дизайн-системой.
  */
 export const useMediaQueries = () => {
+  const mediaQueriesCache = React.useContext(MediaQueriesCacheContext);
   const { window } = useDOM();
 
-  return React.useMemo(
+  return React.useMemo<MediaQueries>(
     function initializeStoreOrUpdateStoreIfWindowChanges() {
-      if (storedMediaQueries.window === window && storedMediaQueries.mediaQueries !== null) {
-        return storedMediaQueries.mediaQueries;
+      const storedMediaQueries = mediaQueriesCache.get(window);
+      if (storedMediaQueries) {
+        return storedMediaQueries;
       }
 
       const matchMedia = window ? window.matchMedia.bind(window) : mediaQueryNull;
 
-      storedMediaQueries.window = window;
-      storedMediaQueries.mediaQueries = {
+      const mediaQueries = {
         desktopPlus: matchMedia(MEDIA_QUERIES.DESKTOP_PLUS),
         smallTabletPlus: matchMedia(MEDIA_QUERIES.SMALL_TABLET_PLUS),
         tablet: matchMedia(MEDIA_QUERIES.TABLET),
@@ -54,8 +31,10 @@ export const useMediaQueries = () => {
         mobileLandscapeHeight: matchMedia(MEDIA_QUERIES.MOBILE_LANDSCAPE_HEIGHT),
       };
 
-      return storedMediaQueries.mediaQueries;
+      mediaQueriesCache.set(window, mediaQueries);
+
+      return mediaQueries;
     },
-    [window],
+    [mediaQueriesCache, window],
   );
 };
