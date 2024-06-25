c2ea3c4303723fcd8d344633e4593434cb87a450
Daniil Suvorov <severecloud@gmail.com>
2024-06-25 16:12:53 +0300

    fix: WeakMap undefined

--- packages/vkui/src/hooks/useMediaQueries.ts
+++ packages/vkui/src/hooks/useMediaQueries.ts
@@ -3,7 +3,19 @@ import { MEDIA_QUERIES, type MediaQueries } from '../lib/adaptivity';
 import { mediaQueryNull } from '../lib/browser';
 import { useDOM } from '../lib/dom';
 
-const mediaQueriesCache = new WeakMap<Window | undefined, MediaQueries>();
+const mediaQueriesCache = new WeakMap<Window, MediaQueries>();
+
+function getMediaQueries(matchMedia: (query: string) => MediaQueryList): MediaQueries {
+  return {
+    desktopPlus: matchMedia(MEDIA_QUERIES.DESKTOP_PLUS),
+    smallTabletPlus: matchMedia(MEDIA_QUERIES.SMALL_TABLET_PLUS),
+    tablet: matchMedia(MEDIA_QUERIES.TABLET),
+    smallTablet: matchMedia(MEDIA_QUERIES.SMALL_TABLET),
+    mobile: matchMedia(MEDIA_QUERIES.MOBILE),
+    mediumHeight: matchMedia(MEDIA_QUERIES.MEDIUM_HEIGHT),
+    mobileLandscapeHeight: matchMedia(MEDIA_QUERIES.MOBILE_LANDSCAPE_HEIGHT),
+  };
+}
 
 /**
  * Возвращает медиа выражения определенные дизайн-системой.
@@ -13,22 +25,16 @@ export const useMediaQueries = () => {
 
   return React.useMemo<MediaQueries>(
     function initializeStoreOrUpdateStoreIfWindowChanges() {
+      if (!window) {
+        return getMediaQueries(mediaQueryNull);
+      }
+
       const storedMediaQueries = mediaQueriesCache.get(window);
       if (storedMediaQueries) {
         return storedMediaQueries;
       }
 
-      const matchMedia = window ? window.matchMedia.bind(window) : mediaQueryNull;
-
-      const mediaQueries = {
-        desktopPlus: matchMedia(MEDIA_QUERIES.DESKTOP_PLUS),
-        smallTabletPlus: matchMedia(MEDIA_QUERIES.SMALL_TABLET_PLUS),
-        tablet: matchMedia(MEDIA_QUERIES.TABLET),
-        smallTablet: matchMedia(MEDIA_QUERIES.SMALL_TABLET),
-        mobile: matchMedia(MEDIA_QUERIES.MOBILE),
-        mediumHeight: matchMedia(MEDIA_QUERIES.MEDIUM_HEIGHT),
-        mobileLandscapeHeight: matchMedia(MEDIA_QUERIES.MOBILE_LANDSCAPE_HEIGHT),
-      };
+      const mediaQueries = getMediaQueries(window.matchMedia.bind(window));
 
       mediaQueriesCache.set(window, mediaQueries);
 
