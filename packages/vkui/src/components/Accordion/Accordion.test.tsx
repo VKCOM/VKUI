51556d113bab8199aae261b6393da49fa3a7c0fe
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2024-06-26 17:39:56 +0300

    fix(AccordionContent): refactor for useCSSKeyframesAnimationController() (#7083)
    Чтобы не зашивать `max-height`, устанавливаем его тогда, когда нужно (с переходом с `transition` на `animation` заменил на использование `height`). Сделал для удобства, через CSS переменную, которую добавляем/удалям в `useLayoutEffect()` относительно `animationState`. По умолчанию, переменная со значением `initial`.
    
    Для `@media (--reduce-motion)` заменяем анимацию на `opacity`.

--- packages/vkui/src/components/Accordion/Accordion.test.tsx
+++ packages/vkui/src/components/Accordion/Accordion.test.tsx
@@ -1,13 +1,13 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { baselineComponent } from '../../testing/utils';
+import { fireEvent, render } from '@testing-library/react';
+import { baselineComponent, waitCSSKeyframesAnimation } from '../../testing/utils';
 import { Accordion } from './Accordion';
 
 describe(Accordion, () => {
   baselineComponent(Accordion.Content);
   baselineComponent(Accordion.Summary, { a11y: false });
 
-  it('toggles on click', () => {
-    render(
+  it('toggles on click', async () => {
+    const result = render(
       <Accordion>
         <Accordion.Summary iconPosition="before" data-testid="summary">
           Title
@@ -15,14 +15,17 @@ describe(Accordion, () => {
         <Accordion.Content data-testid="content">Content</Accordion.Content>
       </Accordion>,
     );
-    const content = screen.getByTestId<HTMLDivElement>('content');
-    const summary = screen.getByTestId('summary');
+    const content = result.getByTestId('content');
+    const contentIn = content.firstElementChild as HTMLElement;
+    const summary = result.getByTestId('summary');
     expect(content.getAttribute('aria-hidden')).toBe('true');
 
     fireEvent.click(summary);
+    await waitCSSKeyframesAnimation(contentIn);
     expect(content.getAttribute('aria-hidden')).toBe('false');
 
     fireEvent.click(summary);
+    await waitCSSKeyframesAnimation(contentIn);
     expect(content.getAttribute('aria-hidden')).toBe('true');
   });
 });
