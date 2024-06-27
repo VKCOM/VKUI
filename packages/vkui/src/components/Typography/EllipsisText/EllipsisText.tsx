3e14cf83b7d00dd4e133beacef6dd2d88225de81
e.muhamethanov <eldarmu.1m@gmail.com>
2024-06-27 12:53:54 +0300

    feat(EllipsisText): add maxLines prop
    Добавил пропс maxLines для указания максимального количества отображаемых строк

--- packages/vkui/src/components/Typography/EllipsisText/EllipsisText.tsx
+++ packages/vkui/src/components/Typography/EllipsisText/EllipsisText.tsx
@@ -1,4 +1,5 @@
 import { classNames } from '@vkontakte/vkjs';
+import { useEffect, useRef } from "react";
 import { HasRootRef } from '../../../types';
 import { RootComponentProps } from '../../RootComponent/RootComponent';
 import styles from './EllipsisText.module.css';
@@ -13,6 +14,13 @@ export interface EllipsisTextProps
    * другими словами, когда ширина не ограничена.
    */
   maxWidth?: number;
+  /**
+   * Максимальное количество видимых строк
+   *
+   * > При `maxLines > 1` используется свойство line-clamp, которое поддерживается не всеми версиями браузеров. Используйте с осторожностью
+   * > @see [line-clamp](https://developer.mozilla.org/en-US/docs/Web/CSS/-webkit-line-clamp)
+   */
+  maxLines?: number;
 }
 
 /** Компонент ограничивает текстовый контент убирая его в многоточие.
@@ -25,13 +33,31 @@ const EllipsisText = ({
   getRootRef,
   children,
   maxWidth,
+  maxLines = 1,
   ...restProps
-}: EllipsisTextProps) => (
-  <span ref={getRootRef} className={classNames(styles['EllipsisText'], className)} {...restProps}>
-    <span style={{ maxWidth }} className={styles['EllipsisText__content']}>
-      {children}
+}: EllipsisTextProps) => {
+  const contentRef = useRef<HTMLSpanElement | null>(null);
+
+  useEffect(() => {
+    if (contentRef && contentRef.current) {
+      contentRef.current.style.setProperty('-webkit-line-clamp', maxLines > 1 ? `${maxLines}` : '');
+    }
+  }, [contentRef, maxLines])
+
+  return (
+    <span ref={getRootRef} className={classNames(styles['EllipsisText'], className)} {...restProps}>
+      <span
+        style={{ maxWidth }}
+        ref={contentRef}
+        className={classNames(
+          styles['EllipsisText__content'],
+          maxLines > 1 && styles['EllipsisText__content--multiline']
+        )}
+      >
+        {children}
+      </span>
     </span>
-  </span>
-);
+  )
+};
 
 export { EllipsisText };
