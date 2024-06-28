3fe4a561977902787d3cca6d652012463897d386
e.muhamethanov <eldarmu.1m@gmail.com>
2024-06-28 13:46:33 +0300

    test(Typography): add tests to Typography

--- packages/vkui/src/components/Typography/Typography.test.tsx
+++ packages/vkui/src/components/Typography/Typography.test.tsx
@@ -1,6 +1,22 @@
+import { render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { Typography } from './Typography';
+import style from './Typography.module.css';
 
 describe('Typography', () => {
   baselineComponent(Typography);
+
+  it('should have specific className when color not send', () => {
+    render(<Typography data-testid="text">Text</Typography>);
+    expect(screen.getByTestId('text')).toHaveClass(style['Typography--color-inherit']);
+  });
+
+  it('should have specific className when color="primary"', () => {
+    render(
+      <Typography data-testid="text" color="primary">
+        Text
+      </Typography>,
+    );
+    expect(screen.getByTestId('text')).toHaveClass(style['Typography--color-primary']);
+  });
 });
