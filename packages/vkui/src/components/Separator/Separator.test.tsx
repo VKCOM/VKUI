ef283e99f092d1ad406b65012569ed2c74e84e4f
e.muhamethanov <eldarmu.1m@gmail.com>
2024-07-05 15:57:31 +0300

    feat(Separator): add mode prop

--- packages/vkui/src/components/Separator/Separator.test.tsx
+++ packages/vkui/src/components/Separator/Separator.test.tsx
@@ -1,6 +1,23 @@
+import { render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { Separator } from './Separator';
+import styles from './Separator.module.css';
 
 describe('Separator', () => {
   baselineComponent(Separator);
+
+  it('should have separator mode className', () => {
+    const getSeparator = () => screen.getByTestId('separator');
+    const { rerender } = render(<Separator mode="primary" data-testid="separator" />);
+
+    expect(getSeparator()).toHaveClass(styles['Separator--mode-primary']);
+
+    rerender(<Separator mode="secondary" data-testid="separator" />);
+
+    expect(getSeparator()).toHaveClass(styles['Separator--mode-secondary']);
+
+    rerender(<Separator mode="primary-alpha" data-testid="separator" />);
+
+    expect(getSeparator()).toHaveClass(styles['Separator--mode-primaryAlpha']);
+  });
 });
--- packages/vkui/src/components/Separator/Separator.tsx
+++ packages/vkui/src/components/Separator/Separator.tsx
@@ -5,18 +5,36 @@ import styles from './Separator.module.css';
 
 export interface SeparatorProps extends HTMLAttributesWithRootRef<HTMLDivElement> {
   /**
+   * Стиль отображения компонента
+   */
+  mode: 'primary' | 'secondary' | 'primary-alpha';
+  /**
    * С этим свойством компонент не будет иметь отступы слева и справа
    */
   wide?: boolean;
 }
 
+const modeClassNames: Record<SeparatorProps['mode'], string> = {
+  'primary': styles['Separator--mode-primary'],
+  'secondary': styles['Separator--mode-secondary'],
+  'primary-alpha': styles['Separator--mode-primaryAlpha'],
+};
+
 /**
  * @see https://vkcom.github.io/VKUI/#/Separator
  */
-export const Separator = ({ wide, ...restProps }: SeparatorProps): React.ReactNode => (
+export const Separator = ({
+  wide,
+  mode = 'primary',
+  ...restProps
+}: SeparatorProps): React.ReactNode => (
   <RootComponent
     {...restProps}
-    baseClassName={classNames(styles['Separator'], !wide && styles['Separator--padded'])}
+    baseClassName={classNames(
+      styles['Separator'],
+      !wide && styles['Separator--padded'],
+      modeClassNames[mode],
+    )}
   >
     <hr className={styles['Separator__in']} />
   </RootComponent>
