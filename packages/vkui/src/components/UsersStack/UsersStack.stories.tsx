f61b9218823756ee7bff78ffc369369c1bfeade6
e.muhamethanov <eldarmu.1m@gmail.com>
2024-07-05 14:36:33 +0300

    feat: add story to storybook

--- packages/vkui/src/components/UsersStack/UsersStack.stories.tsx
+++ packages/vkui/src/components/UsersStack/UsersStack.stories.tsx
@@ -1,8 +1,14 @@
 import { withCartesian } from '@project-tools/storybook-addon-cartesian';
 import { Meta, StoryObj } from '@storybook/react';
 import { CanvasFullLayout } from '../../storybook/constants';
-import { getAvatarUrl } from '../../testing/mock';
-import { UsersStack, UsersStackProps } from './UsersStack';
+import { getAvatarUrl, getRandomUser } from '../../testing/mock';
+import { Tooltip } from '../Tooltip/Tooltip';
+import {
+  UsersStack,
+  UsersStackPhoto,
+  UsersStackProps,
+  UsersStackRenderWrapperProps,
+} from './UsersStack';
 
 const story: Meta<UsersStackProps> = {
   title: 'Blocks/UsersStack',
@@ -20,5 +26,36 @@ export const Playground: Story = {
     children: 'Алексей, Илья, Михаил и ещё 1 человек',
     photos: [getAvatarUrl(), getAvatarUrl(), getAvatarUrl(), getAvatarUrl()],
     direction: 'row',
+  },
+};
+
+const AvatarWrapper = (props: UsersStackRenderWrapperProps) => {
+  const user = getRandomUser();
+
+  return (
+    <Tooltip text={`${user.first_name} ${user.last_name}`}>
+      <div
+        style={{
+          cursor: 'pointer',
+        }}
+        // eslint-disable-next-line no-console
+        onClick={() => console.log('click to avatar with url ' + props['data-src'])}
+      >
+        {props.children}
+      </div>
+    </Tooltip>
+  );
+};
+
+export const WithCustomWrapper: Story = {
+  ...Playground,
+  args: {
+    ...Playground.args,
+    photos: [getAvatarUrl(), getAvatarUrl(), getAvatarUrl(), getAvatarUrl()].map(
+      (photoUrl): UsersStackPhoto => ({
+        src: photoUrl,
+        renderWrapper: AvatarWrapper,
+      }),
+    ),
   },
 };
