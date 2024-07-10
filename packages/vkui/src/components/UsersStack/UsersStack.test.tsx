138bc010a881c2acbc89edc224593f259ef054a2
EldarMuhamethanov <61377022+EldarMuhamethanov@users.noreply.github.com>
2024-07-10 16:23:54 +0300

    feat(UsersStack): add possibility to render custom wrapper to photo and add example to doc (#7116)
    Добавил возможность прокидывания функции рендеринга обертки renderWrapper
    Добавил тест проверяющий новый функционал
    Добавил описание новой возможность в документацию, а также добавил новый пример

--- packages/vkui/src/components/UsersStack/UsersStack.test.tsx
+++ packages/vkui/src/components/UsersStack/UsersStack.test.tsx
@@ -1,6 +1,35 @@
+import { render } from '@testing-library/react';
+import { getAvatarUrl } from '../../testing/mock';
 import { baselineComponent } from '../../testing/utils';
-import { UsersStack } from './UsersStack';
+import { UsersStack, type UsersStackProps } from './UsersStack';
 
 describe('UsersStack', () => {
   baselineComponent(UsersStack);
+
+  it('should render wrapper', () => {
+    const avatarsUrls = [
+      getAvatarUrl('user_mm'),
+      getAvatarUrl('user_ilyagrshn'),
+      getAvatarUrl('user_lihachyov'),
+    ];
+
+    const photos: UsersStackProps['photos'] = avatarsUrls.map((src, index) => ({
+      src,
+      renderWrapper: ({ children }) => <div data-testid={`wrapper-${index}`}>{children}</div>,
+    }));
+
+    const result = render(
+      <UsersStack photos={photos}>
+        <span data-testid="text-content">Текстовое описание</span>
+      </UsersStack>,
+    );
+
+    expect(result.getByTestId('text-content')).toBeInTheDocument();
+    avatarsUrls.forEach((url, index) => {
+      const wrapper = result.getByTestId(`wrapper-${index}`);
+      expect(wrapper).toBeInTheDocument();
+      const image = wrapper.querySelector('image')!;
+      expect(image.getAttribute('href')).toBe(url);
+    });
+  });
 });
