47946c79ac81ca6bf9625174d054d4d110611598
e.muhamethanov <eldarmu.1m@gmail.com>
2024-07-23 15:56:52 +0300

    fix(Card): fix test and documentation

--- packages/vkui/src/components/Card/Card.test.tsx
+++ packages/vkui/src/components/Card/Card.test.tsx
@@ -1,6 +1,7 @@
 import { baselineComponent } from '../../testing/utils';
+import {CardGrid} from "../CardGrid/CardGrid";
 import { Card } from './Card';
 
 describe('Card', () => {
-  baselineComponent(Card);
+  baselineComponent(props => <CardGrid><Card {...props}/></CardGrid>);
 });
