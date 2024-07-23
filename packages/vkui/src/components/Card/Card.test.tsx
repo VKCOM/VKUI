c1408116500a30653c2238825bbb89679d2a7723
e.muhamethanov <eldarmu.1m@gmail.com>
2024-07-23 17:03:09 +0300

    fix(Card): fix formatting

--- packages/vkui/src/components/Card/Card.test.tsx
+++ packages/vkui/src/components/Card/Card.test.tsx
@@ -1,7 +1,11 @@
 import { baselineComponent } from '../../testing/utils';
-import {CardGrid} from "../CardGrid/CardGrid";
+import { CardGrid } from '../CardGrid/CardGrid';
 import { Card } from './Card';
 
 describe('Card', () => {
-  baselineComponent(props => <CardGrid><Card {...props}/></CardGrid>);
+  baselineComponent((props) => (
+    <CardGrid>
+      <Card {...props} />
+    </CardGrid>
+  ));
 });
