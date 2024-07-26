ed3e40feca4d818028596ef27165cdd43fc15452
e.muhamethanov <eldarmu.1m@gmail.com>
2024-07-26 12:08:21 +0300

    test(CalendarTime): fix formatting

--- packages/vkui/src/components/CalendarTime/CalendarTime.test.tsx
+++ packages/vkui/src/components/CalendarTime/CalendarTime.test.tsx
@@ -1,22 +1,20 @@
-import {render, screen} from "@testing-library/react";
-import {setHours, setMinutes} from "../../lib/date";
-import {userEvent} from "../../testing/utils";
-import {CalendarTime} from "./CalendarTime";
+import { render, screen } from '@testing-library/react';
+import { setHours, setMinutes } from '../../lib/date';
+import { userEvent } from '../../testing/utils';
+import { CalendarTime } from './CalendarTime';
 import styles from './CalendarTime.module.css';
 
 const dayDate = new Date('2023-09-01T07:40:00.000Z');
 
 describe('CalendarTime', () => {
   it('check onChange should called when select hours and minutes', async () => {
-    jest.useFakeTimers()
+    jest.useFakeTimers();
     const onChange = jest.fn();
-    const {container} = render(<CalendarTime
-      onChange={onChange}
-      value={dayDate}
-    />)
+    const { container } = render(<CalendarTime onChange={onChange} value={dayDate} />);
 
-    const [hourSelect, minuteSelect] = Array.from(container.getElementsByClassName(styles['CalendarTime__picker']))
-      .map(picker => picker.firstElementChild as HTMLElement);
+    const [hourSelect, minuteSelect] = Array.from(
+      container.getElementsByClassName(styles['CalendarTime__picker']),
+    ).map((picker) => picker.firstElementChild as HTMLElement);
 
     await userEvent.click(hourSelect);
 
@@ -28,23 +26,19 @@ describe('CalendarTime', () => {
     const unselectedMinuteOption = screen.getByRole('option', { selected: false, name: '55' });
     await userEvent.click(unselectedMinuteOption);
 
-    expect(onChange.mock.calls).toEqual([
-      [setHours(dayDate, 1)],
-      [setMinutes(dayDate, 55)],
-    ]);
-  })
+    expect(onChange.mock.calls).toEqual([[setHours(dayDate, 1)], [setMinutes(dayDate, 55)]]);
+  });
 
   it('check onChange should not called when isDisabled true', async () => {
-    jest.useFakeTimers()
+    jest.useFakeTimers();
     const onChange = jest.fn();
-    const {container} = render(<CalendarTime
-      onChange={onChange}
-      value={dayDate}
-      isDayDisabled={() => true}
-    />)
+    const { container } = render(
+      <CalendarTime onChange={onChange} value={dayDate} isDayDisabled={() => true} />,
+    );
 
-    const [hourSelect, minuteSelect] = Array.from(container.getElementsByClassName(styles['CalendarTime__picker']))
-      .map(picker => picker.firstElementChild as HTMLElement);
+    const [hourSelect, minuteSelect] = Array.from(
+      container.getElementsByClassName(styles['CalendarTime__picker']),
+    ).map((picker) => picker.firstElementChild as HTMLElement);
 
     await userEvent.click(hourSelect);
 
@@ -57,5 +51,5 @@ describe('CalendarTime', () => {
     await userEvent.click(unselectedMinuteOption);
 
     expect(onChange).toBeCalledTimes(0);
-  })
-})
+  });
+});
