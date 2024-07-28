71d37efb57d33e0d7dc37ba5f65d4c03b0d2df6a
Daniil Suvorov <severecloud@gmail.com>
2024-07-29 00:52:11 +0300

    feat: use rspack

--- packages/vkui/scripts/postcss.d.ts
+++ packages/vkui/scripts/postcss.d.ts
@@ -1,12 +1,12 @@
 import type postcss from 'postcss';
 
 interface MakePostcssPluginsOptions {
-  isVKUIPackageBuild: boolean;
-  isProduction: boolean;
-  isCssModulesFile: boolean;
-  isESNext: boolean;
-  isStorybook: boolean;
-  disableMinimizer: boolean;
+  isVKUIPackageBuild?: boolean;
+  isProduction?: boolean;
+  isCssModulesFile?: boolean;
+  isESNext?: boolean;
+  isStorybook?: boolean;
+  disableMinimizer?: boolean;
 }
 
 export function makePostcssPlugins(options?: MakePostcssPluginsOptions): postcss.AcceptedPlugin[];
