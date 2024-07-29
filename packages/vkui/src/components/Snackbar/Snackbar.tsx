1f2a553f8be52222a51a1275d587133c31c06c74
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2024-07-29 15:08:45 +0300

    test(Snackbar): add istanbul ignore

--- packages/vkui/src/components/Snackbar/Snackbar.tsx
+++ packages/vkui/src/components/Snackbar/Snackbar.tsx
@@ -139,7 +139,8 @@ export const Snackbar: React.FC<SnackbarProps> & { Basic: typeof Basic } = ({
             : rootRef.current.style.setProperty('--vkui_internal--snackbar_shift_y', `${y}px`);
           direction === null
             ? rootRef.current.style.removeProperty('--vkui_internal--snackbar_direction')
-            : rootRef.current.style.setProperty(
+            : /* istanbul ignore: TODO чтобы протестировать кейс, нужно мокать useMediaQueries(), чтобы перебивать mediaQueries.smallTabletPlus.matches */
+              rootRef.current.style.setProperty(
                 '--vkui_internal--snackbar_direction',
                 `${direction}`,
               );
