VKCOM/VKUI

commit 178703c11a22ea199d781729b518630048c4402b

feat: extend lib/floating (#6150)
* refactor(FocusTrap): rm reachable useState

* feat: extend lib/floating

- Создал `useFloatingMiddlewaresBootstrap()`, который скрывает в себе
   подготовку `middlewares` для `useFloating()`.
- Создал хук useFloatingWithInteractions(), который реализует
   интерактивное поведение для всплывающих элементов. А именно
   открытие/скрытие на `focus`, `click` и `hover`.

packages/vkui/src/lib/floating/useFloatingWithInteractions/types.ts
@@ -1,0 +1,84 @@
+import type { UseFloatingMiddlewaresBootstrapOptions } from '../useFloatingMiddlewaresBootstrap';
+
+export type InteractiveTriggerType = 'click' | 'hover' | 'focus';
+
+export type ManualTriggerType = 'manual';
+
+export type TriggerType = ManualTriggerType | InteractiveTriggerType | InteractiveTriggerType[];
+
+export type ShownChangeReason = 'click-outside' | 'escape-key' | 'click' | 'hover' | 'focus';
+
+export interface UseFloatingWithInteractionsProps
+  extends Pick<
+    UseFloatingMiddlewaresBootstrapOptions,
+    'placement' | 'offsetByMainAxis' | 'offsetByCrossAxis'
+  > {
+  /**
+   * Механика вызова всплывающего окна.
+   *
+   * - `"click"` – показывается/скрывается только при нажатии.
+   * - `"hover"` – будет показываться/скрывается при наведении/отведении мыши.
+   * - `"focus"` – будет показываться/скрывается при фокусе/потере фокуса мыши.
+   * - `"manual"` – будет показываться/скрывается только через параметр `shown`. `onShownChange` будет
+   *    вызываться при нажатии за пределы целевого и всплывающего элемента, а также по кнопке ESC.
+
+   * > ⚠️`"hover"` на тач-устройствах будет работать как `"click"`, с одним лишь нюансом, что не будет закрываться
+   * > при повторном нажатии на целевой элемент. Для закрытия необходимо нажать на область вне целевого элемента
+   * > и выпадающего окна.
+   */
+  trigger?: TriggerType;
+  /**
+   * Количество миллисекунд, после которых произойдёт показ/скрытие всплывающего окна при наведении.
+   *
+   * > Чтобы задать разное время на показ и скрытие, передайте массив типа `[<показ>, <скрытие>]`.
+   *
+   * > Используется только для `trigger="hover"`.
+   */
+  hoverDelay?: number | [number, number];
+  /**
+   * Содержимое всплывающего окна.
+   */
+  content?: React.ReactNode;
+  /**
+   * Блокирует изменение состояния.
+   */
+  disabled?: boolean;
+  /**
+   * Отключает взаимодействие со всплывающим элементом.
+   */
+  disableInteractive?: boolean;
+  /**
+   * Отключает закрытие нажатием на область вне целевого и всплывающего элемента.
+   */
+  disableCloseOnClickOutside?: boolean;
+  /**
+   * Отключает закрытие нажатием на кнопку ESC.
+   */
+  disableCloseOnEscKey?: boolean;
+  /**
+   * Начальное состояние всплывающего окна.
+   */
+  defaultShown?: boolean;
+  /**
+   * Если передан, то всплывающее окно будет показано/скрыто в зависимости от значения свойства.
+   */
+  shown?: boolean;
+  /**
+   * Вызывается при каждом изменении видимости всплывающего окна.
+   */
+  onShownChange?(shown: boolean): void;
+}
+
+export type ReferenceProps<T = HTMLElement> = Omit<
+  React.HTMLAttributes<T>,
+  keyof React.DOMAttributes<T>
+> &
+  Pick<React.DOMAttributes<T>, 'onMouseOver' | 'onMouseLeave' | 'onClick' | 'onFocus' | 'onBlur'>;
+
+export type FloatingProps<T = HTMLElement> = Omit<
+  React.HTMLAttributes<T>,
+  keyof React.DOMAttributes<T> | 'style'
+> & { style: React.CSSProperties } & Pick<
+    React.DOMAttributes<T>,
+    'onMouseOver' | 'onMouseLeave' | 'onClick' | 'onAnimationStart' | 'onAnimationEnd'
+  >;

packages/vkui/src/lib/floating/useFloatingWithInteractions/useFloatingWithInteractions.ts
@@ -1,0 +1,313 @@
+import * as React from 'react';
+import { debounce, noop } from '@vkontakte/vkjs';
+import { getWindow, isHTMLElement } from '@vkontakte/vkui-floating-ui/utils/dom';
+import { useCustomEnsuredControl } from '../../../hooks/useEnsuredControl';
+import { useGlobalOnClickOutside } from '../../../hooks/useGlobalOnClickOutside';
+import { useStableCallback } from '../../../hooks/useStableCallback';
+import { contains, getActiveElementByAnotherElement } from '../../dom';
+import { useIsomorphicLayoutEffect } from '../../useIsomorphicLayoutEffect';
+import { autoUpdateFloatingElement, useFloating } from '../adapters';
+import { convertFloatingDataToReactCSSProperties } from '../functions';
+import { type UseFloatingOptions } from '../types';
+import { useFloatingMiddlewaresBootstrap } from '../useFloatingMiddlewaresBootstrap';
+import { DEFAULT_TRIGGER } from './constants';
+import type {
+  FloatingProps,
+  ReferenceProps,
+  ShownChangeReason,
+  UseFloatingWithInteractionsProps,
+} from './types';
+import { useResolveTriggerType } from './useResolveTriggerType';
+
+const whileElementsMounted: UseFloatingOptions['whileElementsMounted'] = (...args) =>
+  autoUpdateFloatingElement(...args, { elementResize: true });
+
+/**
+ * @private
+ */
+export const useFloatingWithInteractions = <T extends HTMLElement = HTMLElement>({
+  trigger = DEFAULT_TRIGGER,
+
+  // UseFloatingMiddlewaresBootstrapProps
+  placement: expectedPlacement = 'bottom',
+  offsetByMainAxis = 0,
+  offsetByCrossAxis = 0,
+  hoverDelay = 0,
+
+  // disables
+  disabled = false,
+  disableInteractive = false,
+  disableCloseOnClickOutside = false,
+  disableCloseOnEscKey = false,
+
+  // uncontrolled
+  defaultShown = false,
+
+  // controlled
+  shown: shownProp,
+  onShownChange = noop,
+}: UseFloatingWithInteractionsProps) => {
+  const [willBeHide, setWillBeHide] = React.useState(false);
+  const [shownLocalState, setShownLocalState] = useCustomEnsuredControl({
+    value: shownProp,
+    disabled,
+    defaultValue: defaultShown,
+    onChange: useStableCallback(onShownChange),
+  });
+  const [shownFinalState, setShownFinalState] = React.useState(shownLocalState);
+  const wasShowBy = React.useRef<ShownChangeReason | null>(null);
+
+  const hasCSSAnimation = React.useRef(false);
+
+  const blockFocusRef = React.useRef(false);
+  const blurTimeoutRef = React.useRef<ReturnType<typeof setTimeout>>();
+
+  const handleCloseOnReferenceClickOutsideDisabled =
+    disabled || disableCloseOnClickOutside || willBeHide || !shownLocalState;
+  const handleCloseOnFloatingClickOutsideDisabled =
+    disableInteractive || handleCloseOnReferenceClickOutsideDisabled;
+
+  const { triggerOnFocus, triggerOnClick, triggerOnHover } = useResolveTriggerType(trigger);
+
+  // Библиотека `floating-ui`
+  const { middlewares, strictPlacement } = useFloatingMiddlewaresBootstrap({
+    placement: expectedPlacement,
+    offsetByMainAxis,
+    offsetByCrossAxis,
+  });
+  const { placement, x, y, strategy, refs } = useFloating<T>({
+    strategy: 'fixed',
+    placement: strictPlacement,
+    middleware: middlewares,
+    whileElementsMounted,
+  });
+
+  const commitShownLocalState = React.useCallback(
+    (nextShown: boolean, reason: ShownChangeReason) => {
+      setShownLocalState(nextShown);
+      wasShowBy.current = nextShown ? reason : null;
+    },
+    [setShownLocalState],
+  );
+
+  const [mouseEnterDelay, mouseLeaveDelay] =
+    typeof hoverDelay === 'number' ? [hoverDelay, hoverDelay] : hoverDelay;
+
+  const showWithDelay = React.useMemo(
+    () => debounce(() => commitShownLocalState(true, 'hover'), mouseEnterDelay),
+    [mouseEnterDelay, commitShownLocalState],
+  );
+
+  const hideWithDelay = React.useMemo(
+    () => debounce(() => commitShownLocalState(false, 'hover'), mouseLeaveDelay),
+    [mouseLeaveDelay, commitShownLocalState],
+  );
+
+  const handleFocusOnReference = useStableCallback(() => {
+    if (blockFocusRef.current) {
+      blockFocusRef.current = false;
+      return;
+    }
+
+    commitShownLocalState(true, 'focus');
+  });
+
+  const handleBlurOnReference = useStableCallback((event: React.FocusEvent) => {
+    blockFocusRef.current = false;
+
+    if (!shownLocalState) {
+      clearTimeout(blurTimeoutRef.current);
+      return;
+    }
+
+    const relatedTarget = event.relatedTarget;
+    blurTimeoutRef.current = setTimeout(function waitWindowBlurFire() {
+      const reference = refs.reference.current;
+      // Если пользователь покинул текущее окно в открытом состоянии, то
+      // не закрываем всплывающий элемент.
+      if (!relatedTarget && getActiveElementByAnotherElement(reference) === reference) {
+        return;
+      }
+
+      // Если пользователь нажал на всплывающий элемент, то не закрываем всплывающий элемент.
+      // Note: для этого элемент должен быть фокусируемый (например, за счёт `tabindex="-1"`).
+      if (contains(refs.floating.current, relatedTarget) || contains(reference, relatedTarget)) {
+        return;
+      }
+
+      commitShownLocalState(false, 'focus');
+    });
+  });
+
+  const handleClickOnReference = useStableCallback(() => {
+    commitShownLocalState(!shownLocalState, 'click');
+  });
+
+  const handleMouseEnterOnBoth = useStableCallback(() => {
+    showWithDelay.cancel();
+    hideWithDelay.cancel();
+
+    if (!shownLocalState) {
+      showWithDelay();
+    }
+  });
+
+  const handleMouseLeaveOnBothForHoverAndFocusStates = useStableCallback(() => {
+    blockFocusRef.current = false;
+
+    if (triggerOnHover) {
+      showWithDelay.cancel();
+      hideWithDelay.cancel();
+
+      if (wasShowBy.current !== 'focus' && wasShowBy.current !== 'click') {
+        hideWithDelay();
+      }
+    }
+  });
+
+  const handleFloatingAnimationStart = () => {
+    hasCSSAnimation.current = true;
+  };
+
+  const handleFloatingAnimationEnd = () => {
+    if (willBeHide) {
+      setShownFinalState(false);
+      setWillBeHide(false);
+    }
+  };
+
+  const handleRestoreFocus = React.useCallback(
+    () => (triggerOnFocus ? blockFocusRef.current : true),
+    [triggerOnFocus],
+  );
+
+  const handleEscapeKeyDown = React.useCallback(() => {
+    blockFocusRef.current = true;
+    commitShownLocalState(false, 'escape-key');
+  }, [commitShownLocalState]);
+
+  const handleClickOutside = React.useCallback(() => {
+    blockFocusRef.current = true;
+    commitShownLocalState(false, 'click-outside');
+  }, [commitShownLocalState]);
+
+  useGlobalOnClickOutside(
+    handleClickOutside,
+    handleCloseOnReferenceClickOutsideDisabled ? null : refs.reference,
+    handleCloseOnFloatingClickOutsideDisabled ? null : refs.floating,
+  );
+
+  useIsomorphicLayoutEffect(
+    /**
+     * Если пользователь покинул активное окно и:
+     * 1. целевой элемент был в состоянии фокуса;
+     * 2. всплывающий элемент был закрытом состоянии;
+     * то фокус должен быть заблокирован, когда пользователь вернётся обратно. Иначе покажется
+     * всплывающий элемент.
+     */
+    function setGlobalBlurForTriggerOnFocus() {
+      if (!triggerOnFocus || !refs.reference.current) {
+        return;
+      }
+
+      const handleGlobalBlur = () => {
+        const reference = refs.reference.current;
+        if (
+          !shownLocalState &&
+          isHTMLElement(reference) &&
+          reference === getActiveElementByAnotherElement(reference)
+        ) {
+          blockFocusRef.current = true;
+        }
+      };
+
+      const win = getWindow(refs.reference.current);
+      win.addEventListener('blur', handleGlobalBlur);
+      return () => {
+        win.removeEventListener('blur', handleGlobalBlur);
+      };
+    },
+    [triggerOnFocus, refs.reference, shownLocalState],
+  );
+
+  useIsomorphicLayoutEffect(
+    function resolveShownStates() {
+      if (willBeHide || shownLocalState === shownFinalState) {
+        return;
+      }
+
+      if (shownLocalState) {
+        setShownFinalState(true);
+      } else if (hasCSSAnimation.current && !willBeHide) {
+        setWillBeHide(true);
+      } else {
+        setShownFinalState(false);
+      }
+
+      return () => {
+        clearTimeout(blurTimeoutRef.current);
+      };
+    },
+    [shownLocalState, shownFinalState, willBeHide],
+  );
+
+  const referencePropsRef = React.useRef<ReferenceProps>({});
+  const floatingPropsRef = React.useRef<FloatingProps>({ style: {} });
+
+  if (shownFinalState) {
+    floatingPropsRef.current.style = convertFloatingDataToReactCSSProperties(strategy, x, y);
+
+    if (disableInteractive) {
+      floatingPropsRef.current.style.pointerEvents = 'none';
+    }
+  }
+
+  if (triggerOnFocus) {
+    referencePropsRef.current.onFocus = handleFocusOnReference;
+    referencePropsRef.current.onBlur = handleBlurOnReference;
+  }
+
+  if (triggerOnClick) {
+    referencePropsRef.current.onClick = handleClickOnReference;
+  }
+
+  if (triggerOnHover) {
+    referencePropsRef.current.onMouseOver = handleMouseEnterOnBoth;
+
+    if (!disableInteractive) {
+      floatingPropsRef.current.onMouseOver = handleMouseEnterOnBoth;
+    }
+  }
+
+  if (triggerOnHover || triggerOnFocus) {
+    referencePropsRef.current.onMouseLeave = handleMouseLeaveOnBothForHoverAndFocusStates;
+
+    if (!disableInteractive) {
+      floatingPropsRef.current.onMouseLeave = handleMouseLeaveOnBothForHoverAndFocusStates;
+    }
+  }
+
+  if (shownFinalState) {
+    floatingPropsRef.current.onAnimationStart = handleFloatingAnimationStart;
+    floatingPropsRef.current.onAnimationEnd = handleFloatingAnimationEnd;
+  }
+
+  return {
+    placement,
+    shown: shownFinalState,
+    willBeHide,
+    refs,
+    referenceProps: referencePropsRef.current,
+    floatingProps: floatingPropsRef.current,
+    // FocusTrap уже определяет нажатие на ESC, поэтому название события содержит конкретный код
+    // кнопки вместо просто onKeyDown.
+    onEscapeKeyDown: !shownFinalState || disableCloseOnEscKey ? undefined : handleEscapeKeyDown,
+    // [Обход баги с FocusTrap]
+    //
+    // Если сфокусироваться на целевой элемент через нажатие, а потом нажать в область за пределами
+    // целевого и всплывающего элемента, то появляется моргание из-за того, что FocusTrap
+    // восстанавливает фокус, из-за чего всплывающий элемент снова показывается за счёт
+    // `handleFocusOnReference`, а потом скрывается за счёт `handleBlurOnReference`.
+    onRestoreFocus: handleRestoreFocus,
+  };
+};

commit 328f8a27d3e153a014170db63062e1f46ca43b6a

feat(Popover): add onShownChanged prop (#7262)
h2. Описание

С v6 у Popover появилась анимация закрытия и `onShownChange` не учитывает это и вызывается сразу. Для решения проблемы создал колбек `onShownChanged`, который ждёт завершения анимации.

- caused by #6150
- related to #7175

packages/vkui/src/components/Popover/Popover.tsx
@@ -62,6 +62,7 @@ type AllowedFloatingComponentProps = Pick<
   | 'defaultShown'
   | 'shown'
   | 'onShownChange'
+  | 'onShownChanged'
   | 'usePortal'
   | 'sameWidth'
   | 'hideWhenReferenceHidden'
@@ -152,6 +153,7 @@ export const Popover = ({
   // controlled
   shown: shownProp,
   onShownChange,
+  onShownChanged,
 
   // Для AppRootPortal
   usePortal = true,
@@ -209,6 +211,7 @@ export const Popover = ({
     defaultShown,
     shown: shownProp,
     onShownChange,
+    onShownChanged,
   });
 
   usePlacementChangeCallback(expectedPlacement, resolvedPlacement, onPlacementChange);

packages/vkui/src/lib/floating/useFloatingWithInteractions/types.ts
@@ -80,6 +80,10 @@ export interface UseFloatingWithInteractionsProps {
    * Вызывается при каждом изменении видимости всплывающего элемента.
    */
   onShownChange?: OnShownChange;
+  /**
+   * Вызывается при каждом изменении видимости всплывающего элемента, но после завершении анимации.
+   */
+  onShownChanged?: OnShownChange;
 }
 
 export type ReferenceProps<T = HTMLElement> = Omit<

packages/vkui/src/lib/floating/useFloatingWithInteractions/useFloatingWithInteractions.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { debounce } from '@vkontakte/vkjs';
+import { debounce, noop } from '@vkontakte/vkjs';
 import { getWindow, isHTMLElement } from '@vkontakte/vkui-floating-ui/utils/dom';
 import { useCustomEnsuredControl } from '../../../hooks/useEnsuredControl';
 import { useGlobalOnClickOutside } from '../../../hooks/useGlobalOnClickOutside';
@@ -49,6 +49,7 @@ export const useFloatingWithInteractions = <T extends HTMLElement = HTMLElement>
   // controlled
   shown: shownProp,
   onShownChange: onShownChangeProp,
+  onShownChanged: onShownChangedProp,
 }: UseFloatingWithInteractionsProps): UseFloatingWithInteractionsReturn<T> => {
   const memoizedValue = React.useMemo(
     () => (shownProp !== undefined ? { shown: shownProp } : undefined),
@@ -64,6 +65,7 @@ export const useFloatingWithInteractions = <T extends HTMLElement = HTMLElement>
       }
     }),
   });
+  const onShownChanged = useStableCallback(onShownChangedProp ? onShownChangedProp : noop);
   const [shownFinalState, setShownFinalState] = React.useState(() => shownLocalState.shown);
   const [willBeHide, setWillBeHide] = React.useState(false);
 
@@ -215,6 +217,7 @@ export const useFloatingWithInteractions = <T extends HTMLElement = HTMLElement>
     if (willBeHide) {
       setShownFinalState(false);
       setWillBeHide(false);
+      onShownChanged(false, shownLocalState.reason);
     }
   };
 
@@ -288,6 +291,7 @@ export const useFloatingWithInteractions = <T extends HTMLElement = HTMLElement>
 
       if (shownLocalState.shown) {
         setShownFinalState(true);
+        onShownChanged(true, shownLocalState.reason);
       } else if (hasCSSAnimation.current && !willBeHide) {
         setWillBeHide(true);
       } else {
@@ -298,7 +302,7 @@ export const useFloatingWithInteractions = <T extends HTMLElement = HTMLElement>
         clearTimeout(blurTimeoutRef.current);
       };
     },
-    [shownLocalState, shownFinalState, willBeHide],
+    [shownLocalState, shownFinalState, willBeHide, onShownChanged],
   );
 
   const referencePropsRef = React.useRef<ReferenceProps>({});

packages/vkui/src/lib/floating/useFloatingWithInteractions/useFloatingWithInteractions.test.tsx
@@ -87,17 +87,20 @@ describe(useFloatingWithInteractions, () => {
       'should shown by $openBy event and hidden by $closeBy event (trigger: $trigger)',
       async ({ trigger, openBy, closeBy }) => {
         const onShownChange = jest.fn();
+        const onShownChanged = jest.fn();
         const { result } = renderHook(() =>
           useFloatingWithInteractions<HTMLButtonElement>({
             defaultShown: false,
             trigger: trigger,
             onShownChange,
+            onShownChanged,
           }),
         );
         const { rerender } = render(<TestComponent hookResultRef={result} />);
         await waitFor(() => {
           expect(result.current.shown).toBeFalsy();
           expect(onShownChange).not.toHaveBeenCalled();
+          expect(onShownChanged).not.toHaveBeenCalled();
         });
 
         await fireEventPatch(result.current.refs.reference.current, openBy);
@@ -106,6 +109,8 @@ describe(useFloatingWithInteractions, () => {
           expect(result.current.shown).toBeTruthy();
           expect(onShownChange).toHaveBeenCalledTimes(1);
           expect(onShownChange).toHaveBeenLastCalledWith(true, trigger);
+          expect(onShownChanged).toHaveBeenCalledTimes(1);
+          expect(onShownChanged).toHaveBeenLastCalledWith(true, trigger);
         });
 
         let closeReason: ShownChangeReason = trigger;