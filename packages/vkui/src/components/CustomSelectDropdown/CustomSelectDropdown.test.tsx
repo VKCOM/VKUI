aae808ff94ffc0de4d6e902e2d1d2fc29600bc17
e.muhamethanov <eldarmu.1m@gmail.com>
2024-07-30 13:33:31 +0300

    test(CustomSelectDropdown): improve unit-tests coverage

--- packages/vkui/src/components/CustomSelectDropdown/CustomSelectDropdown.test.tsx
+++ packages/vkui/src/components/CustomSelectDropdown/CustomSelectDropdown.test.tsx
@@ -1,6 +1,26 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
-import { CustomSelectDropdown } from './CustomSelectDropdown';
+import { PlacementWithAuto, usePlacementChangeCallback } from '../../lib/floating';
+import { CustomSelectDropdown, CustomSelectDropdownProps } from './CustomSelectDropdown';
+import styles from './CustomSelectDropdown.module.css';
+
+let lastInitialPlacement: PlacementWithAuto | undefined = undefined;
+
+jest.mock(
+  '../../lib/floating/usePlacementChangeCallback',
+  () =>
+    ({
+      usePlacementChangeCallback: (initialPlacement, _, onPlacementChange) => {
+        if (lastInitialPlacement !== initialPlacement) {
+          lastInitialPlacement !== undefined &&
+            initialPlacement !== 'auto' && initialPlacement !== 'auto-end' && initialPlacement !== 'auto-start' &&
+            onPlacementChange &&
+            onPlacementChange(initialPlacement);
+          lastInitialPlacement = initialPlacement;
+        }
+      },
+    }) satisfies { usePlacementChangeCallback: typeof usePlacementChangeCallback },
+);
 
 describe('CustomSelectDropdown', () => {
   it('Displays only spinner if fetching: true', () => {
@@ -19,5 +39,38 @@ describe('CustomSelectDropdown', () => {
       </CustomSelectDropdown>,
     );
     expect(screen.getByTestId('test-content')).not.toBeNull();
+  });
+
+  it('should call onPlacementChange callback when placement is changed', () => {
+    const onPlacementChange = jest.fn();
+
+    const props: CustomSelectDropdownProps = {
+      targetRef: React.createRef(),
+      // @ts-expect-error: TS2353 TS ругается, что нет такого поля
+      ['data-testid']: 'dropdown',
+      placement: 'top',
+      onPlacementChange,
+    };
+
+    const { rerender } = render(<CustomSelectDropdown {...props} />);
+    expect(screen.getByTestId('dropdown')).toHaveClass(styles['CustomSelectDropdown--top']);
+
+    rerender(<CustomSelectDropdown {...props} placement="bottom" />);
+    expect(screen.getByTestId('dropdown')).toHaveClass(styles['CustomSelectDropdown--bottom']);
+
+    expect(onPlacementChange).toBeCalledTimes(1);
+  });
+
+  it('should have className when noMaxHeight = true', () => {
+    render(
+      <CustomSelectDropdown
+        targetRef={React.createRef()}
+        data-testid="dropdown"
+        noMaxHeight={true}
+      />,
+    );
+    expect(screen.getByTestId('dropdown').firstElementChild).not.toHaveClass(
+      styles['CustomSelectDropdown__in--withMaxHeight'],
+    );
   });
 });
