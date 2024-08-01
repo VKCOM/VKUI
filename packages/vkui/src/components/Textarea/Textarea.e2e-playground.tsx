005629fdd67223c0ae9d23ca3936796934da3ce7
Andrey Medvedev <andr.medv.spb@gmail.com>
2024-08-01 11:43:04 +0300

    fix(FormField): Remove fixed height from before/after on desktop (#7264)
    Мы позволяем передать в before/after иконки определённых размеров. За высоту всего FormItem обычно отвечают другие элементы.
    Как будто бы нету смысла в том, чтобы как-то ограничивать высоту котейнера для иконок.
    В то же время, в высоких компонентах на основе FormField, таких как Textarea, напрашивается возможность передать несколько иконок и расположить их вертикально. Ограничение по высоте не даёт так позиционировать элементы.

--- packages/vkui/src/components/Textarea/Textarea.e2e-playground.tsx
+++ packages/vkui/src/components/Textarea/Textarea.e2e-playground.tsx
@@ -1,9 +1,12 @@
+import { Icon20CopyOutline, Icon20TrashSimpleOutline } from '@vkontakte/icons';
 import { ComponentPlayground, type ComponentPlaygroundProps } from '@vkui-e2e/playground-helpers';
 import { BREAKPOINTS } from '../../lib/adaptivity';
 import { AdaptivityProvider } from '../AdaptivityProvider/AdaptivityProvider';
 import { AppRoot } from '../AppRoot/AppRoot';
 import { AppearanceProvider } from '../AppearanceProvider/AppearanceProvider';
 import { Div } from '../Div/Div';
+import { Flex } from '../Flex/Flex';
+import { IconButton } from '../IconButton/IconButton';
 import { Textarea, type TextareaProps } from './Textarea';
 
 export const TextareaPlayground = (props: ComponentPlaygroundProps) => {
@@ -42,6 +45,22 @@ export const TextareaPlayground = (props: ComponentPlaygroundProps) => {
         {
           mode: ['plain'],
           placeholder: ['placeholder'],
+        },
+        {
+          after: [
+            <Flex key="flex" direction="column">
+              <IconButton label="Скопировать">
+                <Icon20CopyOutline />
+              </IconButton>
+              <IconButton label="Очистить">
+                <Icon20TrashSimpleOutline />
+              </IconButton>
+            </Flex>,
+          ],
+          defaultValue: [
+            'Музыка\nСпорт\nФотография\nПлавание\nПрограммирование\nПутешествия\nКниги\nСериалы\nФильмы\nНастольные игры',
+          ],
+          afterAlign: ['start', 'end'],
         },
       ]}
     >
