0781eb2781037d79e7d2d61b7fb86039e3f83c4b
e.muhamethanov <eldarmu.1m@gmail.com>
2024-08-06 16:18:03 +0300

    test(Cell): improve unit-tests coverage

--- packages/vkui/src/components/Cell/Cell.test.tsx
+++ packages/vkui/src/components/Cell/Cell.test.tsx
@@ -1,8 +1,10 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, getByRole, render, screen } from '@testing-library/react';
+import { Platform } from '../../lib/platform';
 import { baselineComponent, userEvent } from '../../testing/utils';
 import { ConfigProvider } from '../ConfigProvider/ConfigProvider';
 import { List } from '../List/List';
 import { Cell } from './Cell';
+import styles from './Cell.module.css';
 
 const label = 'Перенести ячейку';
 
@@ -36,7 +38,7 @@ describe('Cell', () => {
       );
 
       await userEvent.click(
-        screen.getByTestId('list-xyz').querySelector(`[aria-label="${label}"]`) as Element,
+        screen.getByTestId('list-xyz').querySelector<HTMLElement>(`[aria-label="${label}"]`)!,
       );
 
       expect(updatedList).toEqual(initialList);
@@ -148,5 +150,84 @@ describe('Cell', () => {
       expect(removeStub).toHaveBeenCalledTimes(0);
       expect(clickStub).toHaveBeenCalledTimes(1);
     });
+  });
+
+  it('check selectable mode', () => {
+    render(
+      <Cell mode="selectable" data-testid="cell">
+        Саша Колобов
+      </Cell>,
+    );
+    const cell = screen.getByTestId('cell');
+    expect(cell.tagName).toBe('LABEL');
+    expect(cell.parentElement).toHaveClass(styles['Cell--selectable']);
+
+    const checkbox = getByRole(cell, 'checkbox');
+    expect(checkbox).toBeInTheDocument();
+  });
+
+  it('check dragging className add when dragging cell', async () => {
+    render(
+      <Cell data-testid="list-xyz" draggable draggerLabel={label}>
+        xyz
+      </Cell>,
+    );
+
+    const cell = screen.getByTestId('list-xyz');
+    const dragger = cell.querySelector<HTMLElement>(`.${styles['Cell__dragger']}`)!;
+    expect(dragger).toBeInTheDocument();
+
+    fireEvent.mouseDown(dragger, {
+      clientX: 0,
+      clientY: 0,
+    });
+    fireEvent.mouseMove(dragger, {
+      clientX: 0,
+      clientY: 100,
+    });
+
+    expect(cell.parentElement).toHaveClass(styles['Cell--dragging']);
+  });
+
+  it('check that dragger after the content in IOS', () => {
+    render(
+      <ConfigProvider platform={Platform.IOS}>
+        <Cell data-testid="list-xyz" draggable draggerLabel={label}>
+          <div data-testid="content"></div>
+        </Cell>
+      </ConfigProvider>,
+    );
+    const cell = screen.getByTestId('list-xyz');
+    const dragger = cell.querySelector<HTMLElement>(`.${styles['Cell__dragger']}`)!;
+    const content = screen.getByTestId('content');
+
+    // Проверяем, что dragger находится в DOM дереве после контента, значит в after
+    expect(
+      content.compareDocumentPosition(dragger) & Node.DOCUMENT_POSITION_FOLLOWING,
+    ).toBeTruthy();
+  });
+
+  it('should have disabled classNames', () => {
+    render(
+      <Cell data-testid="list-xyz" draggable draggerLabel={label} disabled>
+        xyz
+      </Cell>,
+    );
+
+    const cell = screen.getByTestId('list-xyz');
+    const dragger = cell.querySelector<HTMLElement>(`.${styles['Cell__dragger']}`)!;
+    expect(dragger).toBeInTheDocument();
+
+    fireEvent.mouseDown(dragger, {
+      clientX: 0,
+      clientY: 0,
+    });
+    fireEvent.mouseMove(dragger, {
+      clientX: 0,
+      clientY: 100,
+    });
+
+    expect(cell.parentElement).not.toHaveClass(styles['Cell--dragging']);
+    expect(cell.parentElement).toHaveClass(styles['Cell--disabled']);
   });
 });
