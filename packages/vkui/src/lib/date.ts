555c279d1109b28f00688ca97d29259cd8b7d983
Daniil Suvorov <severecloud@gmail.com>
2024-08-06 17:07:06 +0300

    feat: Move from dayjs to date-fns Переходим от dayjs к date-fns - see #2353

--- packages/vkui/src/lib/date.ts
+++ packages/vkui/src/lib/date.ts
@@ -1,142 +1,11 @@
-import dayjs from 'dayjs';
-
-// Using date-fns-like type for migration
-type DirtyDate = Date | number;
-
-export function startOfDay(date: DirtyDate): Date {
-  return dayjs(date).startOf('day').toDate();
-}
-
-export function endOfDay(date: DirtyDate): Date {
-  return dayjs(date).endOf('day').toDate();
-}
-
-export function startOfWeek(date: DirtyDate, weekStart = 0): Date {
-  weekStart = weekStart % 7;
-
-  const day = dayjs(date);
-  const weekDay = day.day();
-  const diff = (weekDay < weekStart ? 7 : 0) + weekDay - weekStart;
-
-  return day.date(day.date() - diff).toDate();
-}
-
-export function endOfWeek(date: DirtyDate, weekStart = 0): Date {
-  const day = dayjs(startOfWeek(date, weekStart));
-
-  return day.date(day.date() + 6).toDate();
-}
-
-export function startOfMonth(date: DirtyDate): Date {
-  return dayjs(date).startOf('month').toDate();
-}
-
-export function endOfMonth(date: DirtyDate): Date {
-  return dayjs(date).endOf('month').toDate();
-}
-
-export function isFirstDayOfMonth(date: DirtyDate): boolean {
-  return dayjs(date).date() === 1;
-}
-
-export function isLastDayOfMonth(date: DirtyDate): boolean {
-  // isSameDay -- shorter, but not exact with date-fns behavior
-  // return isSameDay(date, dayjs(date).endOf("month"));
-  return dayjs(date).endOf('day').isSame(dayjs(date).endOf('month'));
-}
-
-export function format(date: DirtyDate, format: string): string {
-  return dayjs(date).format(format);
-}
-
-export function isBefore(date1: DirtyDate, date2: DirtyDate): boolean {
-  // Exactly as date-fns does
-  // dayjs().isBefore() for slightly different approach
-  return dayjs(date1) < dayjs(date2);
-}
-
-export function isAfter(date1: DirtyDate, date2: DirtyDate): boolean {
-  return dayjs(date1) > dayjs(date2);
-}
-
-export function isSameDay(date1: DirtyDate, date2: DirtyDate): boolean {
-  return dayjs(date1).isSame(date2, 'day');
-}
-
-export function isSameMonth(date1: DirtyDate, date2: DirtyDate): boolean {
-  return dayjs(date1).isSame(date2, 'month');
-}
-
-export function isWithinInterval(date: DirtyDate, start: DirtyDate, end: DirtyDate): boolean {
-  const day = dayjs(date);
-  return day >= dayjs(start) && day <= dayjs(end);
-}
-
-export function setMinutes(date: DirtyDate, minute: number): Date {
-  return dayjs(date).set('minute', minute).toDate();
-}
-
-export function setHours(date: DirtyDate, hour: number): Date {
-  return dayjs(date).set('hour', hour).toDate();
-}
-
-export function setMonth(date: DirtyDate, month: number): Date {
-  return dayjs(date).set('month', month).toDate();
-}
-
-export function setYear(date: DirtyDate, year: number): Date {
-  return dayjs(date).set('year', year).toDate();
-}
-
-export function addDays(date: DirtyDate, day: number): Date {
-  return dayjs(date).add(day, 'day').toDate();
-}
-
-export function subDays(date: DirtyDate, day: number): Date {
-  return dayjs(date).subtract(day, 'day').toDate();
-}
-
-export function addWeeks(date: DirtyDate, week: number): Date {
-  return dayjs(date).add(week, 'week').toDate();
-}
-
-export function subWeeks(date: DirtyDate, week: number): Date {
-  return dayjs(date).subtract(week, 'week').toDate();
-}
-
-export function addMonths(date: DirtyDate, month: number): Date {
-  return dayjs(date).add(month, 'month').toDate();
-}
-
-export function subMonths(date: DirtyDate, month: number): Date {
-  return dayjs(date).subtract(month, 'month').toDate();
-}
-
-// Rip off date-fns
-export function eachDayOfInterval(start: DirtyDate, end: DirtyDate): Date[] {
-  const dates: Date[] = [];
-  const startDate = dayjs(start).toDate();
-  const endDate = dayjs(end).toDate();
-
-  const endTime = endDate.getTime();
-  const currentDate = startDate;
-  currentDate.setHours(0, 0, 0, 0);
-
-  while (currentDate.getTime() <= endTime) {
-    dates.push(new Date(currentDate.getTime()));
-    currentDate.setDate(currentDate.getDate() + 1);
-    currentDate.setHours(0, 0, 0, 0);
-  }
-
-  return dates;
-}
+import { lightFormat } from 'date-fns';
 
 export function parse(input: string, format: string, referenceDate: Date = new Date()): Date {
   const match2 = /^\d\d/; // 00 - 99
   const match4 = /^\d{4}/; // 0000 - 9999
 
   const entries: Array<[string, RegExp, (val: string) => [string, number, boolean]]> = [
-    ['YYYY', match4, (val) => ['Y', +val, true]],
+    ['yyyy', match4, (val) => ['Y', +val, true]],
     [
       'MM',
       match2,
@@ -147,7 +16,7 @@ export function parse(input: string, format: string, referenceDate: Date = new D
         return ['M', numVal - 1, okay];
       },
     ],
-    ['DD', match2, (val) => ['D', +val, true]],
+    ['dd', match2, (val) => ['D', +val, true]],
     [
       'HH',
       match2,
@@ -175,9 +44,9 @@ export function parse(input: string, format: string, referenceDate: Date = new D
   const store: {
     [key: string]: number;
   } = {
-    Y: referenceDate.getFullYear(),
+    y: referenceDate.getFullYear(),
     M: referenceDate.getMonth(),
-    D: referenceDate.getDate(),
+    d: referenceDate.getDate(),
     h: referenceDate.getHours(),
     m: referenceDate.getMinutes(),
     s: referenceDate.getSeconds(),
@@ -243,10 +112,10 @@ export function parse(input: string, format: string, referenceDate: Date = new D
   return date;
 }
 
-export function isMatch(input: string, format: string): boolean {
-  return !isNaN(+parse(input, format));
+export function format(date: Date | number, format: string): string {
+  return lightFormat(date, format);
 }
 
-export function getMillisecondsToTomorrow(date: DirtyDate): number {
-  return dayjs(endOfDay(date)).diff(dayjs(date), 'ms');
+export function isMatch(input: string, format: string): boolean {
+  return !isNaN(+parse(input, format));
 }
