b283f2a483eb4e6bce04b16edf709c160a4a9372
EldarMuhamethanov <61377022+EldarMuhamethanov@users.noreply.github.com>
2024-08-07 21:31:28 +0300

    test(Skeleton): improve unit-tests coverage (#7321)

--- packages/vkui/src/components/Skeleton/Skeleton.test.tsx
+++ packages/vkui/src/components/Skeleton/Skeleton.test.tsx
@@ -1,6 +1,52 @@
+import { render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
-import { Skeleton } from './Skeleton';
+import { Skeleton, SkeletonProps } from './Skeleton';
+import styles from './Skeleton.module.css';
 
 describe('Skeleton', () => {
   baselineComponent(Skeleton);
+
+  it.each<{
+    props: Partial<SkeletonProps>;
+    cssVariable?: string;
+    expectedValue?: string;
+    className?: string;
+  }>([
+    {
+      props: {
+        colorFrom: '#000',
+      },
+      cssVariable: '--vkui_internal--skeleton_color_from',
+      expectedValue: '#000',
+    },
+    {
+      props: {
+        colorTo: '#fff',
+      },
+      cssVariable: '--vkui_internal--skeleton_color_to',
+      expectedValue: '#fff',
+    },
+    {
+      props: {
+        duration: 100,
+      },
+      cssVariable: '--vkui_internal--skeleton_animation_duration',
+      expectedValue: '100s',
+    },
+    {
+      props: {
+        noAnimation: true,
+      },
+      className: styles['Skeleton--disableAnimation'],
+    },
+  ])(
+    'should have $cssVariable = $expectedValue with props $props',
+    ({ props, expectedValue, cssVariable, className }) => {
+      render(<Skeleton {...props} data-testid="skeleton" />);
+      expectedValue &&
+        cssVariable &&
+        expect(screen.getByTestId('skeleton')).toHaveStyle(`${cssVariable}: ${expectedValue}`);
+      className && expect(screen.getByTestId('skeleton')).toHaveClass(className);
+    },
+  );
 });
