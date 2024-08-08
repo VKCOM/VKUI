VKCOM/VKUI

commit 823980bc747f49baedfe0f7dae0f0e737ab1ae9f

fix(Group): refactor styles for fix adaptivity (#7284)
h2. Описание

- реструктурировал CSS файл, чтобы было понятней что к чему;
- размеры заменил на токены из [@vkonktate/vkui-tokens](https://github.com/VKCOM/vkui-tokens/blob/ecdad88decf36df7ad14f5df493ba5adbddae6a5/src/themeDescriptions/base/paradigm.ts#L749-L759);
- отказался от использования `<Separator />` из-за того, что его задевает `vkuiInternalModalPage`, вместо этого объединил `Group__separator--spacing` и `Group__separator--separator` в один элемент `Group__separator`;
- добавил модификатор `Group--sizeX--regular`, т.к. он определяет `mode` как `"card"` при `sizeX="regular"`, если `mode === "none"`;
- вынес `<Separator />` из `<div />` в `ModalPageHeader.tsx`, чтобы боковые отступы не влияли на размеры сепаратора.

h3. Дизайн

Внутри `ModalPage`

При `platform="android"`/`platform="ios"` и `sizeX="regular"` в `ModalPageHeader` удаляем отступы у `Separator`.

<img width="320" alt="image" src="https://github.com/user-attachments/assets/104f9be1-d7b5-4d60-8618-1d4dbee904f0"> <img width="313" alt="image" src="https://github.com/user-attachments/assets/53149f5b-e662-4908-9ae1-774b8e475dd8">

_до и после_

---

- related to #5344, #5364, #5371, #5642

packages/vkui/src/components/Group/Group.tsx
@@ -7,7 +7,6 @@ import { HTMLAttributesWithRootRef } from '../../types';
 import { AppRootContext } from '../AppRoot/AppRootContext';
 import { ModalRootContext } from '../ModalRoot/ModalRootContext';
 import { RootComponent } from '../RootComponent/RootComponent';
-import { Separator } from '../Separator/Separator';
 import { Footnote } from '../Typography/Footnote/Footnote';
 import styles from './Group.module.css';
 
@@ -60,7 +59,7 @@ export interface GroupProps extends HTMLAttributesWithRootRef<HTMLElement> {
   header?: React.ReactNode;
   description?: React.ReactNode;
   /**
-    `show` - разделитель всегда показывается,
+    `show` (только для `mode="plain"`) - разделитель всегда показывается
     `hide` - разделитель всегда спрятан,
     `auto` - разделитель рисуется автоматически между соседними группами.
    */
@@ -111,6 +110,27 @@ export const Group = ({
 
   const tabIndex = isTabPanel && tabIndexProp === undefined ? 0 : tabIndexProp;
 
+  let siblingSeparatorElement: React.ReactNode = null;
+  switch (separator) {
+    case 'auto':
+      siblingSeparatorElement = <div className={styles['Group__separator-sibling']} />;
+      break;
+    case 'show':
+      siblingSeparatorElement = (
+        <div
+          className={classNames(
+            styles['Group__separator-sibling'],
+            mode === 'plain' || mode === 'none'
+              ? styles['Group__separator-sibling--forced']
+              : undefined,
+          )}
+        />
+      );
+      break;
+    case 'hide':
+      break;
+  }
+
   return (
     <>
       <RootComponent
@@ -120,9 +140,9 @@ export const Group = ({
         baseClassName={classNames(
           'vkuiInternalGroup',
           styles['Group'],
-          isInsideModal && styles['Group--inside-modal'],
           sizeX !== 'regular' && sizeXClassNames[sizeX],
-          mode && stylesMode[mode],
+          mode === 'plain' && isInsideModal && styles['Group--mode-plain-inside-modal'],
+          stylesMode[mode],
           stylesPadding[padding],
         )}
       >
@@ -132,21 +152,7 @@ export const Group = ({
           <Footnote className={styles['Group__description']}>{description}</Footnote>
         )}
       </RootComponent>
-
-      {separator !== 'hide' && (
-        <React.Fragment>
-          <div
-            className={classNames(styles['Group__separator'], styles['Group__separator--spacing'])}
-          />
-          <Separator
-            className={classNames(
-              styles['Group__separator'],
-              styles['Group__separator--separator'],
-              separator === 'show' && styles['Group__separator--force'],
-            )}
-          />
-        </React.Fragment>
-      )}
+      {siblingSeparatorElement}
     </>
   );
 };

packages/vkui/src/components/Group/Group.module.css
@@ -1,149 +1,53 @@
+/* stylelint-disable selector-max-universal */
+
+/*
+ * Автоопределение mode:
+ *
+ * - sizeX="compact" (mobile)  -> mode="plain"
+ * - sizeX="regular" (desktop) -> mode="card"
+ */
 .Group {
+  --vkui_internal--Group_padding_size: 0;
+
   color: var(--vkui--color_text_primary);
-  padding-block: 8px;
+  padding-block: var(--vkui--spacing_size_m);
 }
 
-.Group__header:empty {
-  display: none;
+.Group--padding-s {
+  --vkui_internal--Group_padding_size: var(--vkui--spacing_size_xs);
 }
 
-.Group--mode-plain:not(:first-of-type) > .Group__header {
-  margin-block-start: -8px;
+.Group--padding-m {
+  --vkui_internal--Group_padding_size: var(--vkui--spacing_size_m);
 }
 
-@media (--sizeX-compact) {
-  .Group--sizeX-none.Group--mode-none:not(:first-of-type) > .Group__header {
-    margin-block-start: -8px;
-  }
-}
-
-.Group--mode-card > .Group__header {
-  margin-block-start: -4px;
-}
-
-@media (--sizeX-regular) {
-  .Group--sizeX-none.Group--mode-none > .Group__header {
-    margin-block-start: -4px;
-  }
-}
-
-.Group__separator--spacing,
-.Group__separator--separator {
-  display: none;
-}
-
-.Group__separator--spacing {
+/* разделитель при mode="card" */
+.Group--mode-card,
+.Group--sizeX-regular.Group--mode-none {
+  padding: var(--vkui_internal--Group_padding_size);
   position: relative;
-  box-sizing: border-box;
-}
-
-/* stylelint-disable-next-line selector-max-universal */
-.Group--mode-plain + * + .Group__separator--separator {
-  display: block;
-}
-
-@media (--sizeX-compact) {
-  /* stylelint-disable-next-line selector-max-universal */
-  .Group--sizeX-none.Group--mode-none + * + .Group__separator--separator {
-    display: block;
-  }
-}
-
-.Group--mode-card + .Group__separator--spacing {
-  display: block;
-  block-size: 16px;
-  padding-block: 8px;
-  padding-inline: 0;
-}
-
-.Group--sizeX-compact.Group--mode-card + .Group__separator--spacing {
-  display: block;
-  block-size: 8px;
-  padding-block: 4px;
-}
-
-@media (--sizeX-regular) {
-  .Group--sizeX-none.Group--mode-none + .Group__separator--spacing {
-    display: block;
-    block-size: 16px;
-    padding-block: 8px;
-    padding-inline: 0;
-  }
-}
-@media (--sizeX-compact) {
-  .Group--sizeX-none.Group--mode-card + .Group__separator--spacing {
-    block-size: 8px;
-    padding-block: 4px;
-  }
-}
-
-.Group:last-of-type ~ .Group__separator {
-  display: none;
-}
-
-.Group:last-of-type ~ .Group__separator--force,
-.Group--mode-card:last-of-type + .Group__separator--spacing {
-  display: block;
-}
-
-@media (--sizeX-regular) {
-  .Group--sizeX-none.Group--mode-none:last-of-type + .Group__separator--spacing {
-    display: block;
-  }
-}
-
-.Group--mode-card.Group--padding-s {
-  padding: 4px;
-}
-
-.Group--mode-card.Group--padding-m {
-  padding: 8px;
-}
-
-@media (--sizeX-regular) {
-  .Group--sizeX-none.Group--inside-modal.Group--padding-s,
-  .Group--sizeX-none.Group--mode-none.Group--padding-s {
-    padding: 4px;
-  }
-
-  .Group--sizeX-none.Group--inside-modal.Group--padding-m,
-  .Group--sizeX-none.Group--mode-none.Group--padding-m {
-    padding: 8px;
-  }
-}
-
-.Group--sizeX-compact,
-.Group--sizeX-compact.Group--mode-card {
-  padding-inline: 0;
-}
-
-@media (--sizeX-compact) {
-  .Group--sizeX-none,
-  .Group--sizeX-none.Group--mode-card {
-    padding-inline: 0;
-  }
-}
-
-.Group--mode-card {
   background: var(--vkui--color_background_content);
   border-radius: var(--vkui--size_border_radius_paper--regular);
-  position: relative;
+}
+
+@media (--sizeX-regular) {
+  .Group--sizeX-none.Group--mode-none {
+    padding: var(--vkui_internal--Group_padding_size);
+    position: relative;
+    background: var(--vkui--color_background_content);
+    border-radius: var(--vkui--size_border_radius_paper--regular);
+  }
 }
 
 .Group--sizeX-compact.Group--mode-card {
+  padding-inline: 0;
   border-radius: var(--vkui--size_border_radius_promo--regular);
 }
 
 @media (--sizeX-compact) {
   .Group--sizeX-none.Group--mode-card {
+    padding-inline: 0;
     border-radius: var(--vkui--size_border_radius_promo--regular);
-  }
-}
-@media (--sizeX-regular) {
-  .Group--sizeX-none.Group--mode-none {
-    background: var(--vkui--color_background_content);
-    border-radius: var(--vkui--size_border_radius_paper--regular);
-    position: relative;
   }
 }
 
@@ -163,14 +67,15 @@
   }
 }
 
-/**
+/*
  * Изменено с ::after на ::before
  * потому что при ::after абсолютно позиционированный элемент накладывается
  * поверх любого другого абсолютно позиционированного элемента внутри Group,
  * например поверх Tooltip
  * См. пример: Slider c пропом withTooltip
  */
-.Group--mode-card::before {
+.Group--mode-card::before,
+.Group--sizeX-regular.Group--mode-none::before {
   content: '';
   inset-inline-start: 0;
   inset-block-start: 0;
@@ -206,25 +111,145 @@
   }
 }
 
-.Group--mode-plain + .Group__separator + .Group__separator {
-  padding-block: 8px;
+/*
+ * Header
+ */
+.Group__header:empty {
+  display: none;
+}
+
+.Group--mode-plain:not(:first-of-type) > .Group__header,
+.Group--sizeX-compact.Group--mode-none:not(:first-of-type) > .Group__header {
+  margin-block-start: calc(-1 * var(--vkui--spacing_size_m));
 }
 
 @media (--sizeX-compact) {
-  .Group--sizeX-none.Group--mode-none + .Group__separator + .Group__separator {
-    padding-block: 8px;
+  .Group--sizeX-none.Group--mode-none:not(:first-of-type) > .Group__header {
+    margin-block-start: calc(-1 * var(--vkui--spacing_size_m));
   }
 }
 
+.Group--mode-card > .Group__header,
+.Group--sizeX-regular.Group--mode-none > .Group__header {
+  margin-block-start: calc(-1 * var(--vkui--spacing_size_xs));
+}
+
+@media (--sizeX-regular) {
+  .Group--sizeX-none.Group--mode-none > .Group__header {
+    margin-block-start: calc(-1 * var(--vkui--spacing_size_xs));
+  }
+}
+
+/*
+ * Description
+ */
 .Group__description {
   display: block;
-  padding-block: 4px 16px;
+  padding-block: var(--vkui--spacing_size_xs) var(--vkui--spacing_size_2xl);
   padding-inline: var(--vkui--size_base_padding_horizontal--regular);
   color: var(--vkui--color_text_secondary);
 }
 
+/*
+ * Separator (sibling)
+ *
+ * ⚠️ Находится за пределами Group
+ */
+.Group__separator-sibling {
+  --vkui_internal--Group__separator_hr: none;
+
+  position: relative;
+  box-sizing: border-box;
+}
+
+.Group__separator-sibling::before {
+  display: var(--vkui_internal--Group__separator_hr);
+  content: '';
+  block-size: var(--vkui--size_border--regular);
+  background: var(--vkui--color_separator_primary);
+}
+
+/* разделитель при mode="plain" */
+.Group--mode-plain + .Group__separator-sibling,
+.Group--sizeX-compact.Group--mode-none + .Group__separator-sibling {
+  --vkui_internal--Group__separator_hr: block;
+
+  padding-inline: var(--vkui--size_base_padding_horizontal--regular);
+  padding-block: var(--vkui--spacing_size_m);
+}
+
+@media (--sizeX-compact) {
+  .Group--sizeX-none.Group--mode-none + .Group__separator-sibling {
+    --vkui_internal--Group__separator_hr: block;
+
+    padding-inline: var(--vkui--size_base_padding_horizontal--regular);
+    padding-block: var(--vkui--spacing_size_m);
+  }
+}
+
+.Group--mode-plain:last-of-type + .Group__separator-sibling:not(.Group__separator-sibling--forced) {
+  display: none;
+}
+
+@media (--sizeX-compact) {
+  .Group--sizeX-none.Group--mode-none:last-of-type
+    + .Group__separator-sibling:not(.Group__separator-sibling--forced) {
+    display: none;
+  }
+}
+
+/* разделитель при mode="card" */
+.Group--mode-card + .Group__separator-sibling,
+.Group--sizeX-regular.Group--mode-none + .Group__separator-sibling {
+  block-size: var(--vkui--spacing_size_2xl);
+  padding-block: calc(var(--vkui--spacing_size_2xl) / 2);
+}
+
+@media (--sizeX-regular) {
+  .Group--sizeX-none.Group--mode-none + .Group__separator-sibling {
+    block-size: var(--vkui--spacing_size_2xl);
+    padding-block: calc(var(--vkui--spacing_size_2xl) / 2);
+  }
+}
+
+.Group--sizeX-compact.Group--mode-card + .Group__separator-sibling {
+  block-size: var(--vkui--spacing_size_m);
+  padding-block: calc(var(--vkui--spacing_size_m) / 2);
+}
+
+@media (--sizeX-compact) {
+  .Group--sizeX-none.Group--mode-card + .Group__separator-sibling {
+    block-size: var(--vkui--spacing_size_m);
+    padding-block: calc(var(--vkui--spacing_size_m) / 2);
+  }
+}
+
+/*
+ * Group вложенный в ModalPage
+ *
+ * note: ModalPage форсирует mode="plain" для Group
+ */
+@media (--sizeX-regular) {
+  .Group--mode-plain-inside-modal {
+    padding: var(--vkui--spacing_size_m);
+  }
+
+  .Group--mode-plain-inside-modal + .Group__separator-sibling {
+    padding-inline: calc(
+      var(--vkui--size_base_padding_horizontal--regular) + var(--vkui--spacing_size_m)
+    );
+  }
+}
+
+/*
+ * Group вложенный в Group
+ */
 .Group .Group {
   padding-inline: 0;
+}
+
+.Group .Group + .Group__separator-sibling {
+  padding-inline: var(--vkui--size_base_padding_horizontal--regular);
 }
 
 .Group .Group:first-of-type {
@@ -239,7 +264,6 @@
  * CMP:
  * PanelHeader
  */
-/* stylelint-disable selector-max-universal */
 /* stylelint-disable-next-line selector-pseudo-class-disallowed-list */
 :global(.vkuiInternalPanelHeader--vkcom) ~ .Group:first-of-type,
 :global(.vkuiInternalPanelHeader--vkcom) + * .Group:first-of-type {
@@ -248,4 +272,3 @@
   border-start-end-radius: 0;
   border-start-start-radius: 0;
 }
-/* stylelint-enable selector-max-universal */

packages/vkui/src/components/ModalPage/ModalPage.stories.tsx
@@ -175,7 +175,8 @@ export const ModalPageWithFilters: Story = {
             <FormItem top="Город">
               <SelectMimicry placeholder="Выбрать город" disabled />
             </FormItem>
-
+          </Group>
+          <Group>
             <FormItem top="Пол">
               <Radio name="sex" value={0} defaultChecked>
                 Любой
@@ -187,19 +188,22 @@ export const ModalPageWithFilters: Story = {
                 Женский
               </Radio>
             </FormItem>
-
+          </Group>
+          <Group>
             <FormItem top="Школа">
               <SelectMimicry placeholder="Выбрать школу" disabled />
             </FormItem>
             <FormItem top="Университет">
               <SelectMimicry placeholder="Выбрать университет" disabled />
             </FormItem>
-
+          </Group>
+          <Group>
             <FormItem top="Дополнительно">
               <Checkbox>С фотографией</Checkbox>
               <Checkbox>Сейчас на сайте</Checkbox>
             </FormItem>
-
+          </Group>
+          <Group>
             <FormItem top="Работа">
               <Input placeholder="Место работы" />
             </FormItem>

packages/vkui/src/components/ModalPageHeader/ModalPageHeader.tsx
@@ -32,29 +32,31 @@ export const ModalPageHeader = ({
   const { labelId } = React.useContext(ModalPageContext);
 
   return (
-    <div
-      className={classNames(
-        styles['ModalPageHeader'],
-        platform !== 'vkcom' && styles['ModalPageHeader--withGaps'],
-        isDesktop && styles['ModalPageHeader--desktop'],
-      )}
-      ref={getRootRef}
-    >
-      <PanelHeader
-        className={classNames('vkuiInternalModalPageHeader__in', className)}
-        typographyProps={{
-          Component: 'h2',
-          id: labelId,
-          ...typographyProps,
-        }}
-        {...restProps}
-        fixed={false}
-        delimiter="none"
-        transparent
+    <>
+      <div
+        className={classNames(
+          styles['ModalPageHeader'],
+          platform !== 'vkcom' && styles['ModalPageHeader--withGaps'],
+          isDesktop && styles['ModalPageHeader--desktop'],
+        )}
+        ref={getRootRef}
       >
-        {children}
-      </PanelHeader>
+        <PanelHeader
+          className={classNames('vkuiInternalModalPageHeader__in', className)}
+          typographyProps={{
+            Component: 'h2',
+            id: labelId,
+            ...typographyProps,
+          }}
+          {...restProps}
+          fixed={false}
+          delimiter="none"
+          transparent
+        >
+          {children}
+        </PanelHeader>
+      </div>
       {!noSeparator && <Separator wide={sizeX === 'regular'} />}
-    </div>
+    </>
   );
 };

packages/vkui/src/components/ModalPageHeader/__image_snapshots__/modalpageheader-viewwidth-desktop-android-chromium-dark-1-snap.png
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf95e0d2bbdf0459e549a8eb656906654c2c28cfbc465bb3632006bc5a06ffd8
-size 81299
+oid sha256:eeedafab8ddb7cd9e2bb67fc4168b5fe220ccf8d93f75c732f3fae182189686b
+size 72254

packages/vkui/src/components/ModalPageHeader/__image_snapshots__/modalpageheader-viewwidth-desktop-android-chromium-light-1-snap.png
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e505d09463d502c2a0e64e96fe14a4ae196772f7045c57cdce3faf1e078ffff6
-size 79795
+oid sha256:bffdb91d8b67a1bda584f6974cbb7c222e11076c58c185295b906f03379aa75a
+size 71257

packages/vkui/src/components/ModalPageHeader/__image_snapshots__/modalpageheader-viewwidth-desktop-ios-only-ios-webkit-dark-1-snap.png
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e81db9e129153cff395b68e3625f5cfdc35e02213f45efe2a347556ab897a8b
-size 76035
+oid sha256:dea8f4f90f4b7054864bfae3d4cbb04abdb94d4556faffd4c25775f15f443dd8
+size 76050

packages/vkui/src/components/ModalPageHeader/__image_snapshots__/modalpageheader-viewwidth-desktop-ios-only-ios-webkit-light-1-snap.png
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:131b696964ff4ec92141fceea0d79740eb9f4b063ee991ebb04eff01232d2a80
-size 75558
+oid sha256:cfb77cd9d01cc4b49618c0150aa9f6f4eed8163e16ed6456193b2b5abe272d55
+size 75532

commit 58e02bfa93d58d418446a72947aea96bd8e17a92

fix(Group): add missed CSS class for `sizeX="regular"` (#7345)
Класс `Group--sizeX-regular` должен был быть ещё в PR #7284, но видимо случайно ревертнул когда пушил. Без него не будет автоопределения для `mode="card"`, когда `mode === "none"` и `sizeX === "regular"`.

Также исправляем тест из под #7333 – падал из-за того, что в PR был старый мастер без #7284.

---

- caused by #7284, #7333

packages/vkui/src/components/Group/Group.tsx
@@ -12,7 +12,8 @@ import styles from './Group.module.css';
 
 const sizeXClassNames = {
   none: classNames(styles['Group--sizeX-none'], 'vkuiInternalGroup--sizeX-none'),
-  ['compact']: styles['Group--sizeX-compact'],
+  regular: styles['Group--sizeX-regular'],
+  compact: styles['Group--sizeX-compact'],
 };
 
 const stylesMode = {
@@ -140,7 +141,7 @@ export const Group = ({
         baseClassName={classNames(
           'vkuiInternalGroup',
           styles['Group'],
-          sizeX !== 'regular' && sizeXClassNames[sizeX],
+          sizeXClassNames[sizeX],
           mode === 'plain' && isInsideModal && styles['Group--mode-plain-inside-modal'],
           stylesMode[mode],
           stylesPadding[padding],

packages/vkui/src/components/Group/Group.test.tsx
@@ -34,7 +34,7 @@ describe('Group', () => {
       isInsideModal: true,
       sizeX: undefined,
       layout: undefined,
-      className: classNames(styles['Group--mode-plain'], styles['Group--inside-modal']),
+      className: classNames(styles['Group--mode-plain'], styles['Group--mode-plain-inside-modal']),
     },
     {
       mode: undefined,
@@ -94,15 +94,46 @@ describe('Group', () => {
     },
   );
 
-  it('should force show separator', () => {
-    const { container } = render(
-      <Group separator="show" data-testid="group">
+  it.each(['show', 'hide', 'auto'] as const)('should force show separator', (separator) => {
+    const getSeparatorEl = (container: HTMLElement) =>
+      container.getElementsByClassName(styles['Group__separator-sibling'])[0] ?? null;
+
+    const modeNoneResult = render(
+      <Group separator={separator}>
         <div />
       </Group>,
     );
-    expect(container.getElementsByClassName(styles['Group__separator--separator'])[0]).toHaveClass(
-      styles['Group__separator--force'],
+    const modePlainResult = render(
+      <Group separator={separator} mode="plain">
+        <div />
+      </Group>,
     );
+    const modeCardResult = render(
+      <Group separator={separator} mode="card">
+        <div />
+      </Group>,
+    );
+    const modeNoneSeparatorEl = getSeparatorEl(modeNoneResult.container);
+    const modePlainSeparatorEl = getSeparatorEl(modePlainResult.container);
+    const modeCardSeparatorEl = getSeparatorEl(modeCardResult.container);
+
+    switch (separator) {
+      case 'show':
+        expect(modeNoneSeparatorEl).toHaveClass(styles['Group__separator-sibling--forced']);
+        expect(modePlainSeparatorEl).toHaveClass(styles['Group__separator-sibling--forced']);
+        expect(modeCardSeparatorEl).not.toHaveClass(styles['Group__separator-sibling--forced']);
+        break;
+      case 'auto':
+        expect(modeNoneSeparatorEl).not.toHaveClass(styles['Group__separator-sibling--forced']);
+        expect(modePlainSeparatorEl).not.toHaveClass(styles['Group__separator-sibling--forced']);
+        expect(modeCardSeparatorEl).not.toHaveClass(styles['Group__separator-sibling--forced']);
+        break;
+      case 'hide':
+        expect(modeNoneSeparatorEl).toBeNull();
+        expect(modePlainSeparatorEl).toBeNull();
+        expect(modeCardSeparatorEl).toBeNull();
+        break;
+    }
   });
 
   it('check DEV errors', () => {