eda4f94320f724d34a931b653a24159c13dbabf0
e.muhamethanov <eldarmu.1m@gmail.com>
2024-08-14 13:43:07 +0300

    feat: refactor createFieldWithPressets

--- packages/vkui/src/storybook/createFieldWithPressets.tsx
+++ packages/vkui/src/storybook/createFieldWithPressets.tsx
@@ -1,32 +1,35 @@
 import * as React from 'react';
 import * as icons from '@vkontakte/icons';
 
-type IconName = keyof typeof icons | 'None';
+type IconName = keyof typeof icons;
 
 type IconSize = '12' | '16' | '20' | '24' | '28' | '32' | '36' | '40' | '44' | '48' | '56' | '96';
 
-const SIZE_ICONS_COUNT = 5;
+const sizeToIconsMap: Map<IconSize, Map<IconName, React.ReactNode>> = new Map();
 
-const checkSize = ({
-  iconName,
-  iconSizes,
-  sizeToCountMap,
-  sizeIconsCount,
-}: {
-  iconName: string;
-  iconSizes: IconSize[];
-  sizeToCountMap: Record<string, number>;
-  sizeIconsCount: number;
-}) => {
-  const filteredSizes = iconSizes.filter((size) => {
-    return (sizeToCountMap[size] || 0) < sizeIconsCount;
-  });
-  const iconSize = filteredSizes.find((size) => iconName.startsWith(`Icon${size}`));
-  if (iconSize) {
-    sizeToCountMap[iconSize] = (sizeToCountMap[iconSize] || 0) + 1;
-  }
-  return !!iconSize;
+const getIconSize = (iconName: IconName) => {
+  const match = iconName.match(/Icon(\d+)/);
+  return (match?.[1] || '12') as IconSize;
 };
+
+const fillIconsMap = () => {
+  Object.keys(icons).forEach((iconName) => {
+    const typedIconName = iconName as IconName;
+    const node = icons[typedIconName];
+    const size = getIconSize(typedIconName);
+    const iconsBySize = sizeToIconsMap.get(size);
+    const Icon = node as any;
+    if (iconsBySize) {
+      iconsBySize.set(typedIconName, <Icon />);
+    } else {
+      sizeToIconsMap.set(size, new Map([[typedIconName, <Icon key="icon" />]]));
+    }
+  });
+};
+
+fillIconsMap();
+
+const SIZE_ICONS_COUNT = 5;
 
 export const createFieldWithPresets = <T extends string>({
   iconSizes = [],
@@ -40,24 +43,26 @@ export const createFieldWithPresets = <T extends string>({
   requiredIcons?: IconName[];
   additionalPresets?: Record<T, React.ReactNode>;
 }) => {
-  const options: Array<IconName | T> = ['None'];
+  const options: Array<IconName | T | 'None'> = ['None'];
   // @ts-expect-error: TS2740 ts ругается, что объект должен быть заполнен полями
-  const mapping: Record<IconName | T, React.ReactNode> = {
+  const mapping: Record<IconName | T | 'None', React.ReactNode> = {
     None: null,
   };
-  const typedIcons = icons as Record<IconName, any>;
-  const sizeToCountMap: Record<string, number> = {};
 
-  Object.entries(typedIcons).forEach(([iconName, node]) => {
-    const typedIconName = iconName as IconName;
-    if (
-      requiredIcons.includes(typedIconName) ||
-      checkSize({ iconName, iconSizes, sizeIconsCount, sizeToCountMap })
-    ) {
-      !options.includes(typedIconName) && options.push(typedIconName);
-      const Icon = node;
-      mapping[typedIconName] = <Icon />;
-    }
+  iconSizes.forEach((iconSize) => {
+    const iconsBySize = sizeToIconsMap.get(iconSize)!;
+    const iconsNames = Array.from(iconsBySize.keys()).slice(0, sizeIconsCount);
+
+    iconsNames.forEach((iconName) => {
+      options.push(iconName);
+      mapping[iconName] = iconsBySize.get(iconName);
+    });
+  });
+
+  requiredIcons.forEach((iconName) => {
+    options.push(iconName);
+    const Icon = icons[iconName] as any;
+    mapping[iconName] = <Icon />;
   });
 
   Object.entries(additionalPresets).forEach(([presetName, node]) => {
