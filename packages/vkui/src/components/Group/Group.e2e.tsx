9d6aa70d1604af74a6ee4fdc1edd17305d07375d
Andrey Medvedev <andr.medv.spb@gmail.com>
2024-08-15 20:13:05 +0300

    Add Group.ExpandedContent e2e playground

--- packages/vkui/src/components/Group/Group.e2e.tsx
+++ packages/vkui/src/components/Group/Group.e2e.tsx
@@ -1,7 +1,16 @@
 import { test } from '@vkui-e2e/test';
-import { GroupPlayground } from './Group.e2e-playground';
+import { GroupPlayground, GroupWithExpandedContentPlayground } from './Group.e2e-playground';
 
 test('Group', async ({ mount, expectScreenshotClippedToContent, componentPlaygroundProps }) => {
   await mount(<GroupPlayground {...componentPlaygroundProps} />);
+  await expectScreenshotClippedToContent();
+});
+
+test('Group.ExpandedContent', async ({
+  mount,
+  expectScreenshotClippedToContent,
+  componentPlaygroundProps,
+}) => {
+  await mount(<GroupWithExpandedContentPlayground {...componentPlaygroundProps} />);
   await expectScreenshotClippedToContent();
 });
