4eebfd857414dcf310a7fdbf6e345bb500688690
e.muhamethanov <eldarmu.1m@gmail.com>
2024-08-20 10:11:24 +0300

    fix: remove custom props addBefore and addAfter in Button story

--- packages/vkui/src/components/Button/Button.stories.tsx
+++ packages/vkui/src/components/Button/Button.stories.tsx
@@ -1,12 +1,5 @@
 import { withCartesian } from '@project-tools/storybook-addon-cartesian';
 import { Meta, StoryObj } from '@storybook/react';
-import {
-  Icon12Add,
-  Icon12Tag,
-  Icon16Add,
-  Icon24Add,
-  Icon24ChevronCompactRight,
-} from '@vkontakte/icons';
 import { CanvasFullLayout } from '../../storybook/constants';
 import { createFieldWithPresets } from '../../testing/createFieldWithPresets';
 import { Counter } from '../Counter/Counter';
@@ -28,12 +21,6 @@ const story: Meta<StoryButtonProps> = {
   argTypes: {
     before: iconsPresets,
     after: iconsPresets,
-    addBefore: {
-      control: { type: 'boolean' },
-    },
-    addAfter: {
-      control: { type: 'boolean' },
-    },
   },
   decorators: [withCartesian],
 };
@@ -43,23 +30,8 @@ export default story;
 type Story = StoryObj<StoryButtonProps>;
 
 export const Playground: Story = {
-  render: ({ addBefore, addAfter, ...args }) => {
-    const buttonBefore =
-      args.before ||
-      (addBefore &&
-        (args.size === 's' ? <Icon12Add /> : args.size === 'm' ? <Icon16Add /> : <Icon24Add />));
-    const buttonAfter =
-      args.after ||
-      (addAfter &&
-        (args.size === 's' ? (
-          <Icon12Tag />
-        ) : args.size === 'm' ? (
-          <Icon24ChevronCompactRight />
-        ) : (
-          <Counter>16</Counter>
-        )));
-
-    return <Button {...args} before={buttonBefore} after={buttonAfter} />;
+  render: ({ ...args }) => {
+    return <Button {...args} />;
   },
   args: {
     children: 'Button',
