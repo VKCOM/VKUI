8c53f9274aadc17bb5446e1ea918e094605eeb75
Andrey Medvedev <andr.medv.spb@gmail.com>
2024-08-23 13:33:01 +0300

    Fix types import due to new eslint rule

--- packages/vkui/src/components/Group/GroupContainer.tsx
+++ packages/vkui/src/components/Group/GroupContainer.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { classNames } from '@vkontakte/vkjs';
 import { useAdaptivity } from '../../hooks/useAdaptivity';
-import { SizeTypeValues } from '../../lib/adaptivity';
+import type { SizeTypeValues } from '../../lib/adaptivity';
 import { warnOnce } from '../../lib/warnOnce';
-import { HasComponent, HTMLAttributesWithRootRef } from '../../types';
+import type { HasComponent, HTMLAttributesWithRootRef } from '../../types';
 import { AppRootContext } from '../AppRoot/AppRootContext';
 import { ModalRootContext } from '../ModalRoot/ModalRootContext';
 import { RootComponent } from '../RootComponent/RootComponent';
