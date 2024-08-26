5c1f0a13aa5c03731ab457dbf81291e21948a825
e.muhamethanov <eldarmu.1m@gmail.com>
2024-08-26 15:05:44 +0300

    test(Gallery): add screenshot tests to component

---
+++ packages/vkui/src/components/Gallery/Gallery.e2e.tsx
@@ -0,0 +1,27 @@
+import { test } from '@vkui-e2e/test';
+import { GalleryPlayground, GalleryWithArrowsPlayground } from './Gallery.e2e-playground';
+
+test('Gallery', async ({ mount, expectScreenshotClippedToContent, componentPlaygroundProps }) => {
+  await mount(<GalleryPlayground {...componentPlaygroundProps} />);
+  await expectScreenshotClippedToContent();
+});
+
+test.describe('Gallery', () => {
+  test.use({
+    onlyForPlatforms: ['vkcom'],
+  });
+  test('show arrows', async ({
+    mount,
+    page,
+    expectScreenshotClippedToContent,
+    componentPlaygroundProps,
+  }) => {
+    await mount(<GalleryWithArrowsPlayground {...componentPlaygroundProps} />);
+    const gallery = page.getByTestId('gallery');
+
+    await gallery.hover();
+    await page.waitForTimeout(500);
+
+    await expectScreenshotClippedToContent();
+  });
+});
