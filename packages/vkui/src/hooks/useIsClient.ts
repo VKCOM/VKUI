6026a93d021df538730ebb1688e00e9b050f47a1
Pasha Suprunchuk <suprunchuk@internet.ru>
2024-08-26 10:33:51 +0300

    fix(hooks): updated a non-existent link to the new React website

--- packages/vkui/src/hooks/useIsClient.ts
+++ packages/vkui/src/hooks/useIsClient.ts
@@ -7,7 +7,7 @@ import { useIsomorphicLayoutEffect } from '../lib/useIsomorphicLayoutEffect';
  * ВНИМАНИЕ: Этот подход сделает ваши компоненты медленнее, потому что они
  * должны рендериться дважды, поэтому используйте хук с осторожностью.
  *
- * @see {@link https://beta.reactjs.org/apis/react-dom/hydrate#handling-different-client-and-server-content React Docs}
+ * @see {@link https://react.dev/reference/react-dom/hydrate#handling-different-client-and-server-content React Docs}
  */
 export function useIsClient(initial = false): boolean {
   const [isClient, setIsClient] = React.useState(initial);
