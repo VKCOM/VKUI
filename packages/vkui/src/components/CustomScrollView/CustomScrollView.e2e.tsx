7106725aea0a717f72704ef0b62b2bac89215560
e.muhamethanov <eldarmu.1m@gmail.com>
2024-08-28 17:28:21 +0300

    test(CustomScrollView): add screenshots tests to component

---
+++ packages/vkui/src/components/CustomScrollView/CustomScrollView.e2e.tsx
@@ -0,0 +1,29 @@
+import { test } from '@vkui-e2e/test';
+import {
+  CustomScrollViewWithBothScrollsPlayground,
+  CustomScrollViewWithVerticalPlayground,
+} from './CustomScrollView.e2e-playground';
+
+test.describe('CustomScrollView', () => {
+  test.use({ onlyForPlatforms: ['vkcom'] });
+
+  test('CustomScrollView with vertical scroll', async ({
+    mount,
+    expectScreenshotClippedToContent,
+    componentPlaygroundProps,
+  }) => {
+    await mount(<CustomScrollViewWithVerticalPlayground {...componentPlaygroundProps} />);
+    await expectScreenshotClippedToContent();
+  });
+
+  test('CustomScrollView with vertical and horizontal scroll', async ({
+    mount,
+    page,
+    expectScreenshotClippedToContent,
+    componentPlaygroundProps,
+  }) => {
+    await mount(<CustomScrollViewWithBothScrollsPlayground {...componentPlaygroundProps} />);
+    await page.waitForTimeout(500);
+    await expectScreenshotClippedToContent();
+  });
+});
