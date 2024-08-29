8a0cc926c1c6cb6ba55bb8f32a8b160e728d5d70
Daniil Suvorov <severecloud@gmail.com>
2024-08-29 15:57:55 +0300

    fix(CheckboxInput): rm mutation value - see #6920
    ---
    
    > Mutating a value returned from a function whose return value should not be mutated
    
    Убираем мутации инпута, так как реакт компилятор ругается

--- packages/vkui/src/components/Checkbox/CheckboxInput/CheckboxInput.tsx
+++ packages/vkui/src/components/Checkbox/CheckboxInput/CheckboxInput.tsx
@@ -16,6 +16,10 @@ import { RootComponent } from '../../RootComponent/RootComponent';
 import { VisuallyHidden } from '../../VisuallyHidden/VisuallyHidden';
 import styles from './CheckboxInput.module.css';
 
+function setIndeterminate(el: HTMLInputElement, indeterminate: boolean) {
+  el.indeterminate = indeterminate;
+}
+
 export interface CheckboxInputProps
   extends React.ComponentProps<'input'>,
     HasRootRef<HTMLDivElement>,
@@ -44,7 +48,7 @@ export function CheckboxInput({
     const indeterminateValue = indeterminate === undefined ? defaultIndeterminate : indeterminate;
 
     if (inputRef.current) {
-      inputRef.current.indeterminate = Boolean(indeterminateValue);
+      setIndeterminate(inputRef.current, Boolean(indeterminateValue));
     }
   }, [defaultIndeterminate, indeterminate, inputRef]);
 
@@ -56,10 +60,10 @@ export function CheckboxInput({
         restProps.checked === undefined &&
         inputRef.current
       ) {
-        inputRef.current.indeterminate = false;
+        setIndeterminate(inputRef.current, false);
       }
       if (indeterminate !== undefined && inputRef.current) {
-        inputRef.current.indeterminate = indeterminate;
+        setIndeterminate(inputRef.current, Boolean(indeterminate));
       }
       onChange && onChange(event);
     },
