d8ae13608f021e52b0002a0a75028bce2f5f3824
andrey-medvedev-vk <andrey-medvedev-vk@users.noreply.github.com>
2024-09-19 10:10:57 +0000

    Deploying to gh-pages from @ VKCOM/VKUI@b85d3daf522b82aa5b58b3244dddc8e50f621fb3 🚀

---
+++ 6.2.1-f.7285.1/playground/sb-manager/index.js
@@ -0,0 +1,1 @@
+import{Provider,Root,renderStorybookUI}from"./chunk-LFRML3ZV.js";import"./chunk-MC7RAF2B.js";import"./chunk-GUVK2GTO.js";import"./chunk-BLWCBWKL.js";import"./chunk-B3YDJJJH.js";import"./chunk-ZR5JZWHI.js";export{Provider,Root,renderStorybookUI};
