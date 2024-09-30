b11d48430afd0ff6f7f493b1b6bfe4689cf7bd1d
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2024-09-30 21:28:58 +0300

    feat(codemods): add CellButton to v7

---
+++ packages/codemods/src/transforms/v7/__tests__/cell-button.ts
@@ -0,0 +1,11 @@
+jest.autoMockOff();
+import { defineSnapshotTestFromFixture } from '../../../testHelpers/testHelper';
+
+const name = 'cell-button';
+const fixtures = ['basic'] as const;
+
+describe(name, () => {
+  fixtures.forEach((test) =>
+    defineSnapshotTestFromFixture(__dirname, name, global.TRANSFORM_OPTIONS, `${name}/${test}`),
+  );
+});
---
+++ packages/codemods/src/transforms/v7/cell-button.ts
@@ -0,0 +1,56 @@
+import { API, FileInfo, JSXAttribute } from 'jscodeshift';
+import { getImportInfo } from '../../codemod-helpers';
+import { report } from '../../report';
+import { JSCodeShiftOptions } from '../../types';
+
+export const parser = 'tsx';
+
+export default function transformer(file: FileInfo, api: API, options: JSCodeShiftOptions) {
+  const { alias } = options;
+  const j = api.jscodeshift;
+  const source = j(file.source);
+  const { localName } = getImportInfo(j, file, 'CellButton', alias);
+  if (!localName) {
+    return source.toSource();
+  }
+
+  const attributeToReplace = 'mode';
+  const newAttributeName = 'appearance';
+
+  const modeToAppearance: Record<string, string> = {
+    primary: 'neutral',
+    danger: 'negative',
+  };
+
+  const getValueFromAttribute = (attribute: JSXAttribute): string | null => {
+    if (attribute.value?.type === 'StringLiteral') {
+      return attribute.value.value;
+    }
+    if (attribute.value?.type === 'JSXExpressionContainer') {
+      const expression = attribute.value.expression;
+      if (expression.type === 'StringLiteral') {
+        return expression.value;
+      }
+    }
+    return null;
+  };
+
+  source
+    .find(j.JSXElement, { openingElement: { name: { name: localName } } })
+    .find(j.JSXAttribute, { name: { name: attributeToReplace } })
+    .forEach((path) => {
+      const component = path.node;
+      component.name.name = newAttributeName;
+      const value = getValueFromAttribute(component);
+      if (!value || !modeToAppearance[value]) {
+        report(
+          api,
+          `: ${localName} has been changed. Manual changes required: need to change 'mode' prop to 'appearance'`,
+        );
+        return;
+      }
+      component.value = j.stringLiteral(modeToAppearance[value]);
+    });
+
+  return source.toSource();
+}
