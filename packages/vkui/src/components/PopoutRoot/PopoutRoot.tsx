181219319111c37252d90cbd9c316ed9c1dac6dd
Andrey Medvedev <andrey.medvedev@vk.team>
2024-10-11 17:08:48 +0300

    Render popouts and modals in portal
    Components like Alert/ScreenSpinner/ActionSheet and
    ModalRootDesktop

--- packages/vkui/src/components/PopoutRoot/PopoutRoot.tsx
+++ packages/vkui/src/components/PopoutRoot/PopoutRoot.tsx
@@ -1,31 +1,10 @@
 'use client';
 
 import * as React from 'react';
-import { classNames } from '@vkontakte/vkjs';
 import type { HTMLAttributesWithRootRef } from '../../types';
 import { AppRootContext } from '../AppRoot/AppRootContext';
 import { RootComponent } from '../RootComponent/RootComponent';
 import styles from './PopoutRoot.module.css';
-
-/**
- * @private
- */
-export const PopoutRootPopout = ({
-  className,
-  ...restProps
-}: React.HTMLAttributes<HTMLDivElement>): React.ReactNode => (
-  <div className={classNames(styles.popout, className)} {...restProps} />
-);
-
-/**
- * @private
- */
-export const PopoutRootModal = ({
-  className,
-  ...restProps
-}: React.HTMLAttributes<HTMLDivElement>): React.ReactNode => (
-  <div className={classNames(styles.modal, className)} {...restProps} />
-);
 
 export interface PopoutRootProps extends HTMLAttributesWithRootRef<HTMLDivElement> {
   popout?: React.ReactNode;
@@ -47,7 +26,7 @@ export const PopoutRoot = ({
     <RootComponent {...restProps} baseClassName={styles.host}>
       {children}
       <div ref={popoutModalRoot}>
-        {!!popout && <PopoutRootPopout>{popout}</PopoutRootPopout>}
+        {popout}
         {modal}
       </div>
     </RootComponent>
