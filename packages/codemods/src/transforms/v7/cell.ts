8b2e3761de9439ae9d40a4319bf921b7a7187b25
e.muhamethanov <eldarmu.1m@gmail.com>
2024-10-17 17:29:52 +0300

    feat(SimpleCell,MiniInfoCell): rename expandable to chevron

---
+++ packages/codemods/src/transforms/v7/cell.ts
@@ -0,0 +1,20 @@
+import { API, FileInfo } from 'jscodeshift';
+import { getImportInfo, renameProp } from '../../codemod-helpers';
+import { JSCodeShiftOptions } from '../../types';
+
+export const parser = 'tsx';
+
+export default function transformer(file: FileInfo, api: API, options: JSCodeShiftOptions) {
+  const { alias } = options;
+  const j = api.jscodeshift;
+  const source = j(file.source);
+  const { localName } = getImportInfo(j, file, 'Cell', alias);
+
+  if (localName) {
+    renameProp(j, source, localName, {
+      expandable: 'chevron',
+    });
+  }
+
+  return source.toSource();
+}
