654adafec21d86af815771d165ed28687c1dbe43
Andrey Medvedev <andrey.medvedev@vk.team>
2024-10-30 17:09:54 +0300

    Mark ModalPopoutPortal private

--- packages/vkui/src/components/AppRoot/ModalPopoutPortal.tsx
+++ packages/vkui/src/components/AppRoot/ModalPopoutPortal.tsx
@@ -4,10 +4,14 @@ import * as React from 'react';
 import { AppRootContext } from './AppRootContext';
 import { AppRootPortal, type AppRootPortalProps } from './AppRootPortal';
 
-/* Специальный компонент для рендеринга ModalRoot и
+/**
+ * Специальный компонент для рендеринга ModalRoot и
  * popout компонентов внутри SplitLayout после контента
  * Как было раньше в v6 при передаче модалок и popout компонентов
- * в свойства SplitLayout modal/popout в режиме full */
+ * в свойства SplitLayout modal/popout в режиме full
+ *
+ * @private
+ */
 export function ModalPopoutPortal(props: AppRootPortalProps) {
   const { mode } = React.useContext(AppRootContext);
   const usePortalProp = props.usePortal;
