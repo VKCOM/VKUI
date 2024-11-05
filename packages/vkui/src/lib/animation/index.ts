56407486bddcce4552644c549bf7c8f9ff5ee17e
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2024-11-05 14:17:24 +0300

    feat: create useCSSTransition() (#7876)
    По аналогии `useCSSKeyframesAnimationController`, а также на основе компонента `CSSTransition` из [react-transitiong-group](https://reactcommunity.org/react-transition-group/css-transition) создал хук, чтобы можно было управлять переходами монтирования/размонитрования

--- packages/vkui/src/lib/animation/index.ts
+++ packages/vkui/src/lib/animation/index.ts
@@ -3,3 +3,9 @@ export { REDUCE_MOTION_MEDIA_QUERY, useReducedMotion } from './useReducedMotion'
 export { rubberbandIfOutOfBounds } from './rubberbandIfOutOfBounds';
 export { animationFadeClassNames } from './fades';
 export { transformOriginClassNames } from './transformOrigin';
+export {
+  type UseCSSTransitionState,
+  type UseCSSTransitionOptions,
+  type UseCSSTransition,
+  useCSSTransition,
+} from './useCSSTransition';
