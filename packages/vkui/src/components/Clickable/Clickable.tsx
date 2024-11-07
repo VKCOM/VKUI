VKCOM/VKUI

commit 99e70d878db7363a76044656a35d8e0021684a7b

fix(Button/Link): reset user-agent styles (#6881)

packages/vkui/src/components/Clickable/Clickable.module.css
@@ -2,6 +2,20 @@
   cursor: pointer;
 }
 
+.Clickable__resetButtonStyle {
+  appearance: none;
+  user-select: none;
+  -webkit-tap-highlight-color: none;
+  -webkit-touch-callout: none;
+  -webkit-user-drag: none;
+}
+
+.Clickable__resetLinkStyle {
+  appearance: none;
+  -webkit-tap-highlight-color: none;
+  text-decoration: none;
+}
+
 .Clickable__host:focus,
 .Clickable__host:focus-visible {
   outline: none;

packages/vkui/src/components/Clickable/Clickable.tsx
@@ -150,6 +150,16 @@ function component<T>({
   return {};
 }
 
+const getUserAgentResetClassName = (Component?: React.ElementType) => {
+  if (Component === 'a') {
+    return styles.Clickable__resetLinkStyle;
+  }
+  if (Component === 'button') {
+    return styles.Clickable__resetButtonStyle;
+  }
+  return;
+};
+
 /**
  * Базовый кликабельный корневой компонент.
  *
@@ -169,7 +179,11 @@ export const Clickable = <T,>({
 }: ClickableProps<T>) => {
   const commonProps = component(restProps);
   const isClickable = checkClickable(restProps);
-  const baseClassName = classNames(baseClassNameProp, styles['Clickable__host']);
+  const baseClassName = classNames(
+    baseClassNameProp,
+    getUserAgentResetClassName(commonProps.Component),
+    styles['Clickable__host'],
+  );
 
   if (isClickable) {
     return (

packages/vkui/src/components/Button/Button.module.css
@@ -2,12 +2,10 @@
   position: relative;
   display: inline-block;
   box-sizing: border-box;
-  text-decoration: none;
   border: 0;
   min-block-size: var(--vkui--size_button_small_height--compact);
   margin: 0;
   padding: 0;
-  user-select: none;
   border-radius: var(--vkui--size_border_radius--regular);
   max-inline-size: 100%;
   min-inline-size: var(--vkui--size_button_minimum_width--regular);

packages/vkui/src/components/Link/Link.module.css
@@ -1,15 +1,14 @@
 .Link {
   color: var(--vkui_internal--link-color, var(--vkui--color_text_link));
-  text-decoration: none;
   border: 0;
   background: none;
   margin: 0;
   padding: 0;
-  cursor: pointer;
   font-size: inherit;
   font-weight: inherit;
   font-family: inherit;
   line-height: inherit;
+  text-align: inherit;
   display: inline;
   border-radius: 0;
 }

commit 3751c473077096da0fe6d5d0d27d33643c27089e

fix(Clickable): use correct CSS property values (#7908)
h2. Описание

Поправил значения свойства `-webkit-tap-highlight-color` на валидное `transparent` вместо `none`.

- caused by #6881

**UPD**

см. https://github.com/VKCOM/VKUI/pull/7908#pullrequestreview-2420505937

- перенёс `-webkit-tap-highlight-color` в `.realClicable`;
- перенёс логику `getUserAgentResetClassName` в CSS – увеличили специфичность, но для этих стилей допустимо

h2. Release notes
h2. Исправления
- Tappable: в кликабельных компонентах исправлена проблема, когда при касании через тач появлялся системный оверлей

packages/vkui/src/components/Clickable/Clickable.tsx
@@ -175,16 +175,6 @@ function component<T>({
   return {};
 }
 
-const getUserAgentResetClassName = (Component?: React.ElementType) => {
-  if (Component === 'a') {
-    return styles.resetLinkStyle;
-  }
-  if (Component === 'button') {
-    return styles.resetButtonStyle;
-  }
-  return;
-};
-
 /**
  * Базовый кликабельный корневой компонент.
  *
@@ -204,11 +194,7 @@ export const Clickable = <T,>({
 }: ClickableProps<T>): React.ReactNode => {
   const commonProps = component(restProps);
   const isClickable = checkClickable(restProps);
-  const baseClassName = classNames(
-    baseClassNameProp,
-    getUserAgentResetClassName(commonProps.Component),
-    styles.host,
-  );
+  const baseClassName = classNames(baseClassNameProp, styles.host);
 
   if (isClickable) {
     return (