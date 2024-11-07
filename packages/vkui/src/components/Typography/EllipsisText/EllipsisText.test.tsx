VKCOM/VKUI

commit 830e238f57b60f9c8ad7f117709493ff7866213f

feat: add ellipsistext (#6692)
Компонент EllipsisText – задача компонента, через CSS, убирать текст не помещающийся в ширину контейнера в многоточие.

packages/vkui/src/components/Typography/EllipsisText/EllipsisText.tsx
@@ -1,0 +1,37 @@
+import * as React from 'react';
+import { classNames } from '@vkontakte/vkjs';
+import { HasRootRef } from '../../../types';
+import { RootComponentProps } from '../../RootComponent/RootComponent';
+import styles from './EllipsisText.module.css';
+
+export interface EllipsisTextProps
+  extends RootComponentProps<HTMLElement>,
+    HasRootRef<HTMLElement> {
+  /**
+   * Пользовательская маскимальная ширина.
+   *
+   * Используйте в случаях, когда у контейнера ширина зависит от размера контента,
+   * другими словами, когда ширина не ограничена.
+   */
+  maxWidth?: number;
+}
+
+/** Компонент ограничивает текстовый контент убирая его в многоточие.
+ *
+ * @see https://vkcom.github.io/VKUI/#/EllipsisText
+ */
+const EllipsisText = ({
+  className,
+  getRootRef,
+  children,
+  maxWidth,
+  ...restProps
+}: EllipsisTextProps) => (
+  <span ref={getRootRef} className={classNames(styles['EllipsisText'], className)} {...restProps}>
+    <span style={{ maxWidth }} className={styles['EllipsisText__content']}>
+      {children}
+    </span>
+  </span>
+);
+
+export { EllipsisText };

packages/vkui/src/components/Typography/EllipsisText/EllipsisText.test.tsx
@@ -1,0 +1,7 @@
+import * as React from 'react';
+import { baselineComponent } from '../../../testing/utils';
+import { EllipsisText } from './EllipsisText';
+
+describe('EllipsisText', () => {
+  baselineComponent((props) => <EllipsisText {...props}>EllipsisText</EllipsisText>);
+});

packages/vkui/src/components/HorizontalCell/HorizontalCell.module.css
@@ -8,6 +8,7 @@
 }
 
 .HorizontalCell__body {
+  max-inline-size: inherit;
   text-decoration: none;
   font-family: inherit;
 }
@@ -18,6 +19,7 @@
 }
 
 .HorizontalCell__content {
+  max-inline-size: inherit;
   color: var(--vkui--color_text_primary);
   word-break: break-word;
   text-overflow: ellipsis;

packages/vkui/src/components/Typography/EllipsisText/EllipsisText.e2e-playground.tsx
@@ -1,0 +1,29 @@
+import * as React from 'react';
+import { ComponentPlayground, type ComponentPlaygroundProps } from '@vkui-e2e/playground-helpers';
+import { EllipsisText, EllipsisTextProps } from './EllipsisText';
+
+export const EllipsisTextPlayground = (props: ComponentPlaygroundProps) => {
+  return (
+    <ComponentPlayground
+      {...props}
+      propSets={[
+        {
+          maxWidth: [undefined, 100, 150],
+        },
+      ]}
+    >
+      {(props: EllipsisTextProps) => (
+        <div style={{ width: 200, padding: 20 }}>
+          <EllipsisText {...props}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+          </EllipsisText>
+        </div>
+      )}
+    </ComponentPlayground>
+  );
+};

packages/vkui/src/components/Typography/EllipsisText/EllipsisText.e2e.tsx
@@ -1,0 +1,12 @@
+import * as React from 'react';
+import { test } from '@vkui-e2e/test';
+import { EllipsisTextPlayground } from './EllipsisText.e2e-playground';
+
+test('EllipsisText', async ({
+  mount,
+  expectScreenshotClippedToContent,
+  componentPlaygroundProps,
+}) => {
+  await mount(<EllipsisTextPlayground {...componentPlaygroundProps} />);
+  await expectScreenshotClippedToContent();
+});

packages/vkui/src/components/Typography/EllipsisText/EllipsisText.module.css
@@ -1,0 +1,14 @@
+.EllipsisText {
+  display: inline-flex;
+  min-inline-size: 0;
+  max-inline-size: 100%;
+}
+
+.EllipsisText__content {
+  min-inline-size: 1em;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  flex: 0 0 auto;
+  max-inline-size: inherit;
+}

packages/vkui/src/components/Typography/EllipsisText/EllipsisText.stories.tsx
@@ -1,0 +1,37 @@
+import * as React from 'react';
+import { withCartesian } from '@project-tools/storybook-addon-cartesian';
+import { Meta, StoryObj } from '@storybook/react';
+import { CanvasFullLayout } from '../../../storybook/constants';
+import { EllipsisText, EllipsisTextProps } from './EllipsisText';
+
+const story: Meta<EllipsisTextProps> = {
+  title: 'Typography/EllipsisText',
+  component: EllipsisText,
+  parameters: CanvasFullLayout,
+  decorators: [withCartesian],
+};
+
+export default story;
+
+type Story = StoryObj<EllipsisTextProps>;
+const ellipsisText =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+export const Playground: Story = {
+  render: (args) => (
+    <>
+      <div style={{ width: 200 }}>
+        <EllipsisText {...args}>{ellipsisText}</EllipsisText>
+      </div>
+      <div>
+        <EllipsisText maxWidth={100} {...args}>
+          {ellipsisText}
+        </EllipsisText>
+      </div>
+      <div>
+        <EllipsisText maxWidth={150} {...args}>
+          {ellipsisText}
+        </EllipsisText>
+      </div>
+    </>
+  ),
+};

packages/vkui/src/components/Typography/EllipsisText/__image_snapshots__/ellipsistext-android-chromium-dark-1-snap.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:014169882623b25ab24a6bf37892fd645bf68f38d6460d83059d59dbd351e02c
+size 12357

commit 9a424fd65575fbed22438213543d467a1b85567f

fix(EllipsisText): use RootComponent for host (#7909)
h2. Описание

Компонент наследует типы `RootComponent`, но сам `RootComponent` не использует из-за чего не работает параметр `Component`.

- caused by #6692

h2. Изменения

Заодно покрыл тестами параметр `disableNativeTitle`.

h2. Release notes
h2. Исправления
- EllipsisText: не срабатывал параметр `Component`

packages/vkui/src/components/Typography/EllipsisText/EllipsisText.tsx
@@ -5,7 +5,7 @@ import { classNames } from '@vkontakte/vkjs';
 import { getTextFromChildren } from '../../../lib/children';
 import { useIsomorphicLayoutEffect } from '../../../lib/useIsomorphicLayoutEffect';
 import type { HasRootRef } from '../../../types';
-import type { RootComponentProps } from '../../RootComponent/RootComponent';
+import { RootComponent, type RootComponentProps } from '../../RootComponent/RootComponent';
 import styles from './EllipsisText.module.css';
 
 export interface EllipsisTextProps
@@ -37,8 +37,8 @@ export interface EllipsisTextProps
  * @see https://vkcom.github.io/VKUI/#/EllipsisText
  */
 const EllipsisText = ({
+  Component = 'span',
   className,
-  getRootRef,
   children,
   maxWidth,
   maxLines = 1,
@@ -54,8 +54,8 @@ const EllipsisText = ({
   }, [contentRef, maxLines]);
 
   return (
-    <span
-      ref={getRootRef}
+    <RootComponent
+      Component={Component}
       className={classNames(
         styles.host,
         disableNativeTitle && styles.disableNativeTitle,
@@ -71,7 +71,7 @@ const EllipsisText = ({
       >
         {children}
       </span>
-    </span>
+    </RootComponent>
   );
 };
 

packages/vkui/src/components/Typography/EllipsisText/EllipsisText.test.tsx
@@ -1,24 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { baselineComponent } from '../../../testing/utils';
 import { EllipsisText } from './EllipsisText';
 import styles from './EllipsisText.module.css';
 
-describe('EllipsisText', () => {
+const mockText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+describe(EllipsisText, () => {
   baselineComponent((props) => <EllipsisText {...props}>EllipsisText</EllipsisText>);
 
-  it('should have specific className when maxLines > 1', async () => {
-    render(
+  it('should have specific className when maxLines > 1', () => {
+    const result = render(
       <EllipsisText maxLines={2} data-testid="text-wrapper">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        {mockText}
       </EllipsisText>,
     );
 
-    const text = screen.getByTestId('text-wrapper').firstElementChild as HTMLElement;
+    const text = result.getByTestId('text-wrapper').firstElementChild as HTMLElement;
     expect(text).not.toBeNull();
     expect(text).toHaveClass(styles.contentMultiline);
+  });
+
+  it('should not apply title attribute', () => {
+    const result = render(<EllipsisText data-testid="host">{mockText}</EllipsisText>);
+    expect(result.getByTestId('host')).toHaveAttribute('title');
+    expect(result.getByTestId('host')).not.toHaveClass(styles.disableNativeTitle);
+
+    result.rerender(
+      <EllipsisText data-testid="host" disableNativeTitle>
+        {mockText}
+      </EllipsisText>,
+    );
+    expect(result.getByTestId('host')).not.toHaveAttribute('title');
+    expect(result.getByTestId('host')).toHaveClass(styles.disableNativeTitle);
   });
 });