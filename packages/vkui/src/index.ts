4fe261bf1c1ea3ee4e62472c4b7e885b4ed6f86a
Eldar <61377022+EldarMuhamethanov@users.noreply.github.com>
2024-12-03 14:27:39 +0300

    feat(Popover): add usePopover hook to select anchor of popover (#7969)
    * feat(Popover): add usePopover hook to select anchor of popover
    
    * fix(usePopover): fix type UsePopoverProps
    
    * dox(Popover): add section to usePopover using

--- packages/vkui/src/index.ts
+++ packages/vkui/src/index.ts
@@ -415,6 +415,7 @@ export type {
   PopoverArrowProps,
   PopoverContentRenderProp,
 } from './components/Popover/Popover';
+export { usePopover } from './components/Popover/usePopover';
 
 /**
  * HOCs
