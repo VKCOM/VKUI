5cb14c4a9d924030ad939bf159b923829c31a23f
Eldar <61377022+EldarMuhamethanov@users.noreply.github.com>
2024-12-16 16:16:41 +0300

    feat(AspectRatio): extract ratio prop type (#8088)

--- packages/vkui/src/components/AspectRatio/AspectRatio.test.tsx
+++ packages/vkui/src/components/AspectRatio/AspectRatio.test.tsx
@@ -1,6 +1,25 @@
+import { render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { AspectRatio } from './AspectRatio';
+import styles from './AspectRatio.module.css';
 
 describe(AspectRatio, () => {
   baselineComponent(AspectRatio);
+
+  it('check mode stretch has specific className', () => {
+    render(<AspectRatio data-testid="ratio" ratio={16 / 9} mode="stretch" />);
+    expect(screen.getByTestId('ratio')).toHaveClass(styles.modeStretch);
+  });
+
+  it('check using custom ratio', () => {
+    const { rerender } = render(
+      <AspectRatio data-testid="ratio" ratio="var(--custom-aspect-ratio-var)" />,
+    );
+    expect(screen.getByTestId('ratio')).toHaveStyle(
+      '--vkui_internal--aspect_ratio: var(--custom-aspect-ratio-var);',
+    );
+
+    rerender(<AspectRatio data-testid="ratio" ratio="calc(16 / 9)" />);
+    expect(screen.getByTestId('ratio')).toHaveStyle('--vkui_internal--aspect_ratio: calc(16 / 9);');
+  });
 });
