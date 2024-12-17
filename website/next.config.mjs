3f621c992ec123050580c4d239470c36e6834103
Victoria Zhizhonkova <indarklight@gmail.com>
2024-12-17 19:28:02 +0700

    feat(docs): add infra files

---
+++ website/next.config.mjs
@@ -0,0 +1,31 @@
+import analyzer from '@next/bundle-analyzer';
+import nextra from 'nextra';
+
+const withBundleAnalyzer = analyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const withNextra = nextra({
+  theme: '@vkontakte/vkui-docs-theme',
+  themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true,
+  staticImage: false,
+  // ... your Nextra config
+});
+
+export default withBundleAnalyzer(
+  withNextra({
+    // ... your Next.js config
+    transpilePackages: ['@vkontakte/vkui-docs-theme', '@vkontakte/vkui'],
+    modularizeImports: {
+      '@vkontakte/vkui': {
+        transform: '@vkontakte/vkui/dist/cssm',
+        skipDefaultConversion: true,
+      },
+    },
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+  }),
+);
