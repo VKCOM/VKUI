62448df10fca26055a336015015794cafe7792fb
Eldar <61377022+EldarMuhamethanov@users.noreply.github.com>
2025-01-28 14:12:05 +0300

    fix(RichCell): revert fix with after overflow (#8199) (#8202)
    (cherry picked from commit 40a7218a674a5f7018dbaa69963bc7f76174d700)

--- packages/vkui/src/components/RichCell/RichCell.e2e-playground.tsx
+++ packages/vkui/src/components/RichCell/RichCell.e2e-playground.tsx
@@ -3,8 +3,15 @@ import { ComponentPlayground, type ComponentPlaygroundProps } from '@vkui-e2e/pl
 import { Avatar } from '../Avatar/Avatar';
 import { Button } from '../Button/Button';
 import { ButtonGroup } from '../ButtonGroup/ButtonGroup';
+import { Flex } from '../Flex/Flex';
+import { Text } from '../Typography/Text/Text';
 import { UsersStack } from '../UsersStack/UsersStack';
 import { RichCell, type RichCellProps } from './RichCell';
+
+const longOverTitle = 'Subhead subhead subhead subhead';
+const longChildren = 'Children children children children';
+const longSubtitle = 'Subtitle subtitle subtitle subtitle';
+const longExtraSubtitle = 'Extra subtitle Extra subtitle';
 
 export const RichCellPlayground = (props: ComponentPlaygroundProps) => {
   return (
@@ -13,10 +20,10 @@ export const RichCellPlayground = (props: ComponentPlaygroundProps) => {
       propSets={[
         {
           before: [<Avatar size={72} key="72" />],
-          overTitle: ['Subhead subhead subhead subhead'],
-          children: ['Children children children children'],
-          subtitle: ['Text text text text text text'],
-          extraSubtitle: ['Caption caption caption caption'],
+          overTitle: [longOverTitle],
+          children: [longChildren],
+          subtitle: [longSubtitle],
+          extraSubtitle: [longExtraSubtitle],
           after: ['After'],
           afterAlign: ['start', 'center', 'end'],
           afterCaption: ['After Caption'],
@@ -35,10 +42,10 @@ export const RichCellPlayground = (props: ComponentPlaygroundProps) => {
         },
         {
           before: [<Avatar size={72} key="72" />],
-          overTitle: ['Subhead subhead subhead subhead'],
-          children: ['Children children children children'],
-          subtitle: ['Text text text text text text'],
-          extraSubtitle: ['Caption caption caption caption'],
+          overTitle: [longOverTitle],
+          children: [longChildren],
+          subtitle: [longSubtitle],
+          extraSubtitle: [longExtraSubtitle],
           after: ['After'],
           afterCaption: ['After Caption'],
           bottom: [
@@ -56,6 +63,14 @@ export const RichCellPlayground = (props: ComponentPlaygroundProps) => {
         },
         {
           before: [<Avatar size={48} key="48" />],
+          children: [longChildren],
+          subtitle: [longSubtitle],
+          extraSubtitle: [longExtraSubtitle],
+          after: [<Button key="After">Подписаться</Button>],
+          afterAlign: ['start', 'center'],
+        },
+        {
+          before: [<Avatar size={48} key="48" />],
           children: ['Михаил Лихачев'],
           overTitle: ['Команда ВКонтакте, Санкт-Петербург'],
           after: [
@@ -67,9 +82,25 @@ export const RichCellPlayground = (props: ComponentPlaygroundProps) => {
         {
           before: [<Avatar size={24} key="24" />],
           children: ['Михаил'],
-          after: ['Very very very very very very very long after'],
+          after: [
+            <div key="after" style={{ maxWidth: 150 }}>
+              Very very very very very very very long after
+            </div>,
+          ],
           beforeAlign: ['center', 'end'],
           contentAlign: ['center', 'end'],
+        },
+        // Проверяем, что при отсутствии after контент в основной части рястягивается на всю доступную ширину
+        {
+          children: ['Children'],
+          overTitle: ['Over title'],
+          extraSubtitle: [
+            <Flex key="extraSubtitle" justify="space-between" align="center">
+              <Button>Кнопка 1</Button>
+              <Text>Проможуточный текст</Text>
+              <Button>Кнопка 2</Button>
+            </Flex>,
+          ],
         },
       ]}
     >
