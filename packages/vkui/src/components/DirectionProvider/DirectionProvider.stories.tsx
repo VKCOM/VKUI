a869e69a3af17f74bf430dec2f105dceae7fb087
e.muhamethanov <eldarmu.1m@gmail.com>
2025-02-04 14:09:36 +0300

    feat(DirectionProvider): add story

---
+++ packages/vkui/src/components/DirectionProvider/DirectionProvider.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useConfigDirection } from '../../hooks/useConfigDirection';
+import { CanvasFullLayout, DisableCartesianParam } from '../../storybook/constants';
+import { Div } from '../Div/Div';
+import { Flex } from '../Flex/Flex';
+import { DirectionProvider, type DirectionProviderProps } from './DirectionProvider';
+
+const story: Meta<DirectionProviderProps> = {
+  title: 'Service/DirectionProvider',
+  component: DirectionProvider,
+  parameters: { ...CanvasFullLayout, ...DisableCartesianParam },
+};
+
+export default story;
+
+const Fixture = () => {
+  const dir = useConfigDirection();
+
+  return (
+    <Flex dir={dir}>
+      <Div>1</Div>
+      <Div>2</Div>
+      <Div>3</Div>
+    </Flex>
+  );
+};
+
+export const Playground: StoryObj<DirectionProviderProps> = {
+  render: (args) => (
+    <DirectionProvider {...args}>
+      <Fixture />
+    </DirectionProvider>
+  ),
+};
