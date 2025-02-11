b5db80c2bc71884111cd3f5509586147e5048d09
Eldar <61377022+EldarMuhamethanov@users.noreply.github.com>
2025-02-11 16:12:04 +0300

    doc: add @since to doc
    Co-authored-by: Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>

--- packages/vkui/src/components/DirectionProvider/DirectionProvider.tsx
+++ packages/vkui/src/components/DirectionProvider/DirectionProvider.tsx
@@ -10,6 +10,7 @@ export interface DirectionProviderProps {
 /**
  * Компонент, прокидывающий направление контента
  * @see https://vkcom.github.io/VKUI/#/DirectionProvider
+ * @since 7.2.0
  */
 export function DirectionProvider({ value, children }: DirectionProviderProps): React.ReactNode {
   return <ConfigProviderOverride direction={value}>{children}</ConfigProviderOverride>;
