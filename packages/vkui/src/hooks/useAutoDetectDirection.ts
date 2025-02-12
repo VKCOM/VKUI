4919a8153ecbc58a0a75967e6354163ecc5bc5ca
e.muhamethanov <eldarmu.1m@gmail.com>
2025-02-12 13:32:11 +0300

    fix: improve auto detect direction

--- packages/vkui/src/hooks/useAutoDetectDirection.ts
+++ packages/vkui/src/hooks/useAutoDetectDirection.ts
@@ -1,24 +1,20 @@
 import * as React from 'react';
 import { type Direction } from '../lib/direction';
 import { useDOM } from '../lib/dom';
-import { useIsomorphicLayoutEffect } from '../lib/useIsomorphicLayoutEffect';
 
 export const useAutoDetectDirection = (directionProp: Direction): Direction => {
-  const [direction, setDirection] = React.useState<Direction>(directionProp || 'ltr');
   const { window, document } = useDOM();
 
-  const update = () => {
+  const getGlobalDir = () => {
     if (directionProp) {
-      setDirection(directionProp);
-      return;
+      return directionProp;
     }
     if (!window || !document) {
-      return;
+      return 'ltr';
     }
     const styleDeclaration = window.getComputedStyle(document.body);
-    setDirection(styleDeclaration.direction as Direction);
+    return styleDeclaration.direction as Direction;
   };
-  useIsomorphicLayoutEffect(update, [window, document, directionProp]);
 
-  return direction;
+  return React.useMemo(() => getGlobalDir(), [directionProp, document, window]);
 };
