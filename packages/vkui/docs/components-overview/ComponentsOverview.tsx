873bcbb183f30360d7eb8a0241cf5b9af5d90489
e.muhamethanov <eldarmu.1m@gmail.com>
2025-02-19 15:24:21 +0300

    fix: fix reset scroll when searching in Component overview

--- packages/vkui/docs/components-overview/ComponentsOverview.tsx
+++ packages/vkui/docs/components-overview/ComponentsOverview.tsx
@@ -12,15 +12,58 @@ import {
   Title,
 } from '../../src';
 import { FlexItem } from '../../src/components/Flex/FlexItem/FlexItem';
+import { type Direction } from '../../src/hooks/useDirection';
 import { ComponentOverviewCardWrapper } from './components/ComponentOverviewCardWrapper';
 import { useGetConfigByQuery } from './hooks/useGetConfigByQuery';
 import { useGetGlobalParams } from './hooks/useGetGlobalParams';
 import styles from './ComponentsOverview.module.css';
 
-export const ComponentsOverview = () => {
+const ComponentsOverview: React.FC<{ direction: Direction }> = ({ direction }) => {
+  const { query: searchedQuery, config, loading, onUpdateQuery } = useGetConfigByQuery();
+
+  return (
+    <>
+      <Flex direction="column" gap="2xl" align="start" className={styles.header}>
+        <FlexItem>
+          <Title>Витрина компонентов</Title>
+        </FlexItem>
+
+        <FlexItem>
+          <Group separator="hide">
+            <Search noPadding onChange={onUpdateQuery} />
+          </Group>
+        </FlexItem>
+      </Flex>
+
+      <Flex direction="column" gap="3xl">
+        {loading && <Spinner />}
+        {!loading && !Object.values(config).length && <Footer>Ничего не найдено</Footer>}
+        {Object.entries(config).map(([groupKey, groupData]) => {
+          return (
+            <Flex key={groupKey} direction="column" gap="xl">
+              <Title level="2">{groupData.title}</Title>
+              <ul className={styles.cardsContainer}>
+                {groupData.components.map((componentName) => (
+                  <ComponentOverviewCardWrapper
+                    key={componentName}
+                    searchedQuery={searchedQuery}
+                    componentName={componentName}
+                    groupTitle={groupData.title}
+                    direction={direction}
+                  />
+                ))}
+              </ul>
+            </Flex>
+          );
+        })}
+      </Flex>
+    </>
+  );
+};
+
+export const ComponentsOverviewPage = () => {
   const { colorScheme, platform, direction, hasCustomPanelHeaderAfter, hasPointer } =
     useGetGlobalParams();
-  const { query: searchedQuery, config, loading, onUpdateQuery } = useGetConfigByQuery();
 
   return (
     <ConfigProvider
@@ -30,40 +73,7 @@ export const ComponentsOverview = () => {
     >
       <AdaptivityProvider hasPointer={hasPointer}>
         <AppRoot className="sb-unstyled" dir={direction}>
-          <Flex direction="column" gap="2xl" align="start" className={styles.header}>
-            <FlexItem>
-              <Title>Витрина компонентов</Title>
-            </FlexItem>
-
-            <FlexItem>
-              <Group separator="hide">
-                <Search noPadding onChange={onUpdateQuery} />
-              </Group>
-            </FlexItem>
-          </Flex>
-
-          <Flex direction="column" gap="3xl">
-            {loading && <Spinner />}
-            {!loading && !Object.values(config).length && <Footer>Ничего не найдено</Footer>}
-            {Object.entries(config).map(([groupKey, groupData]) => {
-              return (
-                <Flex key={groupKey} direction="column" gap="xl">
-                  <Title level="2">{groupData.title}</Title>
-                  <ul className={styles.cardsContainer}>
-                    {groupData.components.map((componentName) => (
-                      <ComponentOverviewCardWrapper
-                        key={componentName}
-                        searchedQuery={searchedQuery}
-                        componentName={componentName}
-                        groupTitle={groupData.title}
-                        direction={direction}
-                      />
-                    ))}
-                  </ul>
-                </Flex>
-              );
-            })}
-          </Flex>
+          <ComponentsOverview direction={direction} />
         </AppRoot>
       </AdaptivityProvider>
     </ConfigProvider>
