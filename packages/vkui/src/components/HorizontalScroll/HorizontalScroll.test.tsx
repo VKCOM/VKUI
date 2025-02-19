435c824082e3efb8a029ca3e4a25b28d63d134f8
e.muhamethanov <eldarmu.1m@gmail.com>
2025-02-19 14:13:14 +0300

    test(HorizontalScroll): fix tests

--- packages/vkui/src/components/HorizontalScroll/HorizontalScroll.test.tsx
+++ packages/vkui/src/components/HorizontalScroll/HorizontalScroll.test.tsx
@@ -195,39 +195,6 @@ describe('HorizontalScroll', () => {
       expect(mockedData.scrollLeft).toBe(200);
     });
   });
-
-  it('scroll by arrow', () => {
-    const ref: React.RefObject<HTMLDivElement | null> = {
-      current: null,
-    };
-    render(
-      <HorizontalScroll
-        getRef={ref}
-        data-testid="horizontal-scroll"
-        prevButtonTestId="scroll-arrow-left"
-        nextButtonTestId="scroll-arrow-right"
-      >
-        <div style={{ width: '1800px', height: '50px' }} />
-      </HorizontalScroll>,
-    );
-
-    const mockedData = setup(ref.current!, 50);
-
-    fireEvent.mouseEnter(screen.getByTestId('horizontal-scroll'));
-
-    const arrowLeft = screen.getByTestId('scroll-arrow-left');
-    const arrowRight = screen.getByTestId('scroll-arrow-right');
-
-    fireEvent.wheel(arrowRight, {
-      deltaX: 20,
-    });
-    expect(mockedData.scrollLeft).toBe(70);
-
-    fireEvent.wheel(arrowLeft, {
-      deltaX: 20,
-    });
-    expect(mockedData.scrollLeft).toBe(90);
-  });
 });
 
 function mockRef(element: HTMLDivElement) {
--- packages/vkui/src/components/HorizontalScroll/HorizontalScroll.tsx
+++ packages/vkui/src/components/HorizontalScroll/HorizontalScroll.tsx
@@ -190,7 +190,6 @@ export const HorizontalScroll = ({
   scrollOnAnyWheel = false,
   prevButtonTestId,
   nextButtonTestId,
-  getRootRef,
   // ContentWrapper
   ContentWrapperComponent = 'div',
   contentWrapperRef,
