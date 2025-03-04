7bedb9fc5602af7100f3e32b5bbe19e7c7d63373
Victoria Zhizhonkova <indarklight@gmail.com>
2025-03-04 15:31:53 +0700

    feat(CellButtonGroup): add component

---
+++ packages/vkui/src/components/CellButtonGroup/CellButtonGroup.e2e-playground.tsx
@@ -0,0 +1,18 @@
+import { ComponentPlayground, type ComponentPlaygroundProps } from '@vkui-e2e/playground-helpers';
+import { CellButton } from '../CellButton/CellButton';
+import { CellButtonGroup, type CellButtonGroupProps } from './CellButtonGroup';
+
+export const CellButtonGroupPlayground = (props: ComponentPlaygroundProps) => {
+  return (
+    <ComponentPlayground {...props}>
+      {(props: CellButtonGroupProps) => (
+        <CellButtonGroup {...props}>
+          <CellButton appearance="negative" centered>
+            Пожаловаться
+          </CellButton>
+          <CellButton centered>Отмена</CellButton>
+        </CellButtonGroup>
+      )}
+    </ComponentPlayground>
+  );
+};
