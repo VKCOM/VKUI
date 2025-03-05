8fda57c542edd7db964f06a2a07dc3d8978e0cf4
e.muhamethanov <eldarmu.1m@gmail.com>
2025-03-05 10:32:56 +0300

    test: add test to hook with auto close snackbar

--- packages/vkui/src/components/Snackbar/useSnackbar.test.tsx
+++ packages/vkui/src/components/Snackbar/useSnackbar.test.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { act, render, screen } from '@testing-library/react';
+import { waitCSSKeyframesAnimation } from '../../testing/utils';
 import { type SnackbarApi, useSnackbar } from './useSnackbar';
 
 const TestComponent: React.FC<{ apiRef: React.RefObject<SnackbarApi | null> }> = ({ apiRef }) => {
@@ -24,7 +25,7 @@ describe('useSnackbar', () => {
 
     await act(() => apiRef.current?.open({ children: 'Test Snackbar' }));
 
-    expect(screen.getByText('Test Snackbar')).toBeInTheDocument();
+    expect(screen.queryByText('Test Snackbar')).toBeInTheDocument();
   });
 
   it('try to open two snackbars in same placement', async () => {
@@ -40,14 +41,55 @@ describe('useSnackbar', () => {
   it('should open multiple snackbars in different placements', async () => {
     render(<TestComponent apiRef={apiRef} />);
 
-    await act(async () =>
-      apiRef.current?.open({ children: 'Test Snackbar 1', placement: 'top-start' }),
-    );
-    await act(async () =>
-      apiRef.current?.open({ children: 'Test Snackbar 2', placement: 'bottom-end' }),
+    act(() => {
+      apiRef.current?.open({ children: 'Test Snackbar 1', placement: 'top-start' });
+    });
+    act(() => {
+      apiRef.current?.open({ children: 'Test Snackbar 2', placement: 'bottom-end' });
+    });
+
+    expect(screen.queryByText('Test Snackbar 1')).toBeInTheDocument();
+    expect(screen.queryByText('Test Snackbar 2')).toBeInTheDocument();
+
+    act(() => {
+      apiRef.current?.closeAll();
+    });
+
+    expect(screen.queryByText('Test Snackbar 1')).not.toBeInTheDocument();
+    expect(screen.queryByText('Test Snackbar 2')).not.toBeInTheDocument();
+  });
+
+  it('should close a snackbar when using the API', async () => {
+    render(<TestComponent apiRef={apiRef} />);
+
+    let snackbarId: string | null = null;
+
+    act(() => {
+      snackbarId = apiRef.current?.open({ children: 'Test Snackbar to close' }) || null;
+    });
+    expect(screen.queryByText('Test Snackbar to close')).toBeInTheDocument();
+
+    act(() => apiRef.current?.close(snackbarId!));
+    expect(screen.queryByText('Test Snackbar to close')).not.toBeInTheDocument();
+  });
+
+  it('should close snackbar automatically after duration', async () => {
+    jest.useFakeTimers();
+    const onClose = jest.fn();
+    render(<TestComponent apiRef={apiRef} />);
+
+    await act(() =>
+      apiRef.current?.open({
+        children: 'Auto Close Snackbar',
+        duration: 1000,
+        onClose,
+      }),
     );
 
-    expect(screen.getByText('Test Snackbar 1')).toBeInTheDocument();
-    expect(screen.getByText('Test Snackbar 2')).toBeInTheDocument();
+    expect(screen.queryByText('Auto Close Snackbar')).toBeInTheDocument();
+    await waitCSSKeyframesAnimation(screen.getByRole('alert'), { runOnlyPendingTimers: true });
+
+    expect(onClose).toHaveBeenCalledTimes(1);
+    expect(screen.queryByText('Auto Close Snackbar')).not.toBeInTheDocument();
   });
 });
