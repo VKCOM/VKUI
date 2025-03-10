d8b4b0aa8cb031e9105c10482c1b2d5990c3d6fc
e.muhamethanov <eldarmu.1m@gmail.com>
2025-03-10 12:01:38 +0300

    fix: add throttling to update visibility of go up button

--- packages/vkui/docs/common/components/GoToUpButton.tsx
+++ packages/vkui/docs/common/components/GoToUpButton.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Icon24ArrowUpOutline } from '@vkontakte/icons';
-import { classNames } from '@vkontakte/vkjs';
+import { classNames, throttle } from '@vkontakte/vkjs';
 import { Button } from '../../../src';
 import { useManualScroll } from '../../../src/components/AppRoot/ScrollContext';
 import { useGlobalEventListener } from '../../../src/hooks/useGlobalEventListener';
@@ -14,9 +14,12 @@ export const GoToUpButton = () => {
   const { scrollTo, getScroll } = useManualScroll();
   const { window } = useDOM();
 
-  useGlobalEventListener(window, 'scroll', () => {
-    setVisible(getScroll().y >= (window?.innerHeight || 0));
-  });
+  const updateVisibility = useMemo(
+    () => throttle(() => setVisible(getScroll().y >= (window?.innerHeight || 0)), 1000),
+    [getScroll, window],
+  );
+
+  useGlobalEventListener(window, 'scroll', updateVisibility);
 
   return (
     <div className={classNames(styles.upButton, !visible && styles.hidden)}>
