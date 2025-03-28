13bebafca87a83393a744f7c544915c55b68ea26
Victoria Zhizhonkova <indarklight@gmail.com>
2025-03-28 23:22:49 +0700

    fix(ModalPage, ModalCard): fix id warn (#8406)
    * fix(ModalCard): id not required
    
    * fix(ModalPage) id not required
    
    * fix(ModalPage, ModalCard): move is check

--- packages/vkui/src/components/ModalPage/ModalPage.tsx
+++ packages/vkui/src/components/ModalPage/ModalPage.tsx
@@ -1,16 +1,12 @@
 'use client';
 
-import { useId, useMemo } from 'react';
+import { useMemo } from 'react';
 import { ModalContext } from '../../context/ModalContext';
 import { inRange } from '../../helpers/range';
-import { getNavId } from '../../lib/getNavId';
 import { SNAP_POINT_DETENTS, SNAP_POINT_SAFE_RANGE, type SnapPoint } from '../../lib/sheet';
-import { warnOnce } from '../../lib/warnOnce';
 import { useModalManager } from '../ModalRoot/useModalManager';
 import { ModalPageInternal } from './ModalPageInternal';
 import type { ModalPageProps } from './types';
-
-const warn = warnOnce('ModalPage');
 
 const snapPointCache = new Map<string, Exclude<SnapPoint, 'auto'>>();
 
@@ -33,11 +29,8 @@ export const ModalPage = ({
   keepMounted = false,
   ...restProps
 }: ModalPageProps) => {
-  const generatingId = useId();
-  const id = getNavId({ nav, id: idProp }, warn) || generatingId;
-
-  const { mounted, shouldPreserveSnapPoint, ...resolvedProps } = useModalManager({
-    id,
+  const { mounted, shouldPreserveSnapPoint, id, ...resolvedProps } = useModalManager({
+    id: nav || idProp,
     open,
     keepMounted,
     modalOverlayTestId,
