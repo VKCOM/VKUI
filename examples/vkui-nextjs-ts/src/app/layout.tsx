4de123be6961143fc2b4346a8e579b22d3605d9d
e.muhamethanov <eldarmu.1m@gmail.com>
2025-04-01 18:14:03 +0300

    fix: add meta to adaptivity

--- examples/vkui-nextjs-ts/src/app/layout.tsx
+++ examples/vkui-nextjs-ts/src/app/layout.tsx
@@ -4,6 +4,13 @@ import { Layout } from '@/client/Layout';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru" suppressHydrationWarning>
+      <head>
+        <title>NextJS + VKUI + TS</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </head>
       <body>
         <Layout>{children}</Layout>
       </body>
