b957be0295eaae371a3efaf905e1f54e1e3e5552
Andrey Medvedev <andrey.medvedev@vk.team>
2025-04-20 08:39:30 +0300

    Add DateInput examples in Storybook for QA

--- packages/vkui/src/components/DateInput/DateInput.stories.tsx
+++ packages/vkui/src/components/DateInput/DateInput.stories.tsx
@@ -1,10 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
+import { isToday, isYesterday } from 'date-fns';
 import { CanvasFullLayout, DisableCartesianParam, StringArg } from '../../storybook/constants';
 import { createCalendarDayRenderField } from '../../testing/presets/createCalendarDayRenderField';
 import { createCalendarTimezoneField } from '../../testing/presets/createCalendarTimezoneField';
 import { getFormFieldIconsPresets } from '../../testing/presets/getFormFieldIconsPresets';
 import { createStoryParameters } from '../../testing/storybook/createStoryParameters';
+import { FormItem } from '../FormItem/FormItem';
+import { Group } from '../Group/Group';
 import { DateInput, type DateInputProps } from './DateInput';
 
 const iconsPresets = getFormFieldIconsPresets();
@@ -36,5 +39,84 @@ export const Playground: Story = {
     const renderCustomValue = () => renderCustomValueProp;
 
     return <DateInput value={parsedValue} renderCustomValue={renderCustomValue} {...args} />;
+  },
+};
+
+export const Example: Story = {
+  render: () => {
+    return (
+      <Group>
+        <Group header="Пример без accessible флага">
+          <FormItem top="Дата рождения" htmlFor="no-accessible-flag">
+            <DateInput id="no-accessible-flag" aria-label="Дата рождения" />
+          </FormItem>
+        </Group>
+
+        <Group header="Пример c флагом accessible">
+          <FormItem top="Дата свадьбы" htmlFor="with-accessible-flag">
+            <DateInput accessible id="with-accessible-flag" aria-label="Дата свадьбы" />
+          </FormItem>
+        </Group>
+
+        <Group header="Пример даты со временем">
+          <FormItem top="Дата начала мероприятия" htmlFor="with-time">
+            <DateInput accessible enableTime id="with-time" aria-label="Дата начала мероприятия" />
+          </FormItem>
+        </Group>
+
+        <Group header="Со значением по умолчанию">
+          <FormItem top="Начало рекламной кампании" htmlFor="with-default-value">
+            <DateInput
+              defaultValue={new Date()}
+              accessible
+              enableTime
+              id="with-default-value"
+              aria-label="Начало рекламной кампании"
+            />
+          </FormItem>
+        </Group>
+
+        <Group header="Будущие даты заблокированы">
+          <FormItem top="Дата рождения" htmlFor="future-dates-disabled">
+            <DateInput
+              accessible
+              enableTime
+              disableFuture
+              id="future-dates-disabled"
+              aria-label="Дата рождения"
+            />
+          </FormItem>
+        </Group>
+
+        <Group header="С кастомным форматом отображаемой даты">
+          <FormItem top="Дата выхода фильма" htmlFor="render-custom-value">
+            <DateInput
+              accessible
+              defaultValue={new Date()}
+              id="render-custom-value"
+              aria-label="Дата выхода фильма"
+              renderCustomValue={(date) => {
+                if (!date) {
+                  return undefined;
+                }
+                if (isToday(date)) {
+                  return 'Сегодня';
+                }
+                if (isYesterday(date)) {
+                  return 'Вчера';
+                }
+                return undefined;
+              }}
+            />
+          </FormItem>
+        </Group>
+
+        <Group header="Без явного дублирования label">
+          <FormItem top="Дата публикации" htmlFor="without-label-in-date-input">
+            <DateInput accessible defaultValue={new Date()} id="without-label-in-date-input" />
+          </FormItem>
+        </Group>
+      </Group>
+    );
   },
 };
