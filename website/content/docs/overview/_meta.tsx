c1ebbfab0dc9a6c050f30fb2c3e795114c09a77a
Victoria Zhizhonkova <indarklight@gmail.com>
2025-05-21 16:49:04 +0700

    fix: rewrite docs + add sidebar separator

--- website/content/docs/overview/_meta.tsx
+++ website/content/docs/overview/_meta.tsx
@@ -4,11 +4,19 @@ const meta: MetaRecord = {
   'about': 'Обзор',
   'install': 'Установка',
   'usage': 'Использование',
+  '#': {
+    type: 'separator',
+    title: 'Погружение',
+  },
   'navigation': 'Навигация',
   'adaptivity': 'Адаптивность',
   'ssr': 'Серверный рендеринг',
-  'themes': 'Темы',
+  '##': {
+    type: 'separator',
+    title: 'Кастомизация',
+  },
   'design-tokens': 'Дизайн-токены',
+  'themes': 'Темы',
 };
 
 export default meta;
