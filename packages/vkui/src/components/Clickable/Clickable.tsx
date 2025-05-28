b6796118e629120b8855ef1f365f4b82b2470c27
Victoria Zhizhonkova <indarklight@gmail.com>
2025-05-28 17:04:18 +0700

    fix(Clickable): ignore undefined Component

--- packages/vkui/src/components/Clickable/Clickable.tsx
+++ packages/vkui/src/components/Clickable/Clickable.tsx
@@ -120,6 +120,7 @@ export const Clickable = <T,>(props: ClickableProps<T>): React.ReactNode => {
   const {
     baseClassName,
     disabled, // Игнорируем disabled из пропсов, т.к. он обрабатывается в commonProps
+    Component: ignore,
     ...restProps
   } = props;
 
