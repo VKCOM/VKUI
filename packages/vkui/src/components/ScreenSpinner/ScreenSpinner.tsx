f1dd9b57b3fd8976103e9a2c7eea45cf4c24389e
Daniil Suvorov <severecloud@gmail.com>
2025-06-06 17:26:17 +0300

    feat: add delayVisibility (#8614)
    h2. Описание
    
    Добавляем для компонентов загрузки свойство `visibilityDelay ` которое скрывает элемент в начале отрисовки на заданное количество миллисекунд
    
    h2. Release notes
    h2. Улучшения
    - [Spinner](https://vkcom.github.io/VKUI/${version}/#/Spinner), [ScreenSpinner](https://vkcom.github.io/VKUI/${version}/#/ScreenSpinner), [PanelSpinner](https://vkcom.github.io/VKUI/${version}/#/PanelSpinner), [Skeleton](https://vkcom.github.io/VKUI/${version}/#/Skeleton): свойство `visibilityDelay ` скрывает компонент в начале отрисовки на заданное количество миллисекунд

--- packages/vkui/src/components/ScreenSpinner/ScreenSpinner.tsx
+++ packages/vkui/src/components/ScreenSpinner/ScreenSpinner.tsx
@@ -29,6 +29,7 @@ export const ScreenSpinner: React.FC<ScreenSpinnerProps> & {
   label,
   customIcon,
   usePortal,
+  visibilityDelay,
   ...restProps
 }: ScreenSpinnerProps): React.ReactNode => {
   useScrollLock();
@@ -36,7 +37,13 @@ export const ScreenSpinner: React.FC<ScreenSpinnerProps> & {
   return (
     <AppRootPortal usePortal={usePortal}>
       <PopoutWrapper className={className} style={style} noBackground strategy="fixed">
-        <ScreenSpinnerContainer state={state} mode={mode} label={label} customIcon={customIcon}>
+        <ScreenSpinnerContainer
+          state={state}
+          mode={mode}
+          label={label}
+          customIcon={customIcon}
+          visibilityDelay={visibilityDelay}
+        >
           <ScreenSpinnerLoader {...restProps} />
           <ScreenSpinnerSwapIcon onClick={onClick} cancelLabel={cancelLabel} />
         </ScreenSpinnerContainer>
