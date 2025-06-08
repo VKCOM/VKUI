adb080454fe160a59aa96a9ef056569a522712bf
e.muhamethanov <eldarmu.1m@gmail.com>
2025-06-08 13:16:40 +0300

    fix(storybook): fix inifinite list updating

--- packages/vkui/src/lib/object.ts
+++ packages/vkui/src/lib/object.ts
@@ -12,3 +12,22 @@ export function mapObject<T extends Object, R extends Record<keyof T, any>>(
     {} as R,
   );
 }
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function filterObject<T extends Object, R extends Record<keyof T, any>>(
+  object: T,
+  filter: (value: T[keyof T], key: keyof T) => boolean,
+): R {
+  return Object.entries(object).reduce(
+    (acc, [key, value]) => {
+      if (filter(value, key as keyof T)) {
+        Object.assign(acc, {
+          [key]: value,
+        });
+      }
+      return acc;
+    },
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions, @typescript-eslint/prefer-reduce-type-parameter
+    {} as R,
+  );
+}
