790278b5f7b8d36aa6fd47a24f956affff65e3ce
e.muhamethanov <eldarmu.1m@gmail.com>
2025-06-18 18:51:32 +0300

    feat(storybook): improve search by query on overview page

--- packages/vkui/docs/common/hooks/useGetConfigByQuery.ts
+++ packages/vkui/docs/common/hooks/useGetConfigByQuery.ts
@@ -2,6 +2,87 @@ import { type ChangeEvent, useCallback, useMemo, useState } from 'react';
 import { throttle } from '@vkontakte/vkjs';
 import { useManualScroll } from '../../../src/components/AppRoot/ScrollContext';
 
+function convertCyrillicToLatin(input: string): string {
+  // Проверяем, содержит ли строка кириллические символы
+  if (!/[а-яёА-яЁ]/i.test(input)) {
+    return input;
+  }
+
+  const cyrillicToLatinMap: Record<string, string> = {
+    а: 'f',
+    А: 'F',
+    б: ',',
+    Б: '<',
+    в: 'd',
+    В: 'D',
+    г: 'u',
+    Г: 'U',
+    д: 'l',
+    Д: 'L',
+    е: 't',
+    Е: 'T',
+    ё: '`',
+    Ё: '~',
+    ж: ';',
+    Ж: ':',
+    з: 'p',
+    З: 'P',
+    и: 'b',
+    И: 'B',
+    й: 'q',
+    Й: 'Q',
+    к: 'r',
+    К: 'R',
+    л: 'k',
+    Л: 'K',
+    м: 'v',
+    М: 'V',
+    н: 'y',
+    Н: 'Y',
+    о: 'j',
+    О: 'J',
+    п: 'g',
+    П: 'G',
+    р: 'h',
+    Р: 'H',
+    с: 'c',
+    С: 'C',
+    т: 'n',
+    Т: 'N',
+    у: 'e',
+    У: 'E',
+    ф: 'a',
+    Ф: 'A',
+    х: '[',
+    Х: '{',
+    ц: 'w',
+    Ц: 'W',
+    ч: 'x',
+    Ч: 'X',
+    ш: 'i',
+    Ш: 'I',
+    щ: 'o',
+    Щ: 'O',
+    ъ: ']',
+    Ъ: '}',
+    ы: 's',
+    Ы: 'S',
+    ь: 'm',
+    Ь: 'M',
+    э: "'",
+    Э: '"',
+    ю: '.',
+    Ю: '>',
+    я: 'z',
+    Я: 'Z',
+  };
+
+  return input
+    .split('')
+    .map((char) => cyrillicToLatinMap[char] || char)
+    .join('');
+}
+
 export const useGetConfigByQuery = <CONFIG>(
   config: CONFIG,
   filterConfig: (config: CONFIG, query: string) => CONFIG,
@@ -12,7 +93,7 @@ export const useGetConfigByQuery = <CONFIG>(
 
   const _updateQuery = useMemo(() => {
     return throttle((newValue) => {
-      setQuery(newValue);
+      setQuery(convertCyrillicToLatin(newValue));
       setLoading(false);
     }, 1000);
   }, []);
