7ec7adf247be410b14531ec3555d066393244230
Victoria Zhizhonkova <indarklight@gmail.com>
2025-06-26 16:53:17 +0700

    feat: add overview links

---
+++ website/components/mdx/Overview/OverviewHeaderLinks/types.ts
@@ -0,0 +1,21 @@
+export interface OverviewHeaderLinksProps {
+  group?:
+    | 'layout'
+    | 'forms'
+    | 'dates'
+    | 'buttons'
+    | 'navigation'
+    | 'feedback'
+    | 'modals'
+    | 'data-display'
+    | 'typography'
+    | 'configuration'
+    | 'utils';
+  type?: 'component' | 'hook';
+  forcedPath?: string;
+}
+
+export interface OverviewHeaderLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
