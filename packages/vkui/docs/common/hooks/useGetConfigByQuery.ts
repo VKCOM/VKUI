7b0461d20e7c17fec0c2dd08dcbd67b9d7971e91
Eldar <61377022+EldarMuhamethanov@users.noreply.github.com>
2025-07-02 16:32:42 +0300

    feat(storybook): improve search by query on overview page (#8668)
    * feat(storybook): improve search by query on overview page
    
    * feat(storybook): improve search

--- packages/vkui/docs/common/hooks/useGetConfigByQuery.ts
+++ packages/vkui/docs/common/hooks/useGetConfigByQuery.ts
@@ -2,6 +2,54 @@ import { type ChangeEvent, useCallback, useMemo, useState } from 'react';
 import { throttle } from '@vkontakte/vkjs';
 import { useManualScroll } from '../../../src/components/AppRoot/ScrollContext';
 
+function convertCyrillicToLatin(input: string): string {
+  // Проверяем, содержит ли строка кириллические символы
+  if (!/[а-яё]/i.test(input)) {
+    return input;
+  }
+
+  const cyrillicToLatinMap: Record<string, string> = {
+    а: 'f',
+    б: ',',
+    в: 'd',
+    г: 'u',
+    д: 'l',
+    е: 't',
+    ё: '`',
+    ж: ';',
+    з: 'p',
+    и: 'b',
+    й: 'q',
+    к: 'r',
+    л: 'k',
+    м: 'v',
+    н: 'y',
+    о: 'j',
+    п: 'g',
+    р: 'h',
+    с: 'c',
+    т: 'n',
+    у: 'e',
+    ф: 'a',
+    х: '[',
+    ц: 'w',
+    ч: 'x',
+    ш: 'i',
+    щ: 'o',
+    ъ: ']',
+    ы: 's',
+    ь: 'm',
+    э: "'",
+    ю: '.',
+    я: 'z',
+  };
+
+  return input
+    .split('')
+    .map((char) => cyrillicToLatinMap[char] || char)
+    .join('');
+}
+
 export const useGetConfigByQuery = <CONFIG>(
   config: CONFIG,
   filterConfig: (config: CONFIG, query: string) => CONFIG,
@@ -11,8 +59,8 @@ export const useGetConfigByQuery = <CONFIG>(
   const { scrollTo } = useManualScroll();
 
   const _updateQuery = useMemo(() => {
-    return throttle((newValue) => {
-      setQuery(newValue);
+    return throttle((newValue: string) => {
+      setQuery(convertCyrillicToLatin(newValue.toLocaleLowerCase()));
       setLoading(false);
     }, 1000);
   }, []);
--- packages/vkui/docs/components-overview/ComponentsOverview.tsx
+++ packages/vkui/docs/components-overview/ComponentsOverview.tsx
@@ -14,7 +14,7 @@ const filterConfig = (config: typeof CONFIG, query: string) => {
   const resultConfig: typeof CONFIG = [];
   config.forEach((groupData) => {
     const validComponents = groupData.components.filter((componentName) => {
-      return componentName.toLowerCase().includes(query.toLowerCase());
+      return componentName.toLowerCase().includes(query);
     });
     if (validComponents.length) {
       resultConfig.push({
