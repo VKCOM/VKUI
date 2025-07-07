279e559e61fffadd874402738b8b1147f9c0d925
e.muhamethanov <eldarmu.1m@gmail.com>
2025-07-07 12:41:49 +0300

    feat(storybook): fix component documentation url

--- packages/vkui/.storybook/addons/documentation-button/DocumentationButton.tsx
+++ packages/vkui/.storybook/addons/documentation-button/DocumentationButton.tsx
@@ -1,7 +1,39 @@
 import { IconButton } from 'storybook/internal/components';
-import { useGlobals, useStorybookState } from 'storybook/manager-api';
+import { useStorybookState } from 'storybook/manager-api';
 import { DocumentIcon } from '@storybook/icons';
 import * as React from 'react';
+
+const DOCS_BASE_URL = 'https://vkui.io/';
+
+const COMPONENTS_DOCS_PARENT_MAP: Record<string, string> = {
+  Header: 'Group',
+  Footer: 'Group',
+  SplitCol: 'SplitLayout',
+  WriteBarIcon: 'WriteBar',
+  List: 'Cell',
+  Tabbar: 'Epic',
+  TabbarItem: 'Epic',
+  PanelSpinner: 'Panel',
+  PanelHeaderButton: 'PanelHeader',
+  PanelHeaderContent: 'PanelHeader',
+  SubnavigationButton: 'SubnavigationBar',
+  TabsItem: 'Tabs',
+  ActionSheetItem: 'ActionSheet',
+  HorizontalCellShowMore: 'HorizontalCell',
+  OnboardingTooltipContainer: 'OnboardingTooltip',
+  DisplayTitle: 'Typography',
+  Title: 'Typography',
+  Headline: 'Typography',
+  Text: 'Typography',
+  Paragraph: 'Typography',
+  Subhead: 'Typography',
+  Footnote: 'Typography',
+  Caption: 'Typography',
+};
+
+function toKebabCase(componentName: string) {
+  return componentName.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+}
 
 function getVersionFromUrl() {
   const url = window.location.href;
@@ -9,12 +41,15 @@ function getVersionFromUrl() {
   return match ? match[1] : '';
 }
 
-const getComponentUrl = (docsBaseUrl: string, componentName: string): string => {
+const getComponentUrl = (componentName: string, parent): string => {
   const version = getVersionFromUrl();
+  const url = parent
+    ? `${toKebabCase(parent)}#${toKebabCase(componentName)}`
+    : toKebabCase(componentName);
   if (version) {
-    return `${docsBaseUrl}${version}/components/${componentName}/`;
+    return `${DOCS_BASE_URL}${version}/components/${url}`;
   }
-  return `${docsBaseUrl}/components/${componentName}/`;
+  return `${DOCS_BASE_URL}components/${url}`;
 };
 
 function extractComponentName(path: string): string {
@@ -23,14 +58,21 @@ function extractComponentName(path: string): string {
 }
 
 export const DocumentationButton = () => {
-  // TODO: После https://github.com/VKCOM/VKUI/pull/8598 вернуть логику открытия страницы документации
-  const hasDocumentation = false;
-  if (!hasDocumentation) {
-    return null;
+  const { index, storyId } = useStorybookState();
+  const story = index?.[storyId];
+  const importPath = story && 'importPath' in story && story.importPath;
+
+  const componentName = importPath && extractComponentName(importPath);
+
+  if (!componentName) {
+    return;
   }
+  const parent = COMPONENTS_DOCS_PARENT_MAP[componentName];
+
+  const documentationUrl = getComponentUrl(componentName, parent);
 
   return (
-    <a href="/" target="_blank" rel="noreferrer">
+    <a href={documentationUrl} target="_blank" rel="noreferrer">
       <IconButton>
         <DocumentIcon />
       </IconButton>
