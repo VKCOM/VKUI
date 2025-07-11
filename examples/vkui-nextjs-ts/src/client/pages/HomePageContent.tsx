1afe1299335fdef651138f8c51be93a0c3c0154e
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2025-07-11 16:22:17 +0300

    tech(examples): format templates to new doc (#8736)
    Привёл код шаблонов к примерам из новой документации https://vkui.io/overview/install.
    
    - `/vkui-nextjs-ts`
      - поправил серверную часть под https://vkui.io/overview/ssr
      - заменил отступы в верстке примеров на `gap` вместо `<Spacing />`
      - расширил `.gitignore`
    - `/vkui-vite-ts`
      - добавил недостающие классы в `index.html` и добавил `disableSettingVKUIClassesInRuntime` в `App.tsx`
      - заменил отступы в верстке примеров на `gap` вместо `<Spacing />`
      - отформатировал `src/main.tsx` под https://vkui.io/overview/install
      - расширил `.gitignore`
    
    И наоборот поправил страницы https://vkui.io/overview/install и в https://vkui.io/overview/usage, чтобы было похоже на то, что в `examples/`.

--- examples/vkui-nextjs-ts/src/client/pages/HomePageContent.tsx
+++ examples/vkui-nextjs-ts/src/client/pages/HomePageContent.tsx
@@ -1,18 +1,17 @@
 'use client';
 
 import * as React from 'react';
-import { Flex, Link, Spacing, Title } from '@vkontakte/vkui';
+import { Flex, Link, Title } from '@vkontakte/vkui';
 import NextLink from 'next/link';
 import Copyright from '@/client/common/Copyright';
 import ProTip from '@/client/common/ProTip';
 
 export function HomePageContent() {
   return (
-    <Flex direction="column" justify="center" align="center">
+    <Flex direction="column" justify="center" align="center" gap={16}>
       <Title level="3" Component="h1">
         VKUI — пример использования Next.js App Router на TypeScript
       </Title>
-      <Spacing />
       <Link Component={NextLink} href="/about">
         Перейти на страницу «О проекте»
       </Link>
