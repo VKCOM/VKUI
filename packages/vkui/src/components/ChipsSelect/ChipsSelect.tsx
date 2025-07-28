47c28a44e65ab8db5c9cb74aa7ca8f6b7a79e9af
e.muhamethanov <eldarmu.1m@gmail.com>
2025-07-28 17:23:25 +0300

    fix: remove console log

--- packages/vkui/src/components/ChipsSelect/ChipsSelect.tsx
+++ packages/vkui/src/components/ChipsSelect/ChipsSelect.tsx
@@ -442,7 +442,6 @@ export const ChipsSelect = <Option extends ChipOption>({
   const onDropdownIconClick: MouseEventHandler<SVGSVGElement> = React.useCallback(
     (e) => {
       e.preventDefault();
-      console.log('new opened', !opened);
       setOpened(!opened);
     },
     [opened, setOpened],
