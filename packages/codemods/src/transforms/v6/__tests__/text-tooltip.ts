0f38739b450ed6285f2da6819d3f51f656dafdd5
Eldar <61377022+EldarMuhamethanov@users.noreply.github.com>
2025-09-24 13:43:10 +0300

    tech: rewrite codemods tests from jest to vitest (#8967)

--- packages/codemods/src/transforms/v6/__tests__/text-tooltip.ts
+++ packages/codemods/src/transforms/v6/__tests__/text-tooltip.ts
@@ -1,5 +1,3 @@
-jest.autoMockOff();
-
 import fs from 'fs';
 import path from 'path';
 import getAvailableCodemods from '../../../getAvailableCodemods';
@@ -13,20 +11,25 @@ const name = 'text-tooltip';
 const fixtures = ['basic'] as const;
 const basicFixture = fixtures[0];
 
-describe(name, () => {
-  fixtures.forEach((test) =>
-    defineSnapshotTestFromFixture(__dirname, name, global.TRANSFORM_OPTIONS, `${name}/${test}`),
-  );
+describe(name, async () => {
+  for (const testName of fixtures) {
+    await defineSnapshotTestFromFixture(
+      __dirname,
+      name,
+      global.TRANSFORM_OPTIONS,
+      `${name}/${testName}`,
+    );
+  }
 
-  it('should apply after `transforms/tooltip.ts`', () => {
+  test('should apply after `transforms/tooltip.ts`', async () => {
     const codemodes = getAvailableCodemods('6', path.resolve(__dirname, '../../'));
     const inputPath = getTestFixturesInputPath(__dirname, `${name}/${basicFixture}`);
     let source = fs.readFileSync(inputPath, 'utf8');
 
-    codemodes.forEach((transformName) => {
-      const module = require(path.join(__dirname, '..', transformName));
+    for (const transformName of codemodes) {
+      const module = await import(path.join(__dirname, '..', transformName) + '.ts');
       source = applyTransform(module, global.TRANSFORM_OPTIONS, { source });
-    });
+    }
 
     expect(source).toMatchSnapshot();
   });
