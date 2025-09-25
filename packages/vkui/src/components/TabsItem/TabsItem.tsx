ce33b1a9dfaa63c17a8e6e6d5daacc7911348984
e.muhamethanov <eldarmu.1m@gmail.com>
2025-09-25 15:06:58 +0300

    fix(TabsItem): fix condition

--- packages/vkui/src/components/TabsItem/TabsItem.tsx
+++ packages/vkui/src/components/TabsItem/TabsItem.tsx
@@ -212,7 +212,7 @@ export const TabsItem = ({
         withGaps && styles.withGaps,
         layoutFillMode !== 'auto' && fillModeClassNames[layoutFillMode],
       )}
-      onClick={onClick || controller ? _onClick : undefined}
+      onClick={controller ? _onClick : onClick}
       id={id}
       {...restProps}
     >
