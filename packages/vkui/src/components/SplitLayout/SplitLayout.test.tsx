c280b051203924f4f4cb8738e29752ebf9534983
e.muhamethanov <eldarmu.1m@gmail.com>
2025-10-13 19:01:56 +0300

    feat(SplitLayout): add prop slotsProps

--- packages/vkui/src/components/SplitLayout/SplitLayout.test.tsx
+++ packages/vkui/src/components/SplitLayout/SplitLayout.test.tsx
@@ -1,6 +1,73 @@
+import { createRef } from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { baselineComponent } from '../../testing/utils';
 import { SplitLayout } from './SplitLayout';
 
 describe('SplitLayout', () => {
   baselineComponent(SplitLayout);
+
+  it('should work with slotsProps', () => {
+    const rootRef1 = createRef<HTMLDivElement>();
+    const rootRef2 = createRef<HTMLDivElement>();
+    const contentRef1 = createRef<HTMLDivElement>();
+    const contentRef2 = createRef<HTMLDivElement>();
+    const onClick1 = vi.fn();
+    const onClick2 = vi.fn();
+    const onRootClick = vi.fn();
+
+    render(
+      <SplitLayout
+        data-testid="content"
+        className="contentClassName"
+        getRootRef={rootRef1}
+        getRef={contentRef1}
+        onClick={onClick1}
+        style={{
+          backgroundColor: 'rgb(255, 0, 0)',
+        }}
+        slotsProps={{
+          root: {
+            'data-testid': 'root',
+            'className': 'rootClassName',
+            'style': {
+              color: 'rgb(255, 0, 0)',
+            },
+            'getRootRef': rootRef2,
+            'onClick': onRootClick,
+          },
+          content: {
+            'className': 'contentClassName-2',
+            'getRootRef': contentRef2,
+            'data-testid': 'content-2',
+            'onClick': onClick2,
+          },
+        }}
+      />,
+    );
+
+    expect(screen.queryByTestId('content')).not.toBeInTheDocument();
+    const content = screen.getByTestId('content-2');
+    expect(content).toBeInTheDocument();
+    expect(content).toHaveStyle('background-color: rgb(255, 0, 0)');
+    expect(content).toHaveClass('contentClassName');
+    expect(content).toHaveClass('contentClassName-2');
+
+    const root = screen.getByTestId('root');
+    expect(root).toBeInTheDocument();
+    expect(root).toHaveClass('rootClassName');
+    expect(root).toHaveStyle('color: rgb(255, 0, 0)');
+
+    expect(rootRef1.current).toBe(rootRef2.current);
+    expect(rootRef1.current).toBe(root);
+
+    expect(contentRef1.current).toBe(contentRef2.current);
+    expect(contentRef1.current).toBe(content);
+
+    fireEvent.click(content);
+    expect(onClick1).toHaveBeenCalledTimes(1);
+    expect(onClick2).toHaveBeenCalledTimes(1);
+
+    fireEvent.click(root);
+    expect(onRootClick).toHaveBeenCalledTimes(2);
+  });
 });
--- packages/vkui/src/components/SplitLayout/SplitLayout.tsx
+++ packages/vkui/src/components/SplitLayout/SplitLayout.tsx
@@ -2,13 +2,29 @@
 
 import * as React from 'react';
 import { classNames } from '@vkontakte/vkjs';
+import { useMergeProps } from '../../hooks/useMergeProps';
 import { usePlatform } from '../../hooks/usePlatform';
-import type { HasRef, HTMLAttributesWithRootRef } from '../../types';
+import { warnOnce } from '../../lib/warnOnce';
+import type { HasDataAttribute, HasRootRef, HTMLAttributesWithRootRef } from '../../types';
+import { RootComponent } from '../RootComponent/RootComponent';
 import styles from './SplitLayout.module.css';
 
-export interface SplitLayoutProps
-  extends HTMLAttributesWithRootRef<HTMLDivElement>,
-    HasRef<HTMLDivElement> {
+const warn = warnOnce('SplitLayout');
+
+export interface SplitLayoutProps extends HTMLAttributesWithRootRef<HTMLDivElement> {
+  /**
+   * @deprecated Since 7.9.0. Вместо этого используйте `slotsProps={ content: { getRootRef: ... } }`.
+   */
+  getRef?: React.Ref<HTMLDivElement>;
+  /**
+   * Свойства, которые можно прокинуть внутрь компонента:
+   * - `root`: свойства для прокидывания в корень компонента;
+   * - `content`: свойства для прокидывания в контент;.
+   */
+  slotsProps?: {
+    root?: React.HTMLAttributes<HTMLDivElement> & HasRootRef<HTMLDivElement> & HasDataAttribute;
+    content?: React.HTMLAttributes<HTMLDivElement> & HasRootRef<HTMLDivElement> & HasDataAttribute;
+  };
   /**
    * Свойство для отрисовки `Alert`, `ActionSheet` и `ScreenSpinner`.
    *
@@ -47,27 +63,44 @@ export const SplitLayout = ({
   center,
   modal,
   popout,
+
+  slotsProps,
   ...restProps
 }: SplitLayoutProps): React.ReactNode => {
+  if (process.env.NODE_ENV === 'development' && getRef) {
+    warn('Свойство `getRef` устаревшее, используйте `slotsProps={ content: { getRootRef: ... } }`');
+  }
+
+  const rootRest = useMergeProps({ getRootRef }, slotsProps?.root);
+  const contentRest = useMergeProps(
+    {
+      getRootRef: getRef,
+      className,
+      ...restProps,
+    },
+    slotsProps?.content,
+  );
+
   const platform = usePlatform();
 
   return (
-    <div className={classNames(styles.host, platform === 'ios' && styles.ios)} ref={getRootRef}>
+    <RootComponent
+      baseClassName={classNames(styles.host, platform === 'ios' && styles.ios)}
+      {...rootRest}
+    >
       {header}
-      <div
-        {...restProps}
-        ref={getRef}
-        className={classNames(
+      <RootComponent
+        baseClassName={classNames(
           styles.inner,
           !!header && styles.innerHeader,
           center && styles.innerCenter,
-          className,
         )}
+        {...contentRest}
       >
         {children}
         {modal}
         {popout}
-      </div>
-    </div>
+      </RootComponent>
+    </RootComponent>
   );
 };
