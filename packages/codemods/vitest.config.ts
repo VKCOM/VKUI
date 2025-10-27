0f332b7940f92dd88e873e84efb76ae71500e682
Daniil Suvorov <severecloud@gmail.com>
2025-10-27 13:52:06 +0300

    test(codemods): globals true

--- packages/codemods/vitest.config.ts
+++ packages/codemods/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 const config = defineConfig({
   test: {
+    globals: true,
     include: ['src/transforms/**/__tests__/*.{ts,tsx,js,jsx}'],
     environment: 'node',
   },
