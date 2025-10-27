aa2497df4bab9ad71e1d57adf79ba1c9d7d98704
Daniil Suvorov <severecloud@gmail.com>
2025-10-27 13:37:00 +0300

    test(codemods): use local

--- packages/codemods/vitest.config.ts
+++ packages/codemods/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 const config = defineConfig({
   test: {
-    globals: true,
     include: ['src/transforms/**/__tests__/*.{ts,tsx,js,jsx}'],
     environment: 'node',
   },
--- packages/codemods/vitest.d.ts
+++
@@ -1,1 +0,0 @@
-import 'vitest/globals';
