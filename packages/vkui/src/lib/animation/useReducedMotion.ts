a1721965fd22f5e0873a0e8998959123bc6affcf
Daniil Suvorov <severecloud@gmail.com>
2025-10-31 11:20:35 +0300

    docs: add

--- packages/vkui/src/lib/animation/useReducedMotion.ts
+++ packages/vkui/src/lib/animation/useReducedMotion.ts
@@ -6,6 +6,12 @@ import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect';
 
 export const REDUCE_MOTION_MEDIA_QUERY = 'screen and (prefers-reduced-motion: reduce)';
 
+/**
+ * Хук для отслеживания предпочтения пользователя в уменьшении анимации.
+ *
+ * @returns {boolean | undefined} Возвращает `true`, если пользователь предпочитает уменьшенную анимацию,
+ * `false` - если нет, и `undefined` во время серверного рендеринга или во время первого рендеринга на клиенте.
+ */
 export const useReducedMotion = (): boolean | undefined => {
   const { window } = useDOM();
 
