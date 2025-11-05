6216a3eefed0b9237d69e3eefeebedb19a857de9
fm-designer <55585387+fm-designer@users.noreply.github.com>
2025-11-05 16:41:18 +0300

    feat(SubnavigationBar): add prop noPadding (#9117)
    * add: prop noPadding
    
    * fix: desc prop noPadding
    
    ---------
    
    Co-authored-by: Formaleva Maria <m.formaleva@mon-techsup.one-infra.ru>

--- packages/vkui/src/components/SubnavigationBar/SubnavigationBar.tsx
+++ packages/vkui/src/components/SubnavigationBar/SubnavigationBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { hasReactNode } from '@vkontakte/vkjs';
+import { classNames, hasReactNode } from '@vkontakte/vkjs';
 import type { HTMLAttributesWithRootRef } from '../../types';
 import {
   HorizontalScroll,
@@ -23,6 +23,10 @@ export interface SubnavigationBarProps
    * Отключение возможности прокручивания компонента по горизонтали.
    */
   fixed?: boolean;
+  /**
+   * Отключает отступы. Рекомендуется использовать с `mode="outline"` у [`SubnavigationButton`](https://vkui.io/components/subnavigation-button).
+   */
+  noPadding?: boolean;
 }
 
 const defaultScrollToLeft: ScrollPositionHandler = (x) => x - 240;
@@ -36,6 +40,7 @@ export const SubnavigationBar = ({
   fixed = false,
   children,
   showArrows = true,
+  noPadding = false,
   arrowSize = 's',
   getScrollToLeft = defaultScrollToLeft,
   getScrollToRight = defaultScrollToRight,
@@ -61,7 +66,7 @@ export const SubnavigationBar = ({
   return (
     <RootComponent baseClassName={fixed && styles.modeFixed} {...restProps}>
       <ScrollWrapper className={styles.in} {...scrollWrapperProps}>
-        <ul className={styles.scrollIn}>
+        <ul className={classNames(styles.scrollIn, noPadding && styles.noPadding)}>
           {React.Children.map(children, (child, idx) =>
             hasReactNode(child) ? (
               <li key={idx} className={styles.item}>
