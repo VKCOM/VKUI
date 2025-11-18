c55d7cbf6f1af8a730fa61896372d3bbaa474b48
Eldar <61377022+EldarMuhamethanov@users.noreply.github.com>
2025-11-18 11:16:31 +0300

    tech: add codemods to slotProps (#9087)
    * tech: add codemods for slotProps
    
    * feat(Select): add slotProps codemods
    
    * feat(ChipsSelect,ChipsInput): add slotProps codemods
    
    * fix(Switch,Radio,Checkbox): improve move slotProps
    
    * fix(File): improve move slotProps
    
    * fix: fix boolean value move
    
    * tech: improve codemods
    
    * fix(Search,Input): fix codemods
    
    * fix(Textarea,WriteBar): fix codemods
    
    * fix: remove callbacks moving
    
    * test: import vitest (#9106)
    
    * fix: fix functions calls
    
    * fix(SplitLayout): fix codemod
    
    ---------
    
    Co-authored-by: Daniil Suvorov <severecloud@gmail.com>

---
+++ packages/codemods/src/transforms/v8/native-select.ts
@@ -0,0 +1,48 @@
+import { API, FileInfo } from 'jscodeshift';
+import { getImportInfo } from '../../codemod-helpers';
+import { JSCodeShiftOptions } from '../../types';
+import { moveInputPropsIntoSlotProps } from './common/moveInputPropsIntoSlotProps';
+import {
+  moveAriaAttrsIntoSlotProps,
+  moveDataAttrsIntoSlotProps,
+  movePropIntoSlotProps,
+} from './common/movePropIntoSlotProps';
+
+export const parser = 'tsx';
+
+export default function transformer(file: FileInfo, api: API, options: JSCodeShiftOptions) {
+  const { alias } = options;
+  const j = api.jscodeshift;
+  const source = j(file.source);
+  const { localName } = getImportInfo(j, file, 'NativeSelect', alias);
+
+  if (localName) {
+    movePropIntoSlotProps(j, {
+      root: source,
+      componentName: localName,
+      propName: 'getRef',
+      slotName: 'select',
+      slotPropName: 'getRootRef',
+    });
+
+    moveDataAttrsIntoSlotProps(j, {
+      root: source,
+      componentName: localName,
+      slotName: 'select',
+    });
+    moveAriaAttrsIntoSlotProps(j, {
+      root: source,
+      componentName: localName,
+      slotName: 'select',
+    });
+
+    moveInputPropsIntoSlotProps(j, {
+      root: source,
+      componentName: localName,
+      slotName: 'select',
+      excludedProps: ['disabled', 'readOnly', 'required', 'name', 'value', 'placeholder'],
+    });
+  }
+
+  return source.toSource();
+}
