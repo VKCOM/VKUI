31dd4621bec9413420626e948acb042afb7fbf95
Victoria Zhizhonkova <indarklight@gmail.com>
2025-11-27 02:32:28 +0700

    feat: add common layout props (#9209)
    * feat: add common layout props
    
    * Revert some component layout props
    
    * feat: add docs

--- packages/vkui/src/components/Box/Box.tsx
+++ packages/vkui/src/components/Box/Box.tsx
@@ -1,17 +1,32 @@
+import { classNames } from '@vkontakte/vkjs';
 import { resolveLayoutProps } from '../../lib/layouts';
 import type { LayoutProps } from '../../lib/layouts/types';
 import { RootComponent, type RootComponentProps } from '../RootComponent/RootComponent';
+import styles from './Box.module.css';
+
+const displayClassNames = {
+  'none': styles.displayNone,
+  'inline': styles.displayInline,
+  'inline-block': styles.displayInlineBlock,
+  'block': styles.displayBlock,
+  'contents': styles.displayContents,
+};
 
 export interface BoxProps
   extends Omit<RootComponentProps<HTMLElement>, 'baseClassName' | 'baseStyle'>,
-    LayoutProps {}
+    LayoutProps {
+  /**
+   * Возможность задать css-свойство `display`.
+   */
+  display?: 'none' | 'inline' | 'inline-block' | 'block' | 'contents';
+}
 
 /**
  * @see https://vkui.io/components/box
  *
  * @since 7.9.0
  */
-export const Box = ({ className, style, ...restProps }: BoxProps) => {
+export const Box = ({ className, style, display, ...restProps }: BoxProps) => {
   const resolvedProps = resolveLayoutProps(restProps);
 
   return (
@@ -19,7 +34,7 @@ export const Box = ({ className, style, ...restProps }: BoxProps) => {
       {...resolvedProps}
       baseClassName={resolvedProps.className}
       baseStyle={resolvedProps.style}
-      className={className}
+      className={classNames(className, display && displayClassNames[display])}
       style={style}
     />
   );
