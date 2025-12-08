915893306b00f9a33ba6546010dbc3318e462334
Daniil Suvorov <severecloud@gmail.com>
2025-12-08 13:27:04 +0300

    fix: rm useEffectDev

--- packages/vkui/src/hooks/useEffectDev.ts
+++
@@ -1,5 +0,0 @@
-import { useEffect } from 'react';
-import { noop } from '@vkontakte/vkjs';
-
-export const useEffectDev: typeof useEffect =
-  process.env.NODE_ENV === 'development' ? useEffect : noop;
