8d1833224f38fbfe5fe96b832fdc760f551bc7df
Eldar <61377022+EldarMuhamethanov@users.noreply.github.com>
2025-12-10 11:49:18 +0300

    feat: remove modal root deprecated types (#9278)

--- packages/vkui/src/components/ModalRoot/ModalRoot.tsx
+++ packages/vkui/src/components/ModalRoot/ModalRoot.tsx
@@ -1,14 +1,11 @@
 'use client';
 
 import * as React from 'react';
-import { noop } from '@vkontakte/vkjs';
-import { warnOnce } from '../../lib/warnOnce';
 import { AppRootPortal } from '../AppRoot/AppRootPortal';
 import { ModalOverlay } from '../ModalOverlay/ModalOverlay';
 import { ModalRootContext, ModalRootOverlayContext } from './ModalRootContext';
 import type { ModalRootProps } from './types';
 
-const warn = warnOnce('ModalRoot');
 /**
  * @see https://vkui.io/components/modal-root
  */
@@ -39,24 +36,6 @@ export const ModalRoot = ({
       onOpened,
       onClose,
       onClosed,
-
-      // TODO [>=8] Удалить метод
-      updateModalHeight:
-        /* istanbul ignore next: deprecated */
-        process.env.NODE_ENV === 'development'
-          ? () => {
-              warn('Метод updateModalHeight() устарел и будет удалён в VKUI v8');
-            }
-          : noop,
-
-      // TODO [>=8] Удалить метод
-      registerModal:
-        /* istanbul ignore next: deprecated */
-        process.env.NODE_ENV === 'development'
-          ? () => {
-              warn('Метод registerModal() устарел и будет удалён в VKUI v8');
-            }
-          : noop,
     }),
     [
       activeModal,
