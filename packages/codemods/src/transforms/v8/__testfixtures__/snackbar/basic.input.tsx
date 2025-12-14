cf5cd6e21abf8708b446b39edb15ef96ee2ef8a4
e.muhamethanov <eldarmu.1m@gmail.com>
2025-12-14 13:36:20 +0300

    fix: fix codemods tests

--- packages/codemods/src/transforms/v8/__testfixtures__/snackbar/basic.input.tsx
+++ packages/codemods/src/transforms/v8/__testfixtures__/snackbar/basic.input.tsx
@@ -19,15 +19,6 @@ const App = () => {
       </Snackbar>
       <Snackbar onClose={handleClose}>Message with function reference</Snackbar>
       <Snackbar>Message without onClose</Snackbar>
-      <Snackbar
-        placement="bottom-start"
-        action="Action"
-        onClose={(reason) => {
-          console.log('Closed with reason:', reason);
-        }}
-      >
-        Message with other props
-      </Snackbar>
       <Snackbar onClose={onCloseHandler} duration={5000}>
         Message with duration
       </Snackbar>
@@ -35,7 +26,6 @@ const App = () => {
   );
 };
 
-function onCloseHandler(reason: string) {
-  console.log('handle close', reason);
+function onCloseHandler() {
+  console.log('handle close');
 }
-
