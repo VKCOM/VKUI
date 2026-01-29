VKCOM/VKUI

commit 912912f200655449e7b6ecf3b93316b109412440

feat: add overflowPadding (#9281)

packages/vkui/src/components/Tooltip/useTooltip.tsx
@@ -9,7 +9,7 @@ import {
   type UseFloatingElementProps,
 } from '../../hooks/useFloatingElement';
 import { animationFadeClassNames } from '../../lib/animation';
-import { getArrowCoordsByMiddlewareData } from '../../lib/floating';
+import { getArrowCoordsByMiddlewareData, sizeMiddleware } from '../../lib/floating';
 import { type ReferenceProps } from '../../lib/floating/useFloatingWithInteractions/types';
 import { AppRootPortal } from '../AppRoot/AppRootPortal';
 import { TooltipBase } from '../TooltipBase/TooltipBase';
@@ -46,6 +46,7 @@ export const useTooltip = ({
   disableShiftMiddleware = false,
   disableTriggerOnFocus = false,
   onReferenceHiddenChange,
+  overflowPadding,
 
   // useFloatingWithInteractions
   defaultShown,
@@ -153,6 +154,7 @@ export const useTooltip = ({
     hideWhenReferenceHidden,
     disableFlipMiddleware,
     disableShiftMiddleware,
+    overflowPadding,
 
     defaultShown,
     shown: shownProp,
@@ -169,6 +171,18 @@ export const useTooltip = ({
     renderFloatingComponent,
     externalFloatingElementRef: getRootRef,
     remapReferenceProps,
+
+    customMiddlewares: [
+      sizeMiddleware({
+        apply({ rects, elements, availableWidth }) {
+          const width = Math.min(Math.round(rects.floating.width), Math.floor(availableWidth));
+          Object.assign(elements.floating.style, {
+            width: `${width}px`,
+          });
+        },
+        padding: overflowPadding,
+      }),
+    ],
   });
 
   return {

packages/vkui/src/components/OnboardingTooltip/OnboardingTooltip.tsx
@@ -41,6 +41,7 @@ type AllowedFloatingComponentProps = Pick<
   | 'disableFlipMiddleware'
   | 'disableShiftMiddleware'
   | 'disableFocusTrap'
+  | 'overflowPadding'
 >;
 
 type AllowedTooltipBaseProps = Omit<
@@ -106,6 +107,7 @@ export const OnboardingTooltip = ({
   'aria-labelledby': ariaLabelledBy,
   restoreFocus,
   disableFocusTrap,
+  overflowPadding,
   ...restProps
 }: OnboardingTooltipProps): React.ReactNode => {
   const generatedId = React.useId();
@@ -127,6 +129,7 @@ export const OnboardingTooltip = ({
     arrowPadding,
     disableFlipMiddleware,
     disableShiftMiddleware,
+    overflowPadding,
   });
   const {
     x: floatingDataX,

packages/vkui/src/components/Popper/Popper.tsx
@@ -57,6 +57,7 @@ type AllowedFloatingComponentProps = Pick<
   | 'disableFlipMiddleware'
   | 'disableShiftMiddleware'
   | 'flipMiddlewareFallbackAxisSideDirection'
+  | 'overflowPadding'
 >;
 
 export interface PopperCommonProps
@@ -114,6 +115,7 @@ export const Popper = ({
   disableFlipMiddleware = false,
   disableShiftMiddleware = false,
   flipMiddlewareFallbackAxisSideDirection,
+  overflowPadding,
 
   // UseFloatingProps
   autoUpdateOnTargetResize = false,
@@ -151,6 +153,7 @@ export const Popper = ({
     disableFlipMiddleware,
     disableShiftMiddleware,
     flipMiddlewareFallbackAxisSideDirection,
+    overflowPadding,
   });
 
   const {

packages/vkui/src/components/Tooltip/Tooltip.tsx
@@ -26,6 +26,7 @@ type AllowedFloatingComponentProps = Pick<
   | 'disableFlipMiddleware'
   | 'disableShiftMiddleware'
   | 'strategy'
+  | 'overflowPadding'
 >;
 
 type AllowedTooltipBaseProps = Omit<TooltipBaseProps, 'arrowProps' | 'onCloseIconClick'>;

packages/vkui/src/hooks/useFloatingElement.tsx
@@ -70,6 +70,7 @@ export const useFloatingElement = <
   hideWhenReferenceHidden,
   disableFlipMiddleware = false,
   disableShiftMiddleware = false,
+  overflowPadding,
 
   // useFloatingWithInteractions
   trigger,
@@ -110,6 +111,7 @@ export const useFloatingElement = <
     arrowHeight,
     disableFlipMiddleware,
     disableShiftMiddleware,
+    overflowPadding,
   });
 
   const {

packages/vkui/src/lib/floating/adapters.ts
@@ -19,7 +19,10 @@ export {
   getOverflowAncestors,
 } from '@vkontakte/vkui-floating-ui/react-dom';
 
-export type { FlipOptions as FlipMiddlewareOptions } from '@vkontakte/vkui-floating-ui/react-dom';
+export type {
+  FlipOptions as FlipMiddlewareOptions,
+  ShiftOptions as ShiftMiddlewareOptions,
+} from '@vkontakte/vkui-floating-ui/react-dom';
 
 const defaultOptions = {
   ancestorScroll: true,

packages/vkui/src/lib/floating/useFloatingMiddlewaresBootstrap/index.ts
@@ -7,6 +7,7 @@ import {
   hideMiddleware,
   offsetMiddleware,
   shiftMiddleware,
+  type ShiftMiddlewareOptions,
   sizeMiddleware,
 } from '../adapters';
 import { checkIsNotAutoPlacement, getAutoPlacementAlign } from '../functions';
@@ -44,6 +45,10 @@ export interface UseFloatingMiddlewaresBootstrapOptions {
    * Отступ по вспомогательной оси.
    */
   offsetByCrossAxis?: number;
+  /**
+   * Отступ для смещения.
+   */
+  overflowPadding?: ShiftMiddlewareOptions['padding'];
   arrowRef?: ArrowOptions['element'];
   /**
    * Отображать ли стрелку, указывающую на якорный элемент.
@@ -85,6 +90,7 @@ export const useFloatingMiddlewaresBootstrap = ({
   disableFlipMiddleware = false,
   disableShiftMiddleware = false,
   flipMiddlewareFallbackAxisSideDirection = 'end',
+  overflowPadding,
 }: UseFloatingMiddlewaresBootstrapOptions): {
   middlewares: UseFloatingMiddleware[];
   strictPlacement: Placement | undefined;
@@ -98,7 +104,7 @@ export const useFloatingMiddlewaresBootstrap = ({
       }),
     ];
 
-    const shift = disableShiftMiddleware ? null : shiftMiddleware();
+    const shift = disableShiftMiddleware ? null : shiftMiddleware({ padding: overflowPadding });
 
     // см. https://github.com/floating-ui/floating-ui/blob/%40floating-ui/core%401.7.1/website/pages/docs/flip.mdx#conflict-with-autoplacementjs
     if (isAutoPlacement) {
@@ -173,5 +179,6 @@ export const useFloatingMiddlewaresBootstrap = ({
     hideWhenReferenceHidden,
     arrowRef,
     arrowPadding,
+    overflowPadding,
   ]);
 };

commit aac3afc0021902bc94a42cf66c52bbe8f4b991e8

fix(Tooltip): rm custom size() middleware (#9410)
<!-- Если этот PR закрывает Issue, то укажи ссылку на него. Используй доступные ключевые слова (см. https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/using-keywords-in-issues-and-pull-requests). -->
- caused by #9281
- related to #9389

---

<!-- Чеклист. Лишние пункты можно удалить если изменения не подразумевают их наличие. Иначе, необходимо обоснование по каждому пункту. -->
- ~[ ] Unit-тесты~
- ~[ ] e2e-тесты~
- ~[ ] Дизайн-ревью~
- ~[ ] Документация фичи~
- [x] Release notes

h1. Описание

В [v7.11.0](https://github.com/VKCOM/VKUI/releases/tag/v7.11.0) в `Tooltip` внесли 2 изменения:

1. Свойство `overflowPadding`, чтобы можно было добавить отступы и избавиться от прилипания к краю области видимости.

2. Пересчёт ширины, чтобы `maxWidth` не вызлезал за `viewportWidth` (в [v7.11.2](https://github.com/VKCOM/VKUI/releases/tag/v7.11.2) был фикс округления).

Но **п.2**, к сожалению, создал другая проблему, что ширина стала статичной, поэтому для решения проблемы ничего не остается как откатить **п.2**.

Как воркэраунд, проблему с `maxWidth > viewportWidth` необхоидмо решать локально – задавать `maxWidth` с вычетом `overflowPadding`, учитывая, что эти данные должны быть заведомо известны. Также стоит учитывать какой минимальной ширины устройства у пользователей приложения.

К будущей мажорной версии **v9**, должна будет появится поддержка [min()](https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/min) в CSS. Начиная с поддержки `min()`, будет возможность добавить новое свойство `fitViewport`, чтобы решить проблему с **п.2** и в тоже время решить конфликт свойства с `maxWidth`.

h1. Release notes
h1. Исправления
- Tooltip: откатили изменения из [v7.11.0](https://github.com/VKCOM/VKUI/releases/tag/v7.11.0) и [v7.11.2](https://github.com/VKCOM/VKUI/releases/tag/v7.11.2), связанные с пересчётом ширины

packages/vkui/src/components/Tooltip/useTooltip.tsx
@@ -9,7 +9,7 @@ import {
   type UseFloatingElementProps,
 } from '../../hooks/useFloatingElement';
 import { animationFadeClassNames } from '../../lib/animation';
-import { getArrowCoordsByMiddlewareData, sizeMiddleware } from '../../lib/floating';
+import { getArrowCoordsByMiddlewareData } from '../../lib/floating';
 import { type ReferenceProps } from '../../lib/floating/useFloatingWithInteractions/types';
 import { AppRootPortal } from '../AppRoot/AppRootPortal';
 import { TooltipBase } from '../TooltipBase/TooltipBase';
@@ -171,18 +171,6 @@ export const useTooltip = ({
     renderFloatingComponent,
     externalFloatingElementRef: getRootRef,
     remapReferenceProps,
-
-    customMiddlewares: [
-      sizeMiddleware({
-        apply({ rects, elements, availableWidth }) {
-          const width = Math.min(Math.ceil(rects.floating.width), Math.floor(availableWidth));
-          Object.assign(elements.floating.style, {
-            width: `${width}px`,
-          });
-        },
-        padding: overflowPadding,
-      }),
-    ],
   });
 
   return {