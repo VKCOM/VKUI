87ac1882e15cfa13ae543279f832b363841c8bbc
Daniil Suvorov <severecloud@gmail.com>
2026-03-11 11:32:26 +0300

    fix: paste browserslistrc in mdx

--- website/components/browserslistrc/browserslistrc.tsx
+++
@@ -1,9 +0,0 @@
-import browserslistrc from '../../../.browserslistrc?raw';
-
-const list = Object.fromEntries(
-  Array.from(browserslistrc.matchAll(/(\w+) >= (\d+)/g), (m) => [m[1], m[2]] as const),
-);
-
-export const Chrome = () => list['Chrome'];
-export const Firefox = () => list['Firefox'];
-export const Safari = () => list['Safari'];
