2e439e7068f61e4fc323ae4fb2f1a78ec7e77ef3
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2026-03-11 16:03:16 +0300

    chore: remove benchmark (#9576)
    * chore: remove benchmark
    
    Удаляю, т.к. решение устарело и в целом бенчмарк давно не поддерживался.
    
    h2. Референсы
    
    - В Vitest есть поддержка Playwright (https://vitest.dev/config/browser/playwright.html) и Tinybench (https://vitest.dev/guide/features.html#benchmarking)
    - В issue [https://github.com/mui/mui-public/issues/944](https://redirect.github.com/mui/mui-public/issues/944) есть ссылки на решения
    
    * chore: deduplicate dependencies

--- benchmark/runtime/shared/guards.ts
+++
@@ -1,5 +0,0 @@
-export const isPerformanceMeasure = (data: any): data is PerformanceMeasure =>
-  data !== null &&
-  typeof data === 'object' &&
-  data.hasOwnProperty('startTime') &&
-  data.hasOwnProperty('duration');
