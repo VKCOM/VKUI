b387ff397d406c3b556fa8ba41554e94f85983e3
Daniil Suvorov <severecloud@gmail.com>
2026-03-19 12:21:44 +0300

    fix(FormItem): className in subcomponents

---
+++ packages/vkui/src/components/FormItem/FormItemTop/FormItemTopLabel.test.tsx
@@ -0,0 +1,6 @@
+import { baselineComponent } from '../../../testing/utils';
+import { FormItemTopLabel } from './FormItemTopLabel';
+
+describe('FormItemTopLabel', () => {
+  baselineComponent(FormItemTopLabel);
+});
