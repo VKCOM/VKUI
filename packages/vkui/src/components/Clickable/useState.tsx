d833ff6975ab7938d88174dee2e81fded8b54ed7
e.mukhametkhanov <e.mukhametkhanov@vk.team>
2026-04-20 23:22:41 +0300

    fix: fix Clickable

--- packages/vkui/src/components/Clickable/useState.tsx
+++ packages/vkui/src/components/Clickable/useState.tsx
@@ -100,7 +100,6 @@ export function useHover({
   if (prevHasHover !== hasHover) {
     if (!hasHover) {
       setHoveredStateLocal(false);
-      setParentStateLock(false);
     }
     setPrevHasHover(hasHover);
   }
@@ -108,6 +107,7 @@ export function useHover({
   React.useEffect(() => {
     if (!hasHover) {
       prevIsHoveredRef.current = false;
+      setParentStateLock(false);
     }
   }, [hasHover, setParentStateLock]);
 
