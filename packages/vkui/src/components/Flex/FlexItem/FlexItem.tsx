30da9c2abd8f21f3fafb1d0a52e9e9071dc33d8e
Eldar <61377022+EldarMuhamethanov@users.noreply.github.com>
2026-04-24 12:18:56 +0300

    fix(FlexItem): fix override flex prop by layout props (#9753)
    Co-authored-by: e.mukhametkhanov <e.mukhametkhanov@vk.team>

--- packages/vkui/src/components/Flex/FlexItem/FlexItem.tsx
+++ packages/vkui/src/components/Flex/FlexItem/FlexItem.tsx
@@ -1,15 +1,8 @@
+import { getRequiredValueByKey } from '../../../helpers/getValueByKey';
 import { type LayoutProps, resolveLayoutProps } from '../../../lib/layouts';
 import { warnOnce } from '../../../lib/warnOnce';
 import { RootComponent } from '../../RootComponent/RootComponent';
 import type { RootComponentProps } from '../../RootComponent/RootComponent';
-import styles from './FlexItem.module.css';
-
-const flexClassNames = {
-  grow: styles.flexGrow,
-  shrink: styles.flexShrink,
-  content: styles.flexContent,
-  fixed: styles.flexFixed,
-};
 
 export type FlexItemProps = RootComponentProps<HTMLElement> &
   Pick<LayoutProps, 'alignSelf' | 'justifySelf' | 'flexBasis'> & {
@@ -24,14 +17,48 @@ export type FlexItemProps = RootComponentProps<HTMLElement> &
     flex?: 'grow' | 'shrink' | 'content' | 'fixed' | undefined;
   };
 
+const resolveFlexProps = (flex: FlexItemProps['flex'], flexBasis: FlexItemProps['flexBasis']) => {
+  const overrideProps = flexBasis !== undefined ? { flexBasis } : {};
+  if (!flex) {
+    return overrideProps;
+  }
+  return {
+    ...getRequiredValueByKey<Pick<LayoutProps, 'flexGrow' | 'flexShrink' | 'flexBasis'>>(flex, {
+      grow: {
+        flexGrow: 1,
+        flexShrink: 0,
+        flexBasis: 'auto',
+      },
+      shrink: {
+        flexGrow: 0,
+        flexShrink: 1,
+        flexBasis: 'auto',
+      },
+      content: {
+        flexGrow: 1,
+        flexShrink: 1,
+        flexBasis: 'auto',
+      },
+      fixed: {
+        flexGrow: 0,
+        flexShrink: 0,
+        flexBasis: 'auto',
+      },
+    }),
+    ...overrideProps,
+  };
+};
+
 const warn = warnOnce('Flex.Item');
 
-export const FlexItem = ({ flex, ...restProps }: FlexItemProps): React.ReactNode => {
+export const FlexItem = ({ flex, flexBasis, ...restProps }: FlexItemProps): React.ReactNode => {
   if (process.env.NODE_ENV === 'development') {
     warn('Компонент Flex.Item устарел, используйте компонент Box в качестве альтернативы.');
   }
 
-  const resolvedProps = resolveLayoutProps(restProps);
+  const flexProps = resolveFlexProps(flex, flexBasis);
 
-  return <RootComponent baseClassName={flex && flexClassNames[flex]} {...resolvedProps} />;
+  const resolvedProps = resolveLayoutProps({ ...restProps, ...flexProps });
+
+  return <RootComponent {...resolvedProps} />;
 };
