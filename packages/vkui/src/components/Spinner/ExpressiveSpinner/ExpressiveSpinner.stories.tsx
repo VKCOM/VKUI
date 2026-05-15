8fc4004e735881608701d270c1583ee1746ae72f
Daniil Suvorov <severecloud@gmail.com>
2026-05-15 14:34:44 +0300

    feat: unstable_ExpressiveSpinner (#8613)

---
+++ packages/vkui/src/components/Spinner/ExpressiveSpinner/ExpressiveSpinner.stories.tsx
@@ -0,0 +1,19 @@
+import { withCartesian } from '@project-tools/storybook-addon-cartesian';
+import type { Meta, StoryObj } from '@storybook/react';
+import { CanvasFullLayout } from '../../../storybook/constants';
+import { createStoryParameters } from '../../../testing/storybook/createStoryParameters';
+import { type SpinnerProps } from '../Spinner';
+import { ExpressiveSpinner } from './ExpressiveSpinner';
+
+const story: Meta<SpinnerProps> = {
+  title: 'Feedback/Spinner/ExpressiveSpinner',
+  component: ExpressiveSpinner,
+  parameters: createStoryParameters('ExpressiveSpinner', CanvasFullLayout),
+  decorators: [withCartesian],
+};
+
+export default story;
+
+type Story = StoryObj<SpinnerProps>;
+
+export const Playground: Story = {};
---
+++ packages/vkui/src/components/Spinner/ExpressiveSpinner/ExpressiveSpinner.test.tsx
@@ -0,0 +1,6 @@
+import { baselineComponent } from '../../../testing/utils';
+import { ExpressiveSpinner } from './ExpressiveSpinner';
+
+describe('Spinner', () => {
+  baselineComponent((props) => <ExpressiveSpinner disableAnimation {...props} />);
+});
