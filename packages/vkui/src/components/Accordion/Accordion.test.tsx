b87210e8f02fcec7188469678e31c916928cfb26
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2026-05-28 15:44:52 +0300

    test(coverage): Remove Accordion toggle and unmount tests
    Для проверки https://github.com/VKCOM/VKUI/pull/9859

--- packages/vkui/src/components/Accordion/Accordion.test.tsx
+++ packages/vkui/src/components/Accordion/Accordion.test.tsx
@@ -13,66 +13,6 @@ describe(Accordion, () => {
     </Accordion>
   ));
 
-  baselineComponent(Accordion.Content);
-  baselineComponent((props) => <Accordion.Summary {...props}>Title</Accordion.Summary>);
-
-  it('toggles on click', async () => {
-    const result = render(
-      <Accordion>
-        <Accordion.Summary iconPosition="before" data-testid="summary">
-          Title
-        </Accordion.Summary>
-        <Accordion.Content data-testid="content">Content</Accordion.Content>
-      </Accordion>,
-    );
-    const content = result.getByTestId('content');
-    const contentIn = content.firstElementChild as HTMLElement;
-    const summary = result.getByTestId('summary');
-    expect(content.getAttribute('aria-hidden')).toBe('true');
-
-    fireEvent.click(summary);
-    await waitCSSKeyframesAnimation(contentIn);
-    expect(content.getAttribute('aria-hidden')).toBe('false');
-
-    fireEvent.click(summary);
-    await waitCSSKeyframesAnimation(contentIn);
-    expect(content.getAttribute('aria-hidden')).toBe('true');
-  });
-
-  it('should unmountOnCollapsed', async () => {
-    const result = render(
-      <Accordion unmountOnCollapsed>
-        <Accordion.Summary data-testid="summary">Title</Accordion.Summary>
-        <Accordion.Content data-testid="content">Content</Accordion.Content>
-      </Accordion>,
-    );
-
-    const summary = result.getByTestId('summary');
-
-    // Изначально контент не отображается
-    expect(result.queryByTestId('content')).toBeNull();
-
-    // Кликаем на разворачивание
-    fireEvent.click(summary);
-
-    // Контент отображается сразу не дожидаясь анимации
-    expect(result.queryByTestId('content')).not.toBeNull();
-
-    // Кликаем на сворачивание
-    fireEvent.click(summary);
-
-    // content не исчезает из DOM до окончания анимации сворачивания
-
-    expect(result.queryByTestId('content')).not.toBeNull();
-
-    // Дожидаемя окончания анимации сворачивания
-    const contentIn = result.getByTestId('content').firstElementChild as HTMLElement;
-    await waitCSSKeyframesAnimation(contentIn);
-
-    // content исчез из DOM после окончания анимации сворачивания
-    expect(result.queryByTestId('content')).toBeNull();
-  });
-
   it('useAccordionContext', () => {
     const h = renderHook(useAccordionContext, {
       wrapper: ({ children }) => (
