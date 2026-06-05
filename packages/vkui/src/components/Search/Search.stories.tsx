f473fd1818ba7ddd29e34b52832ff2653aa1428b
e.mukhametkhanov <e.mukhametkhanov@vk.team>
2026-06-05 13:45:11 +0300

    fix: fix default value

--- packages/vkui/src/components/Search/Search.stories.tsx
+++ packages/vkui/src/components/Search/Search.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryFn } from '@storybook/react';
 import { withSinglePanel, withVKUILayout } from '../../storybook/VKUIDecorators';
-import { CanvasFullLayout, DisableCartesianParam, StringArg } from '../../storybook/constants';
+import { CanvasFullLayout, DisableCartesianParam } from '../../storybook/constants';
 import { createStoryParameters } from '../../testing/storybook/createStoryParameters';
 import { Group } from '../Group/Group';
 import { Search, type SearchProps } from './Search';
@@ -10,9 +10,6 @@ const story: Meta<SearchProps> = {
   component: Search,
   parameters: createStoryParameters('Search', CanvasFullLayout, DisableCartesianParam),
   tags: ['Утилиты'],
-  argTypes: {
-    after: StringArg,
-  },
 };
 
 export default story;
