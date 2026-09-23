b953a7cea4d301dba232b03d280fc8fa56998542
Daniil Suvorov <d.suvorov@vk.team>
2026-09-23 13:41:02 +0300

    feat(Gallery): add bullets=overlay

--- packages/vkui/src/components/CarouselBase/Bullets.tsx
+++ packages/vkui/src/components/CarouselBase/Bullets.tsx
@@ -20,6 +20,7 @@ interface BulletsProps extends BulletsTestIds {
 const stylesBullets = {
   dark: styles.bulletsDark,
   light: styles.bulletsLight,
+  overlay: styles.bulletsOverlay,
 };
 
 export const Bullets = ({ bullets, slideIndex, count, bulletTestId }: BulletsProps) => {
